c7c9f1de34127f5c9793c8299bbf40bf86b8647f
David Lawrence <davidl@jlab.org>
2022-07-15 17:09:22 -0400

    Refactored PODIO event source a bit. It broken state at moment due to bizzare bug when calling EventHeader::getEventNumber()

--- I_O/plugins/jana_edm4hep/JEventSourcePODIO.h
+++ I_O/plugins/jana_edm4hep/JEventSourcePODIO.h
@@ -3,6 +3,8 @@
 #ifndef _JEventSourcePODIO_h_
 #define  _JEventSourcePODIO_h_
 
+#include <map>
+
 #include <JANA/JEventSource.h>
 #include <JANA/JEventSourceGeneratorT.h>
 
@@ -20,13 +22,15 @@ class JEventSourcePODIO : public JEventSource {
 public:
     JEventSourcePODIO(std::string resource_name, JApplication* app);
 
-    virtual ~JEventSourcePODIO() = default;
+    virtual ~JEventSourcePODIO();
 
     void Open() override;
 
     void GetEvent(std::shared_ptr<JEvent>) override;
     
     static std::string GetDescription();
+
+    void PrintCollectionTypeTable(void);
 
 protected:
 	podio::ROOTReader reader;
@@ -35,8 +39,11 @@ protected:
 	size_t Nevents_read = 0;
 	bool run_forever=false;
 
-	template <class T, class C>
-	void GetPODIOData( const char *name, std::shared_ptr <JEvent> &event );
+	// This holds the collection name (key) and the collection type (value) contained in the
+	// input file. The collection type is a class name from the EDM4hep data model. Example is:
+	//      collection_name["MRICHHits"] = "SimTrackerHit"
+	std::map<std::string, std::string> collection_names;
+
 };
 
 template <>
