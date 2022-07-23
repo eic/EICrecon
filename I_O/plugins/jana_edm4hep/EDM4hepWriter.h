fe0d3ff2aefa55da909459175fe8fd0a9d4e3071
David Lawrence <davidl@jlab.org>
2022-07-22 22:07:59 -0400

    Copying in ROOTWriter class from podio so it can be modified to work properly. Making it a utlity class within EDM4hepWriter. Also, return collection name from PutPODIOT.

--- I_O/plugins/jana_edm4hep/EDM4hepWriter.h
+++ I_O/plugins/jana_edm4hep/EDM4hepWriter.h
@@ -10,6 +10,10 @@
 #include <podio/EventStore.h>
 #include <podio/podioVersion.h>
 
+
+#include <TFile.h>
+#include <TTree.h>
+
 #include <JANA/JEventProcessor.h>
 
 class EDM4hepWriter : public JEventProcessor {
@@ -30,9 +34,64 @@ protected:
 
     std::string m_OUTPUT_FILE = "podio_output.root";
     std::string m_collection_names_str;
-    std::vector<std::string> m_OUTPUT_COLLECTIONS;
+    std::set<std::string> m_OUTPUT_COLLECTIONS;
+    std::set<std::string> m_registered_collections;
     podio::EventStore store;
 
+//===================================================================================================================
+// Code in this section copied from the example ROOTWriter class in podio.
+//
+// TODO: Copy the changes back into the podio::ROOTWriter class and submit a PR so these are fixed upstream.
+public:
+    struct CollectionBranches {
+        TBranch* data{nullptr};
+        std::vector<TBranch*> refs{};
+        std::vector<TBranch*> vecs{};
+    };
+
+    class ROOTWriter {
+
+    public:
+        ROOTWriter(const std::string& filename, podio::EventStore* store);
+        ~ROOTWriter();
+
+        // non-copyable
+        ROOTWriter(const ROOTWriter&) = delete;
+        ROOTWriter& operator=(const ROOTWriter&) = delete;
+
+        bool registerForWrite(const std::string& name);
+        void writeEvent();
+        void finish();
+
+    private:
+
+        using StoreCollection = std::pair<const std::string&, podio::CollectionBase*>;
+        void createBranches(const std::vector<StoreCollection>& collections);
+        void setBranches(const std::vector<StoreCollection>& collections);
+
+        // members
+        std::string m_filename;
+        podio::EventStore* m_store;
+        TFile* m_file;
+        TTree* m_datatree;
+        TTree* m_metadatatree;
+        TTree* m_runMDtree;
+        TTree* m_evtMDtree;
+        TTree* m_colMDtree;
+        std::vector<std::string> m_collectionsToWrite{};
+        // In order to avoid having to look up the branches from the datatree for
+        // every event, we cache them in this vector, that is populated the first
+        // time we write an event. Since the collections and their order do not
+        // change between events, the assocation between the collections to write
+        // and their branches is simply index based
+        std::vector<EDM4hepWriter::CollectionBranches> m_collectionBranches{};
+
+        bool m_firstEvent{true};
+    };
+
+//===================================================================================================================
+
+    std::unique_ptr<EDM4hepWriter::ROOTWriter> writer;
 };
 
 
