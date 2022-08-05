1cfa0cdd6ea897ec8a63ef02bb00a7f56800649d
David Lawrence <davidl@jlab.org>
2022-08-05 15:42:42 -0400

    Copied contents of jana to make work. Add ability to open root file.

--- src/utilities/eicrecon/eicrecon.cc
+++ src/utilities/eicrecon/eicrecon.cc
@@ -19,7 +19,14 @@ int main( int narg, char **argv)
 
     if (options.flags[jana::ShowUsage]) {
         // Show usage information and exit immediately
+        _DBG__;
         jana::PrintUsage();
+        std::cout << std::endl << "-----------" << std::endl;
+        std::cout << "    eicrecon parameters: (specify with -Pparam=value)" << std::endl;
+        std::cout << std::endl;
+        std::cout << "        histsfile file.root    Create a root file for histograms/trees produced by plugins" << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
         return -1;
     }
     if (options.flags[jana::ShowVersion]) {
@@ -28,12 +35,29 @@ int main( int narg, char **argv)
         return -1;
     }
 
+    // Optionally create root file
+    // TODO: This needs to be done by a service that plugins can use to get the TDirectory from to avoid conflicts with podio files
+    TFile *rootfile = nullptr;
+    if (options.params.count("histsfile")){
+        LOG << "Creating root file: " << options.params["histsfile"] << " for histograms/trees from plugins" << LOG_END;
+        rootfile = new TFile(options.params["histsfile"].c_str(), "RECREATE", "eicrecon plugin histograms/trees");
+    }
+
     japp = jana::CreateJApplication(options);
 
     japp->Add( new JEventSourceGeneratorT<JEventSourcePODIO>() );
     japp->Add( new JFactoryGeneratorT<JFactory_BEMCRawCalorimeterHit>() );
 
     auto exit_code = jana::Execute(japp, options);
+
+    // If root file was created, then close it
+    if( rootfile ){
+        LOG << "Closing histsfile: " << rootfile->GetName() << LOG_END;
+        rootfile->Write();
+        rootfile->Close();
+        delete rootfile;
+    }
+
     delete japp;
     return exit_code;
 }
