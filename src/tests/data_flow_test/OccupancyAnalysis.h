8c5610d18bec5de7394d761cc1070ebc30047471
Dmitry Romanov <romanovda@gmail.com>
2022-08-15 12:10:08 -0400

    data_flow_test plots simple MCParticle histos

--- src/tests/data_flow_test/OccupancyAnalysis.h
+++ src/tests/data_flow_test/OccupancyAnalysis.h
@@ -44,9 +44,12 @@ public:
 
 private:
 
-    TDirectory* _dir_main;               /// Main TDirectory for this plugin 'occupancy_ana'
-    TH1F * _th1_prt_pz;                  /// MC Particles pz
-    TH2F * _th2_prt_pxy;                 /// MC Particles px,py
+    TDirectory* m_dir_main;               /// Main TDirectory for this plugin 'occupancy_ana'
+    TH1F * m_th1_prt_pz;                  /// MC Particles pz
+    TH1F * m_th1_prt_energy;              /// MC Particles total E
+    TH1F * m_th1_prt_theta;               /// MC Particles theta angle
+    TH1F * m_th1_prt_phi;                 /// MC Particles phi angle
+    TH2F * m_th2_prt_pxy;                 /// MC Particles px,py
 
 //    std::string VolNameToDetName(const std::string& vol_name);
 //
