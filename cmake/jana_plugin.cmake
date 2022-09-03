6b4048522f0380648485e1c8fb2e46762d80f4c2
David Lawrence <davidl@jlab.org>
2022-09-03 18:48:42 -0400

    Explicitly get the dedicated fmt package and add it to all plugins. Do not use the one bundled with spdlog (if there) since it requires different include directories.

--- cmake/jana_plugin.cmake
+++ cmake/jana_plugin.cmake
@@ -18,6 +18,8 @@ macro(plugin_add _name)
 
     # include logging by default
     find_package(spdlog REQUIRED)
+    find_package(fmt REQUIRED)
+    set(fmt_INCLUDE_DIR ${fmt_DIR}/../../../include)
 
     # include ROOT by default
     find_package(ROOT REQUIRED)
@@ -25,9 +27,9 @@ macro(plugin_add _name)
     # Define plugin
     add_library(${_name}_plugin SHARED ${PLUGIN_SOURCES})
     target_include_directories(${_name}_plugin PUBLIC ${CMAKE_SOURCE_DIR}/src)
-    target_include_directories(${_name}_plugin SYSTEM PRIVATE ${fmt_INCLUDE_DIR})
     target_include_directories(${_name}_plugin SYSTEM PUBLIC ${JANA_INCLUDE_DIR} )
     target_include_directories(${_name}_plugin SYSTEM PUBLIC ${ROOT_INCLUDE_DIRS} )
+    target_include_directories(${_name}_plugin SYSTEM PUBLIC ${fmt_INCLUDE_DIR} )
     set_target_properties(${_name}_plugin PROPERTIES PREFIX "" OUTPUT_NAME "${_name}" SUFFIX ".so")
     target_link_libraries(${_name}_plugin ${JANA_LIB} spdlog::spdlog)
 
@@ -39,8 +41,8 @@ macro(plugin_add _name)
         # Define library
         add_library(${_name}_library STATIC "")
 	    target_include_directories(${_name}_library PUBLIC ${CMAKE_SOURCE_DIR}/src)
-        target_include_directories(${_name}_library SYSTEM PRIVATE ${fmt_INCLUDE_DIR} ${spdlog_INCLUDE_DIR})
         target_include_directories(${_name}_library SYSTEM PUBLIC ${JANA_INCLUDE_DIR} )
+        target_include_directories(${_name}_library SYSTEM PUBLIC ${fmt_INCLUDE_DIR} )
         set_target_properties(${_name}_library PROPERTIES PREFIX "lib" OUTPUT_NAME "${_name}" SUFFIX ".a")
         target_link_libraries(${_name}_library ${JANA_LIB} spdlog::spdlog)
 
