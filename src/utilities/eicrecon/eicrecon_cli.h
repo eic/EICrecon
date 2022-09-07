d427bbea7ecefb21198aac3b5dc1f22996102c1e
Xinxin Mei <xmei@jlab.org>
2022-09-07 14:15:43 -0400

    Copy JMain.cc/h to EICrecon repo
    Try to remove the dependency of the JMain.cc, so we can commit to EICrecon instead of JANA.

---
+++ src/utilities/eicrecon/eicrecon_cli.h
@@ -0,0 +1,34 @@
+//
+// Created by xmei on 9/7/22.
+//
+
+#ifndef EICRECON_EICRECON_CLI_H
+#define EICRECON_EICRECON_CLI_H
+
+#include <JANA/JApplication.h>
+
+namespace jana {
+
+    enum Flag {Unknown, ShowUsage, ShowVersion, ShowConfigs, LoadConfigs, DumpConfigs, Benchmark};
+
+    struct UserOptions {
+        /// Code representation of all user options.
+        /// This lets us cleanly separate args parsing from execution.
+
+        std::map<Flag, bool> flags;
+        std::map<std::string, std::string> params;
+        std::vector<std::string> eventSources;
+        std::string load_config_file;
+        std::string dump_config_file;
+    };
+
+    void PrintUsage();
+    void PrintUsageOptions();
+    void PrintVersion();
+    UserOptions ParseCommandLineOptions(int nargs, char *argv[], bool expect_extra=true);
+    JApplication* CreateJApplication(UserOptions& options);
+    int Execute(JApplication* app, UserOptions& options);
+
+}
+
+#endif //EICRECON_EICRECON_CLI_H
