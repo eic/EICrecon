3874df7069458b411594826adc317acb0d0245ed
Xinxin Mei <xmei@jlab.org>
2022-09-09 06:38:46 -0400

    Move JANA header img into normal run
    Many modes do not need this header.
    Only print the header img when it is a normal `app->Run()`.

--- src/utilities/eicrecon/print_info.h
+++ src/utilities/eicrecon/print_info.h
@@ -23,4 +23,18 @@ void printFactoryTable(JComponentSummary const& cs) {
     std::cout << ss.str() << std::endl;
 }
 
+void printJANAHeaderIMG() {
+    std::cout << "     ____      _     ___      ___       _               \n"
+                 "     `MM'     dM.    `MM\\     `M'      dM.              \n"
+                 "      MM     ,MMb     MMM\\     M      ,MMb              \n"
+                 "      MM     d'YM.    M\\MM\\    M      d'YM.      ____   \n"
+                 "      MM    ,P `Mb    M \\MM\\   M     ,P `Mb     6MMMMb  \n"
+                 "      MM    d'  YM.   M  \\MM\\  M     d'  YM.   MM'  `Mb \n"
+                 "      MM   ,P   `Mb   M   \\MM\\ M    ,P   `Mb        ,MM \n"
+                 "      MM   d'    YM.  M    \\MM\\M    d'    YM.      ,MM' \n"
+                 "(8)   MM  ,MMMMMMMMb  M     \\MMM   ,MMMMMMMMb    ,M'    \n"
+                 "((   ,M9  d'      YM. M      \\MM   d'      YM. ,M'      \n"
+                 " YMMMM9 _dM_     _dMM_M_      \\M _dM_     _dMM_MMMMMMMM " << std::endl << std::endl;
+}
+
 #endif //EICRECON_PRINT_INFO_H
