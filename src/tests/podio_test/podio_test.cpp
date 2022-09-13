ad0177143fee6b22939ca240c2eb33803dca278d
Nathan Brei <nbrei@jlab.org>
2022-09-13 15:43:53 -0400

    Add podio unit test

---
+++ src/tests/podio_test/podio_test.cpp
@@ -0,0 +1,11 @@
+
+// Copyright 2022, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
+
