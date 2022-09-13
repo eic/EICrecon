fe95adc8a6cf96a28bcf5766491481eeedbef4fe
Xinxin Mei <xmei@jlab.org>
2022-09-13 16:55:49 -0400

    Update out-of-date contents in `custom_environment_example.sh`

--- custom_environment_example.sh
+++ custom_environment_example.sh
@@ -41,5 +41,5 @@ source ${EICTOPDIR}/ACTS/${ACTS_VERSION}/install/bin/this_acts.sh
 export fmt_ROOT=${EICTOPDIR}/detectors/fmt/${FMT_VERSION}/install
 export LD_LIBRARY_PATH=${fmt_ROOT}/lib64:${fmt_ROOT}/lib:${LD_LIBRARY_PATH}
 export IP6_DD4HEP_HOME=${EICTOPDIR}/detectors/ip6
-export EIC_DD4HEP_HOME=${EICTOPDIR}/detectors/ecce
-export EIC_DD4HEP_XML=${EIC_DD4HEP_HOME}/ecce.xml
+export EIC_DD4HEP_HOME=${EICTOPDIR}/detectors/epic
+export EIC_DD4HEP_XML=${EIC_DD4HEP_HOME}/epic.xml
