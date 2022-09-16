2453af05a1306e6c9886a14943c8558168a41b51
Nathan Brei <nbrei@jlab.org>
2022-09-16 13:27:18 -0400

    Finish read-then-write test

--- src/tests/podio_test/podio_test.cpp
+++ src/tests/podio_test/podio_test.cpp
@@ -108,43 +108,42 @@ void write_read_test() {
 
 void read_write_test() {
 
-    eic::EventStore store;
+    eic::EventStore input_store;
+    eic::EventStore output_store;
 
     podio::ROOTReader reader;
     reader.openFile("test_out.root"); // comes from prev test
-    store.setReader(&reader);
+    input_store.setReader(&reader);
 
-    eic::ROOTWriter writer("test2_out.root", &store);
+    eic::ROOTWriter writer("test2_out.root", &output_store);
+    auto& clusters_out = output_store.create<edm4eic::ClusterCollection>("MyFunClusters");
+    auto& clusters_out_filtered = output_store.create<edm4eic::ClusterCollection>("MyExhilaratingClusters");
 
+    writer.registerForWrite("MyFunClusters");
+    writer.registerForWrite("MyExhilaratingClusters"); // Collection needs to be added to store first
 
     auto nevents = reader.getEntries();
     for (int i=0; i<nevents; ++i) {
         std::cout << "Processing event " << i << std::endl;
         reader.goToEvent(i);
-        auto& hits = store.get<edm4eic::CalorimeterHitCollection>("MyFunHits");
-        auto& clusters = store.get<edm4eic::ClusterCollection>("MyFunClusters");
+        auto& hits_in = input_store.get<edm4eic::CalorimeterHitCollection>("MyFunHits");
+        auto& clusters_in = input_store.get<edm4eic::ClusterCollection>("MyFunClusters");
 
-        auto& clusters_filtered = store.create<edm4eic::ClusterCollection>("MyExhilaratingClusters");
-        // auto& clusters_filtered_temp = store.get<edm4eic::ClusterCollection>("MyExhilaratingClusters");
-
-        // auto& clusters_out = store.create<edm4eic::ClusterCollection>("MyExhilaratingClusters");
-        for (auto cluster : clusters) {
+        for (auto cluster : clusters_in) {
+            clusters_out.push_back(cluster.clone());
             if (cluster.getEnergy() < 50) {
                 std::cout << "Adding cluster with energy " << cluster.getEnergy() << std::endl;
-                clusters_filtered.push_back(cluster.clone()); 
+                clusters_out_filtered.push_back(cluster.clone()); 
                 // will this do a deep or shallow copy in memory?
                 // what about in file?
                 // will references be included?
             }
         }
-        // writer.registerForWrite("MyFunHits");
-        // Let's not write out the hits, just to make sure we can
-        writer.registerForWrite("MyFunClusters");
-        writer.registerForWrite("MyExhilaratingClusters"); // Collection needs to be added to store first
-
         writer.writeEvent();
-        store.clear();
         reader.endOfEvent();
+
+        input_store.clear();
+        output_store.clearCollections();
     }
     writer.finish();
     reader.closeFile();
