ee2c8057d65657d24b9ead284c8502ea365c2005
Dmitry Romanov <romanovda@gmail.com>
2022-09-27 09:33:55 -0400

    Acts logging consolidated

---
+++ src/extensions/spdlog/SpdlogToActs.h
@@ -0,0 +1,8 @@
+// Created by Dmitry Romanov
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef EICRECON_SPDLOGTOACTS_H
+#define EICRECON_SPDLOGTOACTS_H
+
+#endif //EICRECON_SPDLOGTOACTS_H
