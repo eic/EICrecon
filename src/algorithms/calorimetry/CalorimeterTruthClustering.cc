e8d7cf1823cdcef3d4905c5897b907967a217653
David Lawrence <davidl@jlab.org>
2022-09-28 11:56:11 -0400

    Workaround bug where array bounds where not being checked and assumption was made that podio collection mechanics were being used.

--- src/algorithms/calorimetry/CalorimeterTruthClustering.cc
+++ src/algorithms/calorimetry/CalorimeterTruthClustering.cc
@@ -41,18 +41,48 @@ void CalorimeterTruthClustering::AlgorithmProcess() {
     // Map mc track ID to protoCluster index
     std::map<int32_t, int32_t> protoIndex;
 
-    // Loop over al calorimeter hits and sort per mcparticle
+    // Loop over all calorimeter hits and sort per mcparticle
     for (const auto& hit : hits) {
-      const auto& mcHit     = mc[hit->getObjectID().index];
-      const auto& trackID   = mcHit->getContributions(0).getParticle().id();
-      // Create a new protocluster if we don't have one for this trackID
-      if (protoIndex.count(trackID) == 0) {
-        auto pcl = new edm4eic::ProtoCluster();
-        protoIndex[trackID] = proto.size() - 1;
-      }
-      // Add hit to the appropriate protocluster
-      proto[protoIndex[trackID]]->addToHits(*hit);
-      proto[protoIndex[trackID]]->addToWeights(1);
+        // The original algorithm used the following to get the mcHit:
+        //
+        //    const auto& mcHit     = mc[hit->getObjectID().index];
+        //
+        // This assumes there is a one-to-one relation between the truth hit
+        // (hits) and the reconstructed hit (mc). At least insofar as the
+        // index in "hits" is being used to index the "mc" container.
+        //
+        // If the objects in "hits" have not been added to a collection,
+        // then they will have getObjectID().index = "untracked" = -1
+        //
+        // The way we handle this is here is to check if getObjectID().index
+        // is within the size limits of mc which includes >=0. If so, then
+        // assume the old code is valid. If not, then we need to search
+        // for the right hit.
+        // FIXME: This is clearly not the right way to do this! Podio needs
+        // FIXME: to be fixed so proper object tracking can be done without
+        // FIXME: requiring Collection classes be used to manage all objects.
+        const edm4hep::SimCalorimeterHit* mcHit = nullptr;
+        if( hit->getObjectID().index>=0 && hit->getObjectID().index<mc.size() ){
+            mcHit = mc[hit->getObjectID().index];
+        }else{
+            for( auto tmpmc : mc ){
+                if( tmpmc->getCellID() == hit->getCellID() ){
+                    mcHit = tmpmc;
+                    break;
+                }
+            }
+        }
+        if( ! mcHit ) continue; // ignore hit if we couldn't match it to truth hit
+
+        const auto &trackID = mcHit->getContributions(0).getParticle().id();
+        // Create a new protocluster if we don't have one for this trackID
+        if (protoIndex.count(trackID) == 0) {
+            proto.push_back( new edm4eic::MutableProtoCluster() );
+            protoIndex[trackID] = proto.size() - 1;
+        }
+        // Add hit to the appropriate protocluster
+        proto[protoIndex[trackID]]->addToHits(*hit);
+        proto[protoIndex[trackID]]->addToWeights(1);
     }
 
     // iterate over proto and push to m_outputProtoClusters
