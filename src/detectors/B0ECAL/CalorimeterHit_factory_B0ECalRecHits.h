cb99a2ac723c68d89bfe836500702a7e9d5fe020
David Lawrence <davidl@jlab.org>
2022-10-22 13:09:57 -0400

    Add B0 ECal

---
+++ src/detectors/B0ECAL/CalorimeterHit_factory_B0ECalRecHits.h
@@ -0,0 +1,99 @@
+
+#pragma once
+
+
+#include <JANA/JFactoryT.h>
+
+#include <algorithms/calorimetry/CalorimeterHitReco.h>
+#include <services/log/Log_service.h>
+#include <extensions/spdlog/SpdlogExtensions.h>
+
+class CalorimeterHit_factory_B0ECalRecHits : public JFactoryT<edm4eic::CalorimeterHit>, CalorimeterHitReco {
+
+public:
+    //------------------------------------------
+    // Constructor
+    CalorimeterHit_factory_B0ECalRecHits(){
+        SetTag("B0ECalRecHits");
+    }
+
+    //------------------------------------------
+    // Init
+    void Init() override{
+        auto app = GetApplication();
+
+        m_input_tag = "B0ECalRawHits";
+
+        // digitization settings, must be consistent with digi class
+        m_capADC=16384;//{this, "capacityADC", 8096};
+        m_dyRangeADC=20. * GeV;//{this, "dynamicRangeADC", 100. * MeV};
+        m_pedMeanADC=100;//{this, "pedestalMean", 400};
+        m_pedSigmaADC=1;//{this, "pedestalSigma", 3.2};
+        m_resolutionTDC=1e-11;//{this, "resolutionTDC", 10 * ps};
+
+        // zero suppression values
+        m_thresholdFactor=4.0;//{this, "thresholdFactor", 0.0};
+        m_thresholdValue=3.0;//{this, "thresholdValue", 0.0};
+
+        // energy correction with sampling fraction
+        m_sampFrac=0.998;//{this, "samplingFraction", 1.0};
+
+        // geometry service to get ids, ignored if no names provided
+        m_geoSvcName="";
+        m_readout="B0ECalHits";  // from ATHENA's reconstruction.py
+        m_layerField="";              // from ATHENA's reconstruction.py (i.e. not defined there)
+        m_sectorField="sector";       // from ATHENA's reconstruction.py
+
+        m_localDetElement="";         // from ATHENA's reconstruction.py (i.e. not defined there)
+        u_localDetFields={};          // from ATHENA's reconstruction.py (i.e. not defined there)
+
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:input_tag",        m_input_tag, "Name of input collection to use");
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:capacityADC",      m_capADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:dynamicRangeADC",  m_dyRangeADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:pedestalMean",     m_pedMeanADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:pedestalSigma",    m_pedSigmaADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:resolutionTDC",    m_resolutionTDC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:thresholdFactor",  m_thresholdFactor);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:thresholdValue",   m_thresholdValue);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:samplingFraction", m_sampFrac);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:geoServiceName",   m_geoSvcName);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:readout",          m_readout);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:layerField",       m_layerField);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:sectorField",      m_sectorField);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:localDetElement",  m_localDetElement);
+        app->SetDefaultParameter("B0ECAL:B0ECalRecHits:localDetFields",   u_localDetFields);
+        m_geoSvc = app->template GetService<JDD4hep_service>(); // TODO: implement named geometry service?
+
+        std::string tag=this->GetTag();
+        std::shared_ptr<spdlog::logger> m_log = app->GetService<Log_service>()->logger(tag);
+
+        // Get log level from user parameter or default
+        std::string log_level_str = "info";
+        auto pm = app->GetJParameterManager();
+        pm->SetDefaultParameter(tag + ":LogLevel", log_level_str, "verbosity: trace, debug, info, warn, err, critical, off");
+        m_log->set_level(eicrecon::ParseLogLevel(log_level_str));
+        AlgorithmInit(m_log);
+    }
+
+    //------------------------------------------
+    // ChangeRun
+    void ChangeRun(const std::shared_ptr<const JEvent> &event) override{
+        AlgorithmChangeRun();
+    }
+
+    //------------------------------------------
+    // Process
+    void Process(const std::shared_ptr<const JEvent> &event) override{
+        // Prefill inputs
+        rawhits = event->Get<edm4hep::RawCalorimeterHit>(m_input_tag);
+
+        // Call Process for generic algorithm
+        AlgorithmProcess();
+
+        // Hand owner of algorithm objects over to JANA
+        Set(hits);
+        hits.clear(); // not really needed, but better to not leave dangling pointers around
+    }
+
+};
+
---
+++ src/detectors/B0ECAL/RawCalorimeterHit_factory_B0ECalRawHits.h
@@ -0,0 +1,103 @@
+// Copyright 2022, David Lawrence
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#pragma once
+
+#include <random>
+
+#include <JANA/JEvent.h>
+#include <JANA/JFactoryT.h>
+#include <services/geometry/dd4hep/JDD4hep_service.h>
+#include <algorithms/calorimetry/CalorimeterHitDigi.h>
+#include <edm4hep/SimCalorimeterHit.h>
+#include <edm4hep/RawCalorimeterHit.h>
+#include <Evaluator/DD4hepUnits.h>
+#include <services/log/Log_service.h>
+#include <extensions/spdlog/SpdlogExtensions.h>
+
+using namespace dd4hep;
+
+
+class RawCalorimeterHit_factory_B0ECalRawHits : public JFactoryT<edm4hep::RawCalorimeterHit>, CalorimeterHitDigi {
+
+public:
+
+    //------------------------------------------
+    // Constructor
+    RawCalorimeterHit_factory_B0ECalRawHits() {
+        SetTag("B0ECalRawHits");
+    }
+
+    //------------------------------------------
+    // Init
+    void Init() override {
+        auto app = GetApplication();
+
+        // Set default values for all config. parameters in CalorimeterHitDigi algorithm
+        m_input_tag = "B0ECalHits";
+        u_eRes = {0.0,0.02,0.0};
+        m_tRes = 0.0 * ns;
+        m_capADC = 16384;
+        m_dyRangeADC = 1 * GeV;
+        m_pedMeanADC = 100;
+        m_pedSigmaADC = 1;
+        m_resolutionTDC = 1e-11;
+        m_corrMeanScale = 1.0;
+        u_fields={};
+        u_refs={};
+        m_geoSvcName = "";
+        m_readout = "";
+        
+        m_geoSvc = app->GetService<JDD4hep_service>(); // TODO: implement named geometry service?
+        
+
+        // This is another option for exposing the data members as JANA configuration parameters.
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:input_tag",        m_input_tag, "Name of input collection to use");
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:energyResolutions",u_eRes);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:timeResolution",   m_tRes);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:capacityADC",      m_capADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:dynamicRangeADC",  m_dyRangeADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:pedestalMean",     m_pedMeanADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:pedestalSigma",    m_pedSigmaADC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:resolutionTDC",    m_resolutionTDC);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:scaleResponse",    m_corrMeanScale);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:signalSumFields",  u_fields);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:fieldRefNumbers",  u_refs);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:geoServiceName",   m_geoSvcName);
+        app->SetDefaultParameter("B0ECAL:B0ECalRawHits:readoutClass",     m_readout);
+
+        // Call Init for generic algorithm
+        std::string tag=this->GetTag();
+        std::shared_ptr<spdlog::logger> m_log = app->GetService<Log_service>()->logger(tag);
+
+        // Get log level from user parameter or default
+        std::string log_level_str = "info";
+        auto pm = app->GetJParameterManager();
+        pm->SetDefaultParameter(tag + ":LogLevel", log_level_str, "verbosity: trace, debug, info, warn, err, critical, off");
+        m_log->set_level(eicrecon::ParseLogLevel(log_level_str));
+        AlgorithmInit(m_log);
+    }
+
+    //------------------------------------------
+    // ChangeRun
+    void ChangeRun(const std::shared_ptr<const JEvent> &event) override {
+        AlgorithmChangeRun();
+    }
+
+    //------------------------------------------
+    // Process
+    void Process(const std::shared_ptr<const JEvent> &event) override {
+        // Prefill inputs
+        simhits = event->Get<edm4hep::SimCalorimeterHit>(m_input_tag);
+
+        // Call Process for generic algorithm
+        AlgorithmProcess();
+
+        // Hand owner of algorithm objects over to JANA
+        Set(rawhits);
+        rawhits.clear(); // not really needed, but better to not leave dangling pointers around
+    }
+
+};
+
