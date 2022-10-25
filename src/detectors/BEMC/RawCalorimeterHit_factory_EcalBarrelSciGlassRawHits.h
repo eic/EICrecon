c94d9cf3c2dce7f8d43d2d231ae319532d8229c1
David Lawrence <davidl@jlab.org>
2022-10-25 16:27:02 -0400

    Use #pragma once

--- src/detectors/BEMC/RawCalorimeterHit_factory_EcalBarrelSciGlassRawHits.h
+++ src/detectors/BEMC/RawCalorimeterHit_factory_EcalBarrelSciGlassRawHits.h
@@ -2,8 +2,7 @@
 // Subject to the terms in the LICENSE file found in the top-level directory.
 //
 
-#ifndef _RawCalorimeterHit_factory_EcalBarrelRawHits_h_
-#define _RawCalorimeterHit_factory_EcalBarrelRawHits_h_
+#pragma once
 
 #include <random>
 
@@ -103,4 +102,3 @@ public:
 
 };
 
-#endif // _RawCalorimeterHit_factory_EcalBarrelRawHits_h_
