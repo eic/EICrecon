04f335489b4f37904a7a6dd5705e81f7b21257ff
Dmitry Romanov <romanovda@gmail.com>
2022-11-06 23:28:24 -0500

    Spdlog mixin respects default logger level

--- src/extensions/spdlog/SpdlogMixin.h
+++ src/extensions/spdlog/SpdlogMixin.h
@@ -22,7 +22,7 @@ namespace eicrecon {
          *      class MyFactory : JFactory, SpdlogMixin<MyFactory> {
          *
          *          void Init() {
-         *              InitLogger("MyPlugin:MyFactory", "info");
+         *              InitLogger("MyPlugin:MyFactory");
          *
          *              // Logger is ready and can be used:
          *              m_log->info("MyFactory logger initialized");
@@ -37,14 +37,21 @@ namespace eicrecon {
         /**
          * Initializes logger through current LogService
          * @param param_prefix - name of both logger and user parameter
-         * @param default_level - default log level: trace, debug, info, warn, err, critical, off
+         * @param default_level - optional - default log level, overrides default logging level
+         *                          : trace, debug, info, warn, err, critical, off
+         *
+         * @remark: Each logger is cloned from spdlog::default_logger(). This allows to set the default level
+         *          and output formatting on a global system level. But sometimes it is useful to provide
+         *          default log level independently on what is set by the system. Again we are about DEFAULT value
+         *          if no user flag is provided
          *
          * @example:
-         *      InitLogger("BTRK:TrackerHits", "info")
+         *      InitLogger("BTRK:TrackerHits")           // Default log level is set the same as in system
+         *      InitLogger("BTRK:TrackerHits", "info")   // By default log level is info
          *
          *  will create "BTRK:TrackerHits" logger and check -PBTRK:TrackerHits:LogLevel user parameter
          */
-        void InitLogger(const std::string &param_prefix, const std::string &default_level) {
+        void InitLogger(const std::string &param_prefix, const std::string &default_level = "") {
 
             JApplication* app = static_cast<T*>(this)->GetApplication();
 
@@ -52,7 +59,9 @@ namespace eicrecon {
             m_log = app->GetService<Log_service>()->logger(param_prefix);
 
             // Get log level from user parameter or default
-            std::string log_level_str = default_level;
+            std::string log_level_str = default_level.empty() ?         // did user provide default level?
+                                        eicrecon::LogLevelToString(m_log->level()) :   //
+                                        default_level;
             app->SetDefaultParameter(param_prefix + ":LogLevel", log_level_str, "LogLevel: trace, debug, info, warn, err, critical, off");
             m_log->set_level(eicrecon::ParseLogLevel(log_level_str));
         }
