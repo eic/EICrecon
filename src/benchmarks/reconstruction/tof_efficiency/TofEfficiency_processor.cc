93344ddb0d77073c767c51e7a1f7ee0a28a7a5ef
Dmitry Romanov <romanovda@gmail.com>
2022-11-09 15:17:32 -0500

    Tof example

--- src/benchmarks/reconstruction/tof_efficiency/TofEfficiency_processor.cc
+++ src/benchmarks/reconstruction/tof_efficiency/TofEfficiency_processor.cc
@@ -10,7 +10,7 @@
 void TofEfficiency_processor::InitWithGlobalRootLock(){
     std::string plugin_name=("tof_efficiency");
 
-    InitLogger(plugin_name, "info");
+    InitLogger(plugin_name);
 
     // Get JANA application
     auto app = GetApplication();
@@ -26,9 +26,6 @@ void TofEfficiency_processor::InitWithGlobalRootLock(){
 
     // Create a directory for this plugin. And subdirectories for series of histograms
     m_dir_main = file->mkdir(plugin_name.c_str());
-
-    //
-
 }
 
 //-------------------------------------------
@@ -37,30 +34,30 @@ void TofEfficiency_processor::InitWithGlobalRootLock(){
 void TofEfficiency_processor::ProcessSequential(const std::shared_ptr<const JEvent>& event) {
 
     // List TOF Barrel hits from barrel
-    logger()->info("TOF barrel hits:");
-    m_log->info("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[time]");
+    logger()->trace("TOF barrel hits:");
+    m_log->trace("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[time]");
     for (auto hit: barrelHits()) {
         auto& pos = hit->getPosition();
-        m_log->info("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.4f}", pos.x, pos.y, pos.z, hit->getTime());
+        m_log->trace("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.4f}", pos.x, pos.y, pos.z, hit->getTime());
     }
 
     // List TOF endcap hits
-    logger()->info("TOF endcap hits:");
-    m_log->info("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[time]");
+    logger()->trace("TOF endcap hits:");
+    m_log->trace("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[time]");
     for (auto hit: endcapHits()) {
         auto& pos = hit->getPosition();
-        m_log->info("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.4f}", pos.x, pos.y, pos.z, hit->getTime());
+        m_log->trace("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.4f}", pos.x, pos.y, pos.z, hit->getTime());
     }
 
     // Now go through reconstructed tracks points
-    logger()->info("Going over tracks:");
-    m_log->info("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[length]");
+    logger()->trace("Going over tracks:");
+    m_log->trace("   {:>10} {:>10} {:>10} {:>10}", "[x]", "[y]", "[z]", "[length]");
     for( auto track_segment : trackSegments() ){
-        logger()->info(" Track trajectory");
+        logger()->trace(" Track trajectory");
 
         for(auto point: track_segment->getPoints()) {
             auto &pos = point.position;
-            m_log->info("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.2f}", pos.x, pos.y, pos.z, point.pathlength);
+            m_log->trace("   {:>10.2f} {:>10.2f} {:>10.2f} {:>10.2f}", pos.x, pos.y, pos.z, point.pathlength);
         }
     }
 }
