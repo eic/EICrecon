9060e03538b74eb8d9afd9bad3af912e23b3a38b
Dmitry Romanov <romanovda@gmail.com>
2022-11-11 12:34:16 -0500

    Improve SourceLinker debug ouptut

--- src/algorithms/tracking/TrackerSourceLinker.cc
+++ src/algorithms/tracking/TrackerSourceLinker.cc
@@ -62,7 +62,9 @@ eicrecon::TrackerSourceLinkerResult *eicrecon::TrackerSourceLinker::produce(std:
         auto surfaceMap = m_acts_context->surfaceMap();
 
         m_log->trace("Hit preparation information: {}", hit_index);
-        m_log->trace("   cov matrix:      {}", cov);
+        m_log->trace("   System id: {}, Cell id: {}", hit->getCellID() &0xFF, hit->getCellID());
+        m_log->trace("   cov matrix:      {:>12.2e} {:>12.2e}", cov(0,0), cov(0,1));
+        m_log->trace("                    {:>12.2e} {:>12.2e}", cov(1,0), cov(1,1));
         m_log->trace("   surfaceMap size: {}", surfaceMap.size());
 
         const auto is = surfaceMap.find(vol_id);
@@ -100,12 +102,14 @@ eicrecon::TrackerSourceLinkerResult *eicrecon::TrackerSourceLinker::produce(std:
             auto volman         = m_acts_context->dd4hepDetector()->volumeManager();
             auto alignment      = volman.lookupDetElement(vol_id).nominal();
             auto local_position = (alignment.worldToLocal({hit_pos.x / mm_conv, hit_pos.y / mm_conv, hit_pos.z / mm_conv})) * mm_conv;
-
-            m_log->trace("   hit position     : {:8<.2f} {:8<.2f} {:8<.2f}", hit_pos.x, hit_pos.y, hit_pos.z);
-            m_log->trace("   local position   : {:8<.2f} {:8<.2f} {:8<.2f}", local_position.x(), local_position.y(), local_position.z());
-            m_log->trace("   surface center   : {}", surface->center(Acts::GeometryContext()).transpose());
-            m_log->trace("   acts local center: {}", pos.transpose());
-            m_log->trace("   acts loc pos     : {:8<.2f}, {:8<.2f}", loc[Acts::eBoundLoc0], loc[Acts::eBoundLoc1]);
+            double surf_center_x = surface->center(Acts::GeometryContext()).transpose()[0];
+            double surf_center_y = surface->center(Acts::GeometryContext()).transpose()[1];
+            double surf_center_z = surface->center(Acts::GeometryContext()).transpose()[2];
+            m_log->trace("   hit position     : {:>10.2f} {:>10.2f} {:>10.2f}", hit_pos.x, hit_pos.y, hit_pos.z);
+            m_log->trace("   local position   : {:>10.2f} {:>10.2f} {:>10.2f}", local_position.x(), local_position.y(), local_position.z());
+            m_log->trace("   surface center   : {:>10.2f} {:>10.2f} {:>10.2f}", surf_center_x, surf_center_y, surf_center_z);
+            m_log->trace("   acts local center: {:>10.2f} {:>10.2f}", pos.transpose()[0], pos.transpose()[1]);
+            m_log->trace("   acts loc pos     : {:>10.2f} {:>10.2f}", loc[Acts::eBoundLoc0], loc[Acts::eBoundLoc1]);
         }
 
 
