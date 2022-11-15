c9ceddc0275c911065d9c3658a51d646264ca63e
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-11-15 01:07:25 -0500

    feat: SiPM pixel gap cuts

--- src/algorithms/digi/PhotoMultiplierHitDigi.h
+++ src/algorithms/digi/PhotoMultiplierHitDigi.h
@@ -21,34 +21,46 @@
 #include <spdlog/spdlog.h>
 
 #include "PhotoMultiplierHitDigiConfig.h"
+#include <algorithms/interfaces/WithPodConfig.h>
 
 namespace eicrecon {
 
-class PhotoMultiplierHitDigi {
+class PhotoMultiplierHitDigi : public WithPodConfig<PhotoMultiplierHitDigiConfig> {
 
     // Insert any member variables here
 
 public:
     PhotoMultiplierHitDigi() = default;
     ~PhotoMultiplierHitDigi(){}
-    void AlgorithmInit(std::shared_ptr<spdlog::logger>& logger);
+    void AlgorithmInit(dd4hep::Detector *detector, std::shared_ptr<spdlog::logger>& logger);
     void AlgorithmChangeRun();
     std::vector<edm4eic::RawPMTHit*> AlgorithmProcess(std::vector<const edm4hep::SimTrackerHit*>& sim_hits);
 
-    // configurations
-    eicrecon::PhotoMultiplierHitDigiConfig& getConfig() {return m_cfg;}
-    void applyConfig(eicrecon::PhotoMultiplierHitDigiConfig cfg) { m_cfg = cfg; }
+    // transform global position `pos` to sensor `id` frame position
+    // IMPORTANT NOTE: this has only been tested for the dRICH; if you use it, test it carefully...
+    dd4hep::Position get_sensor_local_position(uint64_t id, dd4hep::Position pos);
 
     //instantiate new spdlog logger
     std::shared_ptr<spdlog::logger> m_log;
 
+    // random number generators
     TRandomMixMax m_random;
     std::function<double()> m_rngNorm;
     std::function<double()> m_rngUni;
     //Rndm::Numbers m_rngUni, m_rngNorm;
 
+    // convert dd4hep::Position <-> edm4hep::Vector3d
+    edm4hep::Vector3d pos2vec(dd4hep::Position p) {
+      return edm4hep::Vector3d( p.x()/dd4hep::mm, p.y()/dd4hep::mm, p.z()/dd4hep::mm );
+    }
+    dd4hep::Position vec2pos(edm4hep::Vector3d v) {
+      return dd4hep::Position( v.x*dd4hep::mm, v.y*dd4hep::mm, v.z*dd4hep::mm );
+    }
+
+
 private:
-    eicrecon::PhotoMultiplierHitDigiConfig m_cfg;
+
+    std::shared_ptr<const dd4hep::rec::CellIDPositionConverter> m_cellid_converter;
 
     // std::default_random_engine generator; // TODO: need something more appropriate here
     // std::normal_distribution<double> m_normDist; // defaults to mean=0, sigma=1
