fc016db8d3fc833e8f62d1053264b5ae04353ddd
Joe Osborn <osbornjd@ornl.gov>
2022-12-22 16:53:46 -0500

    Add very initial and preliminary, not complete, version of orthogonal seeder

--- src/algorithms/tracking/TrackSeeding.cc
+++ src/algorithms/tracking/TrackSeeding.cc
@@ -4,6 +4,14 @@
 
 #include "TrackSeeding.h"
 
+#include "Acts/Seeding/InternalSeed.hpp"
+#include "Acts/Seeding/SeedFilterConfig.hpp"
+#include "Acts/Seeding/SeedFinderOrthogonalConfig.hpp"
+#include "Acts/Seeding/SpacePointGrid.hpp"
+#include "Acts/Utilities/KDTree.hpp"
+#include "Acts/Seeding/Seed.hpp"
+#include "Acts/Seeding/SeedFilter.hpp"
+#include "Acts/Seeding/SeedFinderOrthogonal.hpp"
 
 #include <TDatabasePDG.h>
 
@@ -15,23 +23,60 @@ void eicrecon::TrackSeeding::init(std::shared_ptr<const ActsGeometryProvider> ge
 
     m_BField = std::dynamic_pointer_cast<const eicrecon::BField::DD4hepBField>(m_geoSvc->getFieldProvider());
     m_fieldctx = eicrecon::BField::BFieldVariant(m_BField);
-
-    // eta bins, chi2 and #sourclinks per surface cutoffs
-    m_sourcelinkSelectorCfg = {
-            {Acts::GeometryIdentifier(),
-             {m_cfg.m_etaBins, m_cfg.m_chi2CutOff,
-              {m_cfg.m_numMeasurementsCutOff.begin(), m_cfg.m_numMeasurementsCutOff.end()}
-             }
-            },
-    };
-
-
+    
+    m_seederConfig = configureSeeder();
 }
 
 std::vector<edm4eic::TrackParameters*> eicrecon::TrackSeeding::produce(std::vector<const edm4eic::TrackerHit*> trk_hits) {
 
-    // getGeneratorStatus = 1 means thrown G4Primary
-    std::vector<edm4eic::TrackParameters*> result;
+  eicrecon::SeedContainer seeds = runSeeder(trk_hits);
+
+// getGeneratorStatus = 1 means thrown G4Primary
+std::vector<edm4eic::TrackParameters*> result = makeTrackParams(seeds);
 
     return result;
+}
+
+eicrecon::SeedContainer eicrecon::TrackSeeding::runSeeder(std::vector<const edm4eic::TrackerHit*>& trk_hits)
+{
+  std::vector<const eicrecon::SpacePoint*> spacePoints = getSpacePoints(trk_hits);
+
+  Acts::SeedFinderOrthogonal<eicrecon::SpacePoint> finder(m_seederConfig.m_seedFinderConfig);
+  eicrecon::SeedContainer seeds = finder.createSeeds(spacePoints);
+  return seeds;
+}
+
+std::vector<const eicrecon::SpacePoint*> eicrecon::TrackSeeding::getSpacePoints(std::vector<const edm4eic::TrackerHit*>& trk_hits)
+{
+  std::vector<const eicrecon::SpacePoint*> spacepoints;
+
+  for(const auto hit : trk_hits)
+    {
+      const eicrecon::SpacePoint* sp = new SpacePoint(*hit);
+      spacepoints.push_back(sp);
+    }
+
+  return spacepoints;
+}
+
+eicrecon::OrthogonalTrackSeedingConfig eicrecon::TrackSeeding::configureSeeder()
+{
+  eicrecon::OrthogonalTrackSeedingConfig cfg;
+  cfg.configure();
+  return cfg;
+}
+
+std::vector<edm4eic::TrackParameters*> eicrecon::TrackSeeding::makeTrackParams(SeedContainer& seeds)
+{
+  std::vector<edm4eic::TrackParameters*> trackparams;
+
+  for(auto& seed : seeds)
+    {
+      for(auto& spptr : seed.sp())
+	{
+	  
+	}
+    }
+
+  return trackparams;
 }
