ccbd8de0326f4638eb6da54f9893efbf3be9b474
Wouter Deconinck <wdconinc@gmail.com>
2023-01-12 19:04:48 -0600

    EEMC:EcalEndcapPInsertIslandProtoClusters:localDistXY

--- src/detectors/EEMC/ProtoCluster_factory_EcalEndcapPInsertIslandProtoClusters.h
+++ src/detectors/EEMC/ProtoCluster_factory_EcalEndcapPInsertIslandProtoClusters.h
@@ -34,7 +34,7 @@ public:
 
         // neighbour checking distances
         m_sectorDist=5.0 * dd4hep::cm;             // from ATHENA reconstruction.py
-        u_localDistXY={1.0 * dd4hep::cm, 1.0 * dd4hep::cm};     //{this, "localDistXY", {}};
+        u_localDistXY={10.0 * dd4hep::cm, 10.0 * dd4hep::cm};     //{this, "localDistXY", {}};
         u_localDistXZ={};     //{this, "localDistXZ", {}};
         u_localDistYZ={};     //{this, "localDistYZ", {}};
         u_globalDistRPhi={};  //{this, "globalDistRPhi", {}};
