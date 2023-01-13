7a70248461c6179b9202346563982168b429276d
Wouter Deconinck <wdconinc@gmail.com>
2023-01-13 15:13:02 -0600

    use correct algo name

--- src/global/tracking/TrackParamTruthInit_factory.cc
+++ src/global/tracking/TrackParamTruthInit_factory.cc
@@ -18,25 +18,32 @@ void eicrecon::TrackParamTruthInit_factory::Init() {
     auto app = GetApplication();
 
     // This prefix will be used for parameters
-    std::string param_prefix = GetDefaultParameterPrefix();
+    std::string plugin_name = eicrecon::str::ReplaceAll(GetPluginName(), ".so", "");
+    std::string param_prefix = plugin_name+ ":" + GetTag();
 
-    InitLogger(param_prefix);
+    // Initialize input tags
     InitDataTags(param_prefix);
+
+    // Initialize logger
+    InitLogger(param_prefix, "info");
+
+    // Get ACTS context from ACTSGeo service
+    auto acts_service = app->GetService<ACTSGeo_service>();
 
     // Algorithm configuration
     auto cfg = GetDefaultConfig();
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MaxVertexX", cfg.m_maxVertexX , "Maximum abs(vertex x) for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MaxVertexY", cfg.m_maxVertexY , "Maximum abs(vertex y) for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MaxVertexZ", cfg.m_maxVertexZ , "Maximum abs(vertex z) for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MinMomentum", cfg.m_minMomentum , "Minimum momentum for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MaxEtaForward", cfg.m_maxEtaForward , "Maximum forward abs(eta) for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MaxEtaBackward", cfg.m_maxEtaBackward , "Maximum backward abs(eta) for truth tracks turned into seed");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MomentumSplit", cfg.m_momentumSplit, "Momentum magnitude fraction to use as width for random trifurcation");
-    app->SetDefaultParameter(param_prefix + ":TruthSeeding:MomentumSmear", cfg.m_momentumSmear, "Momentum magnitude fraction to use as width of gaussian smearing");
+    app->SetDefaultParameter(param_prefix + ":MaxVertexX", cfg.m_maxVertexX , "Maximum abs(vertex x) for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MaxVertexY", cfg.m_maxVertexY , "Maximum abs(vertex y) for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MaxVertexZ", cfg.m_maxVertexZ , "Maximum abs(vertex z) for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MinMomentum", cfg.m_minMomentum , "Minimum momentum for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MaxEtaForward", cfg.m_maxEtaForward , "Maximum forward abs(eta) for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MaxEtaBackward", cfg.m_maxEtaBackward , "Maximum backward abs(eta) for truth tracks turned into seed");
+    app->SetDefaultParameter(param_prefix + ":MomentumSplit", cfg.m_momentumSplit, "Momentum magnitude fraction to use as width for random trifurcation");
+    app->SetDefaultParameter(param_prefix + ":MomentumSmear", cfg.m_momentumSmear, "Momentum magnitude fraction to use as width of gaussian smearing");
 
     // Initialize algorithm
-    m_tracking_algo.applyConfig(cfg);
-    m_tracking_algo.init(acts_service->actsGeoProvider(), m_log);
+    m_truth_track_seeding_algo.applyConfig(cfg);
+    m_truth_track_seeding_algo.init(acts_service->actsGeoProvider(), m_log);
 }
 
 void eicrecon::TrackParamTruthInit_factory::ChangeRun(const std::shared_ptr<const JEvent> &event) {
