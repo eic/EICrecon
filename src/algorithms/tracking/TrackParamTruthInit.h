5aa8801daac50c239a90ca018ca9befa522198f4
Wouter Deconinck <wdconinc@gmail.com>
2023-01-13 15:34:59 -0600

    no CKFTrackingConfig in truth seeding ...

--- src/algorithms/tracking/TrackParamTruthInit.h
+++ src/algorithms/tracking/TrackParamTruthInit.h
@@ -17,7 +17,7 @@
 #include <TDatabasePDG.h>
 
 namespace eicrecon {
-    class TrackParamTruthInit: public WithPodConfig<eicrecon::CKFTrackingConfig> {
+    class TrackParamTruthInit: public WithPodConfig<eicrecon::TrackParamTruthInitConfig> {
 
     public:
 
