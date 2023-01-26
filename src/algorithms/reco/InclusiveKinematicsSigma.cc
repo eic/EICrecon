eacec32d58f3488e45f70c6d3892b3c6ffb5dfa8
Tooba Ali <89534076+2oba@users.noreply.github.com>
2023-01-26 17:02:14 -0600

    Update InclusiveKinematicsSigma.cc

--- src/algorithms/reco/InclusiveKinematicsSigma.cc
+++ src/algorithms/reco/InclusiveKinematicsSigma.cc
@@ -1,20 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2022 Wouter Deconinck, Barak Schmookler
 
-#include "Gaudi/Algorithm.h"
-#include "GaudiAlg/GaudiTool.h"
-#include "GaudiAlg/Producer.h"
-#include "GaudiAlg/Transformer.h"
-#include "GaudiKernel/RndmGenerators.h"
-#include "GaudiKernel/PhysicalConstants.h"
 #include <algorithm>
 #include <cmath>
+#include <vector>
 
-#include "JugBase/IParticleSvc.h"
-#include "JugBase/DataHandle.h"
-
-#include "JugBase/Utilities/Beam.h"
-#include "JugBase/Utilities/Boost.h"
+#include "Beam.h"
+#include "Boost.h"
+#include "InclusiveKinematicsSigma.h"
 
 #include "Math/Vector4D.h"
 using ROOT::Math::PxPyPzEVector;
@@ -24,107 +17,68 @@ using ROOT::Math::PxPyPzEVector;
 #include "edm4eic/MCRecoParticleAssociationCollection.h"
 #include "edm4eic/ReconstructedParticleCollection.h"
 #include "edm4eic/InclusiveKinematicsCollection.h"
+#include "ParticlesWithAssociation.h"
 
-namespace Jug::Reco {
+namespace eicrecon {
 
-class InclusiveKinematicsSigma : public GaudiAlgorithm {
-private:
-  DataHandle<edm4hep::MCParticleCollection> m_inputMCParticleCollection{
-    "inputMCParticles",
-    Gaudi::DataHandle::Reader,
-    this};
-  DataHandle<edm4eic::ReconstructedParticleCollection> m_inputParticleCollection{
-    "inputReconstructedParticles",
-    Gaudi::DataHandle::Reader,
-    this};
-  DataHandle<edm4eic::MCRecoParticleAssociationCollection> m_inputParticleAssociation{
-    "inputParticleAssociations",
-    Gaudi::DataHandle::Reader,
-    this};
-  DataHandle<edm4eic::InclusiveKinematicsCollection> m_outputInclusiveKinematicsCollection{
-    "outputInclusiveKinematics",
-    Gaudi::DataHandle::Writer,
-    this};
-
-  Gaudi::Property<double> m_crossingAngle{this, "crossingAngle", -0.025 * Gaudi::Units::radian};
-
-  SmartIF<IParticleSvc> m_pidSvc;
-  double m_proton{0}, m_neutron{0}, m_electron{0};
-
-public:
-  InclusiveKinematicsSigma(const std::string& name, ISvcLocator* svcLoc)
-      : GaudiAlgorithm(name, svcLoc) {
-    declareProperty("inputMCParticles", m_inputMCParticleCollection, "MCParticles");
-    declareProperty("inputReconstructedParticles", m_inputParticleCollection, "ReconstructedParticles");
-    declareProperty("inputParticleAssociations", m_inputParticleAssociation, "MCRecoParticleAssociation");
-    declareProperty("outputInclusiveKinematics", m_outputInclusiveKinematicsCollection, "InclusiveKinematicsSigma");
+  void InclusiveKinematicsSigma::init(std::shared_ptr<spdlog::logger> logger) {
+    m_log = logger;
+    // m_pidSvc = service("ParticleSvc");
+    // if (!m_pidSvc) {
+    //   if (m_log->level() <= spdlog::level::debug) {
+    //     m_log->debug("Unable to locate Particle Service. "
+    //       "Make sure you have ParticleSvc in the configuration.");
+    //   }
+    // }
   }
 
-  StatusCode initialize() override {
-    if (GaudiAlgorithm::initialize().isFailure())
-      return StatusCode::FAILURE;
+  std::vector<edm4eic::InclusiveKinematics*> InclusiveKinematicsSigma::execute(
+    std::vector<const edm4hep::MCParticle *> mcparts,
+    std::vector<const edm4eic::ReconstructedParticle *> rcparts,
+    std::vector<const edm4eic::MCRecoParticleAssociation *> rcassoc) {
 
-    m_pidSvc = service("ParticleSvc");
-    if (!m_pidSvc) {
-      error() << "Unable to locate Particle Service. "
-              << "Make sure you have ParticleSvc in the configuration."
-              << endmsg;
-      return StatusCode::FAILURE;
-    }
-    m_proton = m_pidSvc->particle(2212).mass;
-    m_neutron = m_pidSvc->particle(2112).mass;
-    m_electron = m_pidSvc->particle(11).mass;
-
-    return StatusCode::SUCCESS;
-  }
-
-  StatusCode execute() override {
-    // input collections
-    const auto& mcparts = *(m_inputMCParticleCollection.get());
-    const auto& rcparts = *(m_inputParticleCollection.get());
-    const auto& rcassoc = *(m_inputParticleAssociation.get());
-    // output collection
-    auto& out_kinematics = *(m_outputInclusiveKinematicsCollection.createAndPut());
+    // Resulting inclusive kinematics
+    std::vector<edm4eic::InclusiveKinematics *> kinematics;
 
     // Get incoming electron beam
-    const auto ei_coll = Jug::Base::Beam::find_first_beam_electron(mcparts);
+    const auto ei_coll = find_first_beam_electron(mcparts);
     if (ei_coll.size() == 0) {
-      if (msgLevel(MSG::DEBUG)) {
-        debug() << "No beam electron found" << endmsg;
+      if (m_log->level() <= spdlog::level::debug) {
+        m_log->debug("No beam electron found");
       }
-      return StatusCode::SUCCESS;
+      return kinematics;
     }
     const PxPyPzEVector ei(
-      Jug::Base::Beam::round_beam_four_momentum(
-        ei_coll[0].getMomentum(),
+      round_beam_four_momentum(
+        ei_coll[0]->getMomentum(),
         m_electron,
         {-5.0, -10.0, -18.0},
         0.0)
       );
 
     // Get incoming hadron beam
-    const auto pi_coll = Jug::Base::Beam::find_first_beam_hadron(mcparts);
+    const auto pi_coll = find_first_beam_hadron(mcparts);
     if (pi_coll.size() == 0) {
-      if (msgLevel(MSG::DEBUG)) {
-        debug() << "No beam hadron found" << endmsg;
+      if (m_log->level() <= spdlog::level::debug) {
+        m_log->debug("No beam hadron found");
       }
-      return StatusCode::SUCCESS;
+      return kinematics;
     }
     const PxPyPzEVector pi(
-      Jug::Base::Beam::round_beam_four_momentum(
-        pi_coll[0].getMomentum(),
-        pi_coll[0].getPDG() == 2212 ? m_proton : m_neutron,
+      round_beam_four_momentum(
+        pi_coll[0]->getMomentum(),
+        pi_coll[0]->getPDG() == 2212 ? m_proton : m_neutron,
         {41.0, 100.0, 275.0},
         m_crossingAngle)
       );
 
     // Get first scattered electron
-    const auto ef_coll = Jug::Base::Beam::find_first_scattered_electron(mcparts);
+    const auto ef_coll = find_first_scattered_electron(mcparts);
     if (ef_coll.size() == 0) {
-      if (msgLevel(MSG::DEBUG)) {
-        debug() << "No truth scattered electron found" << endmsg;
+      if (m_log->level() <= spdlog::level::debug) {
+        m_log->debug("No truth scattered electron found");
       }
-      return StatusCode::SUCCESS;
+      return kinematics;
     }
     // Associate first scattered electron with reconstructed electrons
     //const auto ef_assoc = std::find_if(
@@ -133,17 +87,17 @@ public:
     //  [&ef_coll](const auto& a){ return a.getSimID() == ef_coll[0].getObjectID().index; });
     auto ef_assoc = rcassoc.begin();
     for (; ef_assoc != rcassoc.end(); ++ef_assoc) {
-      if (ef_assoc->getSimID() == (unsigned) ef_coll[0].getObjectID().index) {
+      if ((*ef_assoc)->getSimID() == (unsigned) ef_coll[0]->getObjectID().index) {
         break;
       }
     }
     if (!(ef_assoc != rcassoc.end())) {
-      if (msgLevel(MSG::DEBUG)) {
-        debug() << "Truth scattered electron not in reconstructed particles" << endmsg;
+      if (m_log->level() <= spdlog::level::debug) {
+        m_log->debug("Truth scattered electron not in reconstructed particles");
       }
-      return StatusCode::SUCCESS;
+      return kinematics;
     }
-    const auto ef_rc{ef_assoc->getRec()};
+    const auto ef_rc{(*ef_assoc)->getRec()};
     const auto ef_rc_id{ef_rc.getObjectID().index};
 
     // Loop over reconstructed particles to get all outgoing particles
@@ -159,7 +113,7 @@ public:
     // magnitude of the momentum.
     // -----------------------------------------------------------------
 
-    // Sums in colinear frame
+    //Sums in colinear frame
     double pxsum = 0;
     double pysum = 0;
     double pzsum = 0;
@@ -169,25 +123,26 @@ public:
     double sigma_e = 0;
 
     // Get boost to colinear frame
-    auto boost = Jug::Base::Boost::determine_boost(ei, pi);
+    auto boost = determine_boost(ei, pi);
 
-    for(const auto& p: rcparts) {
+    for (const auto& p: rcparts) {
       // Get the scattered electron index and angle
-      if (p.getObjectID().index == ef_rc_id) {
+      if (p->getObjectID().index == ef_rc_id) {
         // Lorentz vector in lab frame
-        PxPyPzEVector e_lab(p.getMomentum().x, p.getMomentum().y, p.getMomentum().z, p.getEnergy());
+        PxPyPzEVector e_lab(p->getMomentum().x, p->getMomentum().y, p->getMomentum().z, p->getEnergy());
         // Boost to colinear frame
-        PxPyPzEVector e_boosted = Jug::Base::Boost::apply_boost(boost, e_lab);
+        PxPyPzEVector e_boosted = apply_boost(boost, e_lab);
 
         pt_e = e_boosted.Pt();
         sigma_e = e_boosted.E() - e_boosted.Pz();
 
       // Sum over all particles other than scattered electron
-      } else{
+      } else {
         // Lorentz vector in lab frame
-        PxPyPzEVector hf_lab(p.getMomentum().x, p.getMomentum().y, p.getMomentum().z, p.getEnergy());
+        PxPyPzEVector hf_lab(p->getMomentum().x, p->getMomentum().y, p->getMomentum().z, p->getEnergy());
         // Boost to colinear frame
-        PxPyPzEVector hf_boosted = Jug::Base::Boost::apply_boost(boost, hf_lab);
+        PxPyPzEVector hf_boosted = apply_boost(boost, hf_lab);
+
         pxsum += hf_boosted.Px();
         pysum += hf_boosted.Py();
         pzsum += hf_boosted.Pz();
@@ -200,10 +155,10 @@ public:
     auto sigma_tot = sigma_e + sigma_h;
 
     if (sigma_h <= 0) {
-      if (msgLevel(MSG::DEBUG)) {
-        debug() << "No scattered electron found or sigma zero or negative" << endmsg;
+      if (m_log->level() <= spdlog::level::debug) {
+        m_log->debug("No scattered electron found or sigma zero or negative");
       }
-      return StatusCode::SUCCESS;
+      return kinematics;
     }
 
     // Calculate kinematic variables
@@ -212,27 +167,21 @@ public:
     const auto x_sig = Q2_sig / (4.*ei.energy()*pi.energy()*y_sig);
     const auto nu_sig = Q2_sig / (2.*m_proton*x_sig);
     const auto W_sig = sqrt(m_proton*m_proton + 2*m_proton*nu_sig - Q2_sig);
-    auto kin = out_kinematics.create(x_sig, Q2_sig, W_sig, y_sig, nu_sig);
+    edm4eic::MutableInclusiveKinematics kin(x_sig, Q2_sig, W_sig, y_sig, nu_sig);
     kin.setScat(ef_rc);
 
     // Debugging output
-    if (msgLevel(MSG::DEBUG)) {
-      debug() << "pi = " << pi << endmsg;
-      debug() << "ei = " << ei << endmsg;
-      debug() << "x,Q2,W,y,nu = "
-              << kin.getX() << ","
-              << kin.getQ2() << ","
-              << kin.getW() << ","
-              << kin.getY() << ","
-              << kin.getNu()
-              << endmsg;
+    if (m_log->level() <= spdlog::level::debug) {
+      //m_log->debug("pi = ", pi);
+      //m_log->debug("ei = ", ei);
+      m_log->debug("x,Q2,W,y,nu = {},{},{},{},{}", kin.getX(),
+              kin.getQ2(), kin.getW(), kin.getY(), kin.getNu());
     }
 
-    return StatusCode::SUCCESS;
-  }
-};
+    kinematics.push_back(new edm4eic::InclusiveKinematics(kin));
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-DECLARE_COMPONENT(InclusiveKinematicsSigma)
+    return kinematics;  
+    }
+
 
 } // namespace Jug::Reco
