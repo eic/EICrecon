88c4d77d1e1adca559d8897fa2e84a9ffc639e8d
Sakib Rahman <rahmans@myumanitoba.ca>
2023-03-23 09:57:35 -0500

    Skeleton code for geometry navigation test header

---
+++ src/tests/geometry_navigation_test/GeometryNavigationSteps_processor.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <JANA/JEventProcessor.h>
+#include <JANA/JEventProcessorSequentialRoot.h>
+
+#include <services/log/Log_service.h>
+#include <extensions/spdlog/SpdlogMixin.h>
+
+#include <TDirectory.h>
+
+class JEvent;
+class JApplication;
+
+class GeometryNavigationTest_processor:
+        public JEventProcessor,
+        public eicrecon::SpdlogMixin<GeometryNavigationTest_processor>   // this automates proper log initialization
+{
+public:
+    explicit GeometryNavigationTest_processor(JApplication *);
+    ~GeometryNavigationTest_processor() override = default;
+
+    //----------------------------
+    // Init
+    //
+    // This is called once before the first call to the Process method
+    // below. You may, for example, want to open an output file here.
+    // Only one thread will call this.
+    void Init() override;
+
+    //----------------------------
+    // Process
+    //
+    // This is called for every event. Multiple threads may call this
+    // simultaneously. If you write something to an output file here
+    // then make sure to protect it with a mutex or similar mechanism.
+    // Minimize what is done while locked since that directly affects
+    // the multi-threaded performance.
+    void Process(const std::shared_ptr<const JEvent>& event) override;
+
+    //----------------------------
+    // Finish
+    //
+    // This is called once after all events have been processed. You may,
+    // for example, want to close an output file here.
+    // Only one thread will call this.
+    void Finish() override;
+
+private:
+
+    /// Directory to store histograms to
+    TDirectory *m_dir_main{};
+    Acts::GeometryContext m_geoContext;
+    Acts::MagneticFieldContext m_fieldContext;
+    std::shared_ptr<const ActsGeometryProvider> m_geoSvc;
+    std::shared_ptr<spdlog::logger> m_log;
+        
+};
