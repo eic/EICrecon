7941466b6e3a8348df0b4823cc30101a3ba9e361
Derek M Anderson <derek.murphy.anderson@protonmail.com>
2023-04-03 19:59:44 -0400

    Add jet reconstruction (#488)
    This PR introduces basic jet-reconstruction capabilities to EICrecon.
    
    Co-authored-by: Zhongling Ji <jizhongling@gmail.com>

---
+++ src/global/reco/ReconstructedJets_factory.h
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Zhongling Ji, Derek Anderson
+
+#pragma once
+
+#include <extensions/jana/JChainFactoryT.h>
+#include <extensions/spdlog/SpdlogMixin.h>
+#include <spdlog/logger.h>
+#include <algorithms/reco/JetReconstruction.h>
+
+
+namespace eicrecon {
+
+    class ReconstructedJets_factory :
+            public JChainFactoryT<edm4eic::ReconstructedParticle>,
+            public SpdlogMixin<ReconstructedJets_factory> {
+
+    public:
+        explicit ReconstructedJets_factory(std::vector<std::string> default_input_tags):
+            JChainFactoryT<edm4eic::ReconstructedParticle>(std::move(default_input_tags)) {
+        }
+
+        /** One time initialization **/
+        void Init() override;
+
+        /** On run change preparations **/
+        void ChangeRun(const std::shared_ptr<const JEvent> &event) override;
+
+        /** Event by event processing **/
+        void Process(const std::shared_ptr<const JEvent> &event) override;
+
+    protected:
+        JetReconstruction m_jet_algo;
+
+    };
+
+} // eicrecon
