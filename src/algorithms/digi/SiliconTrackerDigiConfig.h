2cb26a58f0c82ac52bc99fd5659417c63dcc98e7
blianggilman <39803526+blianggilman@users.noreply.github.com>
2023-04-04 12:18:21 -0700

    Update src/algorithms/digi/SiliconTrackerDigiConfig.h
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/digi/SiliconTrackerDigiConfig.h
+++ src/algorithms/digi/SiliconTrackerDigiConfig.h
@@ -3,7 +3,7 @@
 namespace eicrecon {
 
     struct SiliconTrackerDigiConfig {
-        double threshold  = 5; //keV
+        double threshold  = 5 * dd4hep::keV;
         double timeResolution = 8;   /// TODO 8 of what units??? Same TODO in juggler. Probably [ns]
     };
 
