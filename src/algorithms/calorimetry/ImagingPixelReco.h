a95f99c987619f6007da76d228db015ff1c86b09
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-04-21 18:21:55 -0400

    ImagingPixelReco: throw on errors in initialize()

--- src/algorithms/calorimetry/ImagingPixelReco.h
+++ src/algorithms/calorimetry/ImagingPixelReco.h
@@ -64,12 +64,12 @@ public:
     void initialize() {
         if (!m_geoSvc) {
             m_log->error("Unable to locate Geometry Service. \nMake sure you have GeoSvc and SimSvc in the right order in the configuration.");
-            return;
+            throw std::runtime_error("Unable to locate Geometry Service. \nMake sure you have GeoSvc and SimSvc in the right order in the configuration.");
         }
 
         if (m_readout.empty()) {
             m_log->error("readoutClass is not provided, it is needed to know the fields in readout ids");
-            return;
+            throw std::runtime_error("readoutClass is not provided, it is needed to know the fields in readout ids");
         }
 
         try {
@@ -78,7 +78,7 @@ public:
             layer_idx = id_dec->index(m_layerField);
         } catch (...) {
             m_log->warn(fmt::format("Failed to load ID decoder for {}", m_readout));
-            return;
+            throw;
         }
     }
 
