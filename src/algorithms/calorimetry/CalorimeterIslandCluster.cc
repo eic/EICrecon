0a05a13de09073cb64da1122a44b30b62da07c17
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-05-02 15:05:41 -0400

    CalorimeterIslandCluster: don't validate/apply u_transverseEnergyProfileMetric unless m_splitCluster is enabled

--- src/algorithms/calorimetry/CalorimeterIslandCluster.cc
+++ src/algorithms/calorimetry/CalorimeterIslandCluster.cc
@@ -159,14 +159,16 @@ void CalorimeterIslandCluster::AlgorithmInit(std::shared_ptr<spdlog::logger>& lo
     }
 
     if (not method_found) {
-        throw std::runtime_error("Cannot determine the clustering coordinates");
+      throw std::runtime_error("Cannot determine the clustering coordinates");
     }
 
-    auto transverseEnergyProfileMetric_it = std::find_if(distMethods.begin(), distMethods.end(), [&](auto &p) { return u_transverseEnergyProfileMetric == p.first; });
-    if (transverseEnergyProfileMetric_it == distMethods.end()) {
-        throw std::runtime_error(fmt::format("Unsupported value \"{}\" for \"transverseEnergyProfileMetric\"", u_transverseEnergyProfileMetric));
+    if (m_splitCluster) {
+      auto transverseEnergyProfileMetric_it = std::find_if(distMethods.begin(), distMethods.end(), [&](auto &p) { return u_transverseEnergyProfileMetric == p.first; });
+      if (transverseEnergyProfileMetric_it == distMethods.end()) {
+          throw std::runtime_error(fmt::format("Unsupported value \"{}\" for \"transverseEnergyProfileMetric\"", u_transverseEnergyProfileMetric));
+      }
+      transverseEnergyProfileMetric = std::get<0>(transverseEnergyProfileMetric_it->second);
     }
-    transverseEnergyProfileMetric = std::get<0>(transverseEnergyProfileMetric_it->second);
 
     return;
 }
