62a14e424dc5eb385ed3d2cc537a7a4a9f6a2be8
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-05-09 23:50:23 -0400

    set EEMC:EcalEndcapNIslandProtoClusters:transverseEnergyProfileScale
    The value obtained by averaging outputs of
    https://github.com/eic/EICrecon/pull/561

--- src/detectors/EEMC/ProtoCluster_factory_EcalEndcapNIslandProtoClusters.h
+++ src/detectors/EEMC/ProtoCluster_factory_EcalEndcapNIslandProtoClusters.h
@@ -49,7 +49,7 @@ public:
         m_minClusterHitEdep=1.0 * dd4hep::MeV;
         m_minClusterCenterEdep=30.0 * dd4hep::MeV;
         u_transverseEnergyProfileMetric = "globalDistEtaPhi";
-        u_transverseEnergyProfileScale = 1.;
+        u_transverseEnergyProfileScale = 0.14;
 
         app->SetDefaultParameter("EEMC:EcalEndcapNIslandProtoClusters:input_tag",        m_input_tag, "Name of input collection to use");
         app->SetDefaultParameter("EEMC:EcalEndcapNIslandProtoClusters:geoServiceName", m_geoSvcName);
