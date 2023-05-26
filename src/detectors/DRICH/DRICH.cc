2bb4ce2bd4f80d5dd1f85e523c5bb61287b295d8
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-05-26 12:35:21 -0400

    fix: add Luigi to copyright headers

--- src/detectors/DRICH/DRICH.cc
+++ src/detectors/DRICH/DRICH.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2022, 2023, Christopher Dilks
+// Copyright (C) 2022, 2023, Christopher Dilks, Luigi Dello Stritto
 // Subject to the terms in the LICENSE file found in the top-level directory.
 
 #include <JANA/JApplication.h>
