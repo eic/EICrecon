22c046a45496fcb91d303c497458eb99dd0b7965
Wouter Deconinck <wdconinc@gmail.com>
2023-06-24 12:43:43 -0500

    fix: Cluster factory factory so cluster matching has associations  (#666)
    ### Briefly, what does this PR introduce?
    In 23.05.1 we realized that cluster matching (though now implementated)
    doesn't actually work yet due to the absence of cluster associations
    being produced in clustering. One could fix this in 17 factories (#636)
    but I really didn't want to do that so here's an approach that reduces
    duplicate code (but otherwise is similar enough, with the exception of
    adding configuration structures).
    
    Todo:
    - [x] depends on #664 
    
    Closes: #636
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: no cluster associations)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [x] Changes have been communicated to collaborators @veprbl
    @nathanwbrei
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    Now associations will be written too.
    
    ---------
    
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/detectors/BEMC/BEMC.cc
+++ src/detectors/BEMC/BEMC.cc
@@ -3,20 +3,20 @@
 //
 //
 
-#include <JANA/JApplication.h>
-#include <JANA/JFactoryGenerator.h>
+#include <extensions/jana/JChainMultifactoryGeneratorT.h>
 
+#include <factories/calorimetry/CalorimeterClusterRecoCoG_factoryT.h>
 
 #include "RawCalorimeterHit_factory_EcalBarrelSciGlassRawHits.h"
 #include "CalorimeterHit_factory_EcalBarrelSciGlassRecHits.h"
+#include "ProtoCluster_factory_EcalBarrelSciGlassTruthProtoClusters.h"
 #include "ProtoCluster_factory_EcalBarrelSciGlassProtoClusters.h"
-#include "Cluster_factory_EcalBarrelSciGlassClusters.h"
+#include "Cluster_factory_EcalBarrelSciGlassMergedTruthClusters.h"
 
 #include "RawCalorimeterHit_factory_EcalBarrelScFiRawHits.h"
 #include "CalorimeterHit_factory_EcalBarrelScFiRecHits.h"
 #include "CalorimeterHit_factory_EcalBarrelScFiMergedHits.h"
 #include "ProtoCluster_factory_EcalBarrelScFiProtoClusters.h"
-#include "Cluster_factory_EcalBarrelScFiClusters.h"
 
 #include "RawCalorimeterHit_factory_EcalBarrelImagingRawHits.h"
 #include "CalorimeterHit_factory_EcalBarrelImagingRecHits.h"
@@ -24,27 +24,87 @@
 #include "Cluster_factory_EcalBarrelImagingClusters.h"
 #include "Cluster_factory_EcalBarrelImagingMergedClusters.h"
 
-#include "ProtoCluster_factory_EcalBarrelSciGlassTruthProtoClusters.h"
-#include "Cluster_factory_EcalBarrelSciGlassTruthClusters.h"
-#include "Cluster_factory_EcalBarrelSciGlassMergedTruthClusters.h"
+
+namespace eicrecon {
+    class Cluster_factory_EcalBarrelSciGlassTruthClusters: public CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelSciGlassTruthClusters> {
+    public:
+        template <typename... Args>
+        Cluster_factory_EcalBarrelSciGlassTruthClusters(Args&&... args)
+        : CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelSciGlassTruthClusters>(std::forward<Args>(args)...) { }
+    };
+
+    class Cluster_factory_EcalBarrelSciGlassClusters: public CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelSciGlassClusters> {
+    public:
+        template <typename... Args>
+        Cluster_factory_EcalBarrelSciGlassClusters(Args&&... args)
+        : CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelSciGlassClusters>(std::forward<Args>(args)...) { }
+    };
+
+    class Cluster_factory_EcalBarrelScFiClusters: public CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelScFiClusters> {
+    public:
+        template <typename... Args>
+        Cluster_factory_EcalBarrelScFiClusters(Args&&... args)
+        : CalorimeterClusterRecoCoG_factoryT<Cluster_factory_EcalBarrelScFiClusters>(std::forward<Args>(args)...) { }
+    };
+
+}
 
 extern "C" {
     void InitPlugin(JApplication *app) {
+
+        using namespace eicrecon;
+
         InitJANAPlugin(app);
+
         app->Add(new JFactoryGeneratorT<RawCalorimeterHit_factory_EcalBarrelSciGlassRawHits>());
         app->Add(new JFactoryGeneratorT<CalorimeterHit_factory_EcalBarrelSciGlassRecHits>());
         app->Add(new JFactoryGeneratorT<ProtoCluster_factory_EcalBarrelSciGlassProtoClusters>());
-        app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelSciGlassClusters>());
+        app->Add(
+          new JChainMultifactoryGeneratorT<Cluster_factory_EcalBarrelSciGlassClusters>(
+             "EcalBarrelSciGlassClusters",
+            {"EcalBarrelSciGlassProtoClusters",        // edm4eic::ProtoClusterCollection
+             "EcalBarrelSciGlassHits"},                // edm4hep::SimCalorimeterHitCollection
+            {"EcalBarrelSciGlassClusters",             // edm4eic::Cluster
+             "EcalBarrelSciGlassClusterAssociations"}, // edm4eic::MCRecoClusterParticleAssociation
+            {
+              .energyWeight = "log",
+              .moduleDimZName = "",
+              .sampFrac = 0.92,
+              .logWeightBase = 6.2,
+              .depthCorrection = 0.0,
+              .enableEtaBounds = true
+            },
+            app   // TODO: Remove me once fixed
+          )
+        );
+
 
         app->Add(new JFactoryGeneratorT<RawCalorimeterHit_factory_EcalBarrelScFiRawHits>());
         app->Add(new JFactoryGeneratorT<CalorimeterHit_factory_EcalBarrelScFiRecHits>());
-        // app->Add(new JFactoryGeneratorT<CalorimeterHit_factory_EcalBarrelScFiMergedHits>());
         app->Add(new JFactoryGeneratorT<ProtoCluster_factory_EcalBarrelScFiProtoClusters>());
-        app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelScFiClusters>());
+        app->Add(
+          new JChainMultifactoryGeneratorT<Cluster_factory_EcalBarrelScFiClusters>(
+             "EcalBarrelScFiClusters",
+            {"EcalBarrelScFiProtoClusters",        // edm4eic::ProtoClusterCollection
+             "EcalBarrelScFiHits"},                // edm4hep::SimCalorimeterHitCollection
+            {"EcalBarrelScFiClusters",             // edm4eic::Cluster
+             "EcalBarrelScFiClusterAssociations"}, // edm4eic::MCRecoClusterParticleAssociation
+             {
+               .energyWeight = "log",
+               .moduleDimZName = "",
+               .sampFrac = 1.0,
+               .logWeightBase = 6.2,
+               .depthCorrection = 0.0,
+               .enableEtaBounds = false
+             },
+            app   // TODO: Remove me once fixed
+          )
+        );
 
         app->Add(new JFactoryGeneratorT<RawCalorimeterHit_factory_EcalBarrelImagingRawHits>());
         app->Add(new JFactoryGeneratorT<CalorimeterHit_factory_EcalBarrelImagingRecHits>());
         app->Add(new JFactoryGeneratorT<ProtoCluster_factory_EcalBarrelImagingProtoClusters>());
+
         app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelImagingClusters>());
         app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelImagingMergedClusters>());
 
@@ -53,7 +113,24 @@ extern "C" {
         app->Add(new JFactoryGeneratorT<JFactoryT<edm4eic::MCRecoClusterParticleAssociation>>("EcalBarrelImagingClusterAssociations"));
 
         app->Add(new JFactoryGeneratorT<ProtoCluster_factory_EcalBarrelSciGlassTruthProtoClusters>());
-        app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelSciGlassTruthClusters>());
+        app->Add(
+          new JChainMultifactoryGeneratorT<Cluster_factory_EcalBarrelSciGlassTruthClusters>(
+             "EcalBarrelSciGlassTruthClusters",
+            {"EcalBarrelSciGlassTruthProtoClusters",        // edm4eic::ProtoClusterCollection
+             "EcalBarrelSciGlassHits"},                     // edm4hep::SimCalorimeterHitCollection
+            {"EcalBarrelSciGlassTruthClusters",             // edm4eic::Cluster
+             "EcalBarrelSciGlassTruthClusterAssociations"}, // edm4eic::MCRecoClusterParticleAssociation
+             {
+               .energyWeight = "log",
+               .moduleDimZName = "",
+               .sampFrac = 1.0,
+               .logWeightBase = 6.2,
+               .depthCorrection = 0.0,
+               .enableEtaBounds = true
+             },
+            app   // TODO: Remove me once fixed
+          )
+        );
         app->Add(new JFactoryGeneratorT<Cluster_factory_EcalBarrelSciGlassMergedTruthClusters>());
     }
 }
