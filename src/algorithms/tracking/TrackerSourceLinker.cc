04f62c462b9e865f9511e6f990550d59753ea916
Sakib Rahman <rahmans@myumanitoba.ca>
2023-06-29 06:07:46 -0500

    Add ACTS units to on Surface Tolerance

--- src/algorithms/tracking/TrackerSourceLinker.cc
+++ src/algorithms/tracking/TrackerSourceLinker.cc
@@ -81,9 +81,9 @@ eicrecon::TrackerSourceLinkerResult *eicrecon::TrackerSourceLinker::produce(std:
         Acts::Vector2 loc = Acts::Vector2::Zero();
         Acts::Vector2 pos;
         auto hit_det = hit->getCellID()&0xFF;
-        auto onSurfaceTolerance = 0.0001;      // By default, ACTS uses 0.1 micron as the on surface tolerance
+        auto onSurfaceTolerance = 0.1*Acts::UnitConstants::um;      // By default, ACTS uses 0.1 micron as the on surface tolerance
         if (hit_det==150){
-         onSurfaceTolerance = 0.001;           // FIXME Ugly hack for testing B0. Should be a way to increase this tolerance in geometry.
+         onSurfaceTolerance = 1*Acts::UnitConstants::um;           // FIXME Ugly hack for testing B0. Should be a way to increase this tolerance in geometry.
         }
 
         try {
