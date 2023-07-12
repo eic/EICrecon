b2391eedf4b19b6285b388fe998e827511823ae6
Shyam Kumar <shyam055119@gmail.com>
2023-07-12 15:18:58 +0200

    Modified Scripts with new detector geometry

--- src/benchmarks/reconstruction/tracking_efficiency/scripts/Plot_eta.C
+++ src/benchmarks/reconstruction/tracking_efficiency/scripts/Plot_eta.C
@@ -36,7 +36,10 @@ void Plot_eta()
    TH1D *h3 = new TH1D("h3","h3",200,-4.,4.);
    TH1D *h4 = new TH1D("h4","h4",200,-4.,4.);
    TH1D *h5 = new TH1D("h5","h5",200,-4.,4.);
-
+   TH1D *h6 = new TH1D("h6","h6",200,-4.,4.);
+   TH1D *h7 = new TH1D("h7","h7",200,-4.,4.);
+   TH1D *h8 = new TH1D("h8","h8",200,-4.,4.);
+   TH1D *h9 = new TH1D("h9","h9",200,-4.,4.);
 
    h0->GetXaxis()->SetTitle("#eta");
    h0->GetYaxis()->SetTitle("Entries (a.u.)");
@@ -49,16 +52,22 @@ void Plot_eta()
    TString ToF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFBarrelHits.position.x*TOFBarrelHits.position.x+TOFBarrelHits.position.y*TOFBarrelHits.position.y),TOFBarrelHits.position.z))/2))>>h3";
    TString ETracker_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TrackerEndcapHits.position.x*TrackerEndcapHits.position.x+TrackerEndcapHits.position.y*TrackerEndcapHits.position.y),TrackerEndcapHits.position.z))/2))>>h4";
    TString ETOF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFEndcapHits.position.x*TOFEndcapHits.position.x+TOFEndcapHits.position.y*TOFEndcapHits.position.y),TOFEndcapHits.position.z))/2))>>h5";
-
+   TString OutBMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(OuterMPGDBarrelHits.position.x*OuterMPGDBarrelHits.position.x+OuterMPGDBarrelHits.position.y*OuterMPGDBarrelHits.position.y),OuterMPGDBarrelHits.position.z))/2))>>h6";
+   TString FwdMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(ForwardMPGDEndcapHits.position.x*ForwardMPGDEndcapHits.position.x+ForwardMPGDEndcapHits.position.y*ForwardMPGDEndcapHits.position.y),ForwardMPGDEndcapHits.position.z))/2))>>h7";
+   TString BwdMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(BackwardMPGDEndcapHits.position.x*BackwardMPGDEndcapHits.position.x+BackwardMPGDEndcapHits.position.y*BackwardMPGDEndcapHits.position.y),BackwardMPGDEndcapHits.position.z))/2))>>h8";
+   TString B0_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(B0TrackerHits.position.x*B0TrackerHits.position.x+B0TrackerHits.position.y*B0TrackerHits.position.y),B0TrackerHits.position.z))/2))>>h9";
 
    tree->Draw(vtx_hits.Data(),"VertexBarrelHits.position.y>0","goff"); // Vtx layers
-   tree->Draw(Barrel_hits.Data(),"SiBarrelHits.position.y>0 ","goff"); // Barrel layers
-   tree->Draw(BMM_hits.Data(),"MPGDBarrelHits.position.y>0 ","goff"); // Barrel layers
-   tree->Draw(ToF_hits.Data(),"TOFBarrelHits.position.y>0","goff"); // BMT layers1
-   tree->Draw(ETracker_hits.Data(),"TrackerEndcapHits.position.y>0","goff"); // BMT layers1
-   tree->Draw(ETOF_hits.Data(),"TOFEndcapHits.position.y>0","goff"); // DIRC layers
-
-
+   tree->Draw(Barrel_hits.Data(),"SiBarrelHits.position.y>0 ","goff"); // Barrel Si layers
+   tree->Draw(BMM_hits.Data(),"MPGDBarrelHits.position.y>0 ","goff"); // MPGD Barrel
+   tree->Draw(ToF_hits.Data(),"TOFBarrelHits.position.y>0","goff"); // TOF Hits
+   tree->Draw(ETracker_hits.Data(),"TrackerEndcapHits.position.y>0","goff"); // Tracker Endcap
+   tree->Draw(ETOF_hits.Data(),"TOFEndcapHits.position.y>0","goff"); // TOF Endcap
+   tree->Draw(OutBMM_hits.Data(),"OuterMPGDBarrelHits.position.y>0","goff"); // Outer Barrel MPGD
+   tree->Draw(FwdMM_hits.Data(),"ForwardMPGDEndcapHits.position.y>0","goff"); // Forward MPGD
+   tree->Draw(BwdMM_hits.Data(),"BackwardMPGDEndcapHits.position.y>0","goff"); // Forward MPGD
+   tree->Draw(B0_hits.Data(),"B0TrackerHits.position.y>0","goff"); // B0 Tracker
+  
    c1->cd();
    c1->SetLogy();
    h0->Scale(1./h0->Integral());
@@ -67,15 +76,23 @@ void Plot_eta()
    h3->Scale(1./h3->Integral());
    h4->Scale(1./h4->Integral());
    h5->Scale(1./h5->Integral());
+   h6->Scale(1./h6->Integral());
+   h7->Scale(1./h7->Integral());
+   h8->Scale(1./h8->Integral());
+   h9->Scale(1./h9->Integral());
    h0->SetLineColor(kBlue);
    h1->SetLineColor(kMagenta);
    h2->SetLineColor(kRed);
    h3->SetLineColor(kBlack);
    h4->SetLineColor(kGreen);
    h5->SetLineColor(kCyan);
+   h6->SetLineColor(9);
+   h7->SetLineColor(28);
+   h8->SetLineColor(46);
+   h9->SetLineColor(38);
 
 
-   h0->SetMaximum(0.1);
+   h0->SetMaximum(0.2);
    h0->GetXaxis()->SetRangeUser(-4.0,4.0);
    h0->Draw("hist");
    h1->Draw("hist-same");
@@ -83,8 +100,12 @@ void Plot_eta()
    h3->Draw("hist-same");
    h4->Draw("hist-same");
    h5->Draw("hist-same");
+   h6->Draw("hist-same");
+   h7->Draw("hist-same");
+   h8->Draw("hist-same");
+   h9->Draw("hist-same");
 
-  TLegend *l1= new TLegend(0.70,0.75,0.90,0.93);
+  TLegend *l1= new TLegend(0.15,0.75,0.35,0.93);
   l1->SetTextSize(0.03);
   l1->SetBorderSize(0);
   l1->AddEntry(h0,"VertexBarrelHits");
@@ -92,7 +113,16 @@ void Plot_eta()
   l1->AddEntry(h2,"MPGDBarrelHits");
   l1->AddEntry(h3,"TOFBarrelHits");
   l1->AddEntry(h4,"TrackerEndcapHits");
+  l1->Draw();
+  l1= new TLegend(0.40,0.75,0.70,0.93);  
+  l1->SetTextSize(0.03);
+  l1->SetBorderSize(0);
   l1->AddEntry(h5,"TOFEndcapHits");
+  l1->AddEntry(h6,"OuterMPGDBarrelHits");
+  l1->AddEntry(h7,"ForwardMPGDEndcapHits");
+  l1->AddEntry(h8,"BackwardMPGDEndcapHits");
+  l1->AddEntry(h9,"B0TrackerHits");
+  l1->Draw("same");
   c1->cd();
   l1->Draw();
   c1->SaveAs("eta_DD4HEP.png");
--- src/benchmarks/reconstruction/tracking_efficiency/scripts/draw_hits.C
+++ src/benchmarks/reconstruction/tracking_efficiency/scripts/draw_hits.C
@@ -20,11 +20,11 @@ void draw_hits()
    gStyle->SetHistLineWidth(2);
    gStyle->SetOptFit(1);
    gStyle->SetOptStat(0);
-
+     
 //=======Reading the root file DD4HEP===========
  TFile *f = TFile::Open("sim.edm4hep.root");
- TTree *sim = (TTree*)f->Get("events");
-
+ TTree *sim = (TTree*)f->Get("events"); 
+  
  TCanvas * c1 = new TCanvas("c1","coutput",1200,1000);
  c1->SetMargin(0.09, 0.03 ,0.1,0.06);
 
@@ -32,7 +32,7 @@ void draw_hits()
  c2->SetMargin(0.09, 0.03 ,0.1,0.06);
  // X-Y Hits
  Int_t nbins = 320;
- Double_t x= 100., y = 100.;
+ Double_t x= 100., y = 100.; 
  TH2D *h1 = new TH2D("h1","h1",nbins,-x,x,nbins,-y,y);
  TH2D *h2 = new TH2D("h2","h2",nbins,-x,x,nbins,-y,y);
  TH2D *h3 = new TH2D("h3","h3",nbins,-x,x,nbins,-y,y);
@@ -41,7 +41,7 @@ void draw_hits()
 
  // Y-Z Hits
  Int_t nbinsx = 400, nbinsy=180.;
- x= 200.; y = 90.;
+ x= 200.; y = 90.; 
  double xmin = -200.;
  TH2D *h1_1 = new TH2D("h1_1","h1_1",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h2_1 = new TH2D("h2_1","h2_1",nbinsx,xmin,x,nbinsy,-y,y);
@@ -49,14 +49,21 @@ void draw_hits()
  TH2D *h4_1 = new TH2D("h4_1","h4_1",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h5_1 = new TH2D("h5_1","h5_1",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h6_1 = new TH2D("h6_1","h6_1",nbinsx,xmin,x,nbinsy,-y,y);
-
+ TH2D *h7_1 = new TH2D("h7_1","h7_1",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h8_1 = new TH2D("h8_1","h8_1",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h9_1 = new TH2D("h9_1","h9_1",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h10_1 = new TH2D("h10_1","h10_1",nbinsx,xmin,x,nbinsy,-y,y);
+ 
  TH2D *h1_2 = new TH2D("h1_2","h1_2",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h2_2 = new TH2D("h2_2","h2_2",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h3_2 = new TH2D("h3_2","h3_2",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h4_2 = new TH2D("h4_2","h4_2",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h5_2 = new TH2D("h5_2","h5_2",nbinsx,xmin,x,nbinsy,-y,y);
  TH2D *h6_2 = new TH2D("h6_2","h6_2",nbinsx,xmin,x,nbinsy,-y,y);
-
+ TH2D *h7_2 = new TH2D("h7_2","h7_2",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h8_2 = new TH2D("h8_2","h8_2",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h9_2 = new TH2D("h9_2","h9_2",nbinsx,xmin,x,nbinsy,-y,y);
+ TH2D *h10_2 = new TH2D("h10_2","h10_2",nbinsx,xmin,x,nbinsy,-y,y);
 
 
 //////////////////////////////////////////////////////////////////////
@@ -65,7 +72,7 @@ void draw_hits()
 	sim->Draw("SiBarrelHits.position.y*0.1:SiBarrelHits.position.x*0.1>>h2","","");
 	sim->Draw("MPGDBarrelHits.position.y*0.1:MPGDBarrelHits.position.x*0.1>>h3","","");
 	sim->Draw("TOFBarrelHits.position.y*0.1:TOFBarrelHits.position.x*0.1>>h4","","");
-	//sim->Draw("TrackerEndcapHits.position.y*0.1:TrackerEndcapHits.position.x*0.1>>h5","","");
+	sim->Draw("OuterMPGDBarrelHits.position.y*0.1:OuterMPGDBarrelHits.position.x*0.1>>h5","",""); 
 	h1->SetMarkerStyle(31);
 	h1->SetTitle("Hit Points");
 	h1->SetMarkerColor(kBlack);
@@ -76,25 +83,25 @@ void draw_hits()
 	h1->GetXaxis()->CenterTitle();
 	h1->GetYaxis()->CenterTitle();
 	h1->Draw();
-
-     h2->SetMarkerStyle(20);
+	
+  h2->SetMarkerStyle(20);
 	h2->SetMarkerSize(0.1);
 	h2->SetMarkerColor(kMagenta);
 	h2->SetLineColor(kMagenta);
 	h2->Draw("same");
-
-     h3->SetMarkerStyle(20);
+	
+  h3->SetMarkerStyle(20);
 	h3->SetMarkerSize(0.1);
 	h3->SetMarkerColor(kBlue);
 	h3->SetLineColor(kBlue);
 	h3->Draw("same");
-
-     h4->SetMarkerStyle(20);
+	
+  h4->SetMarkerStyle(20);
 	h4->SetMarkerSize(0.1);
 	h4->SetMarkerColor(kGreen);
 	h4->SetLineColor(kGreen);
 	h4->Draw("same");
-
+	
 	h5->SetMarkerStyle(20);
 	h5->SetMarkerSize(0.1);
 	h5->SetMarkerColor(kRed);
@@ -108,25 +115,35 @@ void draw_hits()
  l->AddEntry(h2,"SiBarrelHits");
  l->AddEntry(h3,"MPGDBarrelHits");
  l->AddEntry(h4,"TOFBarrelHits");
-// l->AddEntry(h5,"DIRC Hits");
+ l->AddEntry(h5,"OuterMPGDBarrelHits");
  c1->cd();
  l->Draw();
 
-     c2->cd();
+  c2->cd();
 	sim->Draw("sqrt(VertexBarrelHits.position.x*0.1*VertexBarrelHits.position.x*0.1+VertexBarrelHits.position.y*0.1*VertexBarrelHits.position.y*0.1):VertexBarrelHits.position.z*0.1>>h1_1","VertexBarrelHits.position.y>0",""); // Multiply by 0.1 for cm
 	sim->Draw("sqrt(SiBarrelHits.position.x*0.1*SiBarrelHits.position.x*0.1+SiBarrelHits.position.y*0.1*SiBarrelHits.position.y*0.1):SiBarrelHits.position.z*0.1>>h2_1","SiBarrelHits.position.y>0","");
 	sim->Draw("sqrt(MPGDBarrelHits.position.x*0.1*MPGDBarrelHits.position.x*0.1+MPGDBarrelHits.position.y*0.1*MPGDBarrelHits.position.y*0.1):MPGDBarrelHits.position.z*0.1>>h3_1","MPGDBarrelHits.position.y>0","");
 	sim->Draw("sqrt(TOFBarrelHits.position.x*0.1*TOFBarrelHits.position.x*0.1+TOFBarrelHits.position.y*0.1*TOFBarrelHits.position.y*0.1):TOFBarrelHits.position.z*0.1>>h4_1","TOFBarrelHits.position.y>0","");
 	sim->Draw("sqrt(TrackerEndcapHits.position.x*0.1*TrackerEndcapHits.position.x*0.1+TrackerEndcapHits.position.y*0.1*TrackerEndcapHits.position.y*0.1):TrackerEndcapHits.position.z*0.1>>h5_1","TrackerEndcapHits.position.y>0","");
 	sim->Draw("sqrt(TOFEndcapHits.position.x*0.1*TOFEndcapHits.position.x*0.1+TOFEndcapHits.position.y*0.1*TOFEndcapHits.position.y*0.1):TOFEndcapHits.position.z*0.1>>h6_1","TOFEndcapHits.position.y>0","");
-
-     sim->Draw("-1.0*sqrt(VertexBarrelHits.position.x*0.1*VertexBarrelHits.position.x*0.1+VertexBarrelHits.position.y*0.1*VertexBarrelHits.position.y*0.1):VertexBarrelHits.position.z*0.1>>h1_2","VertexBarrelHits.position.y<0",""); // Multiply by 0.1 for cm
+  sim->Draw("sqrt(OuterMPGDBarrelHits.position.x*0.1*OuterMPGDBarrelHits.position.x*0.1+OuterMPGDBarrelHits.position.y*0.1*OuterMPGDBarrelHits.position.y*0.1):OuterMPGDBarrelHits.position.z*0.1>>h7_1","OuterMPGDBarrelHits.position.y>0","");
+  sim->Draw("sqrt(ForwardMPGDEndcapHits.position.x*0.1*ForwardMPGDEndcapHits.position.x*0.1+ForwardMPGDEndcapHits.position.y*0.1*ForwardMPGDEndcapHits.position.y*0.1):ForwardMPGDEndcapHits.position.z*0.1>>h8_1","ForwardMPGDEndcapHits.position.y>0","");
+	sim->Draw("sqrt(BackwardMPGDEndcapHits.position.x*0.1*BackwardMPGDEndcapHits.position.x*0.1+BackwardMPGDEndcapHits.position.y*0.1*BackwardMPGDEndcapHits.position.y*0.1):BackwardMPGDEndcapHits.position.z*0.1>>h9_1","BackwardMPGDEndcapHits.position.y>0","");
+	sim->Draw("sqrt(B0TrackerHits.position.x*0.1*B0TrackerHits.position.x*0.1+B0TrackerHits.position.y*0.1*B0TrackerHits.position.y*0.1):B0TrackerHits.position.z*0.1>>h10_1","B0TrackerHits.position.y>0","");
+		
+	
+  sim->Draw("-1.0*sqrt(VertexBarrelHits.position.x*0.1*VertexBarrelHits.position.x*0.1+VertexBarrelHits.position.y*0.1*VertexBarrelHits.position.y*0.1):VertexBarrelHits.position.z*0.1>>h1_2","VertexBarrelHits.position.y<0",""); // Multiply by 0.1 for cm
 	sim->Draw("-1.0*sqrt(SiBarrelHits.position.x*0.1*SiBarrelHits.position.x*0.1+SiBarrelHits.position.y*0.1*SiBarrelHits.position.y*0.1):SiBarrelHits.position.z*0.1>>h2_2","SiBarrelHits.position.y<0","");
 	sim->Draw("-1.0*sqrt(MPGDBarrelHits.position.x*0.1*MPGDBarrelHits.position.x*0.1+MPGDBarrelHits.position.y*0.1*MPGDBarrelHits.position.y*0.1):MPGDBarrelHits.position.z*0.1>>h3_2","MPGDBarrelHits.position.y<0","");
 	sim->Draw("-1.0*sqrt(TOFBarrelHits.position.x*0.1*TOFBarrelHits.position.x*0.1+TOFBarrelHits.position.y*0.1*TOFBarrelHits.position.y*0.1):TOFBarrelHits.position.z*0.1>>h4_2","TOFBarrelHits.position.y<0","");
 	sim->Draw("-1.0*sqrt(TrackerEndcapHits.position.x*0.1*TrackerEndcapHits.position.x*0.1+TrackerEndcapHits.position.y*0.1*TrackerEndcapHits.position.y*0.1):TrackerEndcapHits.position.z*0.1>>h5_2","TrackerEndcapHits.position.y<0","");
 	sim->Draw("-1.0*sqrt(TOFEndcapHits.position.x*0.1*TOFEndcapHits.position.x*0.1+TOFEndcapHits.position.y*0.1*TOFEndcapHits.position.y*0.1):TOFEndcapHits.position.z*0.1>>h6_2","TOFEndcapHits.position.y<0","");
+  sim->Draw("-1.0*sqrt(OuterMPGDBarrelHits.position.x*0.1*OuterMPGDBarrelHits.position.x*0.1+OuterMPGDBarrelHits.position.y*0.1*OuterMPGDBarrelHits.position.y*0.1):OuterMPGDBarrelHits.position.z*0.1>>h7_2","OuterMPGDBarrelHits.position.y<0","");
+  sim->Draw("-1.0*sqrt(ForwardMPGDEndcapHits.position.x*0.1*ForwardMPGDEndcapHits.position.x*0.1+ForwardMPGDEndcapHits.position.y*0.1*ForwardMPGDEndcapHits.position.y*0.1):ForwardMPGDEndcapHits.position.z*0.1>>h8_2","ForwardMPGDEndcapHits.position.y<0","");
+	sim->Draw("-1.0*sqrt(BackwardMPGDEndcapHits.position.x*0.1*BackwardMPGDEndcapHits.position.x*0.1+BackwardMPGDEndcapHits.position.y*0.1*BackwardMPGDEndcapHits.position.y*0.1):BackwardMPGDEndcapHits.position.z*0.1>>h9_2","BackwardMPGDEndcapHits.position.y<0","");
+	sim->Draw("-1.0*sqrt(B0TrackerHits.position.x*0.1*B0TrackerHits.position.x*0.1+B0TrackerHits.position.y*0.1*B0TrackerHits.position.y*0.1):B0TrackerHits.position.z*0.1>>h10_2","B0TrackerHits.position.y<0","");
 
+	
 	h1_1->SetMarkerStyle(31);
 	h1_1->SetTitle("Hit Points");
 	h1_1->SetMarkerSize(0.1);
@@ -137,81 +154,133 @@ void draw_hits()
 	h1_1->GetXaxis()->CenterTitle();
 	h1_1->GetYaxis()->CenterTitle();
 	h1_1->Draw();
-     h1_2->SetMarkerSize(0.1);
+  
+  h1_2->SetMarkerSize(0.1);
 	h1_2->SetLineColor(kBlack);
 	h1_2->SetMarkerColor(kBlack);
-	h1_2->Draw("same");
-
-
+	h1_2->Draw("same");	
+	
+	
 	h2_1->SetMarkerStyle(20);
 	h2_1->SetMarkerSize(0.1);
 	h2_1->SetMarkerColor(kMagenta);
 	h2_1->SetLineColor(kMagenta);
 	h2_1->Draw("same");
-
-     h2_2->SetMarkerSize(0.1);
+	
+  h2_2->SetMarkerSize(0.1);
 	h2_2->SetLineColor(kMagenta);
 	h2_2->SetMarkerColor(kMagenta);
-	h2_2->Draw("same");
-
-
+	h2_2->Draw("same");	
+	
+	
 	h3_1->SetMarkerStyle(20);
 	h3_1->SetMarkerSize(0.1);
 	h3_1->SetLineColor(kBlue);
 	h3_1->SetMarkerColor(kBlue);
 	h3_1->Draw("same");
-
+	
 	h3_2->SetMarkerSize(0.1);
 	h3_2->SetLineColor(kBlue);
 	h3_2->SetMarkerColor(kBlue);
-	h3_2->Draw("same");
-
+	h3_2->Draw("same");	
+	
 
 	h4_1->SetMarkerStyle(20);
 	h4_1->SetMarkerSize(0.1);
 	h4_1->SetLineColor(kGreen);
 	h4_1->SetMarkerColor(kGreen);
 	h4_1->Draw("same");
-
+	
 	h4_2->SetMarkerSize(0.1);
 	h4_2->SetLineColor(kGreen);
 	h4_2->SetMarkerColor(kGreen);
-	h4_2->Draw("same");
-
-     h5_1->SetMarkerStyle(20);
+	h4_2->Draw("same");	
+	
+  h5_1->SetMarkerStyle(20);
 	h5_1->SetMarkerSize(0.1);
 	h5_1->SetMarkerColor(kRed);
 	h5_1->SetLineColor(kRed);
 	h5_1->Draw("same");
-
-     h5_2->SetMarkerSize(0.1);
+	
+  h5_2->SetMarkerSize(0.1);
 	h5_2->SetLineColor(kRed);
 	h5_2->SetMarkerColor(kRed);
-	h5_2->Draw("same");
-
+	h5_2->Draw("same");	
+	
 	h6_1->SetMarkerStyle(20);
 	h6_1->SetMarkerSize(0.1);
 	h6_1->SetMarkerColor(kCyan);
 	h6_1->SetLineColor(kCyan);
 	h6_1->Draw("same");
-
-     h6_2->SetMarkerSize(0.1);
+	
+  h6_2->SetMarkerSize(0.1);
 	h6_2->SetLineColor(kCyan);
 	h6_2->SetMarkerColor(kCyan);
-	h6_2->Draw("same");
+	h6_2->Draw("same");	
+	
+	h7_1->SetMarkerStyle(20);
+	h7_1->SetMarkerSize(0.1);
+	h7_1->SetMarkerColor(9);
+	h7_1->SetLineColor(9);
+	h7_1->Draw("same");
+	
+  h7_2->SetMarkerSize(0.1);
+	h7_2->SetLineColor(9);
+	h7_2->SetMarkerColor(9);
+	h7_2->Draw("same");	
+	
+  h8_1->SetMarkerStyle(20);
+	h8_1->SetMarkerSize(0.1);
+	h8_1->SetMarkerColor(28);
+	h8_1->SetLineColor(28);
+	h8_1->Draw("same");
+	
+  h8_2->SetMarkerSize(0.1);
+	h8_2->SetLineColor(28);
+	h8_2->SetMarkerColor(28);
+	h8_2->Draw("same");	
+	
+	h9_1->SetMarkerStyle(20);
+	h9_1->SetMarkerSize(0.1);
+	h9_1->SetMarkerColor(46);
+	h9_1->SetLineColor(46);
+	h9_1->Draw("same");
+	
+  h9_2->SetMarkerSize(0.1);
+	h9_2->SetLineColor(46);
+	h9_2->SetMarkerColor(46);
+	h9_2->Draw("same");		
+	
+	h10_1->SetMarkerStyle(20);
+	h10_1->SetMarkerSize(0.1);
+	h10_1->SetMarkerColor(38);
+	h10_1->SetLineColor(38);
+	h10_1->Draw("same");
+	
+  h10_2->SetMarkerSize(0.1);
+	h10_2->SetLineColor(38);
+	h10_2->SetMarkerColor(38);
+	h10_2->Draw("same");	
 
-
- l= new TLegend(0.64,0.87,0.85,1.0);
- l->SetTextSize(0.03);
- l->SetBorderSize(0);
- l->AddEntry(h1_1,"VertexBarrelHits");
- l->AddEntry(h2_1,"SiBarrelHits");
- l->AddEntry(h3_1,"MPGDBarrelHits");
- l->AddEntry(h4_1,"TOFBarrelHits");
- l->AddEntry(h5_1,"TrackerEndcapHits");
- l->AddEntry(h6_1,"TOFEndcapHits");
- c2->cd();
- l->Draw();
+  c2->cd();
+  l= new TLegend(0.15,0.88,0.35,0.99);
+  l->SetTextSize(0.025);
+  l->SetBorderSize(0);
+  l->AddEntry(h1_1,"VertexBarrelHits");
+  l->AddEntry(h2_1,"SiBarrelHits");
+  l->AddEntry(h3_1,"MPGDBarrelHits");
+  l->AddEntry(h4_1,"TOFBarrelHits");
+  l->AddEntry(h5_1,"TrackerEndcapHits");
+  l->Draw();
+  l= new TLegend(0.60,0.88,0.90,0.99);  
+  l->SetTextSize(0.025);
+  l->SetBorderSize(0);
+  l->AddEntry(h6_1,"TOFEndcapHits");
+  l->AddEntry(h7_1,"OuterMPGDBarrelHits");
+  l->AddEntry(h8_1,"ForwardMPGDEndcapHits");
+  l->AddEntry(h9_1,"BackwardMPGDEndcapHits");
+ // l->AddEntry(h10_1,"B0TrackerHits");
+  l->Draw("same");
 
  c1->SaveAs("hitsxy_dd4hep.png");
  c2->SaveAs("hitsrz_dd4hep.png");
