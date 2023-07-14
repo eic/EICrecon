358deef705b02c5d46479d0d6c1e63550a7bf3f3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-07-14 17:47:00 -0400

    fix syntax

--- src/algorithms/calorimetry/CalorimeterHitDigi.cc
+++ src/algorithms/calorimetry/CalorimeterHitDigi.cc
@@ -172,7 +172,7 @@ std::unique_ptr<edm4hep::RawCalorimeterHitCollection> CalorimeterHitDigi::signal
     auto rawhits = std::make_unique<edm4hep::RawCalorimeterHitCollection>();
 
     // find the hits that belong to the same group (for merging)
-    std::unordered_map<uint64_t, std::vector<std::size_t> merge_map;
+    std::unordered_map<uint64_t, std::vector<std::size_t>> merge_map;
     std::size_t ix = 0;
     for (const auto &ahit : simhits) {
         uint64_t hid = ahit.getCellID() & id_mask;
