eic/EICrecon

commit 95aa36c2b652b4b78b0c8024786c0d5c9dbcad85

fix: rewrite TrackParamTruthInit to create edm4eic::TrackParameters (#766)
### Briefly, what does this PR introduce?
This reworks `TrackParamTruthInit` to create
`edm4eic::TrackParametersCollection`s. That then means that JANA2
doesn't need to clean up the `SinglyChargedBoundParameters` it seems to
be having trouble with. It also means we can actually store the truth
track seeds in the edm4eic output trees.

### What kind of change does this PR introduce?
- [ ] Bug fix (issue #__)
- [x] New feature (issue #__)
- [ ] Documentation update
- [ ] Other: __

### Please check if this PR fulfills the following:
- [ ] Tests for the changes have been added
- [ ] Documentation has been added / updated
- [x] Changes have been communicated to collaborators @veprbl
@bschmookler

### Does this PR introduce breaking changes? What changes might users
need to make to their code?
No.

### Does this PR change default behavior?
Yes, stores truth track seeds in output ROOT tree.

---------

Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

src/algorithms/tracking/TrackParamTruthInit.cc
@@ -23,81 +23,96 @@ void eicrecon::TrackParamTruthInit::init(const std::shared_ptr<spdlog::logger> &
     m_pdg_db = std::make_shared<TDatabasePDG>();
 }
 
-eicrecon::TrackParameters *eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticle *part) {
+std::unique_ptr<edm4eic::TrackParametersCollection>
+eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcparticles) {
+    // MCParticles uses numerical values in its specified units,
+    // while m_cfg is in the Acts unit system
     using Acts::UnitConstants::GeV;
     using Acts::UnitConstants::MeV;
     using Acts::UnitConstants::mm;
     using Acts::UnitConstants::um;
     using Acts::UnitConstants::ns;
 
-    // getGeneratorStatus = 1 means thrown G4Primary, but dd4gun uses getGeneratorStatus == 0
-    if (part->getGeneratorStatus() > 1 ) {
-        m_log->trace("ignoring particle with generatorStatus = {}", part->getGeneratorStatus());
-        return nullptr;
+    // Create output collection
+    auto track_parameters = std::make_unique<edm4eic::TrackParametersCollection>();
+
+    // Loop over input particles
+    for (const auto& mcparticle: *mcparticles) {
+
+        // require generatorStatus == 1 for stable generated particles in HepMC3 and DDSim gun
+        if (mcparticle.getGeneratorStatus() != 1 ) {
+            m_log->trace("ignoring particle with generatorStatus = {}", mcparticle.getGeneratorStatus());
+            continue;
+        }
+
+        // require close to interaction vertex
+        if (abs(mcparticle.getVertex().x) * mm > m_cfg.m_maxVertexX
+            || abs(mcparticle.getVertex().y) * mm > m_cfg.m_maxVertexY
+            || abs(mcparticle.getVertex().z) * mm > m_cfg.m_maxVertexZ) {
+            m_log->trace("ignoring particle with vs = {} [mm]", mcparticle.getVertex());
+            continue;
+        }
+
+        // require minimum momentum
+        const auto& p = mcparticle.getMomentum();
+        const auto pmag = std::hypot(p.x, p.y, p.z);
+        if (pmag * GeV < m_cfg.m_minMomentum) {
+            m_log->trace("ignoring particle with p = {} GeV ", pmag);
+            continue;
+        }
+
+        // require minimum pseudorapidity
+        const auto phi   = std::atan2(p.y, p.x);
+        const auto theta = std::atan2(std::hypot(p.x, p.y), p.z);
+        const auto eta   = -std::log(std::tan(theta/2));
+        if (eta > m_cfg.m_maxEtaForward || eta < -std::abs(m_cfg.m_maxEtaBackward)) {
+            m_log->trace("ignoring particle with Eta = {}", eta);
+            continue;
+        }
+
+        // get the particle charge
+        // note that we cannot trust the mcparticles charge, as DD4hep
+        // sets this value to zero! let's lookup by PDGID instead
+        //const double charge = m_pidSvc->particle(mcparticle.getPDG()).charge;
+        const auto pdg = mcparticle.getPDG();
+        const auto* particle = m_pdg_db->GetParticle(pdg);
+        if (particle == nullptr) {
+            m_log->debug("particle with PDG {} not in TDatabasePDG", pdg);
+            continue;
+        }
+        double charge = std::copysign(1.0,particle->Charge());
+        if (abs(charge) < std::numeric_limits<double>::epsilon()) {
+            m_log->trace("ignoring neutral particle");
+            continue;
+        }
+
+        // modify initial momentum to avoid bleeding truth to results when fit fails
+        const auto pinit = pmag*(1.0 + m_cfg.m_momentumSmear * m_normDist(generator));
+
+        // Insert into edm4eic::TrackParameters, which uses numerical values in its specified units
+        auto track_parameter = track_parameters->create();
+        track_parameter.setType(-1); // type --> seed(-1)
+        track_parameter.setLoc({0.0, 0.0}); // 2d location on surface [mm]
+        track_parameter.setLocError({1.0, 1.0}); // sqrt(variance) of location [mm]
+        track_parameter.setTheta(theta); //theta [rad]
+        track_parameter.setPhi(phi); // phi [rad]
+        track_parameter.setQOverP(charge / pinit); // Q/p [e/GeV]
+        track_parameter.setMomentumError({0.05, 0.01, 0.1}); // sqrt(variance) on theta, phi, q/p [rad, rad, e/GeV]
+        track_parameter.setTime(mcparticle.getTime()); // time [ns]
+        track_parameter.setTimeError(10e9); // error on time [ns]
+        track_parameter.setCharge(charge); // charge
+
+        // Debug output
+        if (m_log->level() <= spdlog::level::debug) {
+            m_log->debug("Invoke track finding seeded by truth particle with:");
+            m_log->debug("   p     = {} GeV (smeared to {} GeV)", pmag, pinit);
+            m_log->debug("   q     = {}", charge);
+            m_log->debug("   q/p   = {} e/GeV (smeared to {} e/GeV)", charge / pmag, charge / pinit);
+            m_log->debug("   theta = {}", theta);
+            m_log->debug("   phi   = {}", phi);
+        }
     }
 
-
-    // require close to interaction vertex
-    if (abs(part->getVertex().x) * mm > m_cfg.m_maxVertexX
-        || abs(part->getVertex().y) * mm > m_cfg.m_maxVertexY
-        || abs(part->getVertex().z) * mm > m_cfg.m_maxVertexZ) {
-        m_log->trace("ignoring particle with vs = {} [mm]", part->getVertex());
-        return nullptr;
-    }
-
-    // require minimum momentum
-    const auto& p = part->getMomentum();
-    const auto pmag = std::hypot(p.x, p.y, p.z);
-    if (pmag * GeV < m_cfg.m_minMomentum) {
-        m_log->trace("ignoring particle with p = {} GeV ", pmag);
-        return nullptr;
-    }
-
-    // require minimum pseudorapidity
-    const auto phi   = std::atan2(p.y, p.x);
-    const auto theta = std::atan2(std::hypot(p.x, p.y), p.z);
-    const auto eta   = -std::log(std::tan(theta/2));
-    if (eta > m_cfg.m_maxEtaForward || eta < -std::abs(m_cfg.m_maxEtaBackward)) {
-        m_log->trace("ignoring particle with Eta = {}", eta);
-        return nullptr;
-    }
-
-    // get the particle charge
-    // note that we cannot trust the mcparticles charge, as DD4hep
-    // sets this value to zero! let's lookup by PDGID instead
-    //const double charge = m_pidSvc->particle(part->getPDG()).charge;
-    double charge = std::copysign(1.0,m_pdg_db->GetParticle(part->getPDG())->Charge());
-    if (abs(charge) < std::numeric_limits<double>::epsilon()) {
-        m_log->trace("ignoring neutral particle");
-        return nullptr;
-    }
-
-    // modify initial momentum to avoid bleeding truth to results when fit fails
-    const auto pinit = pmag*(1.0 + m_cfg.m_momentumSmear * m_normDist(generator));
-
-    // build some track cov matrix
-    Acts::BoundSymMatrix cov                    = Acts::BoundSymMatrix::Zero();
-    cov(Acts::eBoundLoc0, Acts::eBoundLoc0)     = 1000*um*1000*um;
-    cov(Acts::eBoundLoc1, Acts::eBoundLoc1)     = 1000*um*1000*um;
-    cov(Acts::eBoundPhi, Acts::eBoundPhi)       = 0.05*0.05;
-    cov(Acts::eBoundTheta, Acts::eBoundTheta)   = 0.01*0.01;
-    cov(Acts::eBoundQOverP, Acts::eBoundQOverP) = (0.1*0.1) / (GeV*GeV);
-    cov(Acts::eBoundTime, Acts::eBoundTime)     = 10.0e9*ns*10.0e9*ns;
-
-    Acts::BoundVector  params;
-    params(Acts::eBoundLoc0)   = 0.0 * mm ;  // cylinder radius
-    params(Acts::eBoundLoc1)   = 0.0 * mm ;  // cylinder length
-    params(Acts::eBoundPhi)    = phi;
-    params(Acts::eBoundTheta)  = theta;
-    params(Acts::eBoundQOverP) = charge / (pinit * GeV);
-    params(Acts::eBoundTime)   = part->getTime() * ns;
-
-    //// Construct a perigee surface as the target surface
-    auto pSurface = Acts::Surface::makeShared<Acts::PerigeeSurface>(
-            Acts::Vector3{part->getVertex().x * mm, part->getVertex().y * mm, part->getVertex().z * mm});
-
-    //params(Acts::eBoundQOverP) = charge/p;
-    auto result = new eicrecon::TrackParameters({pSurface, params, charge, cov});
-    return result;
+    return std::move(track_parameters);
 
 }

src/algorithms/tracking/TrackParamTruthInit.h
@@ -4,9 +4,9 @@
 
 #pragma once
 
-#include <algorithms/interfaces/IObjectProducer.h>
-#include <algorithms/tracking/JugTrack/Track.hpp>
-#include <edm4hep/MCParticle.h>
+#include <edm4hep/MCParticleCollection.h>
+#include <edm4eic/TrackParametersCollection.h>
+
 #include <spdlog/logger.h>
 
 #include "TrackParamTruthInitConfig.h"
@@ -22,7 +22,8 @@ namespace eicrecon {
 
         void init(const std::shared_ptr<spdlog::logger> &logger);
 
-        eicrecon::TrackParameters * produce(const edm4hep::MCParticle *);
+	std::unique_ptr<edm4eic::TrackParametersCollection>
+            produce(const edm4hep::MCParticleCollection* parts);
 
     private:
         std::shared_ptr<spdlog::logger> m_log;

src/global/tracking/CKFTracking_factory.cc
@@ -2,15 +2,18 @@
 // Subject to the terms in the LICENSE file found in the top-level directory.
 //
 
+#include "CKFTracking_factory.h"
 
 #include <Acts/Propagator/Navigator.hpp>
-#include "CKFTracking_factory.h"
-#include "extensions/spdlog/SpdlogExtensions.h"
+#include <Acts/Surfaces/PerigeeSurface.hpp>
+#include <edm4eic/TrackParametersCollection.h>
 #include <JANA/JEvent.h>
-#include "services/geometry/acts/ACTSGeo_service.h"
-#include "services/log/Log_service.h"
+
+#include "extensions/spdlog/SpdlogExtensions.h"
 #include "extensions/string/StringHelpers.h"
-#include <services/geometry/dd4hep/JDD4hep_service.h>
+#include "services/geometry/acts/ACTSGeo_service.h"
+#include "services/geometry/dd4hep/JDD4hep_service.h"
+#include "services/log/Log_service.h"
 
 void eicrecon::CKFTracking_factory::Init() {
     auto app = GetApplication();
@@ -46,21 +49,41 @@ void eicrecon::CKFTracking_factory::ChangeRun(const std::shared_ptr<const JEvent
 }
 
 void eicrecon::CKFTracking_factory::Process(const std::shared_ptr<const JEvent> &event) {
-    // Now we check that user provided an input names
-    std::string input_tag = GetInputTags()[0];
-
-    // Collect all hits
-    auto source_linker_result = event->GetSingle<eicrecon::TrackerSourceLinkerResult>(input_tag);
+    // Collect all inputs
+    auto track_parameters = static_cast<const edm4eic::TrackParametersCollection*>(event->GetCollectionBase(GetInputTags()[0]));
+    auto source_linker_result = event->GetSingle<eicrecon::TrackerSourceLinkerResult>(GetInputTags()[1]);
 
     if(!source_linker_result) {
         m_log->warn("TrackerSourceLinkerResult is null (hasn't been produced?). Skipping tracking for the whole event!");
         return;
     }
 
-    auto track_parameters = event->Get<eicrecon::TrackParameters>("InitTrackParams");
     eicrecon::TrackParametersContainer acts_track_params;
-    for(auto track_params_item: track_parameters) {
-        acts_track_params.push_back(*track_params_item);
+    for (const auto& track_parameter: *track_parameters) {
+
+        Acts::BoundVector params;
+        params(Acts::eBoundLoc0)   = track_parameter.getLoc().a * Acts::UnitConstants::mm;  // cylinder radius
+        params(Acts::eBoundLoc1)   = track_parameter.getLoc().b * Acts::UnitConstants::mm;  // cylinder length
+        params(Acts::eBoundTheta)  = track_parameter.getTheta();
+        params(Acts::eBoundPhi)    = track_parameter.getPhi();
+        params(Acts::eBoundQOverP) = track_parameter.getQOverP() / Acts::UnitConstants::GeV;
+        params(Acts::eBoundTime)   = track_parameter.getTime() * Acts::UnitConstants::ns;
+
+        double charge = track_parameter.getCharge();
+
+        Acts::BoundSymMatrix cov                    = Acts::BoundSymMatrix::Zero();
+        cov(Acts::eBoundLoc0, Acts::eBoundLoc0)     = std::pow( track_parameter.getLocError().xx ,2)*Acts::UnitConstants::mm*Acts::UnitConstants::mm;
+        cov(Acts::eBoundLoc1, Acts::eBoundLoc1)     = std::pow( track_parameter.getLocError().yy,2)*Acts::UnitConstants::mm*Acts::UnitConstants::mm;
+        cov(Acts::eBoundTheta, Acts::eBoundTheta)   = std::pow( track_parameter.getMomentumError().xx,2);
+        cov(Acts::eBoundPhi, Acts::eBoundPhi)       = std::pow( track_parameter.getMomentumError().yy,2);
+        cov(Acts::eBoundQOverP, Acts::eBoundQOverP) = std::pow( track_parameter.getMomentumError().zz,2) / (Acts::UnitConstants::GeV*Acts::UnitConstants::GeV);
+        cov(Acts::eBoundTime, Acts::eBoundTime)     = std::pow( track_parameter.getTimeError(),2)*Acts::UnitConstants::ns*Acts::UnitConstants::ns;
+
+        // Construct a perigee surface as the target surface
+        auto pSurface = Acts::Surface::makeShared<const Acts::PerigeeSurface>(Acts::Vector3(0,0,0));
+
+        // Create parameters
+        acts_track_params.emplace_back(pSurface, params, charge, cov);
     }
 
     // Reading the geometry may take a long time and if the JANA ticker is enabled, it will keep printing

src/global/tracking/TrackParamTruthInit_factory.cc
@@ -2,17 +2,7 @@
 // Subject to the terms in the LICENSE file found in the top-level directory.
 //
 
-#include <vector>
-
 #include "TrackParamTruthInit_factory.h"
-#include "services/geometry/acts/ACTSGeo_service.h"
-#include "extensions/string/StringHelpers.h"
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/fmt/ostr.h>
-#include <JANA/JEvent.h>
-
-#include <algorithms/tracking/TrackParamTruthInit.h>
-#include <algorithms/tracking/TrackParamTruthInitConfig.h>
 
 void eicrecon::TrackParamTruthInit_factory::Init() {
     auto app = GetApplication();
@@ -38,52 +28,19 @@ void eicrecon::TrackParamTruthInit_factory::Init() {
     app->SetDefaultParameter(param_prefix + ":MomentumSmear", cfg.m_momentumSmear, "Momentum magnitude fraction to use as width of gaussian smearing");
 
     // Initialize algorithm
-    m_truth_track_seeding_algo.applyConfig(cfg);
-    m_truth_track_seeding_algo.init(m_log);
+    m_seeding_algo.applyConfig(cfg);
+    m_seeding_algo.init(m_log);
 }
 
 void eicrecon::TrackParamTruthInit_factory::ChangeRun(const std::shared_ptr<const JEvent> &event) {
-
 }
 
 void eicrecon::TrackParamTruthInit_factory::Process(const std::shared_ptr<const JEvent> &event) {
-
-    // Get MCParticles
-    auto mc_particles = event->Get<edm4hep::MCParticle>(GetInputTags()[0]);
+    auto mc_particles = static_cast<const edm4hep::MCParticleCollection*>(event->GetCollectionBase(GetInputTags()[0]));
 
     try {
-        // Produce track parameters out of MCParticles
-        std::vector<eicrecon::TrackParameters *> results;
-        for (auto mc_particle: mc_particles) {
-
-            // Only stable particles from MC
-            if (mc_particle->getGeneratorStatus() != 1) continue;
-
-            // Do conversion
-            auto result = m_truth_track_seeding_algo.produce(mc_particle);
-
-            if (!result) continue;   // result might be null
-
-            results.push_back(result);
-
-            // >oO debug output
-            if (m_log->level() <= spdlog::level::debug) {
-                const auto p = std::hypot(mc_particle->getMomentum().x, mc_particle->getMomentum().y,
-                                          mc_particle->getMomentum().z);
-                const auto theta = std::atan2(std::hypot(mc_particle->getMomentum().x,
-                                                         mc_particle->getMomentum().y),
-                                              mc_particle->getMomentum().z);
-                const auto phi = std::atan2(mc_particle->getMomentum().y, mc_particle->getMomentum().x);
-                const auto charge = result->charge();
-                m_log->debug("Invoke track finding seeded by truth particle with:");
-                m_log->debug("   p =  {} GeV", p);
-                m_log->debug("   theta = {}", theta);
-                m_log->debug("   phi = {}", phi);
-                m_log->debug("   charge = {}", charge);
-                m_log->debug("   q/p =  {}", charge / p);
-            }
-        }
-        Set(results);
+        auto output = m_seeding_algo.produce(mc_particles);
+        SetCollection(std::move(output));
     }
     catch(std::exception &e) {
         throw JException(e.what());

src/global/tracking/TrackParamTruthInit_factory.h
@@ -4,23 +4,21 @@
 
 #pragma once
 
-#include <spdlog/spdlog.h>
-#include <extensions/jana/JChainFactoryT.h>
-#include <extensions/spdlog/SpdlogMixin.h>
+#include "extensions/jana/JChainMultifactoryT.h"
+#include "extensions/spdlog/SpdlogMixin.h"
 
-#include <algorithms/tracking/TrackParamTruthInit.h>
-
-
+#include "algorithms/tracking/TrackParamTruthInit.h"
+#include "algorithms/tracking/TrackParamTruthInitConfig.h"
 
 namespace eicrecon {
 
-class TrackParamTruthInit_factory :
-        public JChainFactoryT<eicrecon::TrackParameters, TrackParamTruthInitConfig, JFactoryT>,
-        public SpdlogMixin<TrackParamTruthInit_factory>  {
+    class TrackParamTruthInit_factory :
+            public JChainFactoryT<edm4eic::TrackParameters, TrackParamTruthInitConfig>,
+            public SpdlogMixin<TrackParamTruthInit_factory> {
 
     public:
         TrackParamTruthInit_factory( std::vector<std::string> default_input_tags, TrackParamTruthInitConfig cfg):
-                JChainFactoryT<eicrecon::TrackParameters, TrackParamTruthInitConfig, JFactoryT>(std::move(default_input_tags), cfg ) {
+            JChainFactoryT<edm4eic::TrackParameters, TrackParamTruthInitConfig>(std::move(default_input_tags), cfg) {
         }
 
         /** One time initialization **/
@@ -33,8 +31,7 @@ class TrackParamTruthInit_factory :
         void Process(const std::shared_ptr<const JEvent> &event) override;
 
     private:
-
-        eicrecon::TrackParamTruthInit m_truth_track_seeding_algo;  /// Truth track seeding algorithm
+        eicrecon::TrackParamTruthInit m_seeding_algo;
     };
 
 } // eicrecon

src/global/tracking/tracking.cc
@@ -15,9 +15,9 @@
 #include "TrackParamTruthInit_factory.h"
 #include "TrackingResult_factory.h"
 #include "CKFTracking_factory.h"
-#include "TrackSeeding_factory.h"
 #include "TrackerHitCollector_factory.h"
 #include "TrackProjector_factory.h"
+#include "TrackSeeding_factory.h"
 #include "ParticlesWithTruthPID_factory.h"
 #include "IterativeVertexFinder_factory.h"
 
@@ -54,7 +54,7 @@ void InitPlugin(JApplication *app) {
             {"CentralTrackingRecHits"}, "CentralTrackerSourceLinker"));
 
     app->Add(new JChainFactoryGeneratorT<CKFTracking_factory>(
-            {"CentralTrackerSourceLinker"}, "CentralCKFTrajectories"));
+            {"InitTrackParams", "CentralTrackerSourceLinker"}, "CentralCKFTrajectories"));
 
     app->Add(new JChainFactoryGeneratorT<TrackSeeding_factory>(
             {"CentralTrackingRecHits"}, "CentralTrackSeedingResults"));

commit 8d8e93e22d63c39f1bae027f92a345193536b3f2

fix: place truth vertex on perigee surface in truth seeding (#783)
### Briefly, what does this PR introduce?
This fixes the truth vertex issue introduced in #766 and discussed in
#779 by placing the truth vertex on the perigee surface with its
two-vector. This 'mostly' recovers the original behavior, but it
necessarily has differences because the truth vertex propagation is
modeled differently (but now identical to how this is done in realistic
seeding).

### What kind of change does this PR introduce?
- [x] Bug fix (issue #__)
- [ ] New feature (issue #__)
- [ ] Documentation update
- [ ] Other: __

### Please check if this PR fulfills the following:
- [ ] Tests for the changes have been added
- [ ] Documentation has been added / updated
- [x] Changes have been communicated to collaborators @veprbl
@bschmookler

### Does this PR introduce breaking changes? What changes might users
need to make to their code?
No.

### Does this PR change default behavior?
Yes, some changes in truth seeded tracks to be expected.

src/algorithms/tracking/TrackParamTruthInit.cc
@@ -46,10 +46,11 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         }
 
         // require close to interaction vertex
-        if (abs(mcparticle.getVertex().x) * mm > m_cfg.m_maxVertexX
-            || abs(mcparticle.getVertex().y) * mm > m_cfg.m_maxVertexY
-            || abs(mcparticle.getVertex().z) * mm > m_cfg.m_maxVertexZ) {
-            m_log->trace("ignoring particle with vs = {} [mm]", mcparticle.getVertex());
+        auto v = mcparticle.getVertex();
+        if (abs(v.x) * mm > m_cfg.m_maxVertexX ||
+            abs(v.y) * mm > m_cfg.m_maxVertexY ||
+            abs(v.z) * mm > m_cfg.m_maxVertexZ) {
+            m_log->trace("ignoring particle with vs = {} [mm]", v);
             continue;
         }
 
@@ -92,7 +93,7 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         // Insert into edm4eic::TrackParameters, which uses numerical values in its specified units
         auto track_parameter = track_parameters->create();
         track_parameter.setType(-1); // type --> seed(-1)
-        track_parameter.setLoc({0.0, 0.0}); // 2d location on surface [mm]
+        track_parameter.setLoc({static_cast<float>(std::hypot(v.x, v.y)), static_cast<float>(v.z)}); // 2d location on surface [mm]
         track_parameter.setLocError({1.0, 1.0}); // sqrt(variance) of location [mm]
         track_parameter.setTheta(theta); //theta [rad]
         track_parameter.setPhi(phi); // phi [rad]