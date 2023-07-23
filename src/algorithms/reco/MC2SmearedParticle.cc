433985e27bcb24b3dc957315c4d30d2d533034d5
Wouter Deconinck <wdconinc@gmail.com>
2023-07-23 18:43:51 -0500

    feat: rewrite MC2SmearedParticle to operate on collections (#778)
    ### Briefly, what does this PR introduce?
    This reworks the MC2SmearedParticle algorithm and factory to operate on
    podio collections, which are passed to the algorithm. It moves the loop
    over MCParticles into the algorithm (maybe at some point the factories
    can parallelize this, but we're not there, and consistency in how
    algorithms interact with collections is more important right now). It
    also removes the (defunct) smearing configuration.
    
    This also removes the `generatorStatus == 0` entries as done in #772.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [x] New feature (issue: use collections in MC2SmearedParticles)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

--- src/algorithms/reco/MC2SmearedParticle.cc
+++ src/algorithms/reco/MC2SmearedParticle.cc
@@ -2,61 +2,46 @@
 // Copyright (C) 2022 Sylvester Joosten, Whitney Armstrong, Wouter Deconinck, Dmitry Romanov
 
 #include "MC2SmearedParticle.h"
+
 #include <cmath>
 #include <edm4eic/vector_utils.h>
-#include <edm4eic/MutableReconstructedParticle.h>
+#include <edm4eic/ReconstructedParticleCollection.h>
 
 
 void eicrecon::MC2SmearedParticle::init(std::shared_ptr<spdlog::logger> logger) {
     m_log = logger;
-
-    // Create random gauss function
-    m_gauss = [&]() {
-        return m_random.Gaus(0, m_cfg.momentum_smearing);
-    };
 }
 
-edm4eic::ReconstructedParticle *eicrecon::MC2SmearedParticle::produce(const edm4hep::MCParticle *mc_particle) {
+std::unique_ptr<edm4eic::ReconstructedParticleCollection> eicrecon::MC2SmearedParticle::produce(const edm4hep::MCParticleCollection* mc_particles) {
+    auto rec_particles = std::make_unique<edm4eic::ReconstructedParticleCollection>();
+    for (const auto& mc_particle: *mc_particles) {
 
-    if (mc_particle->getGeneratorStatus() != 1) {
-        m_log->debug("ignoring particle with generatorStatus = {}", mc_particle->getGeneratorStatus());
-        return nullptr;
+        if (mc_particle.getGeneratorStatus() != 1) {
+            m_log->debug("ignoring particle with generatorStatus = {}", mc_particle.getGeneratorStatus());
+            continue;
+        }
+
+        // make sure we keep types consistent
+        using MomType = decltype(edm4eic::ReconstructedParticle().getMomentum().x);
+
+        const MomType px = mc_particle.getMomentum().x;
+        const MomType py = mc_particle.getMomentum().y;
+        const MomType pz = mc_particle.getMomentum().z;
+
+        const MomType vx = mc_particle.getVertex().x;
+        const MomType vy = mc_particle.getVertex().y;
+        const MomType vz = mc_particle.getVertex().z;
+
+        auto rec_part = rec_particles->create();
+        rec_part.setType(mc_particle.getGeneratorStatus()); // @TODO: determine type codes
+        rec_part.setEnergy(mc_particle.getEnergy());
+        rec_part.setMomentum({px, py, pz});
+        rec_part.setReferencePoint({vx, vy, vz}); // @FIXME: probably not what we want?
+        rec_part.setCharge(mc_particle.getCharge());
+        rec_part.setMass(mc_particle.getMass());
+        rec_part.setGoodnessOfPID(1); // Perfect PID
+        rec_part.setCovMatrix({0, 0, 0, 0});
+        rec_part.setPDG(mc_particle.getPDG());
     }
-
-    // for now just use total momentum smearing as this is the largest effect,
-    // ideally we should also smear the angles but this should be good enough
-    // for now.
-    // TODO smearing was removed, should we add it again? And is it needed at all?
-    const auto energy_true = mc_particle->getEnergy();
-    // make sure we keep energy consistent
-    using MomType = decltype(edm4eic::ReconstructedParticle().getMomentum().x);
-
-    const MomType px = mc_particle->getMomentum().x;
-    const MomType py = mc_particle->getMomentum().y;
-    const MomType pz = mc_particle->getMomentum().z;
-
-    const MomType dpx = 0;  // TODO it is 0 as smearing is removed
-    const MomType dpy = 0;  // TODO it is 0 as smearing is removed
-    const MomType dpz = 0;  // TODO it is 0 as smearing is removed
-    const MomType dE =  0;  // TODO it is 0 as smearing is removed
-
-    // ignore covariance for now
-    // @TODO: vertex smearing
-    const MomType vx = mc_particle->getVertex().x;
-    const MomType vy = mc_particle->getVertex().y;
-    const MomType vz = mc_particle->getVertex().z;
-
-    edm4eic::MutableReconstructedParticle rec_part;
-    rec_part.setType(mc_particle->getGeneratorStatus()); // @TODO: determine type codes
-    rec_part.setEnergy(energy_true);
-    rec_part.setMomentum({px, py, pz});
-    rec_part.setReferencePoint({vx, vy, vz}); // @FIXME: probably not what we want?
-    rec_part.setCharge(mc_particle->getCharge());
-    rec_part.setMass(mc_particle->getMass());
-    rec_part.setGoodnessOfPID(1); // Perfect PID
-    rec_part.setCovMatrix({0, 0, 0, 0});
-    rec_part.setPDG(mc_particle->getPDG());
-
-    return new edm4eic::ReconstructedParticle(rec_part);
-
+    return std::move(rec_particles);
 }
