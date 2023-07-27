72182b9682c7a9467298639a6ccca9df911995a6
Wouter Deconinck <wdconinc@gmail.com>
2023-07-27 18:14:27 -0500

    Apply suggestions from code review (spdx headers)
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/calorimetry/CalorimeterHitRecoConfig.h
+++ src/algorithms/calorimetry/CalorimeterHitRecoConfig.h
@@ -1,5 +1,5 @@
-// Subject to the terms in the LICENSE file found in the top-level directory.
-//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Wouter Deconinck
 
 #pragma once
 
