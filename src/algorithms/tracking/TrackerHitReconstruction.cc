6f32297c823e63f4d8480f56fd8de77f8cdb92cb
Wouter Deconinck <wdconinc@gmail.com>
2023-08-10 11:26:15 -0500

    feat: use podio collections in tracking digi/reco; rm unused interfaces

--- src/algorithms/tracking/TrackerHitReconstruction.cc
+++ src/algorithms/tracking/TrackerHitReconstruction.cc
@@ -1,8 +1,9 @@
-// Created by Dmitry Romanov
-// Subject to the terms in the LICENSE file found in the top-level directory.
-//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2022 Whitney Armstrong, Sylvester Joosten, Wouter Deconinck, Dmitry Romanov
 
 #include "TrackerHitReconstruction.h"
+
+namespace eicrecon {
 
 namespace {
     inline double get_resolution(const double pixel_size) {
@@ -15,48 +16,58 @@ namespace {
     }
 } // namespace
 
-void eicrecon::TrackerHitReconstruction::init(dd4hep::Detector* detector, std::shared_ptr<spdlog::logger>& logger) {
+void TrackerHitReconstruction::init(const dd4hep::Detector* detector, std::shared_ptr<spdlog::logger>& logger) {
 
     m_log = logger;
 
     // Create CellID converter
-    m_cellid_converter = std::make_shared<const dd4hep::rec::CellIDPositionConverter>(*detector);
+    m_cellid_converter = std::make_shared<const dd4hep::rec::CellIDPositionConverter>(const_cast<dd4hep::Detector&>(*detector));
 }
 
-edm4eic::TrackerHit *eicrecon::TrackerHitReconstruction::produce(const edm4eic::RawTrackerHit *raw_hit) {
-    constexpr auto mm = dd4hep::mm;
+std::unique_ptr<edm4eic::TrackerHitCollection> TrackerHitReconstruction::process(const edm4eic::RawTrackerHitCollection& raw_hits) {
+    using dd4hep::mm;
 
-    auto id = raw_hit->getCellID();
+    auto rec_hits { std::make_unique<edm4eic::TrackerHitCollection>() };
 
-    // Get position and dimension
-    auto pos = m_cellid_converter->position(id);
-    auto dim = m_cellid_converter->cellDimensions(id);
+    for (const auto& raw_hit : raw_hits) {
 
-    // >oO trace
-    if(m_log->level() == spdlog::level::trace) {
-        m_log->trace("position x={:.2f} y={:.2f} z={:.2f} [mm]: ", pos.x()/ mm, pos.y()/ mm, pos.z()/ mm);
-        m_log->trace("dimension size: {}", dim.size());
-        for (size_t j = 0; j < std::size(dim); ++j) {
-            m_log->trace(" - dimension {:<5} size: {:.2}",  j, dim[j]);
+        auto id = raw_hit.getCellID();
+
+        // Get position and dimension
+        auto pos = m_cellid_converter->position(id);
+        auto dim = m_cellid_converter->cellDimensions(id);
+
+        // >oO trace
+        if(m_log->level() == spdlog::level::trace) {
+            m_log->trace("position x={:.2f} y={:.2f} z={:.2f} [mm]: ", pos.x()/ mm, pos.y()/ mm, pos.z()/ mm);
+            m_log->trace("dimension size: {}", dim.size());
+            for (size_t j = 0; j < std::size(dim); ++j) {
+                m_log->trace(" - dimension {:<5} size: {:.2}",  j, dim[j]);
+            }
         }
+
+        // Note about variance:
+        //    The variance is used to obtain a diagonal covariance matrix.
+        //    Note that the covariance matrix is written in DD4hep surface coordinates,
+        //    *NOT* global position coordinates. This implies that:
+        //      - XY segmentation: xx -> sigma_x, yy-> sigma_y, zz -> 0, tt -> 0
+        //      - XZ segmentation: xx -> sigma_x, yy-> sigma_z, zz -> 0, tt -> 0
+        //      - XYZ segmentation: xx -> sigma_x, yy-> sigma_y, zz -> sigma_z, tt -> 0
+        //    This is properly in line with how we get the local coordinates for the hit
+        //    in the TrackerSourceLinker.
+        rec_hits->create(
+            raw_hit.getCellID(), // Raw DD4hep cell ID
+            edm4hep::Vector3f{static_cast<float>(pos.x() / mm), static_cast<float>(pos.y() / mm), static_cast<float>(pos.z() / mm)}, // mm
+            edm4eic::CovDiag3f{get_variance(dim[0] / mm), get_variance(dim[1] / mm), // variance (see note above)
+            std::size(dim) > 2 ? get_variance(dim[2] / mm) : 0.},
+                static_cast<float>((double)(raw_hit.getTimeStamp()) / 1000.0), // ns
+            m_cfg.timeResolution,                            // in ns
+            static_cast<float>(raw_hit.getCharge() / 1.0e6),   // Collected energy (GeV)
+            0.0F);                                       // Error on the energy
+
     }
 
-    // Note about variance:
-    //    The variance is used to obtain a diagonal covariance matrix.
-    //    Note that the covariance matrix is written in DD4hep surface coordinates,
-    //    *NOT* global position coordinates. This implies that:
-    //      - XY segmentation: xx -> sigma_x, yy-> sigma_y, zz -> 0, tt -> 0
-    //      - XZ segmentation: xx -> sigma_x, yy-> sigma_z, zz -> 0, tt -> 0
-    //      - XYZ segmentation: xx -> sigma_x, yy-> sigma_y, zz -> sigma_z, tt -> 0
-    //    This is properly in line with how we get the local coordinates for the hit
-    //    in the TrackerSourceLinker.
-    return new edm4eic::TrackerHit(
-            raw_hit->getCellID(), // Raw DD4hep cell ID
-         {static_cast<float>(pos.x() / mm), static_cast<float>(pos.y() / mm), static_cast<float>(pos.z() / mm)}, // mm
-         {get_variance(dim[0] / mm), get_variance(dim[1] / mm), // variance (see note above)
-          std::size(dim) > 2 ? get_variance(dim[2] / mm) : 0.},
-            static_cast<float>((double)(raw_hit->getTimeStamp()) / 1000.0), // ns
-         m_cfg.time_resolution,                            // in ns
-         static_cast<float>(raw_hit->getCharge() / 1.0e6),   // Collected energy (GeV)
-         0.0F);                                       // Error on the energy
+    return std::move(rec_hits);
 }
+
+} // namespace eicrecon
