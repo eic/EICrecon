b89b07d1eecfa2a254da8f3ed7599b9b911cb36e
Wouter Deconinck <wdconinc@gmail.com>
2023-08-13 23:17:51 -0500

    fix: move jana ticker pausing from CKFTracking into ActsGeo service (#846)
    ### Briefly, what does this PR introduce?
    The reason for pausing the jana ticker is because the ActsGeo takes long
    to initialize. This moves the pausing to that initialization.
    
    Also, opportunistically:
    - renamed class member `init_flag` to `m_init_flag` for consistency,
    - removed commented lines,
    - corrected license header for explicitly derived code.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: logically incorrect location for addressing this
    behavior)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No. Maybe during long-duration tracking steps, there will now be tickers
    shown whereas previously they were suppressed completely during all
    tracking.

--- src/global/tracking/CKFTracking_factory.cc
+++ src/global/tracking/CKFTracking_factory.cc
@@ -85,13 +85,6 @@ void eicrecon::CKFTracking_factory::Process(const std::shared_ptr<const JEvent> 
         acts_track_params.emplace_back(pSurface, params, charge, cov);
     }
 
-    // Reading the geometry may take a long time and if the JANA ticker is enabled, it will keep printing
-    // while no other output is coming which makes it look like something is wrong. Disable the ticker
-    // while parsing and loading the geometry
-    auto tickerEnabled = GetApplication()->IsTickerEnabled();
-    GetApplication()->SetTicker( false );
-
-
     // Convert vector of source links to a sorted in geometry order container used in tracking
     eicrecon::IndexSourceLinkContainer source_links;
     auto measurements_ptr = source_linker_result->measurements;
@@ -125,7 +118,4 @@ void eicrecon::CKFTracking_factory::Process(const std::shared_ptr<const JEvent> 
     catch(std::exception &e) {
         throw JException(e.what());
     }
-
-    // Enable ticker back
-    GetApplication()->SetTicker(tickerEnabled);
 }
