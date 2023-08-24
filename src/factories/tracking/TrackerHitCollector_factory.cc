2ef30ad5e7fd2902502580e61159a1055995a75d
Wouter Deconinck <wdconinc@gmail.com>
2023-08-24 12:31:45 -0500

    fix: restore merging of hits in TrackerHitCollector
    This restores merging of all tracker hits in the TrackerHitCollector. It
    does not assume that all collections are present but empty if not
    supported by the geometry configuration. Instead, it catches the
    exception on JEvent::GetCollectionBase if a podio collection is
    requested that does not exist, and continues with the other (existing)
    podio collections. The transition to podio collections upstream, in
    
    This also reworks the factory to the new multifactory approach, moves it
    into src/factories, and uses the modular algorithms for the operations
    on podio collections instead of replicating the functionality in the
    factory.

---
+++ src/factories/tracking/TrackerHitCollector_factory.cc
@@ -0,0 +1,43 @@
+// Created by Dmitry Romanov
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#include <JANA/JEvent.h>
+#include <edm4eic/TrackerHitCollection.h>
+
+#include "TrackerHitCollector_factory.h"
+#include "services/log/Log_service.h"
+#include "extensions/spdlog/SpdlogExtensions.h"
+
+namespace eicrecon {
+    void TrackerHitCollector_factory::Init() {
+        auto app = GetApplication();
+
+        // SpdlogMixin logger initialization, sets m_log
+        InitLogger(app, GetPrefix(), "info");
+
+        m_algo.init(logger());
+    }
+
+    void TrackerHitCollector_factory::Process(const std::shared_ptr<const JEvent> &event) {
+        std::vector<const edm4eic::TrackerHitCollection*> hit_collections;
+        for (const auto& input_tag : GetInputTags()) {
+            try {
+                hit_collections.emplace_back(static_cast<const edm4eic::TrackerHitCollection*>(event->GetCollectionBase(input_tag)));
+            }
+            catch(std::exception &e) {
+                // ignore missing collections, but print them in debug mode
+                m_log->debug(e.what());
+            }
+        }
+
+        try {
+            auto hits = m_algo.process(hit_collections);
+            SetCollection<edm4eic::TrackerHit>(GetOutputTags()[0], std::move(hits));
+        }
+        catch(std::exception &e) {
+            throw JException(e.what());
+        }
+    }
+
+} // eicrecon
---
+++ src/factories/tracking/TrackerHitCollector_factory.h
@@ -0,0 +1,75 @@
+// Created by Dmitry Romanov
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#pragma once
+
+#include <spdlog/logger.h>
+#include <edm4eic/TrackerHit.h>
+#include "extensions/jana/JChainMultifactoryT.h"
+#include "extensions/spdlog/SpdlogMixin.h"
+#include "algorithms/tracking/TrackerHitCollector.h"
+
+namespace eicrecon {
+
+/// This factory just collects reconstructed hits edm4eic::TrackerHit from different sources
+/// And makes a single array out of them
+class TrackerHitCollector_factory :
+    public JChainMultifactoryT<NoConfig>,
+    public SpdlogMixin {
+
+  public:
+    explicit TrackerHitCollector_factory(
+        std::string tag,
+        const std::vector<std::string>& input_tags,
+        const std::vector<std::string>& output_tags)
+    : JChainMultifactoryT<NoConfig>(std::move(tag), input_tags, output_tags) {
+
+      bool owns_data = false; // this produces a subset collection
+      DeclarePodioOutput<edm4eic::TrackerHit>(GetOutputTags()[0], owns_data);
+
+    }
+
+    /** One time initialization **/
+    void Init() override {
+      auto app = GetApplication();
+
+      // SpdlogMixin logger initialization, sets m_log
+      InitLogger(app, GetPrefix(), "info");
+
+      m_algo.init(logger());
+    }
+
+    /** Event by event processing **/
+    void Process(const std::shared_ptr<const JEvent> &event) override {
+
+      std::vector<const edm4eic::TrackerHitCollection*> hit_collections;
+      for (const auto& input_tag : GetInputTags()) {
+        // TODO: we could avoid exceptions here by using JEvent::GetAllCollectionNames,
+        // but until that returns a std::set and we can use c++20 contains, there is
+        // not that much benefit to that approach since it requires searching through
+        // a std::vector of names.
+        try {
+          hit_collections.emplace_back(static_cast<const edm4eic::TrackerHitCollection*>(event->GetCollectionBase(input_tag)));
+        }
+        catch(std::exception &e) {
+          // ignore missing collections, but notify them in debug mode
+          m_log->debug(e.what());
+        }
+      }
+
+      try {
+        auto hits = m_algo.process(hit_collections);
+        SetCollection<edm4eic::TrackerHit>(GetOutputTags()[0], std::move(hits));
+      }
+      catch(std::exception &e) {
+        throw JException(e.what());
+      }
+    }
+
+  private:
+    TrackerHitCollector m_algo;
+
+  };
+
+} // eicrecon
