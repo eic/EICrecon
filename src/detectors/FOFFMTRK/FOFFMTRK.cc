f2a037fbef2853564b30b869590bc689c40b81ac
simonge <simongardner1@hotmail.com>
2023-08-31 10:38:32 +0100

    Changed comment to Roman Pots

--- src/detectors/FOFFMTRK/FOFFMTRK.cc
+++ src/detectors/FOFFMTRK/FOFFMTRK.cc
@@ -19,17 +19,17 @@ void InitPlugin(JApplication *app) {
 
     MatrixTransferStaticConfig recon_cfg;
 
-    //Static transport matrix for OffMomentum detectors
+    //Static transport matrix for Off Momentum detectors
     recon_cfg.aX = {{1.6248, 12.966293},
 		    {0.1832, -2.8636535}};
     recon_cfg.aY = {{0.0001674, -28.6003},
 		    {0.0000837, -2.87985}};
 
     recon_cfg.local_x_offset       = -11.9872;  // in mm --> this is from mis-alignment of the detector
-    recon_cfg.local_y_offset       = -0.0146;   //in mm --> this is from mis-alignment of the detector
-    recon_cfg.local_x_slope_offset = -14.75315; //in mrad
-    recon_cfg.local_y_slope_offset = -0.0073;   //in mrad
-    recon_cfg.nomMomentum          =  137.5;    //in GEV --> exactly half of the top energy momentum (for proton spectators from deuteron breakup)
+    recon_cfg.local_y_offset       = -0.0146;   // in mm --> this is from mis-alignment of the detector
+    recon_cfg.local_x_slope_offset = -14.75315; // in mrad
+    recon_cfg.local_y_slope_offset = -0.0073;   // in mrad
+    recon_cfg.nomMomentum          =  137.5;    // in GEV --> exactly half of the top energy momentum (for proton spectators from deuteron breakup)
 
     recon_cfg.hit1minZ = 22499.0;
     recon_cfg.hit1maxZ = 22522.0;
--- src/detectors/RPOTS/RPOTS.cc
+++ src/detectors/RPOTS/RPOTS.cc
@@ -19,17 +19,17 @@ void InitPlugin(JApplication *app) {
 
     MatrixTransferStaticConfig recon_cfg;
 
-    //Static transport matrix for OffMomentum detectors
+    //Static transport matrix for Roman Pots detectors
     recon_cfg.aX = {{2.102403743, 29.11067626},
 		    {0.186640381, 0.192604619}};
     recon_cfg.aY = {{0.0000159900, 3.94082098},
 		    {0.0000079946, -0.1402995}};
 
-    recon_cfg.local_x_offset       =  0.0;        //in mm --> this is from mis-alignment of the detector
-    recon_cfg.local_y_offset       =  0.0;        //in mm --> this is from mis-alignment of the detector
-    recon_cfg.local_x_slope_offset = -0.00622147; //in mrad
-    recon_cfg.local_y_slope_offset = -0.0451035;  //in mrad
-    recon_cfg.nomMomentum          =  275.0;      //in GEV --> exactly half of the top energy momentum (for proton spectators from deuteron breakup)
+    recon_cfg.local_x_offset       =  0.0;        // in mm --> this is from mis-alignment of the detector
+    recon_cfg.local_y_offset       =  0.0;        // in mm --> this is from mis-alignment of the detector
+    recon_cfg.local_x_slope_offset = -0.00622147; // in mrad
+    recon_cfg.local_y_slope_offset = -0.0451035;  // in mrad
+    recon_cfg.nomMomentum          =  275.0;      // in GEV --> exactly half of the top energy momentum (for proton spectators from deuteron breakup)
 
     recon_cfg.hit1minZ = 25099.0;
     recon_cfg.hit1maxZ = 26022.0;
