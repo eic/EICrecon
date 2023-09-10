ab26d5be8c4e26933172e3e5992b363083d449c8
Wouter Deconinck <wdconinc@gmail.com>
2023-09-10 12:35:20 -0500

    fix: split EcalBarrelScFiCheck plugin code from processor code

---
+++ src/benchmarks/detectors/EcalBarrelScFiCheck/EcalBarrelScFiCheck.cc
@@ -0,0 +1,10 @@
+#include "EcalBarrelScFiCheckProcessor.h"
+
+// The following just makes this a JANA plugin
+extern "C" {
+    void InitPlugin(JApplication *app) {
+        InitJANAPlugin(app);
+        app->Add(new EcalBarrelScFiCheckProcessor);
+    }
+}
+
--- src/benchmarks/detectors/EcalBarrelScFiCheck/EcalBarrelScFiCheckProcessor.cc
+++ src/benchmarks/detectors/EcalBarrelScFiCheck/EcalBarrelScFiCheckProcessor.cc
@@ -8,14 +8,6 @@
 
 #include <Evaluator/DD4hepUnits.h>
 
-// The following just makes this a JANA plugin
-extern "C" {
-    void InitPlugin(JApplication *app) {
-        InitJANAPlugin(app);
-        app->Add(new EcalBarrelScFiCheckProcessor);
-    }
-}
-
 //-------------------------------------------
 // InitWithGlobalRootLock
 //-------------------------------------------
