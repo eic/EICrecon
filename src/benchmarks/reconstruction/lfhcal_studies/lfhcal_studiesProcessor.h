6754e9d35dfbef1a71ea32f4f667145a0fce076a
Wouter Deconinck <wdconinc@gmail.com>
2023-09-12 17:04:43 -0500

    fix: use dd4hep::DDSegmentation::BitFieldCoder in benchmarks

--- src/benchmarks/reconstruction/lfhcal_studies/lfhcal_studiesProcessor.h
+++ src/benchmarks/reconstruction/lfhcal_studies/lfhcal_studiesProcessor.h
@@ -95,7 +95,7 @@ public:
     int nEventsWithCaloHits = 0;
     bool isLFHCal = true;
     std::shared_ptr<spdlog::logger> m_log;
-    dd4hep::BitFieldCoder* m_decoder;
+    dd4hep::DDSegmentation::BitFieldCoder* m_decoder;
     std::string nameSimHits         = "LFHCALHits";
     std::string nameRecHits         = "LFHCALRecHits";
     std::string nameClusters        = "LFHCALClusters";
