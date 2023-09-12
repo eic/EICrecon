eic/EICrecon

commit 2c7550a21588f59bf889253f32a0dfeb43ccdeaa

reco: add *ChargedJets (#881)
### Briefly, what does this PR introduce?
This adds charged jets that may be more usable for physics studies until
hadronic subtraction is implemented.

### What kind of change does this PR introduce?
- [ ] Bug fix (issue #__)
- [ ] New feature (issue #__)
- [ ] Documentation update
- [ ] Other: __

### Please check if this PR fulfills the following:
- [ ] Tests for the changes have been added
- [ ] Documentation has been added / updated
- [x] Changes have been communicated to collaborators @ruse-traveler
@bspage912

### Does this PR introduce breaking changes? What changes might users
need to make to their code?

### Does this PR change default behavior?

src/algorithms/reco/ChargedParticleSelector.h
@@ -1,0 +1,28 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Dmitry Kalinkin
+
+#pragma once
+
+#include <memory>
+
+#include <edm4hep/MCParticleCollection.h>
+#include <spdlog/logger.h>
+
+#include "algorithms/interfaces/WithPodConfig.h"
+
+namespace eicrecon {
+
+  class ChargedParticleSelector : public WithPodConfig<NoConfig> {
+
+  private:
+
+    std::shared_ptr<spdlog::logger> m_log;
+
+  public:
+
+    void init(std::shared_ptr<spdlog::logger>& logger);
+    std::unique_ptr<edm4hep::MCParticleCollection> process(const edm4hep::MCParticleCollection &inputs);
+
+  };
+
+}

src/algorithms/reco/ChargedParticleSelector.cxx
@@ -1,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Dmitry Kalinkin
+
+#pragma once
+
+#include "ChargedParticleSelector.h"
+
+namespace eicrecon {
+
+  void ChargedParticleSelector::init(std::shared_ptr<spdlog::logger>& logger) {
+    m_log = logger;
+  }
+
+  std::unique_ptr<edm4hep::MCParticleCollection> ChargedParticleSelector::process(const edm4hep::MCParticleCollection &inputs) {
+    auto outputs = std::make_unique<edm4hep::MCParticleCollection>();
+    output->setSubsetCollection();
+
+    for (cont auto &particle : inputs) {
+      if (particle.getCharge() != 0.) {
+        outputs.push_back(particle);
+      }
+    }
+
+    return std::move(outputs)
+  }
+
+}

src/global/reco/ChargedParticleSelector_factory.h
@@ -1,0 +1,60 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Wouter Deconinck, Dmitry Kalinkin
+
+#include <vector>
+
+#include <spdlog/logger.h>
+#include <edm4hep/MCParticle.h>
+
+#include "algorithms/reco/ChargedParticleSelector.h"
+#include "extensions/jana/JChainMultifactoryT.h"
+#include "extensions/spdlog/SpdlogMixin.h"
+
+namespace eicrecon {
+
+/// This factory selects a subset of particles that are charged
+class ChargedParticleSelector_factory :
+    public JChainMultifactoryT<NoConfig>,
+    public SpdlogMixin {
+
+  public:
+    explicit ChargedParticleSelector_factory(
+        std::string tag,
+        const std::vector<std::string>& input_tags,
+        const std::vector<std::string>& output_tags)
+    : JChainMultifactoryT<NoConfig>(std::move(tag), input_tags, output_tags) {
+
+      bool owns_data = false; // this produces a subset collection
+      DeclarePodioOutput<edm4hep::MCParticle>(GetOutputTags()[0], owns_data);
+
+    }
+
+    /** One time initialization **/
+    void Init() override {
+      auto app = GetApplication();
+
+      // SpdlogMixin logger initialization, sets m_log
+      InitLogger(app, GetPrefix(), "info");
+
+      m_algo.init(logger());
+    }
+
+    /** Event by event processing **/
+    void Process(const std::shared_ptr<const JEvent> &event) override {
+      auto particles = static_cast<const edm4hep::MCParticleCollection*>(event->GetCollectionBase(GetInputTags()[0]));
+
+      try {
+        auto charged_particles = m_algo.process(*particles);
+        SetCollection<edm4hep::MCParticle>(GetOutputTags()[0], std::move(charged_particles));
+      }
+      catch(std::exception &e) {
+        throw JException(e.what());
+      }
+    }
+
+  private:
+    ChargedParticleSelector m_algo;
+
+  };
+
+}

src/global/reco/reco.cc
@@ -9,6 +9,7 @@
 #include "extensions/jana/JChainFactoryGeneratorT.h"
 #include "extensions/jana/JChainMultifactoryGeneratorT.h"
 
+#include "ChargedParticleSelector_factory.h"
 #include "MC2SmearedParticle_factory.h"
 #include "MatchClusters_factory.h"
 #include "InclusiveKinematicsElectron_factory.h"
@@ -79,11 +80,34 @@ void InitPlugin(JApplication *app) {
             {"GeneratedJets"},
             app
     ));
+
     app->Add(new JChainMultifactoryGeneratorT<ReconstructedJets_factory>(
             "ReconstructedJets",
             {"ReconstructedParticles"},
             {"ReconstructedJets"},
             app
     ));
+
+    app->Add(new JChainMultifactoryGeneratorT<ChargedParticleSelector_factory>(
+            "MCChargedParticles",
+            {"MCParticles"},
+            {"MCChargedParticles"},
+            app
+    ));
+
+    app->Add(new JChainMultifactoryGeneratorT<GeneratedJets_factory>(
+            "GeneratedChargedJets",
+            {"MCChargedParticles"},
+            {"GeneratedChargedJets"},
+            app
+    ));
+
+    app->Add(new JChainMultifactoryGeneratorT<ReconstructedJets_factory>(
+            "ReconstructedChargedJets",
+            {"ReconstructedChargedParticles"},
+            {"ReconstructedChargedJets"},
+            app
+    ));
+
 }
 } // extern "C"

commit 81c3cadbc1d881f40b8ac28a58f69c98f905a5a0

ChargedParticleSelector: fix compilation (#945)
Fixes: #944 
Fixes: 2c7550a21588f59bf889253f32a0dfeb43ccdeaa (reco: add *ChargedJets
(#881))

---------

Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

src/algorithms/reco/ChargedParticleSelector.cc
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2023 Dmitry Kalinkin
 
-#pragma once
-
 #include "ChargedParticleSelector.h"
 
 namespace eicrecon {
@@ -13,15 +11,15 @@ namespace eicrecon {
 
   std::unique_ptr<edm4hep::MCParticleCollection> ChargedParticleSelector::process(const edm4hep::MCParticleCollection &inputs) {
     auto outputs = std::make_unique<edm4hep::MCParticleCollection>();
-    output->setSubsetCollection();
+    outputs->setSubsetCollection();
 
-    for (cont auto &particle : inputs) {
+    for (const auto &particle : inputs) {
       if (particle.getCharge() != 0.) {
-        outputs.push_back(particle);
+        outputs->push_back(particle);
       }
     }
 
-    return std::move(outputs)
+    return std::move(outputs);
   }
 
 }

src/algorithms/reco/ChargedParticleSelector.h
@@ -8,11 +8,9 @@
 #include <edm4hep/MCParticleCollection.h>
 #include <spdlog/logger.h>
 
-#include "algorithms/interfaces/WithPodConfig.h"
-
 namespace eicrecon {
 
-  class ChargedParticleSelector : public WithPodConfig<NoConfig> {
+  class ChargedParticleSelector {
 
   private:
 

src/services/io/podio/JEventProcessorPODIO.cc
@@ -98,7 +98,9 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
             "InclusiveKinematicsElectron",
             "InclusiveKinematicsTruth",
             "GeneratedJets",
+            "GeneratedChargedJets",
             "ReconstructedJets",
+            "ReconstructedChargedJets",
             "ReconstructedElectrons",
 
             // Track projections