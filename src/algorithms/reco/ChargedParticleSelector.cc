b890bfe9b85f9233c96bfb132a393dcf68e4575b
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-09-13 09:31:54 -0400

    [Backport v1.5] ChargedParticleSelector: fix compilation (#953)
    # Description
    Backport of #945 to `v1.5`.
    
    like #952
    
    ---------
    
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- src/algorithms/reco/ChargedParticleSelector.cc
+++ src/algorithms/reco/ChargedParticleSelector.cc
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2023 Dmitry Kalinkin
 
-#pragma once
-
 #include "ChargedParticleSelector.h"
 
 namespace eicrecon {
@@ -13,15 +11,15 @@ namespace eicrecon {
 
   std::unique_ptr<edm4hep::MCParticleCollection> ChargedParticleSelector::process(const edm4hep::MCParticleCollection &inputs) {
     auto outputs = std::make_unique<edm4hep::MCParticleCollection>();
-    output->setSubsetCollection();
+    outputs->setSubsetCollection();
 
-    for (cont auto &particle : inputs) {
+    for (const auto &particle : inputs) {
       if (particle.getCharge() != 0.) {
-        outputs.push_back(particle);
+        outputs->push_back(particle);
       }
     }
 
-    return std::move(outputs)
+    return std::move(outputs);
   }
 
 }
