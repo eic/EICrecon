c1b4fa3b4bb38d1eef493867c3fafefa019f1dbc
Shyam Kumar <shyam055119@gmail.com>
2023-09-14 19:29:41 +0200

    Updated file with the implemented comments.
    I implemented the comments. I am still using tree formula but I added a timer and checked it's running faster than Tree Reader.

--- src/benchmarks/reconstruction/tracking_efficiency/scripts/Plot_eta.C
+++ src/benchmarks/reconstruction/tracking_efficiency/scripts/Plot_eta.C
@@ -1,12 +1,12 @@
 // Eta coverage of detectors
 // Shyam Kumar:INFN Bari, shyam.kumar@ba.infn.it; shyam055119@gmail.com
 
-#include <TGraphErrors.h>
-#include <TF1.h>
-#include <TRandom.h>
-#include <TCanvas.h>
-#include <TLegend.h>
-#include <TMath.h>
+#include "TGraphErrors.h"
+#include "TF1.h"
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TMath.h"
 #define mpi 0.139  // 1.864 GeV/c^2
 
 void Plot_eta()
@@ -26,75 +26,112 @@ void Plot_eta()
    TFile *f = TFile::Open("sim.edm4hep.root");
    TTree *tree = (TTree*)f->Get("events");
 
+ // Timer Start
+  TStopwatch timer;
+  timer.Start();
+
    TCanvas * c1 = new TCanvas("c1","coutput",1400,1000);
    c1->SetMargin(0.10, 0.05 ,0.1,0.05);
    c1->SetGridx();
 
-   TH1D *h0 = new TH1D("h0","h0",200,-4.,4.);
-   TH1D *h1 = new TH1D("h1","h1",200,-4.,4.); // Barr1
-   TH1D *h2 = new TH1D("h2","h2",200,-4.,4.); // BMT1
-   TH1D *h3 = new TH1D("h3","h3",200,-4.,4.);
-   TH1D *h4 = new TH1D("h4","h4",200,-4.,4.);
-   TH1D *h5 = new TH1D("h5","h5",200,-4.,4.);
+   TH1D *hits_vtx_si = new TH1D("hits_vtx_si","hits_vtx_si",200,-4.,4.);
+   TH1D *hits_barrel_si = new TH1D("hits_barrel_si","hits_barrel_si",200,-4.,4.); // Barr1
+   TH1D *hits_barrel_mpgd_in = new TH1D("hits_barrel_mpgd_in","hits_barrel_mpgd_in",200,-4.,4.); // BMT1
+   TH1D *hits_barrel_tof = new TH1D("hits_barrel_tof","hits_barrel_tof",200,-4.,4.);
+   TH1D *hits_disks_si = new TH1D("hits_disks_si","hits_disks_si",200,-4.,4.);
+   TH1D *hits_endcap_tof = new TH1D("hits_endcap_tof","hits_endcap_tof",200,-4.,4.);
+   TH1D *hits_barrel_mpgd_out = new TH1D("hits_barrel_mpgd_out","hits_barrel_mpgd_out",200,-4.,4.);
+   TH1D *hits_fwd_mpgd = new TH1D("hits_fwd_mpgd","hits_fwd_mpgd",200,-4.,4.);
+   TH1D *hits_bwd_mpgd = new TH1D("hits_bwd_mpgd","hits_bwd_mpgd",200,-4.,4.);
+   TH1D *hits_b0tracker = new TH1D("hits_b0tracker","hits_b0tracker",200,-4.,4.);
 
+   hits_vtx_si->GetXaxis()->SetTitle("#eta");
+   hits_vtx_si->GetYaxis()->SetTitle("Entries (a.u.)");
+   hits_vtx_si->GetXaxis()->CenterTitle();
+   hits_vtx_si->GetYaxis()->CenterTitle();
 
-   h0->GetXaxis()->SetTitle("#eta");
-   h0->GetYaxis()->SetTitle("Entries (a.u.)");
-   h0->GetXaxis()->CenterTitle();
-   h0->GetYaxis()->CenterTitle();
-
-   TString vtx_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(VertexBarrelHits.position.x*VertexBarrelHits.position.x+VertexBarrelHits.position.y*VertexBarrelHits.position.y),VertexBarrelHits.position.z))/2))>>h0";
-   TString Barrel_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(SiBarrelHits.position.x*SiBarrelHits.position.x+SiBarrelHits.position.y*SiBarrelHits.position.y),SiBarrelHits.position.z))/2))>>h1";
-   TString BMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(MPGDBarrelHits.position.x*MPGDBarrelHits.position.x+MPGDBarrelHits.position.y*MPGDBarrelHits.position.y),MPGDBarrelHits.position.z))/2))>>h2";
-   TString ToF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFBarrelHits.position.x*TOFBarrelHits.position.x+TOFBarrelHits.position.y*TOFBarrelHits.position.y),TOFBarrelHits.position.z))/2))>>h3";
-   TString ETracker_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TrackerEndcapHits.position.x*TrackerEndcapHits.position.x+TrackerEndcapHits.position.y*TrackerEndcapHits.position.y),TrackerEndcapHits.position.z))/2))>>h4";
-   TString ETOF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFEndcapHits.position.x*TOFEndcapHits.position.x+TOFEndcapHits.position.y*TOFEndcapHits.position.y),TOFEndcapHits.position.z))/2))>>h5";
-
+   TString vtx_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(VertexBarrelHits.position.x*VertexBarrelHits.position.x+VertexBarrelHits.position.y*VertexBarrelHits.position.y),VertexBarrelHits.position.z))/2))>>hits_vtx_si";
+   TString Barrel_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(SiBarrelHits.position.x*SiBarrelHits.position.x+SiBarrelHits.position.y*SiBarrelHits.position.y),SiBarrelHits.position.z))/2))>>hits_barrel_si";
+   TString BMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(MPGDBarrelHits.position.x*MPGDBarrelHits.position.x+MPGDBarrelHits.position.y*MPGDBarrelHits.position.y),MPGDBarrelHits.position.z))/2))>>hits_barrel_mpgd_in";
+   TString ToF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFBarrelHits.position.x*TOFBarrelHits.position.x+TOFBarrelHits.position.y*TOFBarrelHits.position.y),TOFBarrelHits.position.z))/2))>>hits_barrel_tof";
+   TString ETracker_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TrackerEndcapHits.position.x*TrackerEndcapHits.position.x+TrackerEndcapHits.position.y*TrackerEndcapHits.position.y),TrackerEndcapHits.position.z))/2))>>hits_disks_si";
+   TString ETOF_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(TOFEndcapHits.position.x*TOFEndcapHits.position.x+TOFEndcapHits.position.y*TOFEndcapHits.position.y),TOFEndcapHits.position.z))/2))>>hits_endcap_tof";
+   TString OutBMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(OuterMPGDBarrelHits.position.x*OuterMPGDBarrelHits.position.x+OuterMPGDBarrelHits.position.y*OuterMPGDBarrelHits.position.y),OuterMPGDBarrelHits.position.z))/2))>>hits_barrel_mpgd_out";
+   TString FwdMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(ForwardMPGDEndcapHits.position.x*ForwardMPGDEndcapHits.position.x+ForwardMPGDEndcapHits.position.y*ForwardMPGDEndcapHits.position.y),ForwardMPGDEndcapHits.position.z))/2))>>hits_fwd_mpgd";
+   TString BwdMM_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(BackwardMPGDEndcapHits.position.x*BackwardMPGDEndcapHits.position.x+BackwardMPGDEndcapHits.position.y*BackwardMPGDEndcapHits.position.y),BackwardMPGDEndcapHits.position.z))/2))>>hits_bwd_mpgd";
+   TString B0_hits = "-TMath::Log(TMath::Tan((TMath::ATan2(sqrt(B0TrackerHits.position.x*B0TrackerHits.position.x+B0TrackerHits.position.y*B0TrackerHits.position.y),B0TrackerHits.position.z))/2))>>hits_b0tracker";
 
    tree->Draw(vtx_hits.Data(),"VertexBarrelHits.position.y>0","goff"); // Vtx layers
-   tree->Draw(Barrel_hits.Data(),"SiBarrelHits.position.y>0 ","goff"); // Barrel layers
-   tree->Draw(BMM_hits.Data(),"MPGDBarrelHits.position.y>0 ","goff"); // Barrel layers
-   tree->Draw(ToF_hits.Data(),"TOFBarrelHits.position.y>0","goff"); // BMT layers1
-   tree->Draw(ETracker_hits.Data(),"TrackerEndcapHits.position.y>0","goff"); // BMT layers1
-   tree->Draw(ETOF_hits.Data(),"TOFEndcapHits.position.y>0","goff"); // DIRC layers
-
-
+   tree->Draw(Barrel_hits.Data(),"SiBarrelHits.position.y>0 ","goff"); // Barrel Si layers
+   tree->Draw(BMM_hits.Data(),"MPGDBarrelHits.position.y>0 ","goff"); // MPGD Barrel
+   tree->Draw(ToF_hits.Data(),"TOFBarrelHits.position.y>0","goff"); // TOF Hits
+   tree->Draw(ETracker_hits.Data(),"TrackerEndcapHits.position.y>0","goff"); // Tracker Endcap
+   tree->Draw(ETOF_hits.Data(),"TOFEndcapHits.position.y>0","goff"); // TOF Endcap
+   tree->Draw(OutBMM_hits.Data(),"OuterMPGDBarrelHits.position.y>0","goff"); // Outer Barrel MPGD
+   tree->Draw(FwdMM_hits.Data(),"ForwardMPGDEndcapHits.position.y>0","goff"); // Forward MPGD
+   tree->Draw(BwdMM_hits.Data(),"BackwardMPGDEndcapHits.position.y>0","goff"); // Forward MPGD
+   tree->Draw(B0_hits.Data(),"B0TrackerHits.position.y>0","goff"); // B0 Tracker
+  
    c1->cd();
    c1->SetLogy();
-   h0->Scale(1./h0->Integral());
-   h1->Scale(1./h1->Integral());
-   h2->Scale(1./h2->Integral());
-   h3->Scale(1./h3->Integral());
-   h4->Scale(1./h4->Integral());
-   h5->Scale(1./h5->Integral());
-   h0->SetLineColor(kBlue);
-   h1->SetLineColor(kMagenta);
-   h2->SetLineColor(kRed);
-   h3->SetLineColor(kBlack);
-   h4->SetLineColor(kGreen);
-   h5->SetLineColor(kCyan);
+   hits_vtx_si->Scale(1./hits_vtx_si->Integral());
+   hits_barrel_si->Scale(1./hits_barrel_si->Integral());
+   hits_barrel_mpgd_in->Scale(1./hits_barrel_mpgd_in->Integral());
+   hits_barrel_tof->Scale(1./hits_barrel_tof->Integral());
+   hits_disks_si->Scale(1./hits_disks_si->Integral());
+   hits_endcap_tof->Scale(1./hits_endcap_tof->Integral());
+   hits_barrel_mpgd_out->Scale(1./hits_barrel_mpgd_out->Integral());
+   hits_fwd_mpgd->Scale(1./hits_fwd_mpgd->Integral());
+   hits_bwd_mpgd->Scale(1./hits_bwd_mpgd->Integral());
+   hits_b0tracker->Scale(1./hits_b0tracker->Integral());
+   hits_vtx_si->SetLineColor(kBlue);
+   hits_barrel_si->SetLineColor(kMagenta);
+   hits_barrel_mpgd_in->SetLineColor(kRed);
+   hits_barrel_tof->SetLineColor(kBlack);
+   hits_disks_si->SetLineColor(kGreen);
+   hits_endcap_tof->SetLineColor(kCyan);
+   hits_barrel_mpgd_out->SetLineColor(9);
+   hits_fwd_mpgd->SetLineColor(28);
+   hits_bwd_mpgd->SetLineColor(46);
+   hits_b0tracker->SetLineColor(38);
 
 
-   h0->SetMaximum(0.1);
-   h0->GetXaxis()->SetRangeUser(-4.0,4.0);
-   h0->Draw("hist");
-   h1->Draw("hist-same");
-   h2->Draw("hist-same");
-   h3->Draw("hist-same");
-   h4->Draw("hist-same");
-   h5->Draw("hist-same");
+   hits_vtx_si->SetMaximum(0.2);
+   hits_vtx_si->GetXaxis()->SetRangeUser(-4.0,4.0);
+   hits_vtx_si->Draw("hist");
+   hits_barrel_si->Draw("hist-same");
+   hits_barrel_mpgd_in->Draw("hist-same");
+   hits_barrel_tof->Draw("hist-same");
+   hits_disks_si->Draw("hist-same");
+   hits_endcap_tof->Draw("hist-same");
+   hits_barrel_mpgd_out->Draw("hist-same");
+   hits_fwd_mpgd->Draw("hist-same");
+   hits_bwd_mpgd->Draw("hist-same");
+   hits_b0tracker->Draw("hist-same");
 
-  TLegend *l1= new TLegend(0.70,0.75,0.90,0.93);
-  l1->SetTextSize(0.03);
+  TLegend *l1=  new TLegend(0.11,0.70,0.70,0.90);
+  l1->SetNColumns(2);
+  l1->SetTextSize(0.025);
   l1->SetBorderSize(0);
-  l1->AddEntry(h0,"VertexBarrelHits");
-  l1->AddEntry(h1,"SiBarrelHits");
-  l1->AddEntry(h2,"MPGDBarrelHits");
-  l1->AddEntry(h3,"TOFBarrelHits");
-  l1->AddEntry(h4,"TrackerEndcapHits");
-  l1->AddEntry(h5,"TOFEndcapHits");
+  l1->AddEntry(hits_vtx_si,"VertexBarrelHits");
+  l1->AddEntry(hits_barrel_si,"SiBarrelHits");
+  l1->AddEntry(hits_barrel_mpgd_in,"MPGDBarrelHits");
+  l1->AddEntry(hits_barrel_tof,"TOFBarrelHits");
+  l1->AddEntry(hits_disks_si,"TrackerEndcapHits");
+  l1->AddEntry(hits_endcap_tof,"TOFEndcapHits");
+  l1->AddEntry(hits_barrel_mpgd_out,"OuterMPGDBarrelHits");
+  l1->AddEntry(hits_fwd_mpgd,"ForwardMPGDEndcapHits");
+  l1->AddEntry(hits_bwd_mpgd,"BackwardMPGDEndcapHits");
+  l1->AddEntry(hits_b0tracker,"B0TrackerHits");
+  l1->Draw("same");
   c1->cd();
   l1->Draw();
   c1->SaveAs("eta_DD4HEP.png");
+  
+ // Timer Stop     
+  timer.Stop();
+  Double_t realtime = timer.RealTime();
+  Double_t cputime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",realtime,cputime);  
 
 }
