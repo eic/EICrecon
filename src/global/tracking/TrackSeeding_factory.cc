eic/EICrecon

commit 959979cbb7c9e4f03d093b8424bcd81bc6b8d248

fix: address memory leaks in TrackSeeding (#983)
### Briefly, what does this PR introduce?
This converts the TrackSeeding to podio collections, thereby fixing
major memory leaks in all three because of not deleting of pointers in
std::vectors.

### What kind of change does this PR introduce?
- [x] Bug fix (issue: memory leaks)
- [ ] New feature (issue #__)
- [ ] Documentation update
- [ ] Other: __

### Please check if this PR fulfills the following:
- [ ] Tests for the changes have been added
- [ ] Documentation has been added / updated
- [ ] Changes have been communicated to collaborators

### Does this PR introduce breaking changes? What changes might users
need to make to their code?
No.

### Does this PR change default behavior?
No.

src/global/tracking/TrackSeeding_factory.cc
@@ -4,8 +4,6 @@
 
 #include <JANA/JEvent.h>
 
-#include <edm4eic/TrackParametersCollection.h>
-
 #include "TrackSeeding_factory.h"
 #include "extensions/spdlog/SpdlogExtensions.h"
 #include "services/geometry/acts/ACTSGeo_service.h"
@@ -63,20 +61,19 @@ void eicrecon::TrackSeeding_factory::ChangeRun(const std::shared_ptr<const JEven
 
 void eicrecon::TrackSeeding_factory::Process(const std::shared_ptr<const JEvent> &event) {
     // Collect all hits
-    std::vector<const edm4eic::TrackerHit*> total_hits;
+    // FIXME Collection is better done with a TrackerHitCollector factory
+    edm4eic::TrackerHitCollection total_hits;
 
     for(auto input_tag: GetInputTags()) {
-        auto hits = event->Get<edm4eic::TrackerHit>(input_tag);
-        for (const auto *const hit : hits) {
+        auto hits = static_cast<const edm4eic::TrackerHitCollection*>(event->GetCollectionBase(input_tag));
+        for (const auto& hit : *hits) {
             total_hits.push_back(hit);
         }
     }
 
-    m_log->debug("Process method");
-
     try {
-        auto result = m_seeding_algo.produce(total_hits);
-        Set(result);    // Set() - is what factory produced
+        auto track_params = m_seeding_algo.produce(total_hits);
+        SetCollection(std::move(track_params));
     }
     catch(std::exception &e) {
         throw JException(e.what());

src/algorithms/tracking/TrackSeeding.cc
@@ -112,38 +112,38 @@ void eicrecon::TrackSeeding::configure() {
       std::pow(m_seedFinderConfig.highland / m_seedFinderConfig.pTPerHelixRadius,2);
 }
 
-std::vector<edm4eic::TrackParameters*> eicrecon::TrackSeeding::produce(std::vector<const edm4eic::TrackerHit*> trk_hits) {
+std::unique_ptr<edm4eic::TrackParametersCollection> eicrecon::TrackSeeding::produce(const edm4eic::TrackerHitCollection& trk_hits) {
 
   std::vector<const eicrecon::SpacePoint*> spacePoints = getSpacePoints(trk_hits);
 
   Acts::SeedFinderOrthogonal<eicrecon::SpacePoint> finder(m_seedFinderConfig);
   eicrecon::SeedContainer seeds = finder.createSeeds(spacePoints);
 
-  std::vector<edm4eic::TrackParameters*> result = makeTrackParams(seeds);
+  std::unique_ptr<edm4eic::TrackParametersCollection> trackparams = makeTrackParams(seeds);
 
   for (auto& sp: spacePoints) {
     delete sp;
   }
 
-  return result;
+  return std::move(trackparams);
 }
 
-std::vector<const eicrecon::SpacePoint*> eicrecon::TrackSeeding::getSpacePoints(std::vector<const edm4eic::TrackerHit*>& trk_hits)
+std::vector<const eicrecon::SpacePoint*> eicrecon::TrackSeeding::getSpacePoints(const edm4eic::TrackerHitCollection& trk_hits)
 {
   std::vector<const eicrecon::SpacePoint*> spacepoints;
 
   for(const auto hit : trk_hits)
     {
-      const eicrecon::SpacePoint* sp = new SpacePoint(*hit);
+      const eicrecon::SpacePoint* sp = new SpacePoint(hit);
       spacepoints.push_back(sp);
     }
 
   return spacepoints;
 }
 
-std::vector<edm4eic::TrackParameters*> eicrecon::TrackSeeding::makeTrackParams(SeedContainer& seeds)
+std::unique_ptr<edm4eic::TrackParametersCollection> eicrecon::TrackSeeding::makeTrackParams(SeedContainer& seeds)
 {
-  std::vector<edm4eic::TrackParameters*> trackparams;
+  auto trackparams = std::make_unique<edm4eic::TrackParametersCollection>();
 
   for(auto& seed : seeds)
     {
@@ -208,23 +208,20 @@ std::vector<edm4eic::TrackParameters*> eicrecon::TrackSeeding::makeTrackParams(S
           localpos = local.value();
         }
 
-      auto *params = new edm4eic::TrackParameters{
-        -1, // type --> seed(-1)
-        {(float)localpos(0), (float)localpos(1)}, // 2d location on surface
-        {0.1,0.1}, //covariance of location
-        theta, //theta [rad]
-        (float)phi, // phi [rad]
-        qOverP, // Q/p [e/GeV]
-        {0.05,0.05,0.05}, // covariance on theta/phi/q/p
-        10, // time in ns
-        0.1, // error on time
-        (float)charge // charge
-      };
-
-      trackparams.push_back(params);
+      auto trackparam = trackparams->create();
+      trackparam.setType(-1); // type --> seed(-1)
+      trackparam.setLoc({(float)localpos(0), (float)localpos(1)}); // 2d location on surface
+      trackparam.setLocError({0.1,0.1}); //covariance of location
+      trackparam.setTheta(theta); //theta [rad]
+      trackparam.setPhi((float)phi); // phi [rad]
+      trackparam.setQOverP(qOverP); // Q/p [e/GeV]
+      trackparam.setMomentumError({0.05,0.05,0.05}); // covariance on theta/phi/q/p
+      trackparam.setTime(10); // time in ns
+      trackparam.setTimeError(0.1); // error on time
+      trackparam.setCharge((float)charge); // charge
     }
 
-  return trackparams;
+  return std::move(trackparams);
 }
 std::pair<float, float> eicrecon::TrackSeeding::findPCA(std::tuple<float,float,float>& circleParams) const
 {

src/algorithms/tracking/TrackSeeding.h
@@ -31,7 +31,7 @@ namespace eicrecon {
             public eicrecon::WithPodConfig<eicrecon::OrthogonalTrackSeedingConfig> {
     public:
         void init(std::shared_ptr<const ActsGeometryProvider> geo_svc, std::shared_ptr<spdlog::logger> log);
-        std::vector<edm4eic::TrackParameters*> produce(std::vector<const edm4eic::TrackerHit*> trk_hits);
+        std::unique_ptr<edm4eic::TrackParametersCollection> produce(const edm4eic::TrackerHitCollection& trk_hits);
 
     private:
         void configure();
@@ -47,8 +47,8 @@ namespace eicrecon {
 
         int determineCharge(std::vector<std::pair<float,float>>& positions) const;
         std::pair<float,float> findPCA(std::tuple<float,float,float>& circleParams) const;
-        std::vector<const eicrecon::SpacePoint*> getSpacePoints(std::vector<const edm4eic::TrackerHit*>& trk_hits);
-        std::vector<edm4eic::TrackParameters*> makeTrackParams(SeedContainer& seeds);
+        std::vector<const eicrecon::SpacePoint*> getSpacePoints(const edm4eic::TrackerHitCollection& trk_hits);
+        std::unique_ptr<edm4eic::TrackParametersCollection> makeTrackParams(SeedContainer& seeds);
 
         std::tuple<float,float,float> circleFit(std::vector<std::pair<float,float>>& positions) const;
         std::tuple<float,float> lineFit(std::vector<std::pair<float,float>>& positions) const;

commit 03c7bf7edaff191146efeccf0d6be53b68177141

fix: use subset collection when collecting hits in TrackSeeding (#1050)
### Briefly, what does this PR introduce?
This fixes a regression introduced in #983 that caused realistic track
seeding to fail. Essentially, you cannot just add an object from one
podio collection into another podio collection "by reference", i.e.
without cloning. You can do that with pointers in std::vector, which was
done here before #983. We can achieve the same behavior in podio, but we
have to be more intentional about it and define a subset collection.

Of course, podio complains loudly when you try to add an object from one
collection into another collection. It emits an exception: `Object
already in a collection. Cannot add it to a second collection`. But
JANA2 eats that exception and doesn't even print it :angry:

TODO:
- [x] check diffs

### What kind of change does this PR introduce?
- [x] Bug fix (issue: no track seeding results after #983)
- [ ] New feature (issue #__)
- [ ] Documentation update
- [ ] Other: __

### Please check if this PR fulfills the following:
- [ ] Tests for the changes have been added
- [ ] Documentation has been added / updated
- [x] Changes have been communicated to collaborators @ShujieL 

### Does this PR introduce breaking changes? What changes might users
need to make to their code?
No.

### Does this PR change default behavior?
Yes, CentralTrackSeedingResults and dependents are restored.

src/global/tracking/TrackSeeding_factory.cc
@@ -63,6 +63,7 @@ void eicrecon::TrackSeeding_factory::Process(const std::shared_ptr<const JEvent>
     // Collect all hits
     // FIXME Collection is better done with a TrackerHitCollector factory
     edm4eic::TrackerHitCollection total_hits;
+    total_hits.setSubsetCollection();
 
     for(auto input_tag: GetInputTags()) {
         auto hits = static_cast<const edm4eic::TrackerHitCollection*>(event->GetCollectionBase(input_tag));