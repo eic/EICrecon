bb306899f5a20025e73c53037b6f4805472786fe
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-10-20 13:37:46 -0400

    Add method to construct cluster-reco particle map, start adding documentation comments

--- src/algorithms/reco/ParticleFlow.cc
+++ src/algorithms/reco/ParticleFlow.cc
@@ -19,10 +19,19 @@ namespace eicrecon {
 
 
 
-  std::unique_ptr<edm4eic::ReconstructedParticleCollection> ParticleFlow::process() {
+  std::unique_ptr<edm4eic::ReconstructedParticleCollection> ParticleFlow::process(
+    const ParticleFlow::ParInput    inPars,
+    const ParticleFlow::CaloInput   inCalos,
+    const ParticleFlow::CaloAssocIn inRecoCaloAssoc
+  ) {
+
+    // set inputs
+    m_inPars          = inPars;
+    m_inCalos         = inCalos;
+    m_inRecoCaloAssoc = inRecoCaloAssoc;
 
     // instantiate collection to hold produced reco particles
-    auto out_collection = std::make_unique<edm4eic::ReconstructedParticleCollection>();
+    auto outPars = std::make_unique<edm4eic::ReconstructedParticleCollection>();
 
     // run selected algorithm:
     //   - if unknown option is selected, throw exception
@@ -30,7 +39,7 @@ namespace eicrecon {
 
       case FlowAlgo::Alpha:
         m_log -> trace(" Running PF Alpha algorithm");
-        out_collection = do_pf_alpha();
+        outPars = do_pf_alpha();
         break;
 
       default:
@@ -38,21 +47,119 @@ namespace eicrecon {
         throw JException("invalid argument");
         break;
     }
-    return out_collection;
+    return outPars;
 
-  }  // end 'process()'
+  }  // end 'process(ParticleFlow::ParInput, ParticleFlow::CaloInput, ParticleFlow::CaloAssoc)'
 
 
 
-  std::unique_ptr<edm4eic::ReconstructedParticleCollection> do_pf_alpha() {
+
+  //! PF Alpha Algorithm.
+  /*! A rudimentary energy flow algorithm. This algorithm servers as a baseline against
+   *  which we can compare subsequent iterations
+   */ 
+  std::unique_ptr<edm4eic::ReconstructedParticleCollection> ParticleFlow::do_pf_alpha() {
 
     // instantiate collection to hold produced reco particles
-    auto out_collection = std::make_unique<edm4eic::ReconstructedParticleCollection>();
+    auto outPars = std::make_unique<edm4eic::ReconstructedParticleCollection>();
 
-    /* TODO fill in alpha algorithm */
+    // construct cluster-particle maps
+    std::map<edm4eic::Cluster, int> ecalMap = create_cluster_map(m_inPars, m_inCalos.first,  m_inRecoCaloAssoc.first);
+    std::map<edm4eic::Cluster, int> hcalMap = create_cluster_map(m_inPars, m_inCalos.second, m_inRecoCaloAssoc.second);
 
-    return out_collection;
+    /* TODO sum energy in calos */
+    /* TODO subtract reco particle energy from calo energy sums */
+    /* TODO remove clusters with no energy after subtraction */
+    /* TODO combine leftover clusters into neutral particles */
+
+    return outPars;
 
   }  // end 'do_pf_alpha()'
+
+
+
+  //! Construct cluster map.
+  /*! This method loops over a collection of clusters and creates a map of the
+   *  cluster onto the associated reconstructed particles' ID. This is done in
+   *  2 steps:
+   *    1. identify cluster's associated mc particle via `MCRecoClusterParticleAssociation`
+   *    2. identify mc particle's associated reconstructed particle via
+   *       `MCRecoParticleAssociation`
+   * If no associated reconstructed particle is found, the reco particle ID
+   * field is set to be -1.
+   *
+   * FIXME once available, this should be replaced by a map of clusters
+   * onto track projections.
+   */ 
+  std::map<edm4eic::Cluster, int> ParticleFlow::create_cluster_map(
+    const ParticleFlow::ParInput   inParticles,
+    const ParticleFlow::CalCollect clusters,
+    const ParticleFlow::CalMcAssoc clustMcAssoc
+  ) {
+
+    // instantiate cluster map
+    std::map<edm4eic::Cluster, int> clustMap = {};
+    m_log -> trace(" Preparing to construct cluster map");
+
+    // loop over clusters and identify associated reconstruted particles
+    for (const auto cluster : *clusters) {
+
+      // 1. identify matching mc particle
+      int  mcParID    = -1;
+      bool foundMcPar = false;
+      for (const auto mcAssoc : *clustMcAssoc) {
+        if (mcAssoc.getRec() == cluster) {
+          mcParID    = mcAssoc.getSimID();
+          foundMcPar = true;
+          break;
+        }
+      }  // end mc association loop
+
+      if (foundMcPar) {
+        m_log -> trace("   Found associated MC particle! Cluster energy = {}, MC particle ID = {}", cluster.getEnergy(), mcParID); 
+      } else {
+        m_log -> trace("   Did not find associated MC particle! Cluster energy = ()", cluster.getEnergy());
+      }
+
+      // 2. identify matching reco particle
+      int  recParID    = -1;
+      bool foundRecPar = false;
+      if (foundMcPar) {
+        for (const auto mcPar : *std::get<0>(m_inPars)) {
+
+          // select mc particle associated to cluster
+          if (mcPar.getObjectID().index != mcParID) {
+            continue;
+          }
+
+          // identify associated reconstructed particle
+          for (const auto recAssoc : *std::get<2>(m_inPars)) {
+            if (recAssoc.getSim() == mcPar) {
+              recParID    = recAssoc.getRecID();
+              foundRecPar = true;
+              break;
+            }
+          }  // end reco-mc association loop
+
+          if (foundRecPar) {
+            break;
+          }
+        }  // end mc particle loop
+
+        if (foundRecPar) {
+          m_log -> trace("   Found associated reco. particle! Reco particle ID = {}", recParID); 
+        } else {
+          m_log -> trace("   Did not find associated MC particle!");
+        }
+      }  // end if (foundMcPar)
+
+      // add cluster-reco id to map
+      clustMap[cluster] = recParID;
+    }  // end cluster loop
+
+    m_log -> trace(" Constructed cluster map");
+    return clustMap;
+
+  }  // end 'create_cluster_map()'
 
 }  // end eicrecon namespace
--- src/algorithms/reco/ParticleFlow.h
+++ src/algorithms/reco/ParticleFlow.h
@@ -4,40 +4,78 @@
 #ifndef EICRECON_PARTICLEFLOW_H
 #define EICRECON_PARTICLEFLOW_H
 
+#include <map>
+#include <utility>
 #include <algorithm>
 #include <spdlog/spdlog.h>
 // event data model definitions
-#include <edm4eic/ReconstructedParticle.h>
+#include <edm4eic/Cluster.h>
+#include <edm4eic/ClusterCollection.h>
 #include <edm4eic/ReconstructedParticleCollection.h>
+#include <edm4eic/MCRecoParticleAssociationCollection.h>
+#include <edm4eic/MCRecoClusterParticleAssociationCollection.h>
+#include <edm4hep/MCParticleCollection.h>
 // for algorithm configuration
 #include "algorithms/interfaces/WithPodConfig.h"
 #include "ParticleFlowConfig.h"
 
 namespace eicrecon{
 
+  //! Particle Flow
+  /*! This algorithm takes a collection of tracks (and their projections)
+   *  and a pair of calorimeter collections, from an ECal and an HCal,
+   *  and returns a collection of reconstructed particles.
+   *
+   *  As there will likely need to be multiple PF algorithms at any given
+   *  time, the particular choice of algorithm is set by a user-configurable
+   *  flag. The `process()` call will then run whichever algorithm is
+   *  specified.
+   *
+   *  This flexibility will hopefully allow for easier testing/benchmarking.
+   */ 
   class ParticleFlow : public WithPodConfig<ParticleFlowConfig> {
 
     public:
 
+      // aliases for brevity
+      using CalCollect  = edm4eic::ClusterCollection*;
+      using CalMcAssoc  = edm4eic::MCRecoClusterParticleAssociationCollection*;
+      using ParInput    = std::tuple<edm4hep::MCParticleCollection*, edm4eic::ReconstructedParticleCollection*, edm4eic::MCRecoParticleAssociationCollection*>;
+      using CaloInput   = std::pair<edm4eic::ClusterCollection*, edm4eic::ClusterCollection*>;
+      using CaloAssocIn = std::pair<edm4eic::MCRecoClusterParticleAssociationCollection*, edm4eic::MCRecoClusterParticleAssociationCollection*>;
+
       // algorithm initialization
       void init(std::shared_ptr<spdlog::logger> logger);
 
-      // primary algorithm call:
-      //   - the particular choice of algorithm to be used is indentified
-      //     by a user-configurable flag
-      //   - this method will run whichever algorithm is specified
-      std::unique_ptr<edm4eic::ReconstructedParticleCollection> process();
+      // primary algorithm call
+      // - FIXME the mc/reco particle input will need to be replaced
+      //   with tracks once ready
+      // - FIXME the reco particle/cluster association input will
+      //   need to be replaced with track projections once ready
+      std::unique_ptr<edm4eic::ReconstructedParticleCollection> process(
+        const ParInput    inPars,
+        const CaloInput   inCalos,
+        const CaloAssocIn inRecoCaloAssoc
+      );
 
     private:
 
       std::shared_ptr<spdlog::logger> m_log;
 
-      // particle flow algorithms:
-      //   - the idea is that alternate algorithms will be added here as they're
-      //     developed
-      //   - the hope is that testing/benchmarking will be be much easier by
-      //     letting the user easily select which algorithm to use
+      // input collections
+      ParInput    m_inPars;
+      CaloInput   m_inCalos;
+      CaloAssocIn m_inRecoCaloAssoc;
+
+      // particle flow algorithms
       std::unique_ptr<edm4eic::ReconstructedParticleCollection> do_pf_alpha();
+
+      // helper methods
+      std::map<edm4eic::Cluster, int> create_cluster_map(
+        const ParInput   inParticles,
+        const CalCollect clusters,
+        const CalMcAssoc clustMcAssoc
+      );
 
       // algorithm options
       enum FlowAlgo  {Alpha};
