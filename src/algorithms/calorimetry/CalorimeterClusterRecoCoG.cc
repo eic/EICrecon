8ea29b5948e9d05f73bbf167698e793ee40656b1
Wouter Deconinck <wdconinc@gmail.com>
2023-11-30 12:44:33 -0600

    fix: iwyu

--- src/algorithms/calorimetry/CalorimeterClusterRecoCoG.cc
+++ src/algorithms/calorimetry/CalorimeterClusterRecoCoG.cc
@@ -8,6 +8,7 @@
  *  Author: Chao Peng (ANL), 09/27/2020
  */
 
+#include <Evaluator/DD4hepUnits.h>
 #include <boost/algorithm/string/join.hpp>
 #include <boost/range/adaptor/map.hpp>
 #include <edm4eic/CalorimeterHitCollection.h>
@@ -20,15 +21,12 @@
 #include <podio/RelationRange.h>
 #include <Eigen/Core>
 #include <Eigen/Eigenvalues>
-#include <Evaluator/DD4hepUnits.h>
 #include <cctype>
-#include <exception>
+#include <complex>
 #include <gsl/pointers>
 #include <limits>
 #include <map>
 #include <optional>
-#include <tuple>
-#include <type_traits>
 #include <vector>
 
 #include "CalorimeterClusterRecoCoG.h"
