94d040e4e74047997ae678af4528afe64204b2a6
Sebouh Paul <sebouh.paul@gmail.com>
2023-12-06 00:30:10 -0500

    added files that I had missed earlier

---
+++ src/algorithms/calorimetry/LogWeightRecoConfig.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Sebouh Paul
+
+#pragma once
+
+namespace eicrecon {
+
+  struct LogWeightRecoConfig {
+    // strawman energy reconstruction
+    double                   sampling_fraction{0.0203};
+    //parameters to determine the cut-off parameter w0
+    // as a function of energy based on an optimized 
+    double                   E0{50};
+    double                   w0_a{5.0};
+    double                   w0_b{0.65};
+    double                   w0_c{0.31};
+
+  };
+
+} // eicrecon
