d084bb4252e65391d795c30b1da81a99aa00849c
Wouter Deconinck <wdconinc@gmail.com>
2024-01-02 11:34:26 -0600

    fix: use DD4hep units in TrackParamTruthInit.cc

--- src/algorithms/tracking/TrackParamTruthInit.cc
+++ src/algorithms/tracking/TrackParamTruthInit.cc
@@ -4,7 +4,7 @@
 
 #include "TrackParamTruthInit.h"
 
-#include <Acts/Definitions/Units.hpp>
+#include <Evaluate/DD4hepUnits.hpp>
 #include <TParticlePDG.h>
 #include <edm4hep/Vector3d.h>
 #include <edm4hep/Vector3f.h>
@@ -29,12 +29,7 @@ void eicrecon::TrackParamTruthInit::init(const std::shared_ptr<spdlog::logger> &
 std::unique_ptr<edm4eic::TrackParametersCollection>
 eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcparticles) {
     // MCParticles uses numerical values in its specified units,
-    // while m_cfg is in the Acts unit system
-    using Acts::UnitConstants::GeV;
-    using Acts::UnitConstants::MeV;
-    using Acts::UnitConstants::mm;
-    using Acts::UnitConstants::um;
-    using Acts::UnitConstants::ns;
+    // while m_cfg is in the DD4hep unit system
 
     // Create output collection
     auto track_parameters = std::make_unique<edm4eic::TrackParametersCollection>();
@@ -50,9 +45,9 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
 
         // require close to interaction vertex
         auto v = mcparticle.getVertex();
-        if (abs(v.x) * mm > m_cfg.m_maxVertexX ||
-            abs(v.y) * mm > m_cfg.m_maxVertexY ||
-            abs(v.z) * mm > m_cfg.m_maxVertexZ) {
+        if (abs(v.x) * dd4hep::mm > m_cfg.m_maxVertexX ||
+            abs(v.y) * dd4hep::mm > m_cfg.m_maxVertexY ||
+            abs(v.z) * dd4hep::mm > m_cfg.m_maxVertexZ) {
             m_log->trace("ignoring particle with vs = {} [mm]", v);
             continue;
         }
@@ -60,7 +55,7 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         // require minimum momentum
         const auto& p = mcparticle.getMomentum();
         const auto pmag = std::hypot(p.x, p.y, p.z);
-        if (pmag * GeV < m_cfg.m_minMomentum) {
+        if (pmag * dd4hep::GeV < m_cfg.m_minMomentum) {
             m_log->trace("ignoring particle with p = {} GeV ", pmag);
             continue;
         }
@@ -91,16 +86,16 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         }
 
         // modify initial momentum to avoid bleeding truth to results when fit fails
-        const auto pinit = pmag*(1.0 + m_cfg.m_momentumSmear * m_normDist(generator));
+        const auto pinit = pmag * (1.0 + m_cfg.m_momentumSmear * m_normDist(generator));
 
         // Insert into edm4eic::TrackParameters, which uses numerical values in its specified units
         auto track_parameter = track_parameters->create();
         track_parameter.setType(-1); // type --> seed(-1)
-        track_parameter.setLoc({static_cast<float>(std::hypot(v.x, v.y)), static_cast<float>(v.z)}); // 2d location on surface [mm]
+        track_parameter.setLoc({static_cast<float>(std::hypot(v.x, v.y) / dd4hep::mm), static_cast<float>(v.z / dd4hep::mm)}); // 2d location on surface [mm]
         track_parameter.setLocError({1.0, 1.0}); // sqrt(variance) of location [mm]
-        track_parameter.setTheta(theta); //theta [rad]
+        track_parameter.setTheta(theta); // heta [rad]
         track_parameter.setPhi(phi); // phi [rad]
-        track_parameter.setQOverP(charge / pinit); // Q/p [e/GeV]
+        track_parameter.setQOverP(charge / (pinit / dd4hep::GeV)); // Q/p [e/GeV]
         track_parameter.setMomentumError({0.01, 0.05, 0.1}); // sqrt(variance) on theta, phi, q/p [rad, rad, e/GeV]
         track_parameter.setTime(mcparticle.getTime()); // time [ns]
         track_parameter.setTimeError(10e9); // error on time [ns]
@@ -109,9 +104,9 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         // Debug output
         if (m_log->level() <= spdlog::level::debug) {
             m_log->debug("Invoke track finding seeded by truth particle with:");
-            m_log->debug("   p     = {} GeV (smeared to {} GeV)", pmag, pinit);
+            m_log->debug("   p     = {} GeV (smeared to {} GeV)", pmag / dd4hep::GeV, pinit / dd4hep::GeV);
             m_log->debug("   q     = {}", charge);
-            m_log->debug("   q/p   = {} e/GeV (smeared to {} e/GeV)", charge / pmag, charge / pinit);
+            m_log->debug("   q/p   = {} e/GeV (smeared to {} e/GeV)", charge / (pmag / dd4hep::GeV), charge / (pinit / dd4hep::GeV));
             m_log->debug("   theta = {}", theta);
             m_log->debug("   phi   = {}", phi);
         }
