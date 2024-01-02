2f1bf5f3846e16264acae1d743c7ddc4630a8810
Wouter Deconinck <wdconinc@gmail.com>
2024-01-02 13:08:09 -0600

    fix: convert ImagingClusterReco to algorithms interface

--- src/factories/calorimetry/ImagingClusterReco_factory.h
+++ src/factories/calorimetry/ImagingClusterReco_factory.h
@@ -9,32 +9,35 @@
 
 namespace eicrecon {
 
-class ImagingClusterReco_factory: public JOmniFactory<ImagingClusterReco_factory, ImagingClusterRecoConfig> {
+class ImagingClusterReco_factory :
+    public JOmniFactory<ImagingClusterReco_factory, ImagingClusterRecoConfig> {
+
+public:
+    using AlgoT = eicrecon::ImagingClusterReco;
 private:
-      eicrecon::ImagingClusterReco m_algo;
+    std::unique_ptr<AlgoT> m_algo;
 
-      PodioInput<edm4eic::ProtoCluster> m_protos_input {this};
-      PodioInput<edm4hep::SimCalorimeterHit> m_mchits_input {this};
+    PodioInput<edm4eic::ProtoCluster> m_protos_input {this};
+    PodioInput<edm4hep::SimCalorimeterHit> m_mchits_input {this};
 
-      PodioOutput<edm4eic::Cluster> m_clusters_output {this};
-      PodioOutput<edm4eic::MCRecoClusterParticleAssociation> m_assocs_output {this};
-      PodioOutput<edm4eic::Cluster> m_layers_output {this};
+    PodioOutput<edm4eic::Cluster> m_clusters_output {this};
+    PodioOutput<edm4eic::MCRecoClusterParticleAssociation> m_assocs_output {this};
+    PodioOutput<edm4eic::Cluster> m_layers_output {this};
 
-      ParameterRef<int> m_trackStopLayer {this, "trackStopLayer", config().trackStopLayer};
+    ParameterRef<int> m_trackStopLayer {this, "trackStopLayer", config().trackStopLayer};
 
 public:
     void Configure() {
-        m_algo.applyConfig(config());
-        m_algo.init(logger());
+        m_algo->applyConfig(config());
+        m_algo->init(logger());
     }
 
     void ChangeRun(int64_t run_number) {
     }
 
     void Process(int64_t run_number, uint64_t event_number) {
-        std::tie(m_clusters_output(),
-                 m_assocs_output(),
-                 m_layers_output()) = m_algo.process(*m_protos_input(), *m_mchits_input());
+        m_algo->process({m_protos_input(), m_mchits_input()},
+                        {m_clusters_output().get(), m_assocs_output().get(), m_layers_output().get()});
     }
 };
 
