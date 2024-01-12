7fbc9c4457bfb71c62b64d87459cdec256eed8f6
Wouter Deconinck <wdconinc@gmail.com>
2024-01-12 08:48:13 -0600

    fix: no unit conversion needed from MCParticle to TrackParameter

--- src/algorithms/tracking/TrackParamTruthInit.cc
+++ src/algorithms/tracking/TrackParamTruthInit.cc
@@ -91,7 +91,7 @@ eicrecon::TrackParamTruthInit::produce(const edm4hep::MCParticleCollection* mcpa
         // Insert into edm4eic::TrackParameters, which uses numerical values in its specified units
         auto track_parameter = track_parameters->create();
         track_parameter.setType(-1); // type --> seed(-1)
-        track_parameter.setLoc({static_cast<float>(std::hypot(v.x, v.y) / dd4hep::mm), static_cast<float>(v.z / dd4hep::mm)}); // 2d location on surface [mm]
+        track_parameter.setLoc({static_cast<float>(std::hypot(v.x, v.y)), static_cast<float>(v.z)}); // 2d location on surface [mm]
         track_parameter.setLocError({1.0, 1.0}); // sqrt(variance) of location [mm]
         track_parameter.setTheta(theta); // theta [rad]
         track_parameter.setPhi(phi); // phi [rad]
