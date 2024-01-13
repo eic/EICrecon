f6263c7a33022c886bf5deb2c2fa53443d37ee6a
Sebouh Paul <sebouh.paul@gmail.com>
2024-01-13 09:51:23 -0600

    Update HEXPLIT_factory.h
    removed transformation from local to global from the configuration parameters

--- src/factories/calorimetry/HEXPLIT_factory.h
+++ src/factories/calorimetry/HEXPLIT_factory.h
@@ -22,12 +22,6 @@ private:
     ParameterRef<double> m_tmax     {this, "tmax",          config().tmax};
     ParameterRef<double> m_sl       {this, "side_length",   config().side_length};
     ParameterRef<double> m_ls       {this, "layer_spacing", config().layer_spacing};
-    ParameterRef<double> m_rot_x    {this, "rot_x",         config().rot_x};
-    ParameterRef<double> m_rot_y    {this, "rot_y",         config().rot_y};
-    ParameterRef<double> m_rot_z    {this, "rot_z",         config().rot_z};
-    ParameterRef<double> m_trans_x  {this, "trans_x",       config().trans_x};
-    ParameterRef<double> m_trans_y  {this, "trans_y",       config().trans_y};
-    ParameterRef<double> m_trans_z  {this, "trans_z",       config().trans_z};
 
     Service<DD4hep_service> m_geoSvc {this};
 
