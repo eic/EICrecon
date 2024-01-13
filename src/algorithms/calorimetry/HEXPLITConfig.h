680392e1ece5898dc553660c7a57b8de5edf5ffc
Sebouh Paul <sebouh.paul@gmail.com>
2024-01-13 09:50:09 -0600

    Update HEXPLITConfig.h
    Remove the rotation and translation to global coordinates from the configuration

--- src/algorithms/calorimetry/HEXPLITConfig.h
+++ src/algorithms/calorimetry/HEXPLITConfig.h
@@ -12,13 +12,7 @@ namespace eicrecon {
     double                   MIP{0.000472};
     double                   Emin_in_MIPs{0.1};
     double                   tmax{325*dd4hep::ns};
-    //conversion from local to global coordinates.  translate first, then rotate
-    double                   trans_x{0};
-    double                   trans_y{0};
-    double                   trans_z{0};
-    double                   rot_x{0};
-    double                   rot_y{0};
-    double                   rot_z{0};
+
   };
 
 } // eicrecon
