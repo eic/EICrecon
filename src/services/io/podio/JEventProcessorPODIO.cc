23733028e81c320d0b19a85b090318fa23bb968a
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-01-15 14:35:06 -0500

    Revert "fix: iwyu in JEventProcessorPODIO.cc"
    This reverts commit ab4a6c68a5b1cad397ee8a37c893ff72269523cb.

--- src/services/io/podio/JEventProcessorPODIO.cc
+++ src/services/io/podio/JEventProcessorPODIO.cc
@@ -2,6 +2,7 @@
 #include "JEventProcessorPODIO.h"
 
 #include <JANA/JApplication.h>
+#include <JANA/JLogger.h>
 #include <JANA/Services/JParameterManager.h>
 #include <JANA/Utils/JTypeInfo.h>
 #include <fmt/core.h>
@@ -9,7 +10,6 @@
 #include <podio/Frame.h>
 #include <spdlog/common.h>
 #include <exception>
-#include <iosfwd>
 
 #include "services/log/Log_service.h"
 
