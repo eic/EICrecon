911c11f345869c5c00fcbed38348ec0c769940ff
Sebouh Paul <sebouh.paul@gmail.com>
2024-01-15 15:11:26 -0500

    removed broken include statement from previous commit

--- src/detectors/ZDC/ZDC.cc
+++ src/detectors/ZDC/ZDC.cc
@@ -16,7 +16,6 @@
 #include "factories/calorimetry/ImagingTopoCluster_factory.h"
 #include "factories/calorimetry/CalorimeterTruthClustering_factory.h"
 #include "factories/calorimetry/HEXPLIT_factory.h"
-#include "factories/calorimetry/LogWeightReco_factory.h"
 
 extern "C" {
     void InitPlugin(JApplication *app) {
