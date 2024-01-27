4d7e3ed419739b789a1e0d8bc1e2d8456ec0abcb
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-01-27 15:58:21 -0500

    Update JetReconstruction.h: remove dead code

--- src/algorithms/reco/JetReconstruction.h
+++ src/algorithms/reco/JetReconstruction.h
@@ -33,36 +33,6 @@ namespace eicrecon {
 
     private:
 
-      // generic method to add constituent to jet kinematic info
-      template <typename T> void add_to_jet_kinematics(const T& addend, edm4eic::MutableReconstructedParticle& kinematics, edm4eic::ReconstructedParticleCollection*);
-
-      // specialization for ReconstructedParticle input
-      void add_to_jet_kinematics(const edm4eic::ReconstructedParticle& addend, edm4eic::MutableReconstructedParticle& kinematics, edm4eic::ReconstructedParticleCollection*) {
-        kinematics.addToParticles(addend);
-      };
-
-      // specialization for MCParticle input
-      void add_to_jet_kinematics(const edm4hep::MCParticle& addend, edm4eic::MutableReconstructedParticle& kinematics, edm4eic::ReconstructedParticleCollection *output_coll) {
-        kinematics.addToParticles(copy_mcparticle_onto_recoparticle(addend, output_coll));
-      };
-
-      // helper function to copy edm4hep::MCParticle onto edm4eic::ReconstructedParticle
-      edm4eic::ReconstructedParticle copy_mcparticle_onto_recoparticle(const edm4hep::MCParticle& mc, edm4eic::ReconstructedParticleCollection *output_coll) {
-        const float mom = std::hypot(mc.getMomentum().x, mc.getMomentum().y, mc.getMomentum().z);
-        const float energy = std::hypot(mc.getMass(), mom);
-
-        // create reco particle
-        edm4eic::MutableReconstructedParticle mutable_reco = output_coll->create();
-        mutable_reco.setMomentum(mc.getMomentum());
-        mutable_reco.setEnergy(energy);
-        mutable_reco.setCharge(mc.getCharge());
-        mutable_reco.setMass(mc.getMass());
-        mutable_reco.setPDG(mc.getPDG());
-
-        // return non-mutable representation
-        return static_cast<edm4eic::ReconstructedParticle>(mutable_reco);
-      }
-
       std::shared_ptr<spdlog::logger> m_log;
 
       // maps of user input onto fastjet options
