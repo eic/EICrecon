ac0c2bf7e6c7d72683e28c26375288c86054b1e2
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-01-27 22:24:08 -0500

    Apply suggestions from code review

--- src/global/tracking/TrackPropagation_factory.cc
+++ src/global/tracking/TrackPropagation_factory.cc
@@ -60,10 +60,8 @@ void eicrecon::TrackPropagation_factory::Process(const std::shared_ptr<const JEv
         for(auto& surf : m_target_surface_list) {
             auto prop_point = m_track_propagation_algo.propagate(traj, surf);
             if(!prop_point) continue;
-#if EDM4EIC_VERSION_MAJOR >= 3
             prop_point->surface = surf->geometryId().layer();
             prop_point->system  = surf->geometryId().extra();
-#endif
             this_propagated_track.addToPoints(*prop_point);
         }
         propagated_tracks.push_back(this_propagated_track);
