6641546b83e75b1801ede9f8e9b21f6e431091b2
Jeetendra Gupta <jeetendragupta@Jeetendras-MacBook-Air.local>
2024-02-02 01:38:52 -0600

    phi calculation independent of charge

--- src/algorithms/tracking/TrackSeeding.cc
+++ src/algorithms/tracking/TrackSeeding.cc
@@ -4,6 +4,7 @@
 
 #include "TrackSeeding.h"
 
+#include "TVector2.h"
 #include <Acts/Definitions/Algebra.hpp>
 #include <Acts/Seeding/Seed.hpp>
 #include <Acts/Seeding/SeedConfirmationRangeConfig.hpp>
@@ -211,10 +212,20 @@ std::unique_ptr<edm4eic::TrackParametersCollection> eicrecon::TrackSeeding::make
       auto xpos = xypos.first;
       auto ypos = xypos.second;
 
-      auto vxpos = -1.*charge*(ypos-Y0);
-      auto vypos = charge*(xpos-X0);
 
-      auto phi = atan2(vypos,vxpos);
+	const auto& firstpos = xyHitPositions.at(0); 
+	
+	TVector2 tangent_vector_candidate_1( (Y0-ypos) , -(X0-xpos) );
+	TVector2 tangent_vector_candidate_2( -(Y0-ypos) , (X0-xpos) );
+
+	TVector2 first_hit_vector(firstpos.first-xpos,firstpos.second-ypos);
+
+	auto dot_1 = tangent_vector_candidate_1*first_hit_vector;
+	auto dot_2 = tangent_vector_candidate_2*first_hit_vector;
+
+	TVector2 tangent_vector = (dot_1>dot_2) ? tangent_vector_candidate_1 : tangent_vector_candidate_2;
+
+	auto phi = atan2(tangent_vector.Py(),tangent_vector.Px());	
 
       const float z0 = seed.z();
       auto perigee = Acts::Surface::makeShared<Acts::PerigeeSurface>(Acts::Vector3(0,0,0));
