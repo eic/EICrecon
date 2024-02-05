4eb3753d6499f6ab1005b42f56d460c6f5835978
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-02-05 17:19:26 -0500

    workaround PODIO memory leaks

--- src/tests/algorithms_test/calorimetry_CalorimeterClusterRecoCoG.cc
+++ src/tests/algorithms_test/calorimetry_CalorimeterClusterRecoCoG.cc
@@ -8,7 +8,8 @@
 #include <spdlog/spdlog.h>
 #include <edm4eic/CalorimeterHitCollection.h>
 #include <edm4eic/ProtoClusterCollection.h>
-#include <edm4hep/MutableCaloHitContribution.h>
+#include <edm4hep/CaloHitContributionCollection.h>
+#include <edm4hep/MCParticleCollection.h>
 #include <edm4hep/Vector3f.h>
 #include <edm4eic/unit_system.h>
 
@@ -58,22 +59,24 @@ TEST_CASE( "the cluster assembly algorithm runs", "[CalorimeterClusterCoG]" ) {
 
     edm4hep::SimCalorimeterHitCollection mchit_coll;
     bool with_matching_mc_hit;
-    edm4hep::MCParticle part;
+    edm4hep::MCParticleCollection part_coll;
+    edm4hep::CaloHitContributionCollection contrib_coll;
     SECTION( "with matching MC hit" ) {
       with_matching_mc_hit = true;
+      part_coll.create();
       auto mchit = mchit_coll.create(
         0xBEEF, // std::uint64_t cellID,
         6.0 * edm4eic::unit::MeV, // float energy,
         edm4hep::Vector3f(0.0 * edm4eic::unit::mm, 0.0 * edm4eic::unit::mm, 0.0 * edm4eic::unit::mm) // edm4hep::Vector3f position
       );
-      edm4hep::MutableCaloHitContribution contrib = {
+      contrib_coll.create(
         0, // std::int32_t PDG,
         0 * edm4eic::unit::MeV, // float energy,
         0 * edm4eic::unit::ns, // float time,
         edm4hep::Vector3f(0.0 * edm4eic::unit::mm, 0.0 * edm4eic::unit::mm, 0.0 * edm4eic::unit::mm) // edm4hep::Vector3f stepPosition
-      };
-      contrib.setParticle(part);
-      mchit.addToContributions(contrib);
+      );
+      contrib_coll[0].setParticle(part_coll[0]);
+      mchit.addToContributions(contrib_coll[0]);
     }
 
     SECTION( "without matching MC hit" ) {
@@ -96,8 +99,8 @@ TEST_CASE( "the cluster assembly algorithm runs", "[CalorimeterClusterCoG]" ) {
 
     if (with_matching_mc_hit) {
       REQUIRE( (*assoc_coll).size() == 1 );
-      REQUIRE( (*assoc_coll)[0].getSim() == part );
-      REQUIRE( (*assoc_coll)[0].getSim().id() == part.id() );
+      REQUIRE( (*assoc_coll)[0].getSim() == part_coll[0] );
+      REQUIRE( (*assoc_coll)[0].getSim().id() == part_coll[0].id() );
       REQUIRE( (*assoc_coll)[0].getRec() == (*clust_coll)[0] );
       REQUIRE( (*assoc_coll)[0].getRec().id() == (*clust_coll)[0].id() );
     } else {
