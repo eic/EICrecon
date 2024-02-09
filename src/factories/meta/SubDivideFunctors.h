8efed3401cbbab90107b833944ac266f1fdde89b
simonge <simon.gardner@glasgow.ac.uk>
2024-02-09 14:39:18 +0000

    Added template functor with variadic member functions to filter on e.g. select generatorStatus and PDG simultaneously

--- src/factories/meta/SubDivideFunctors.h
+++ src/factories/meta/SubDivideFunctors.h
@@ -3,13 +3,13 @@
 //
 #pragma once
 
-#include <JANA/JApplication.h>
+#include <JANA/JApplication.h> 
 #include <DDSegmentation/BitFieldCoder.h>
 
 namespace eicrecon {
 
 // ----------------------------------------------------------------------------
-// Functor to split hits based on a range of values
+// Functor to split collection based on a range of values
 // ----------------------------------------------------------------------------
 template <auto MemberFunctionPtr>
 class RangeSplit {
@@ -30,12 +30,12 @@ public:
     }
 
 private:
-    std::vector<std::pair<long int,long int>> m_ranges;
+    std::vector<std::pair<long int,long int>> m_ranges;  
 
 };
 
 // ----------------------------------------------------------------------------
-// Functor to split hits based on geometry
+// Functor to split collection based on geometry
 // ----------------------------------------------------------------------------
 class GeometrySplit {
 public:
@@ -44,7 +44,7 @@ public:
         m_id_dec = app->GetService<DD4hep_service>()->detector()->readout(readout).idSpec().decoder();
         for (auto d : div){
             m_div.push_back(m_id_dec->index(d));
-        }
+        }        
     };
 
     template <typename T>
@@ -55,15 +55,43 @@ public:
         std::vector<long int> det_ids {m_id_dec->get(cellID, m_div[0]),m_id_dec->get(cellID, m_div[1])};
         auto index = std::find(m_ids.begin(),m_ids.end(),det_ids);
         if(index != m_ids.end()){
-            ids.push_back(index-m_ids.begin());
+            ids.push_back(index-m_ids.begin());      
         }
         return ids;
     }
 
 private:
-    dd4hep::DDSegmentation::BitFieldCoder* m_id_dec;
+    dd4hep::DDSegmentation::BitFieldCoder* m_id_dec;    
     std::vector<std::vector<long int>> m_ids;
     std::vector<size_t> m_div;
+
+};
+
+
+// ----------------------------------------------------------------------------
+// Functor to split collection based on any number of collection values
+// ----------------------------------------------------------------------------
+template <auto... MemberFunctionPtrs>
+class ValueSplit {
+public:
+
+    ValueSplit(std::vector<std::vector<int>> ids) : m_ids(ids) {};
+
+    template <typename T>
+    std::vector<int> operator()(T& instance) const {
+        std::vector<int> ids;
+        //Check if requested value is within the ranges
+        std::vector<int> values;
+        (values.push_back((instance.*MemberFunctionPtrs)()), ...);
+        auto index = std::find(m_ids.begin(),m_ids.end(),values);
+        if(index != m_ids.end()){
+            ids.push_back(index-m_ids.begin());      
+        }
+        return ids;
+    }
+
+private:
+    std::vector<std::vector<int>> m_ids;
 
 };
 
