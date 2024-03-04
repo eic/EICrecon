9af67b77600ffff752f9d3681d4de2555a6a43e1
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-03-04 12:02:54 -0500

    Update copyright for TrackPropagation.cc

--- src/algorithms/tracking/TrackPropagation.cc
+++ src/algorithms/tracking/TrackPropagation.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022, 2023 Wenqing Fan, Barak Schmookler, Whitney Armstrong, Sylvester Joosten, Dmitry Romanov, Christopher Dilks
+// Copyright (C) 2022, 2023 Wenqing Fan, Barak Schmookler, Whitney Armstrong, Sylvester Joosten, Dmitry Romanov, Christopher Dilks, Wouter Deconinck
 
 #include <Acts/Definitions/Algebra.hpp>
 #include <Acts/Definitions/Direction.hpp>
