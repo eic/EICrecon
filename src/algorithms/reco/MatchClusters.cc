bad3ff09c739b2d933bd8b8b74e7ff357dcdb615
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-03-17 18:02:09 -0400

    MatchClusters.cc: drop "original license" nonsense

--- src/algorithms/reco/MatchClusters.cc
+++ src/algorithms/reco/MatchClusters.cc
@@ -1,4 +1,4 @@
-// Original header license: SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2022, 2024 Sylvester Joosten, Dmitry Romanov, Wouter Deconinck
 
 // Takes a list of particles (presumed to be from tracking), and all available clusters.
