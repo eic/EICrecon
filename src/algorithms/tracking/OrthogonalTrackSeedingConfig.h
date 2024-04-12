92c42e5898c23f271a8cd1aa50a6131c2b5dd3c7
Simon Gardner <simon.gardner@glasgow.ac.uk>
2024-04-12 14:29:54 +0100

    Remove m_ member tags from tracking configuration structs (#1377)
    ### Briefly, what does this PR introduce?
    Tidies the code so that the tracker configuration structs don't have the
    m_ tag. None of the other algorithm configuration structs are prefixed
    by m_ and it is my understanding that these are conventionally for class
    members which presumably they may have originally been before moving to
    a separate config.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [x] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No
    ### Does this PR change default behavior?
    No

--- src/algorithms/tracking/OrthogonalTrackSeedingConfig.h
+++ src/algorithms/tracking/OrthogonalTrackSeedingConfig.h
@@ -14,30 +14,30 @@ namespace eicrecon {
 
     //////////////////////////////////////////////////////////////////////////
     /// SEED FINDER GENERAL PARAMETERS
-    float m_rMax = 440. * Acts::UnitConstants::mm; // max r to look for hits to compose seeds
-    float m_rMin = 33. * Acts::UnitConstants::mm; // min r to look for hits to compose seeds
-    float m_zMax = 1700. * Acts::UnitConstants::mm; // max z to look for hits to compose seeds
-    float m_zMin = -1500. * Acts::UnitConstants::mm; // min z to look for hits to compose seeds
-    float m_deltaRMinTopSP = 10. * Acts::UnitConstants::mm; // Min distance in r between middle and top SP in one seed
-    float m_deltaRMaxTopSP = 200. * Acts::UnitConstants::mm; // Max distance in r between middle and top SP in one seed
-    float m_deltaRMinBottomSP = 10. * Acts::UnitConstants::mm; // Min distance in r between middle and bottom SP in one seed
-    float m_deltaRMaxBottomSP = 200. * Acts::UnitConstants::mm; // Max distance in r between middle and bottom SP in one seed
-    float m_collisionRegionMin = -250 * Acts::UnitConstants::mm; // Min z for primary vertex
-    float m_collisionRegionMax = 250 * Acts::UnitConstants::mm; // Max z for primary vertex
+    float rMax = 440. * Acts::UnitConstants::mm; // max r to look for hits to compose seeds
+    float rMin = 33. * Acts::UnitConstants::mm; // min r to look for hits to compose seeds
+    float zMax = 1700. * Acts::UnitConstants::mm; // max z to look for hits to compose seeds
+    float zMin = -1500. * Acts::UnitConstants::mm; // min z to look for hits to compose seeds
+    float deltaRMinTopSP     = 10. * Acts::UnitConstants::mm; // Min distance in r between middle and top SP in one seed
+    float deltaRMaxTopSP     = 200. * Acts::UnitConstants::mm; // Max distance in r between middle and top SP in one seed
+    float deltaRMinBottomSP  = 10. * Acts::UnitConstants::mm; // Min distance in r between middle and bottom SP in one seed
+    float deltaRMaxBottomSP  = 200. * Acts::UnitConstants::mm; // Max distance in r between middle and bottom SP in one seed
+    float collisionRegionMin = -250 * Acts::UnitConstants::mm; // Min z for primary vertex
+    float collisionRegionMax = 250 * Acts::UnitConstants::mm; // Max z for primary vertex
 
-    unsigned int m_maxSeedsPerSpM = 0; // max number of seeds a single middle sp can belong to - 1
-    float m_cotThetaMax = 1.0 / tan(2. * atan(exp(-4.0))); // Cotangent of max theta angle (based on eta)
+    unsigned int maxSeedsPerSpM = 0; // max number of seeds a single middle sp can belong to - 1
+    float cotThetaMax = 1.0 / tan(2. * atan(exp(-4.0))); // Cotangent of max theta angle (based on eta)
 
-    float m_sigmaScattering = 5; // How many standard devs of scattering angles to consider
-    float m_radLengthPerSeed = 0.1; // Average radiation lengths of material on the length of a seed
-    float m_minPt = (100. * Acts::UnitConstants::MeV) / m_cotThetaMax; // MeV (in Acts units of GeV) - minimum transverse momentum
-    float m_bFieldInZ = 1.7 * Acts::UnitConstants::T; // T (in Acts units of GeV/[e*mm]) - Magnetic field strength
-    float m_beamPosX = 0; // x offset for beam position
-    float m_beamPosY = 0; // y offset for beam position
-    float m_impactMax = 3. * Acts::UnitConstants::mm; // Maximum transverse PCA allowed
-    float m_bFieldMin = 0.1 * Acts::UnitConstants::T; // T (in Acts units of GeV/[e*mm]) - Minimum Magnetic field strength
-    float m_rMinMiddle = 20. * Acts::UnitConstants::mm; // Middle spacepoint must fall between these two radii
-    float m_rMaxMiddle = 400. * Acts::UnitConstants::mm;
+    float sigmaScattering  = 5; // How many standard devs of scattering angles to consider
+    float radLengthPerSeed = 0.1; // Average radiation lengths of material on the length of a seed
+    float minPt            = (100. * Acts::UnitConstants::MeV) / cotThetaMax; // MeV (in Acts units of GeV) - minimum transverse momentum
+    float bFieldInZ        = 1.7 * Acts::UnitConstants::T; // T (in Acts units of GeV/[e*mm]) - Magnetic field strength
+    float beamPosX         = 0; // x offset for beam position
+    float beamPosY         = 0; // y offset for beam position
+    float impactMax        = 3. * Acts::UnitConstants::mm; // Maximum transverse PCA allowed
+    float bFieldMin        = 0.1 * Acts::UnitConstants::T; // T (in Acts units of GeV/[e*mm]) - Minimum Magnetic field strength
+    float rMinMiddle       = 20. * Acts::UnitConstants::mm; // Middle spacepoint must fall between these two radii
+    float rMaxMiddle       = 400. * Acts::UnitConstants::mm;
 
     //////////////////////////////////////////////////////////////////////////
     /// SEED FILTER GENERAL PARAMETERS
@@ -46,46 +46,46 @@ namespace eicrecon {
     /// to global settings (more loose) followed by more strict cuts for the central
     /// and forward/backward regions separately.
 
-    float m_maxSeedsPerSpM_filter = 0; // max number of seeds a single middle sp can belong to - 1
-    float m_deltaRMin = 5* Acts::UnitConstants::mm;
-    bool m_seedConfirmation = false;
-    float m_deltaInvHelixDiameter = 0.00003 * 1. / Acts::UnitConstants::mm;
-    float m_impactWeightFactor = 1.;
-    float m_zOriginWeightFactor = 1.;
-    float m_compatSeedWeight = 200.;
-    size_t m_compatSeedLimit = 2;
-    float m_seedWeightIncrement = 0;
+    float  maxSeedsPerSpM_filter = 0; // max number of seeds a single middle sp can belong to - 1
+    float  deltaRMin             = 5* Acts::UnitConstants::mm;
+    bool   seedConfirmation      = false;
+    float  deltaInvHelixDiameter = 0.00003 * 1. / Acts::UnitConstants::mm;
+    float  impactWeightFactor    = 1.;
+    float  zOriginWeightFactor   = 1.;
+    float  compatSeedWeight      = 200.;
+    size_t compatSeedLimit       = 2;
+    float  seedWeightIncrement   = 0;
 
     ///////////////////////////////////////
     /// CENTRAL SEED FILTER PARAMETERS
-    float  m_zMinSeedConf_cent = -250 * Acts::UnitConstants::mm;
-    float  m_zMaxSeedConf_cent = 250 * Acts::UnitConstants::mm;
-    float  m_rMaxSeedConf_cent = 140 * Acts::UnitConstants::mm;
-    size_t m_nTopForLargeR_cent = 1;
-    size_t m_nTopForSmallR_cent = 2;
-    float  m_seedConfMinBottomRadius_cent = 60.0 * Acts::UnitConstants::mm;
-    float  m_seedConfMaxZOrigin_cent = 150.0 * Acts::UnitConstants::mm;
-    float  m_minImpactSeedConf_cent = 1.0 * Acts::UnitConstants::mm;
+    float  zMinSeedConfCentral  = -250 * Acts::UnitConstants::mm;
+    float  zMaxSeedConfCentral  = 250 * Acts::UnitConstants::mm;
+    float  rMaxSeedConfCentral  = 140 * Acts::UnitConstants::mm;
+    size_t nTopForLargeRCentral = 1;
+    size_t nTopForSmallRCentral = 2;
+    float  seedConfMinBottomRadiusCentral = 60.0 * Acts::UnitConstants::mm;
+    float  seedConfMaxZOriginCentral      = 150.0 * Acts::UnitConstants::mm;
+    float  minImpactSeedConfCentral       = 1.0 * Acts::UnitConstants::mm;
 
     ///////////////////////////////////////
     /// FORWARD / BACKWARD SEED FILTER PARAMETERS
-    float  m_zMinSeedConf_forw = -3000 * Acts::UnitConstants::mm;
-    float  m_zMaxSeedConf_forw = 3000 * Acts::UnitConstants::mm;
-    float  m_rMaxSeedConf_forw = 140 * Acts::UnitConstants::mm;
-    size_t m_nTopForLargeR_forw = 1;
-    size_t m_nTopForSmallR_forw = 2;
-    float  m_seedConfMinBottomRadius_forw = 60.0 * Acts::UnitConstants::mm;
-    float  m_seedConfMaxZOrigin_forw = 150.0 * Acts::UnitConstants::mm;
-    float  m_minImpactSeedConf_forw = 1.0 * Acts::UnitConstants::mm;
+    float  zMinSeedConfForward  = -3000 * Acts::UnitConstants::mm;
+    float  zMaxSeedConfForward  = 3000 * Acts::UnitConstants::mm;
+    float  rMaxSeedConfForward  = 140 * Acts::UnitConstants::mm;
+    size_t nTopForLargeRForward = 1;
+    size_t nTopForSmallRForward = 2;
+    float  seedConfMinBottomRadiusForward = 60.0 * Acts::UnitConstants::mm;
+    float  seedConfMaxZOriginForward      = 150.0 * Acts::UnitConstants::mm;
+    float  minImpactSeedConfForward       = 1.0 * Acts::UnitConstants::mm;
 
     //////////////////////////////////////
     ///Seed Covariance Error Matrix
-    float m_loc_a_Error = 1.5 * Acts::UnitConstants::mm;     //Error on Loc a
-    float m_loc_b_Error = 1.5 * Acts::UnitConstants::mm;     //Error on Loc b
-    float m_phi_Error = 0.02 * Acts::UnitConstants::rad;     //Error on phi
-    float m_theta_Error = 0.002 * Acts::UnitConstants::rad;  //Error on theta
-    float m_qOverP_Error = 0.025 / Acts::UnitConstants::GeV; //Error on q over p
-    float m_time_Error = 0.1 * Acts::UnitConstants::mm;      //Error on time
+    float locaError   = 1.5 * Acts::UnitConstants::mm;     //Error on Loc a
+    float locbError   = 1.5 * Acts::UnitConstants::mm;     //Error on Loc b
+    float phiError    = 0.02 * Acts::UnitConstants::rad;     //Error on phi
+    float thetaError  = 0.002 * Acts::UnitConstants::rad;  //Error on theta
+    float qOverPError = 0.025 / Acts::UnitConstants::GeV; //Error on q over p
+    float timeError   = 0.1 * Acts::UnitConstants::mm;      //Error on time
     // Note: Acts native time units are mm: https://acts.readthedocs.io/en/latest/core/definitions/units.html
   };
 }
