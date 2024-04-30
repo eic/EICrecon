d7702bc4df79228b4c2d1dcf5e2bd65e92d5fc5f
Simon Gardner <simon.gardner@glasgow.ac.uk>
2024-04-30 07:00:05 +0100

    Add simple tagger tracker hit clustering (#921)
    Adds hit clustering and hit point to the LowQ2 tagger reconstruction.
    - [x] Requires #919 first.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [x] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    no
    ### Does this PR change default behavior?
    Adds clustered lowq2 tracker hit points to the output
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

---
+++ src/algorithms/fardetectors/FarDetectorTrackerClusterConfig.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 - 2024, Simon Gardner
+
+#pragma once
+
+#include <edm4eic/unit_system.h>
+
+namespace eicrecon {
+  struct FarDetectorTrackerClusterConfig {
+
+    // Readout identifiers for dividing detector
+    std::string readout{""};
+    std::string x_field{"x"};
+    std::string y_field{"y"};
+
+    // Timing limit to add a hit to a cluster
+    double hit_time_limit{10*edm4eic::unit::ns};
+
+  };
+}
