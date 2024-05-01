f07ebf742013d1cdb69acffa47b39f8a1555c19e
Sebouh Paul <sebouh.paul@gmail.com>
2024-05-01 15:49:39 -0400

    corrected the copywrite info

--- src/algorithms/calorimetry/NeutronReconstruction.cc
+++ src/algorithms/calorimetry/NeutronReconstruction.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2023 Daniel Brandenburg
+// Copyright (C) 2024 Sebouh Pual
 #include "NeutronReconstruction.h"
 
 #include <edm4eic/ClusterCollection.h>
