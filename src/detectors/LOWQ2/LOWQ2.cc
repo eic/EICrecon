5618010162b69d54a240291087dc118ef58cad05
simonge <simon.gardner@glasgow.ac.uk>
2024-05-02 15:55:51 +0100

    Fix optimum pi

--- src/detectors/LOWQ2/LOWQ2.cc
+++ src/detectors/LOWQ2/LOWQ2.cc
@@ -107,7 +107,7 @@ extern "C" {
             .n_layer = 4,
             .restrict_direction = true,
             .optimum_theta = -M_PI+0.026,
-            .optimum_phi = 0,
+            .optimum_phi = M_PI,
             .step_angle_tolerance = 0.05,
           },
           app
