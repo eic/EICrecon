1f002e3d3fe035431867ab896b15850b16cedeb1
Wouter Deconinck <wdconinc@gmail.com>
2024-05-02 11:43:02 -0500

    fix: spdx header
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/tracking/AmbiguitySolverConfig.h
+++ src/algorithms/tracking/AmbiguitySolverConfig.h
@@ -1,5 +1,5 @@
-// Created by Minjung Kim (minjung.kim@lbl.gov)
-// Subject to the terms in the LICENSE file found in the top-level directory.
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2024 Minjung Kim
 
 #pragma once
 
