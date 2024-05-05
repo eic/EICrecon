7fbb29a81155d9ff09468b13498ed14600d8e3af
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-05 17:32:49 -0400

    clang-format -i src/global/tracking/TracksToParticles_factory.h

--- src/global/tracking/TracksToParticles_factory.h
+++ src/global/tracking/TracksToParticles_factory.h
@@ -3,30 +3,32 @@
 
 #pragma once
 
-#include <memory>
-#include <edm4eic/Track.h>
-#include <edm4eic/ReconstructedParticle.h>
-#include <edm4eic/MCRecoParticleAssociation.h>
-#include <edm4hep/MCParticleCollection.h>
-#include <edm4hep/ParticleID.h>
 #include "algorithms/tracking/TracksToParticles.h"
 #include "algorithms/tracking/TracksToParticlesConfig.h"
+#include <edm4eic/MCRecoParticleAssociation.h>
+#include <edm4eic/ReconstructedParticle.h>
+#include <edm4eic/Track.h>
+#include <edm4hep/MCParticleCollection.h>
+#include <edm4hep/ParticleID.h>
+#include <memory>
 
 namespace eicrecon {
 
-class TracksToParticles_factory : public JOmniFactory<TracksToParticles_factory, TracksToParticlesConfig> {
+class TracksToParticles_factory
+    : public JOmniFactory<TracksToParticles_factory, TracksToParticlesConfig> {
 public:
-using AlgoT = eicrecon::TracksToParticles;
+  using AlgoT = eicrecon::TracksToParticles;
 
 private:
   std::unique_ptr<AlgoT> m_algo;
 
-  PodioInput<edm4hep::MCParticle> m_particles_input {this};
-  PodioInput<edm4eic::Track> m_tracks_input {this};
-  PodioOutput<edm4eic::ReconstructedParticle> m_recoparticles_output {this};
-  PodioOutput<edm4eic::MCRecoParticleAssociation> m_recoassocs_output {this};
+  PodioInput<edm4hep::MCParticle> m_particles_input{this};
+  PodioInput<edm4eic::Track> m_tracks_input{this};
+  PodioOutput<edm4eic::ReconstructedParticle> m_recoparticles_output{this};
+  PodioOutput<edm4eic::MCRecoParticleAssociation> m_recoassocs_output{this};
 
-  ParameterRef<double> m_momentum_relative_tolerance{this, "momentumRelativeTolerance", config().momentumRelativeTolerance};
+  ParameterRef<double> m_momentum_relative_tolerance{this, "momentumRelativeTolerance",
+                                                     config().momentumRelativeTolerance};
   ParameterRef<double> m_phi_tolerance{this, "phiTolerance", config().phiTolerance};
   ParameterRef<double> m_eta_tolerance{this, "etaTolerance", config().etaTolerance};
 
@@ -36,14 +38,12 @@ public:
     m_algo->level((algorithms::LogLevel)logger()->level());
     m_algo->applyConfig(config());
     m_algo->init();
-  }; 
+  };
 
   void Process(int64_t run_number, uint64_t event_number) {
-    m_algo->process(
-      {m_particles_input(), m_tracks_input()},
-      {m_recoparticles_output().get(), m_recoassocs_output().get()}
-    );
+    m_algo->process({m_particles_input(), m_tracks_input()},
+                    {m_recoparticles_output().get(), m_recoassocs_output().get()});
   }
 };
 
-}
+} // namespace eicrecon
