9ee95dd9891887bed5dc865a14272440bfffa3dd
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-04 23:18:03 -0400

    add TOF LUT

--- src/detectors/BTOF/BTOF.cc
+++ src/detectors/BTOF/BTOF.cc
@@ -11,6 +11,7 @@
 #include "extensions/jana/JOmniFactoryGeneratorT.h"
 #include "factories/digi/SiliconTrackerDigi_factory.h"
 #include "factories/tracking/TrackerHitReconstruction_factory.h"
+#include "factories/pid_lut/PIDLookup_factory.h"
 
 extern "C" {
 void InitPlugin(JApplication *app) {
@@ -45,6 +46,29 @@ void InitPlugin(JApplication *app) {
         },
         app
     ));         // Hit reco default config for factories
+
+    int BarrelTOF_ID = 0;
+    try {
+        auto detector = app->GetService<DD4hep_service>()->detector();
+        BarrelTOF_ID = detector->constant<int>("BarrelTOF_ID");
+    } catch(const std::runtime_error&) {
+        // Nothing
+    }
+    app->Add(new JOmniFactoryGeneratorT<PIDLookup_factory>(
+          "TOFPID",
+          {"ReconstructedParticles", "ReconstructedParticleAssociations"},
+          {"TOFPID", "TOFParticleIDs"},
+          {
+            .filename="calibrations/tof.lut",
+            .system=BarrelTOF_ID,
+            .pdg_values={11, 211, 321, 2212},
+            .charge_values={1},
+            .momentum_edges={0.15, 0.45, 0.75, 1.05, 1.35, 1.65, 1.95, 2.25, 2.55, 2.85, 3.15, 3.45, 3.75, 4.05, 4.35, 4.65, 4.95, 5.25, 5.55, 5.85, 6.15},
+            .polar_edges={6.72, 15.17, 23.62, 32.07, 40.52, 48.97, 57.42, 65.88, 74.32, 82.78, 91.22, 99.67, 108.12, 116.57, 125.02, 133.47, 141.92, 150.38},
+            .azimuthal_binning={0., 360., 360.}, // lower, upper, step
+          },
+          app
+          ));
 
 }
 } // extern "C"
