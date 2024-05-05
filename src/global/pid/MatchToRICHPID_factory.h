b4cce0b685bc4a69d6bea0d1b6ff4f6b55e78983
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-05 17:35:01 -0400

    clang-format -i src/global/pid/MatchToRICHPID_factory.h

--- src/global/pid/MatchToRICHPID_factory.h
+++ src/global/pid/MatchToRICHPID_factory.h
@@ -3,26 +3,26 @@
 
 #pragma once
 
-#include <memory>
-#include <edm4eic/ReconstructedParticle.h>
-#include <edm4eic/CherenkovParticleID.h>
-#include <edm4hep/ParticleID.h>
 #include "algorithms/pid/MatchToRICHPID.h"
 #include "extensions/jana/JOmniFactory.h"
+#include <edm4eic/CherenkovParticleID.h>
+#include <edm4eic/ReconstructedParticle.h>
+#include <edm4hep/ParticleID.h>
+#include <memory>
 
 namespace eicrecon {
 
 class MatchToRICHPID_factory : public JOmniFactory<MatchToRICHPID_factory, MatchToRICHPIDConfig> {
 public:
-using AlgoT = eicrecon::MatchToRICHPID;
+  using AlgoT = eicrecon::MatchToRICHPID;
 
 private:
   std::unique_ptr<AlgoT> m_algo;
 
-  PodioInput<edm4eic::ReconstructedParticle> m_recoparticles_input {this};
-  PodioInput<edm4eic::CherenkovParticleID> m_cherenkov_particle_ids_input {this};
-  PodioOutput<edm4eic::ReconstructedParticle> m_recoparticles_output {this};
-  PodioOutput<edm4hep::ParticleID> m_pids_output {this};
+  PodioInput<edm4eic::ReconstructedParticle> m_recoparticles_input{this};
+  PodioInput<edm4eic::CherenkovParticleID> m_cherenkov_particle_ids_input{this};
+  PodioOutput<edm4eic::ReconstructedParticle> m_recoparticles_output{this};
+  PodioOutput<edm4hep::ParticleID> m_pids_output{this};
 
 public:
   void Configure() {
@@ -33,11 +33,9 @@ public:
   };
 
   void Process(int64_t run_number, uint64_t event_number) {
-    m_algo->process(
-      {m_recoparticles_input(), m_cherenkov_particle_ids_input()},
-      {m_recoparticles_output().get(), m_pids_output().get()}
-    );
+    m_algo->process({m_recoparticles_input(), m_cherenkov_particle_ids_input()},
+                    {m_recoparticles_output().get(), m_pids_output().get()});
   }
 };
 
-}
+} // namespace eicrecon
