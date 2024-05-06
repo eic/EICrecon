5d24dfe490c1ebec236c5313b017161856a891de
Tyler Kutz <tkutz@mit.edu>
2024-05-05 23:26:38 -0400

    Created algorithm and factory for calculating hadronic final state variables (#1405)
    ### Briefly, what does this PR introduce?
    
    This adds and algorithm and corresponding factory for calculating
    hadronic final state variables. Since this uses a new `EDM4eic` data
    type not included in major release 5, the algorithm is only called for
    major release >= 6.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [x] New feature (issue #1362 )
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    
    ### Does this PR change default behavior?
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/global/reco/reco.cc
+++ src/global/reco/reco.cc
@@ -8,6 +8,7 @@
 #include <edm4eic/MCRecoClusterParticleAssociation.h>
 #include <edm4eic/MCRecoParticleAssociation.h>
 #include <edm4eic/ReconstructedParticle.h>
+#include <edm4eic/EDM4eicVersion.h>
 #include <edm4hep/MCParticle.h>
 #include <algorithm>
 #include <map>
@@ -19,6 +20,9 @@
 #include "algorithms/reco/InclusiveKinematicsJB.h"
 #include "algorithms/reco/InclusiveKinematicsSigma.h"
 #include "algorithms/reco/InclusiveKinematicseSigma.h"
+#if EDM4EIC_VERSION_MAJOR >= 6
+#include "algorithms/reco/HadronicFinalState.h"
+#endif
 #include "extensions/jana/JOmniFactoryGeneratorT.h"
 #include "factories/meta/CollectionCollector_factory.h"
 #include "factories/meta/FilterMatching_factory.h"
@@ -27,6 +31,9 @@
 #include "factories/reco/InclusiveKinematicsTruth_factory.h"
 #include "factories/reco/JetReconstruction_factory.h"
 #include "factories/reco/TransformBreitFrame_factory.h"
+#if EDM4EIC_VERSION_MAJOR >= 6
+#include "factories/reco/HadronicFinalState_factory.h"
+#endif
 #include "global/reco/ChargedReconstructedParticleSelector_factory.h"
 #include "global/reco/MC2SmearedParticle_factory.h"
 #include "global/reco/MatchClusters_factory.h"
@@ -276,6 +283,21 @@ void InitPlugin(JApplication *app) {
             {},
             app
     ));
+
+#if EDM4EIC_VERSION_MAJOR >= 6
+    app->Add(new JOmniFactoryGeneratorT<HadronicFinalState_factory<HadronicFinalState>>(
+        "HadronicFinalState",
+        {
+          "MCParticles",
+          "ReconstructedParticles",
+          "ReconstructedParticleAssociations"
+        },
+        {
+          "HadronicFinalState"
+        },
+        app
+    ));
+#endif
 
 }
 } // extern "C"
