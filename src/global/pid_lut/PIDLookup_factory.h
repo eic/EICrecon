5e81b89a54bad93e4c44062f9a8b74f0ab9bbc11
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-07 01:26:19 -0400

    static_cast

--- src/global/pid_lut/PIDLookup_factory.h
+++ src/global/pid_lut/PIDLookup_factory.h
@@ -33,7 +33,7 @@ private:
 public:
   void Configure() {
     m_algo = std::make_unique<AlgoT>(this->GetPrefix());
-    m_algo->level((algorithms::LogLevel)logger()->level());
+    m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
     m_algo->applyConfig(config());
     m_algo->init();
   }
