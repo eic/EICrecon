c1ef8090552e1483b07f0a809676251098b97521
Wouter Deconinck <wdconinc@gmail.com>
2024-05-07 23:00:28 -0500

    fix: rm include unused SpdlogMixin (#1429)
    ### Briefly, what does this PR introduce?
    This PR removes the unused includes of SpdlogMixin in various factories.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

--- src/benchmarks/reconstruction/femc_studies/femc_studiesProcessor.cc
+++ src/benchmarks/reconstruction/femc_studies/femc_studiesProcessor.cc
@@ -45,7 +45,6 @@
 void femc_studiesProcessor::Init() {
   std::string plugin_name = ("femc_studies");
 
-  // InitLogger(plugin_name);
   // ===============================================================================================
   // Get JANA application and seup general variables
   // ===============================================================================================
