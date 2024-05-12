a38749b4e1318ac36da9a89d4c80af60ba7fe188
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-05-12 17:54:53 -0400

    Add code to copy unused clusters into output collection

--- src/algorithms/calorimetry/TrackClusterMergeSplitter.cc
+++ src/algorithms/calorimetry/TrackClusterMergeSplitter.cc
@@ -7,12 +7,16 @@
 #include <iostream>  // TEST
 // dd4hep utilities
 #include <DD4hep/Readout.h>
+// jana utilities
+#include <JANA/JException.h>
 // edm4eic types
 #include <edm4eic/TrackSegment.h>
 #include <edm4eic/CalorimeterHit.h>
 
 // algorithm definition
 #include "TrackClusterMergeSplitter.h"
+
+
 
 namespace eicrecon {
 
@@ -29,6 +33,8 @@ namespace eicrecon {
 
   }  // end 'init(dd4hep::Detector*)'
 
+
+
   // --------------------------------------------------------------------------
   //! Process inputs
   // --------------------------------------------------------------------------
@@ -41,10 +47,60 @@ namespace eicrecon {
     const auto [in_clusters, in_projections] = input;
     auto [out_clusters] = output;
 
+    // exit if no clusters in collection
+    if (in_clusters -> size() == 0) {
+      throw JException("No clusters in input collection!");      
+    }
+
+    // reset bookkeeping containers
+    reset_bookkeepers();
+
+    // flag all clusters as not being consumed
+    for (auto cluster : *in_clusters) {
+      m_mapIsConsumed.insert(
+        {cluster.getObjectID().index, false}
+      );
+    }
+
     // collect relevant projections
     get_projections(in_projections, (*in_clusters)[0].getHits(0));
 
+    /* TODO
+     *   - Add merging step
+     *   - Splitting step
+     */
+
+    // copy unused clusters to output
+    for (auto in_cluster : *in_clusters) {
+
+      // ignore used clusters
+      if (m_mapIsConsumed[in_cluster.getObjectID().index]) {
+        continue;
+      }
+
+      // copy cluster and add to output collection
+      edm4eic::MutableCluster out_cluster = out_clusters->create();
+      copy_cluster(in_cluster, out_cluster);
+
+    }  // end cluster loop
+    trace("Copied unused clusters into output collection");
+
   }  // end 'process(Input&, Output&)'
+
+
+
+  // --------------------------------------------------------------------------
+  //! Reset bookkeeping containers
+  // --------------------------------------------------------------------------
+  void TrackClusterMergeSplitter::reset_bookkeepers() const {
+
+    m_mapIsConsumed.clear();
+    m_vecProject.clear();
+    trace("Reset bookkeeping containers");
+
+  }  // end 'reset_bookkeepers()'
+
+
 
   // --------------------------------------------------------------------------
   //! Collect projections pointing to calorimeter
@@ -71,9 +127,7 @@ namespace eicrecon {
     const int id = m_detector -> constant<int>(
       std::regex_replace(readout.name(), std::regex("Hits"), "_ID")
     );
-
-    // make sure projection vector is empty
-    m_vecProject.clear();
+    debug("Collecting projections to detector with system id {}", id);
 
     // collect projections
     for (auto project : *projections) {
@@ -85,7 +139,53 @@ namespace eicrecon {
         }
       }  // end point loop
     }  // end projection loop
+    trace("Collected relevant projections: {} to process", m_vecProject.size());
 
   }  // end 'get_projections(edm4eic::CalorimeterHit&, edm4eic::TrackSegmentCollection&)'
+
+
+
+  // --------------------------------------------------------------------------
+  //! Copy cluster onto new one
+  // --------------------------------------------------------------------------
+  void TrackClusterMergeSplitter::copy_cluster(
+    const edm4eic::Cluster& old_clust,
+    edm4eic::MutableCluster& new_clust
+  ) const {
+
+    // set main variables
+    new_clust.setType( old_clust.getType() );
+    new_clust.setEnergy( old_clust.getEnergy() );
+    new_clust.setTime( old_clust.getTime() );
+    new_clust.setTimeError( old_clust.getTimeError() );
+    new_clust.setNhits( old_clust.getHits().size() );
+    new_clust.setPosition( old_clust.getPosition() );
+    new_clust.setPositionError( old_clust.getPositionError() );
+    new_clust.setIntrinsicTheta( old_clust.getIntrinsicTheta() );
+    new_clust.setIntrinsicPhi( old_clust.getIntrinsicPhi() );
+    new_clust.setIntrinsicDirectionError( old_clust.getIntrinsicDirectionError() );
+
+    // set vector members
+    for (float old_parameter : old_clust.getShapeParameters()) {
+      new_clust.addToShapeParameters( old_parameter );
+    }
+    for (float old_contribution : old_clust.getHitContributions()) {
+      new_clust.addToHitContributions( old_contribution );
+    }
+    for (float old_energy : old_clust.getSubdetectorEnergies()) {
+      new_clust.addToSubdetectorEnergies( old_energy );
+    }
+
+    // set one-to-many relations
+    for (auto old_hit : old_clust.getHits()) {
+      new_clust.addToHits( old_hit );
+    }
+    for (auto old_pid : old_clust.getParticleIDs()) {
+      new_clust.addToParticleIDs( old_pid );
+    }
+    new_clust.addToClusters( old_clust );
+    trace("Copied input cluster {} onto output cluster {}", old_clust.getObjectID().index, new_clust.getObjectID().index);
+
+  }  // end 'copy_cluster(edm4eic::Cluster&, edm4eic::MutableCluster&)'
 
 }  // end eicrecon namespace
--- src/algorithms/calorimetry/TrackClusterMergeSplitter.h
+++ src/algorithms/calorimetry/TrackClusterMergeSplitter.h
@@ -4,6 +4,7 @@
 #ifndef EICRECON_TRACKCLUSTERMERGESPLITTER_H
 #define EICRECON_TRACKCLUSTERMERGESPLITTER_H
 
+#include <map>
 #include <string>
 #include <vector>
 #include <utility>
@@ -20,6 +21,8 @@
 // for algorithm configuration
 #include "algorithms/interfaces/WithPodConfig.h"
 #include "TrackClusterMergeSplitterConfig.h"
+
+
 
 namespace eicrecon {
 
@@ -42,7 +45,7 @@ namespace eicrecon {
    *
    *  Heavily inspired by Eur. Phys. J. C (2017) 77:466
    */
-  class TrackClusterMergeSplitter :
+  class TrackClusterMergeSplitter : 
     public TrackClusterMergeSplitterAlgorithm,
     public WithPodConfig<TrackClusterMergeSplitterConfig>
   {
@@ -65,13 +68,16 @@ namespace eicrecon {
     private:
 
       // private methods
+      void reset_bookkeepers() const;
       void get_projections(const edm4eic::TrackSegmentCollection* projections, const edm4eic::CalorimeterHit& cluster) const;
+      void copy_cluster(const edm4eic::Cluster& old_clust, edm4eic::MutableCluster& new_clust) const;
 
       // additional services
       const dd4hep::Detector* m_detector {NULL};
       const dd4hep::rec::CellIDPositionConverter* m_converter {NULL};
 
       // bookkeeping members
+      mutable std::map<int, bool> m_mapIsConsumed;
       mutable std::vector<edm4eic::TrackPoint> m_vecProject;
 
   };  // end TrackClusterMergeSplitter
@@ -79,3 +85,4 @@ namespace eicrecon {
 }  // end eicrecon namespace
 
 #endif
+
