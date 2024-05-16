726d70e99a4ee382f2304754fbf68c7ba20ad01a
Sebouh Paul <sebouh.paul@gmail.com>
2024-05-16 13:27:32 -0400

    attempt to appease Iwyu

--- src/global/reco/reco.cc
+++ src/global/reco/reco.cc
@@ -11,7 +11,7 @@
 #include <edm4eic/MCRecoParticleAssociation.h>
 #include <edm4eic/ReconstructedParticle.h>
 #include <edm4hep/MCParticle.h>
-#include <algorithm>
+//#include <algorithm>
 #include <map>
 #include <memory>
 
