70b2eb4bb142c6212ecc48f6906e387b906ff0a0
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-20 13:27:09 -0400

    Drop support for EDM4eic 4.0.0 and older (#1435)
    Resolves #1389

--- src/algorithms/tracking/CKFTracking.cc
+++ src/algorithms/tracking/CKFTracking.cc
@@ -29,7 +29,7 @@
 #include <ActsExamples/EventData/MeasurementCalibration.hpp>
 #include <ActsExamples/EventData/Track.hpp>
 #include <edm4eic/Cov3f.h>
-#include <edm4eic/EDM4eicVersion.h>
+#include <edm4eic/Cov6f.h>
 #include <edm4eic/Measurement2DCollection.h>
 #include <edm4eic/TrackParametersCollection.h>
 #include <edm4eic/TrajectoryCollection.h>
@@ -46,10 +46,6 @@
 #include <optional>
 #include <utility>
 
-#if EDM4EIC_VERSION_MAJOR >= 5
-#include <edm4eic/Cov6f.h>
-#endif
-
 #include "ActsGeometryProvider.h"
 #include "DD4hepBField.h"
 #include "extensions/spdlog/SpdlogFormatters.h" // IWYU pragma: keep
@@ -59,22 +55,20 @@ namespace eicrecon {
 
     using namespace Acts::UnitLiterals;
 
-    #if EDM4EIC_VERSION_MAJOR >= 5
-      // This array relates the Acts and EDM4eic covariance matrices, including
-      // the unit conversion to get from Acts units into EDM4eic units.
-      //
-      // Note: std::map is not constexpr, so we use a constexpr std::array
-      // std::array initialization need double braces since arrays are aggregates
-      // ref: https://en.cppreference.com/w/cpp/language/aggregate_initialization
-      static constexpr std::array<std::pair<Acts::BoundIndices, double>, 6> edm4eic_indexed_units{{
-        {Acts::eBoundLoc0, Acts::UnitConstants::mm},
-        {Acts::eBoundLoc1, Acts::UnitConstants::mm},
-        {Acts::eBoundPhi, 1.},
-        {Acts::eBoundTheta, 1.},
-        {Acts::eBoundQOverP, 1. / Acts::UnitConstants::GeV},
-        {Acts::eBoundTime, Acts::UnitConstants::ns}
-      }};
-    #endif
+    // This array relates the Acts and EDM4eic covariance matrices, including
+    // the unit conversion to get from Acts units into EDM4eic units.
+    //
+    // Note: std::map is not constexpr, so we use a constexpr std::array
+    // std::array initialization need double braces since arrays are aggregates
+    // ref: https://en.cppreference.com/w/cpp/language/aggregate_initialization
+    static constexpr std::array<std::pair<Acts::BoundIndices, double>, 6> edm4eic_indexed_units{{
+      {Acts::eBoundLoc0, Acts::UnitConstants::mm},
+      {Acts::eBoundLoc1, Acts::UnitConstants::mm},
+      {Acts::eBoundPhi, 1.},
+      {Acts::eBoundTheta, 1.},
+      {Acts::eBoundQOverP, 1. / Acts::UnitConstants::GeV},
+      {Acts::eBoundTime, Acts::UnitConstants::ns}
+    }};
 
     CKFTracking::CKFTracking() {
     }
@@ -163,22 +157,13 @@ namespace eicrecon {
             double charge = std::copysign(1., track_parameter.getQOverP());
 
             Acts::BoundSquareMatrix cov = Acts::BoundSquareMatrix::Zero();
-            #if EDM4EIC_VERSION_MAJOR >= 5
-              for (size_t i = 0; const auto& [a, x] : edm4eic_indexed_units) {
-                for (size_t j = 0; const auto& [b, y] : edm4eic_indexed_units) {
-                  cov(a, b) = track_parameter.getCovariance()(i,j) * x * y;
-                  ++j;
-                }
-                ++i;
+            for (size_t i = 0; const auto& [a, x] : edm4eic_indexed_units) {
+              for (size_t j = 0; const auto& [b, y] : edm4eic_indexed_units) {
+                cov(a, b) = track_parameter.getCovariance()(i,j) * x * y;
+                ++j;
               }
-            #else
-              cov(Acts::eBoundLoc0, Acts::eBoundLoc0)     = std::pow( track_parameter.getLocError().xx ,2)*Acts::UnitConstants::mm*Acts::UnitConstants::mm;
-              cov(Acts::eBoundLoc1, Acts::eBoundLoc1)     = std::pow( track_parameter.getLocError().yy,2)*Acts::UnitConstants::mm*Acts::UnitConstants::mm;
-              cov(Acts::eBoundTheta, Acts::eBoundTheta)   = std::pow( track_parameter.getMomentumError().xx,2);
-              cov(Acts::eBoundPhi, Acts::eBoundPhi)       = std::pow( track_parameter.getMomentumError().yy,2);
-              cov(Acts::eBoundQOverP, Acts::eBoundQOverP) = std::pow( track_parameter.getMomentumError().zz,2) / (Acts::UnitConstants::GeV*Acts::UnitConstants::GeV);
-              cov(Acts::eBoundTime, Acts::eBoundTime)     = std::pow( track_parameter.getTimeError(),2)*Acts::UnitConstants::ns*Acts::UnitConstants::ns;
-            #endif
+              ++i;
+            }
 
             // Construct a perigee surface as the target surface
             auto pSurface = Acts::Surface::makeShared<const Acts::PerigeeSurface>(Acts::Vector3(0,0,0));
@@ -351,10 +336,6 @@ namespace eicrecon {
 
             // Create trajectory
             auto trajectory = trajectories->create();
-            #if EDM4EIC_VERSION_MAJOR < 5
-              trajectory.setChi2(trajectoryState.chi2Sum);
-              trajectory.setNdf(trajectoryState.NDF);
-            #endif
             trajectory.setNMeasurements(trajectoryState.nMeasurements);
             trajectory.setNStates(trajectoryState.nStates);
             trajectory.setNOutliers(trajectoryState.nOutliers);
@@ -390,66 +371,46 @@ namespace eicrecon {
             pars.setPhi(static_cast<float>(parameter[Acts::eBoundPhi]));
             pars.setQOverP(static_cast<float>(parameter[Acts::eBoundQOverP]));
             pars.setTime(static_cast<float>(parameter[Acts::eBoundTime]));
-            #if EDM4EIC_VERSION_MAJOR >= 5
-              edm4eic::Cov6f cov;
-              for (size_t i = 0; const auto& [a, x] : edm4eic_indexed_units) {
-                for (size_t j = 0; const auto& [b, y] : edm4eic_indexed_units) {
-                  // FIXME why not pars.getCovariance()(i,j) = covariance(a,b) / x / y;
-                  cov(i,j) = covariance(a,b) / x / y;
-                }
+            edm4eic::Cov6f cov;
+            for (size_t i = 0; const auto& [a, x] : edm4eic_indexed_units) {
+              for (size_t j = 0; const auto& [b, y] : edm4eic_indexed_units) {
+                // FIXME why not pars.getCovariance()(i,j) = covariance(a,b) / x / y;
+                cov(i,j) = covariance(a,b) / x / y;
               }
-              pars.setCovariance(cov);
-            #else
-              pars.setCharge(static_cast<float>(boundParam.charge()));
-              pars.setLocError({
-                    static_cast<float>(covariance(Acts::eBoundLoc0, Acts::eBoundLoc0)),
-                    static_cast<float>(covariance(Acts::eBoundLoc1, Acts::eBoundLoc1)),
-                    static_cast<float>(covariance(Acts::eBoundLoc0, Acts::eBoundLoc1))
-                });
-              pars.setMomentumError({
-                    static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundTheta)),
-                    static_cast<float>(covariance(Acts::eBoundPhi, Acts::eBoundPhi)),
-                    static_cast<float>(covariance(Acts::eBoundQOverP, Acts::eBoundQOverP)),
-                    static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundPhi)),
-                    static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundQOverP)),
-                    static_cast<float>(covariance(Acts::eBoundPhi, Acts::eBoundQOverP))
-                });
-              pars.setTimeError(sqrt(static_cast<float>(covariance(Acts::eBoundTime, Acts::eBoundTime))));
-            #endif
+            }
+            pars.setCovariance(cov);
 
             trajectory.addToTrackParameters(pars);
 
             // Fill tracks
-            #if EDM4EIC_VERSION_MAJOR >= 5
-              auto track = tracks->create();
-              track.setType(                             // Flag that defines the type of track
-                pars.getType()
-              );
-              track.setPosition(                         // Track 3-position at the vertex
-                edm4hep::Vector3f()
-              );
-              track.setMomentum(                         // Track 3-momentum at the vertex [GeV]
-                edm4hep::Vector3f()
-              );
-              track.setPositionMomentumCovariance(       // Covariance matrix in basis [x,y,z,px,py,pz]
-                edm4eic::Cov6f()
-              );
-              track.setTime(                             // Track time at the vertex [ns]
-                static_cast<float>(parameter[Acts::eBoundTime])
-              );
-              track.setTimeError(                        // Error on the track vertex time
-                sqrt(static_cast<float>(covariance(Acts::eBoundTime, Acts::eBoundTime)))
-              );
-              track.setCharge(                           // Particle charge
-                std::copysign(1., parameter[Acts::eBoundQOverP])
-              );
-              track.setChi2(trajectoryState.chi2Sum);    // Total chi2
-              track.setNdf(trajectoryState.NDF);         // Number of degrees of freedom
-              track.setPdg(                              // PDG particle ID hypothesis
-                boundParam.particleHypothesis().absolutePdg()
-              );
-              track.setTrajectory(trajectory);           // Trajectory of this track
-            #endif
+            auto track = tracks->create();
+            track.setType(                             // Flag that defines the type of track
+              pars.getType()
+            );
+            track.setPosition(                         // Track 3-position at the vertex
+              edm4hep::Vector3f()
+            );
+            track.setMomentum(                         // Track 3-momentum at the vertex [GeV]
+              edm4hep::Vector3f()
+            );
+            track.setPositionMomentumCovariance(       // Covariance matrix in basis [x,y,z,px,py,pz]
+              edm4eic::Cov6f()
+            );
+            track.setTime(                             // Track time at the vertex [ns]
+              static_cast<float>(parameter[Acts::eBoundTime])
+            );
+            track.setTimeError(                        // Error on the track vertex time
+              sqrt(static_cast<float>(covariance(Acts::eBoundTime, Acts::eBoundTime)))
+            );
+            track.setCharge(                           // Particle charge
+              std::copysign(1., parameter[Acts::eBoundQOverP])
+            );
+            track.setChi2(trajectoryState.chi2Sum);    // Total chi2
+            track.setNdf(trajectoryState.NDF);         // Number of degrees of freedom
+            track.setPdg(                              // PDG particle ID hypothesis
+              boundParam.particleHypothesis().absolutePdg()
+            );
+            track.setTrajectory(trajectory);           // Trajectory of this track
 
             // save measurement2d to good measurements or outliers according to srclink index
             // fix me: ideally, this should be integrated into multitrajectoryhelper
@@ -472,22 +433,14 @@ namespace eicrecon {
                     } else {
                         auto meas2D = meas2Ds[srclink_index];
                         if (typeFlags.test(Acts::TrackStateFlag::MeasurementFlag)) {
-                          #if EDM4EIC_VERSION_MAJOR >= 5
-                            track.addToMeasurements(meas2D);
-                            trajectory.addToMeasurements_deprecated(meas2D);
-                          #else
-                            trajectory.addToMeasurementHits(meas2D);
-                          #endif
+                          track.addToMeasurements(meas2D);
+                          trajectory.addToMeasurements_deprecated(meas2D);
                           m_log->debug("Measurement on geo id={}, index={}, loc={},{}",
                                 geoID, srclink_index, meas2D.getLoc().a, meas2D.getLoc().b);
 
                         }
                         else if (typeFlags.test(Acts::TrackStateFlag::OutlierFlag)) {
-                          #if EDM4EIC_VERSION_MAJOR >= 5
-                            trajectory.addToOutliers_deprecated(meas2D);
-                          #else
-                            trajectory.addToOutlierHits(meas2D);
-                          #endif
+                          trajectory.addToOutliers_deprecated(meas2D);
                           m_log->debug("Outlier on geo id={}, index={}, loc={},{}",
                                 geoID, srclink_index, meas2D.getLoc().a, meas2D.getLoc().b);
 
