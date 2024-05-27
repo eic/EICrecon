21105510b9ebe6096b172dfab386ff3e24e88435
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-27 16:18:55 -0400

    fix typo

--- src/algorithms/tracking/ActsToTracks.h
+++ src/algorithms/tracking/ActsToTracks.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2024 Dmitry Kalinkin
 
-#pragma one
+#pragma once
 
 #include <ActsExamples/EventData/Track.hpp>
 #include <ActsExamples/EventData/Trajectories.hpp>
