c5acb93af6f63866af980295f23a27df9165d6df
Barak Schmookler <bschmookler1@gmail.com>
2024-06-01 09:04:22 -0700

    Update src/algorithms/tracking/AmbiguitySolver.cc
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/tracking/AmbiguitySolver.cc
+++ src/algorithms/tracking/AmbiguitySolver.cc
@@ -53,7 +53,7 @@ static std::size_t sourceLinkHash(const Acts::SourceLink& a) {
   return static_cast<std::size_t>(a.get<ActsExamples::IndexSourceLink>().index());
 }
 
-bool sourceLinkEquality(const Acts::SourceLink& a, const Acts::SourceLink& b) {
+static bool sourceLinkEquality(const Acts::SourceLink& a, const Acts::SourceLink& b) {
   return a.get<ActsExamples::IndexSourceLink>().index() ==
          b.get<ActsExamples::IndexSourceLink>().index();
 }
