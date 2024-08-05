e9e2df38dc01282837fff256ade4fa741d37b351
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-08-05 12:15:10 -0400

    Update src/algorithms/tracking/TrackerHitReconstruction.cc

--- src/algorithms/tracking/TrackerHitReconstruction.cc
+++ src/algorithms/tracking/TrackerHitReconstruction.cc
@@ -75,7 +75,9 @@ std::unique_ptr<edm4eic::TrackerHitCollection> TrackerHitReconstruction::process
             m_cfg.timeResolution,                            // in ns
             static_cast<float>(raw_hit.getCharge() / 1.0e6),   // Collected energy (GeV)
             0.0F);                                       // Error on the energy
-        rec_hits->setRawHit(raw_hit);
+#if EDM4EIC_VERSION_MAJOR >= 7
+                rec_hits->setRawHit(raw_hit);
+#endif
 
     }
 
