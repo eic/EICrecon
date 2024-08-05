b4f8dfac6894d0f56c60f554ba376e65d43db35b
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-08-05 12:17:11 -0400

    Update src/detectors/ZDC/ZDC.cc
    Co-authored-by: Derek M Anderson <derek.murphy.anderson@protonmail.com>

--- src/detectors/ZDC/ZDC.cc
+++ src/detectors/ZDC/ZDC.cc
@@ -163,7 +163,7 @@ extern "C" {
                 .layerMode=eicrecon::ImagingTopoClusterConfig::ELayerMode::xy,
                 .sectorDist = 10.0 * dd4hep::cm,
                 .minClusterHitEdep = 100.0 * dd4hep::keV,
-                .minClusterCenterEdep =3.0 * dd4hep::MeV,
+                .minClusterCenterEdep = 3.0 * dd4hep::MeV,
                 .minClusterEdep = 11.0 * dd4hep::MeV,
                 .minClusterNhits = 100,
             },
