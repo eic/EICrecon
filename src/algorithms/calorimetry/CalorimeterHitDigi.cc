4ba2fa761c8ccb8af2ac6b0c09c5adf9d796d9a1
Wouter Deconinck <wdconinc@gmail.com>
2024-08-06 19:42:04 -0500

    fix: use correct comment character

--- src/algorithms/calorimetry/CalorimeterHitDigi.cc
+++ src/algorithms/calorimetry/CalorimeterHitDigi.cc
@@ -183,10 +183,10 @@ void CalorimeterHitDigi::process(
                 : 0;
         double corrMeanScale_value = corrMeanScale(leading_hit);
 
-        # Pedestal in ADC channels must be positive
+        // Pedestal in ADC channels must be positive
         double ped = std::max(m_cfg.pedMeanADC + m_gaussian(m_generator) * m_cfg.pedSigmaADC, 0);
 
-        # Note: both adc and tdc values must be positive numbers to avoid integer wraparound
+        // Note: both adc and tdc values must be positive numbers to avoid integer wraparound
         unsigned long long adc = std::llround(ped + edep * corrMeanScale_value * (1.0 + eResRel) / m_cfg.dyRangeADC * m_cfg.capADC);
         unsigned long long tdc = std::llround((time + m_gaussian(m_generator) * tRes) * stepTDC);
 
