39e66cdbecf83fa33b94df441aba8f61e91c1af7
Wouter Deconinck <wdconinc@gmail.com>
2024-08-15 18:14:40 -0500

    fix: iwyu

--- src/detectors/BEMC/BEMC.cc
+++ src/detectors/BEMC/BEMC.cc
@@ -3,10 +3,14 @@
 
 #include <Evaluator/DD4hepUnits.h>
 #include <JANA/JApplicationFwd.h>
+#include <edm4eic/CalorimeterHitCollection.h>
+#include <edm4eic/EDM4eicVersion.h>
 #include <edm4eic/unit_system.h>
 #include <cmath>
+#include <map>
 #include <string>
 #include <variant>
+#include <vector>
 
 #include "algorithms/calorimetry/CalorimeterHitDigiConfig.h"
 #include "algorithms/calorimetry/SimCalorimeterHitProcessorConfig.h"
