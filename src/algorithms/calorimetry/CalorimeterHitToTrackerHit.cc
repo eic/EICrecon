9ff6a88706792fad9314844f4dd1441ad5dc1beb
Wouter Deconinck <wdconinc@gmail.com>
2024-08-15 15:41:41 -0500

    feat: inject the barrel imaging calorimeter hits into track reconstruction

---
+++ src/algorithms/calorimetry/CalorimeterHitToTrackerHit.cc
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2024 Wouter Deconinck
+
+#include "algorithms/calorimetry/CalorimeterHitToTrackerHit.h"
+
+#include <DD4hep/Alignments.h>
+#include <DD4hep/IDDescriptor.h>
+#include <DD4hep/Objects.h>
+#include <DD4hep/Readout.h>
+#include <DD4hep/Segmentations.h>
+#include <DD4hep/Shapes.h>
+#include <DD4hep/VolumeManager.h>
+#include <DD4hep/Volumes.h>
+#include <DD4hep/config.h>
+#include <DDSegmentation/BitFieldCoder.h>
+#include <Evaluator/DD4hepUnits.h>
+#include <Math/GenVector/Cartesian3D.h>
+#include <Math/GenVector/DisplacementVector3D.h>
+#include <algorithms/service.h>
+#include <edm4eic/EDM4eicVersion.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <algorithm>
+#include <cctype>
+#include <gsl/pointers>
+#include <map>
+#include <ostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "services/evaluator/EvaluatorSvc.h"
+
+using namespace dd4hep;
+
+namespace eicrecon {
+
+void CalorimeterHitToTrackerHit::init() { }
+
+void CalorimeterHitToTrackerHit::process(
+      const CalorimeterHitToTrackerHit::Input& input,
+      const CalorimeterHitToTrackerHit::Output& output) const {
+
+    const auto [calorimeter_hits] = input;
+    auto [tracker_hits] = output;
+
+    for (const auto &calorimeter_hit: *calorimeter_hits) {
+
+        edm4eic::CovDiag3f position_error;
+
+        [[maybe_unused]]
+        auto tracker_hit = tracker_hits->create(
+            calorimeter_hit.getCellID(),
+            calorimeter_hit.getPosition(),
+            position_error,
+            calorimeter_hit.getTime(),
+            calorimeter_hit.getTimeError(),
+            calorimeter_hit.getEnergy(),
+            calorimeter_hit.getEnergyError()
+        );
+
+    }
+}
+
+} // namespace eicrecon
--- src/detectors/BEMC/BEMC.cc
+++ src/detectors/BEMC/BEMC.cc
@@ -14,11 +14,14 @@
 #include "factories/calorimetry/CalorimeterClusterRecoCoG_factory.h"
 #include "factories/calorimetry/CalorimeterHitDigi_factory.h"
 #include "factories/calorimetry/CalorimeterHitReco_factory.h"
+#include "factories/calorimetry/CalorimeterHitToTrackerHit_factory.h"
 #include "factories/calorimetry/CalorimeterIslandCluster_factory.h"
 #include "factories/calorimetry/EnergyPositionClusterMerger_factory.h"
 #include "factories/calorimetry/ImagingClusterReco_factory.h"
 #include "factories/calorimetry/ImagingTopoCluster_factory.h"
 #include "factories/calorimetry/TruthEnergyPositionClusterMerger_factory.h"
+#include "factories/meta/SubDivideCollection_factory.h"
+#include "algorithms/meta/SubDivideFunctors.h"
 
 
 extern "C" {
@@ -155,8 +158,18 @@ extern "C" {
             .layerField = "layer",
             .sectorField = "sector",
           },
-           app   // TODO: Remove me once fixed
+          app   // TODO: Remove me once fixed
         ));
+        app->Add(new JOmniFactoryGeneratorT<SubDivideCollection_factory<edm4eic::CalorimeterHit>>(
+          "EcalBarrelImaging1stLayerRecHits", {"EcalBarrelImagingRecHits"}, {"EcalBarrelImaging1stLayerRecHits"},
+          {
+            .function = ValueSplit<&edm4eic::CalorimeterHit::getLayer>{{{1}}},
+          },
+          app));
+        app->Add(new JOmniFactoryGeneratorT<CalorimeterHitToTrackerHit_factory>(
+          "EcalBarrelImagingTrackerRecHits", {"EcalBarrelImaging1stLayerRecHits"}, {"EcalBarrelImagingTrackerRecHits"},
+          app));
+
         app->Add(new JOmniFactoryGeneratorT<ImagingTopoCluster_factory>(
           "EcalBarrelImagingProtoClusters", {"EcalBarrelImagingRecHits"}, {"EcalBarrelImagingProtoClusters"},
           {
