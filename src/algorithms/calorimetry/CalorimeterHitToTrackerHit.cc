7adabf3837f08e15c7775468fd45b432af6234b9
Wouter Deconinck <wdconinc@gmail.com>
2024-08-16 19:30:27 -0500

    feat: set position error for CartesianGridXY as dim / sqrt(12)

--- src/algorithms/calorimetry/CalorimeterHitToTrackerHit.cc
+++ src/algorithms/calorimetry/CalorimeterHitToTrackerHit.cc
@@ -21,12 +21,35 @@ void CalorimeterHitToTrackerHit::process(const CalorimeterHitToTrackerHit::Input
 
   for (const auto& calorimeter_hit : *calorimeter_hits) {
 
-    edm4eic::CovDiag3f position_error;
+    // Get CellID
+    auto cell_id{calorimeter_hit.getCellID()};
 
-    [[maybe_unused]] auto tracker_hit = tracker_hits->create(
-        calorimeter_hit.getCellID(), calorimeter_hit.getPosition(), position_error,
-        calorimeter_hit.getTime(), calorimeter_hit.getTimeError(), calorimeter_hit.getEnergy(),
-        calorimeter_hit.getEnergyError());
+    // Determine detector element
+    auto det_element = m_volume_manager.lookupDetElement(cell_id);
+
+    // Cache position errors for detector elements
+    static std::map<dd4hep::DetElement, edm4eic::CovDiag3f> position_error;
+    if (position_error.count(det_element) == 0) {
+
+      // Determine readout and segmentation
+      auto readout      = m_converter->findReadout(det_element);
+      auto segmentation = readout.segmentation();
+
+      // Determine position uncertainty
+      if (segmentation.type() == "CartesianGridXY") {
+        auto cell_dimensions           = m_converter->cellDimensions(cell_id);
+        position_error[det_element].xx = cell_dimensions[0] / std::sqrt(12);
+        position_error[det_element].yy = cell_dimensions[1] / std::sqrt(12);
+      } else {
+        continue;
+      }
+    }
+
+    // Create tracker hit
+    tracker_hits->create(calorimeter_hit.getCellID(), calorimeter_hit.getPosition(),
+                         position_error[det_element], calorimeter_hit.getTime(),
+                         calorimeter_hit.getTimeError(), calorimeter_hit.getEnergy(),
+                         calorimeter_hit.getEnergyError());
   }
 }
 
