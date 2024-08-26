cdeaf5b746f0a1df3b2340dc2b2794ff9effca3c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-08-26 15:35:06 -0400

    add EPSILON definition

--- src/algorithms/tracking/IterativeVertexFinder.cc
+++ src/algorithms/tracking/IterativeVertexFinder.cc
@@ -240,8 +240,9 @@ std::unique_ptr<edm4eic::VertexCollection> eicrecon::IterativeVertexFinder::prod
           const auto& traj = trk.getTrajectory();
           const auto& trkPars = traj.getTrackParameters();
           for (const auto par : trkPars) {
-            constexpr double acts_to_edm4eic = edm4eic::unit::mm / Acts::UnitConstants::mm;
-            if(fabs(par.getLoc().a - loc_a * acts_to_edm4eic) < 1.e-4 && fabs(par.getLoc().b - loc_b * acts_to_edm4eic) < 1.e-4) {
+            const double EPSILON = 1.0e-4; // mm
+            if (fabs((par.getLoc().a / edm4eic::unit::mm) - (loc_a / Acts::UnitConstants::mm)) < EPSILON
+              && fabs((par.getLoc().b / edm4eic::unit::mm) - (loc_b / Acts::UnitConstants::mm)) < EPSILON) {
               m_log->trace("From ReconParticles, track local position [Loc a, Loc b] = {} mm, {} mm", par.getLoc().a / edm4eic::unit::mm, par.getLoc().b / edm4eic::unit::mm);
               eicvertex.addToAssociatedParticles(part);
             } // endif
