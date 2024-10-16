468c0a74e1fd37450c16f92279fbaee7952d21ba
Michael Pitt <m_u_pitt@yahoo.com>
2024-10-16 22:18:33 +0300

    update dynamic range of B0 ECAL (#1623)
    ### Briefly, what does this PR introduce?
    This RP solves the truncation in energy response of the B0 calorimeter
    as reported in slide 11
    [here](https://indico.bnl.gov/event/24923/#12-b0-emcal). The energy
    threshold based on the simulated light yields of calorimeter crystals
    interfaces with the readout (slide 12 from the presentation above)
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [X] Other: update thresholds in reconstruction
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    no 
    ### Does this PR change default behavior?
    restore correct energy response for B0 ECAL.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/detectors/B0ECAL/B0ECAL.cc
+++ src/detectors/B0ECAL/B0ECAL.cc
@@ -33,11 +33,12 @@ extern "C" {
           {"B0ECalRawHits"},
 #endif
           {
-            .eRes = {0.0 * sqrt(dd4hep::GeV), 0.02, 0.0 * dd4hep::GeV},
+                         // The stocastic term is set using light yield in PbOW4 of N_photons = 145.75 / GeV / mm, for 6x6 mm2 sensors with PDE=0.18 (a=1/sqrt(145.75*36*0.18))
+            .eRes = {0.0326 * sqrt(dd4hep::GeV), 0.00, 0.0 * dd4hep::GeV},
             .tRes = 0.0 * dd4hep::ns,
             .threshold= 5.0 * dd4hep::MeV,
             .capADC = 16384,
-            .dyRangeADC = 20 * dd4hep::GeV,
+            .dyRangeADC = 170 * dd4hep::GeV,
             .pedMeanADC = 100,
             .pedSigmaADC = 1,
             .resolutionTDC = 1e-11,
@@ -50,12 +51,12 @@ extern "C" {
           "B0ECalRecHits", {"B0ECalRawHits"}, {"B0ECalRecHits"},
           {
             .capADC = 16384,
-            .dyRangeADC = 20. * dd4hep::GeV,
+            .dyRangeADC = 170. * dd4hep::GeV,
             .pedMeanADC = 100,
             .pedSigmaADC = 1,
             .resolutionTDC = 1e-11,
             .thresholdFactor = 0.0,
-            .thresholdValue = 0.0,
+            .thresholdValue = 1.0, // using threshold of 10 photons = 10 MeV = 1 ADC
             .sampFrac = "0.998",
             .readout = "B0ECalHits",
             .sectorField = "sector",
