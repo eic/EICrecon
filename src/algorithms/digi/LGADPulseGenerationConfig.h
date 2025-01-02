00936904681e4eaa1b1cf23acc17b6dff564aac1
Tommy Chun Yuen Tsang <tommytsang3108@hotmail.com.hk>
2025-01-02 17:37:14 -0500

    Update src/algorithms/digi/LGADPulseGenerationConfig.h
    Co-authored-by: Derek M Anderson <derek.murphy.anderson@protonmail.com>

--- src/algorithms/digi/LGADPulseGenerationConfig.h
+++ src/algorithms/digi/LGADPulseGenerationConfig.h
@@ -22,8 +22,8 @@ struct LGADPulseGenerationConfig {
   // total number of TDC/ADC values
   // Since digitizer starts at zero, max ADC value = adc_range - 1
   // Similar for TDC
-  int adc_range = pow(2, adc_bit);
-  int tdc_range = pow(2, tdc_bit);
+  int adc_range = std::pow(2, adc_bit);
+  int tdc_range = std::pow(2, tdc_bit);
 
   // period of the sensor clock. Time internal to sensor will all be digitized to integer multiple
   // of tInterval
