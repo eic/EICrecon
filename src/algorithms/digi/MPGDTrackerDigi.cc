30bc6057c105f15293adfb82c88db4a488e9c60d
ybedfer <yann.bedfer@cern.ch>
2025-01-08 08:39:45 +0100

    Update src/algorithms/digi/MPGDTrackerDigi.cc
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/digi/MPGDTrackerDigi.cc
+++ src/algorithms/digi/MPGDTrackerDigi.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022 Whitney Armstrong, Wouter Deconinck, Sylvester Joosten, Dmitry Romanov
+// Copyright (C) 2022 - 2024 Whitney Armstrong, Wouter Deconinck, Sylvester Joosten, Dmitry Romanov, Yann Bedfer
 #include "MPGDTrackerDigi.h"
 
 #include <DD4hep/Alignments.h>
