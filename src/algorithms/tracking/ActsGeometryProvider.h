f90502d82b6c164d136c8c597747b018ce03cc23
Wouter Deconinck <wdconinc@gmail.com>
2025-01-16 15:21:31 -0600

    refactor: Make ViewConfig usable with designated initializers (#3613) @37.0.0:

--- src/algorithms/tracking/ActsGeometryProvider.h
+++ src/algorithms/tracking/ActsGeometryProvider.h
@@ -109,11 +109,19 @@ private:
     std::shared_ptr<spdlog::logger> m_init_log;
 
     /// Configuration for obj export
+#if Acts_VERSION_MAJOR >= 37
+    Acts::ViewConfig m_containerView{.color = {220, 220, 220}};
+    Acts::ViewConfig m_volumeView{.color = {220, 220, 0}};
+    Acts::ViewConfig m_sensitiveView{.color = {0, 180, 240}};
+    Acts::ViewConfig m_passiveView{.color = {240, 280, 0}};
+    Acts::ViewConfig m_gridView{.color = {220, 0, 0}};
+#else
     Acts::ViewConfig m_containerView{{220, 220, 220}};
     Acts::ViewConfig m_volumeView{{220, 220, 0}};
     Acts::ViewConfig m_sensitiveView{{0, 180, 240}};
     Acts::ViewConfig m_passiveView{{240, 280, 0}};
     Acts::ViewConfig m_gridView{{220, 0, 0}};
+#endif
     bool m_objWriteIt{false};
     bool m_plyWriteIt{false};
     std::string m_outputTag{""};
