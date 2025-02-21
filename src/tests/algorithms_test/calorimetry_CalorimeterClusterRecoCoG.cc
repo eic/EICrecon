84c21340483d296e247ce66eee958ab2613503b1
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-02-20 21:07:13 -0500

    Add shape algo to cog unit test

--- src/tests/algorithms_test/calorimetry_CalorimeterClusterRecoCoG.cc
+++ src/tests/algorithms_test/calorimetry_CalorimeterClusterRecoCoG.cc
@@ -31,29 +31,42 @@
 
 #include "algorithms/calorimetry/CalorimeterClusterRecoCoG.h"        // for CalorimeterClusterRecoCoG
 #include "algorithms/calorimetry/CalorimeterClusterRecoCoGConfig.h"  // for CalorimeterClusterRecoCoGConfig
+#include "algorithms/calorimetry/CalorimeterClusterShape.h"          // for CalorimeterClusterShape
+#include "algorithms/calorimetry/CalorimeterClusterShapeConfig.h"    // for CalorimeterClusterShapeConfig
 
 using eicrecon::CalorimeterClusterRecoCoG;
 using eicrecon::CalorimeterClusterRecoCoGConfig;
+using eicrecon::CalorimeterClusterShape;
+using eicrecon::CalorimeterClusterShapeConfig;
 
 using edm4eic::CalorimeterHit;
 
-TEST_CASE( "the calorimeter CoG algorithm runs", "[CalorimeterClusterRecoCoG]" ) {
+TEST_CASE( "the calorimeter CoG and shape algorithm runs", "[CalorimeterClusterRecoCoG]" ) {
   const float EPSILON = 1e-5;
 
-  CalorimeterClusterRecoCoG algo("CalorimeterClusterRecoCoG");
+  CalorimeterClusterRecoCoG algo_reco("CalorimeterClusterRecoCoG");
+  CalorimeterClusterShape algo_shape("CalorimeterClusterShape");
 
   std::shared_ptr<spdlog::logger> logger = spdlog::default_logger()->clone("CalorimeterClusterRecoCoG");
   logger->set_level(spdlog::level::trace);
 
-  CalorimeterClusterRecoCoGConfig cfg;
-  cfg.energyWeight = "log";
-  cfg.sampFrac = 0.0203;
-  cfg.logWeightBaseCoeffs = {5.0,0.65,0.31};
-  cfg.logWeightBase_Eref = 50 * edm4eic::unit::GeV;
+  // configure RecoCoG
+  CalorimeterClusterRecoCoGConfig cfg_reco;
+  cfg_reco.energyWeight = "log";
+  cfg_reco.sampFrac = 0.0203;
+  cfg_reco.logWeightBaseCoeffs = {5.0,0.65,0.31};
+  cfg_reco.logWeightBase_Eref = 50 * edm4eic::unit::GeV;
 
+  // configure Shape
+  CalorimeterClusterShapeConfig cfg_shape;
+  cfg_shape.longitudinalShowerInfoAvailable = true;
 
-  algo.applyConfig(cfg);
-  algo.init();
+  // apply configurations
+  algo_reco.applyConfig(cfg_reco);
+  algo_reco.init();
+
+  algo_shape.applyConfig(cfg_shape);
+  algo_shape.init();
 
   edm4hep::RawCalorimeterHitCollection rawhits_coll;
   edm4eic::CalorimeterHitCollection hits_coll;
@@ -64,8 +77,8 @@ TEST_CASE( "the calorimeter CoG algorithm runs", "[CalorimeterClusterRecoCoG]" )
 #endif
   edm4hep::CaloHitContributionCollection contribs_coll;
   edm4hep::MCParticleCollection mcparts_coll;
-  auto assoc_coll = std::make_unique<edm4eic::MCRecoClusterParticleAssociationCollection>();
-  auto clust_coll = std::make_unique<edm4eic::ClusterCollection>();
+  auto assoc_coll_reco = std::make_unique<edm4eic::MCRecoClusterParticleAssociationCollection>();
+  auto clust_coll_reco = std::make_unique<edm4eic::ClusterCollection>();
 
   //create a protocluster with 3 hits
   auto pclust = pclust_coll.create();
@@ -204,30 +217,41 @@ TEST_CASE( "the calorimeter CoG algorithm runs", "[CalorimeterClusterRecoCoG]" )
 
  // Constructing input and output as per the algorithm's expected signature
 #if EDM4EIC_VERSION_MAJOR >= 7
-  auto input = std::make_tuple(&pclust_coll, &hitassocs_coll);
+  auto input_reco = std::make_tuple(&pclust_coll, &hitassocs_coll);
 #else
-  auto input = std::make_tuple(&pclust_coll, &simhits_coll);
+  auto input_reco = std::make_tuple(&pclust_coll, &simhits_coll);
 #endif
-  auto output = std::make_tuple(clust_coll.get(), assoc_coll.get());
+  auto output_reco = std::make_tuple(clust_coll_reco.get(), assoc_coll_reco.get());
 
-  algo.process(input, output);
+  algo_reco.process(input_reco, output_reco);
 
-  REQUIRE(clust_coll->size() == 1);
-  auto clust = (*clust_coll)[0];
+  // create collections to hold shape output
+  auto assoc_coll_shape = std::make_unique<edm4eic::MCRecoClusterParticleAssociationCollection>();
+  auto clust_coll_shape = std::make_unique<edm4eic::ClusterCollection>();
+
+  // collect input/output for shape algorithm
+  auto input_shape = std::make_tuple(clust_coll_reco.get(), assoc_coll_reco.get());
+  auto output_shape = std::make_tuple(clust_coll_shape.get(), assoc_coll_shape.get());
+
+  // Run shape calculation on output of RecoCoG
+  algo_shape.process(input_shape, output_shape);
+
+  REQUIRE(clust_coll_shape->size() == 1);
+  auto clust = (*clust_coll_shape)[0];
   REQUIRE_THAT(clust.getIntrinsicTheta(), Catch::Matchers::WithinAbs(M_PI / 4, EPSILON));
   // std::abs() checks if we land on -M_PI
   REQUIRE_THAT(std::abs(clust.getIntrinsicPhi()), Catch::Matchers::WithinAbs(M_PI, EPSILON));
 
-  REQUIRE(assoc_coll->size() == 2);
+  REQUIRE(assoc_coll_shape->size() == 2);
 
   // Half of the energy comes from mcpart11 and its daughter mcpart12
-  REQUIRE_THAT((*assoc_coll)[0].getWeight(), Catch::Matchers::WithinAbs(0.5, EPSILON));
-  REQUIRE((*assoc_coll)[0].getRec() == clust);
-  REQUIRE((*assoc_coll)[0].getSim() == mcpart11);
+  REQUIRE_THAT((*assoc_coll_shape)[0].getWeight(), Catch::Matchers::WithinAbs(0.5, EPSILON));
+  REQUIRE((*assoc_coll_shape)[0].getRec() == clust);
+  REQUIRE((*assoc_coll_shape)[0].getSim() == mcpart11);
 
   // Half of the energy comes from mcpart2
-  REQUIRE_THAT((*assoc_coll)[1].getWeight(), Catch::Matchers::WithinAbs(0.5, EPSILON));
-  REQUIRE((*assoc_coll)[1].getRec() == clust);
-  REQUIRE((*assoc_coll)[1].getSim() == mcpart2);
+  REQUIRE_THAT((*assoc_coll_shape)[1].getWeight(), Catch::Matchers::WithinAbs(0.5, EPSILON));
+  REQUIRE((*assoc_coll_shape)[1].getRec() == clust);
+  REQUIRE((*assoc_coll_shape)[1].getSim() == mcpart2);
 
 }
