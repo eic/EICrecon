12aae846e2a9730f80937b0d989940da9a200f30
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-03-11 22:52:55 -0400

    restore a valid use of podioVersion.h

--- src/services/io/podio/JEventSourcePODIO.cc
+++ src/services/io/podio/JEventSourcePODIO.cc
@@ -17,6 +17,7 @@
 #include <fmt/core.h>
 #include <podio/CollectionBase.h>
 #include <podio/Frame.h>
+#include <podio/podioVersion.h>
 #include <algorithm>
 #include <exception>
 #include <iostream>
