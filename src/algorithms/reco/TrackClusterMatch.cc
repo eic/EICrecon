58eaad8ab8f15dfbf85e431fdaccec82c7203e91
EIC Container Build Service <gitlab@eicweb.phy.anl.gov>
2025-03-25 12:43:46 -0400

    Avoid double matching tracks

--- src/algorithms/reco/TrackClusterMatch.cc
+++ src/algorithms/reco/TrackClusterMatch.cc
@@ -28,6 +28,7 @@ namespace eicrecon {
         auto [matched_particles] = output;
         m_log->trace("We have {} tracks and {} clusters", tracks->size(), clusters->size());
         
+        std::set<int> used_tracks;
         // Loop across each cluster, and find the cloeset projected track
         for (auto cluster : *clusters) {
             const double cluster_eta = edm4hep::utils::eta(cluster.getPosition());
@@ -36,17 +37,23 @@ namespace eicrecon {
             // TODO: Get the detector ID so I can check if a projection is in the appropriate calorimeter
 
             std::optional<edm4eic::TrackSegment> closest_segment;
-            double closest_distance = std::numeric_limits<double>::max();
+            int closest_segment_id = -1;
+            double closest_distance = m_cfg.matching_distance;
             // Loop through each track segment, and its points
-            for (auto track : *tracks) {
+            for (int closest_id = 0; auto track : *tracks) {
+                if (used_tracks.count(closest_id) > 0) {
+                    m_log->trace("Skipping track segment already used");
+                    continue;
+                }
                 for (auto point : track.getPoints()) {
                     // Check if the point is at the calorimeter
                     int id = m_detector->volumeManager().lookupDetector(cluster.getHits()[0].getCellID()).id();
-                    m_log->trace("Track point at detector ID {}", id);
-                    if (id == m_detector->constant<int>("EcalBarrel")) {
-                        m_log->trace("Skipping track point not at the calorimeter");
-                    }
-                    if (point.system != id || point.surface != 1) { 
+                    // m_log->trace("Track point at detector ID {}", id);
+                    // m_log->trace("EcalBarrel ID is {}", m_detector->constant<uint32_t>("EcalBarrel_ID"));
+                    // m_log->trace("HcalBarrel ID is {}", m_detector->constant<uint32_t>("HcalBarrel_ID"));
+                    uint32_t ecal_barrel_id = 101;
+                    uint32_t hcal_barrel_id = 111;
+                    if (!((point.system == ecal_barrel_id) || (point.system == hcal_barrel_id)) ||  point.surface != 1) { // Just try the barrel ECal for now...
                         m_log->trace("Skipping track point not at the calorimeter");
                         continue;
                     }
@@ -59,8 +66,10 @@ namespace eicrecon {
                     if (delta < closest_distance) {
                         closest_distance = delta;
                         closest_segment = track;
+                        closest_segment_id = closest_id;
                     }
                 }
+                closest_id++;
             }
             // If we found a point, create a new particle
             if (closest_segment) {
@@ -68,10 +77,10 @@ namespace eicrecon {
                 auto particle = matched_particles->create();
                 particle.setCluster(cluster);
                 particle.setTrack(closest_segment.value().getTrack());
+                used_tracks.insert(closest_segment_id);
                 // compare object ID of track from track segment to track in output collection
             }
         }
-        m_log->trace("Matched {} particles", matched_particles->size());
     }
 
     double TrackClusterMatch::distance(const edm4hep::Vector3f& v1, const edm4hep::Vector3f& v2) const {
