2853e4a5a9c76c5f488fabee02df738cebdad646
Alexander Jentsch <ajentsch@bnl.gov>
2025-03-25 10:18:21 -0400

    separate OMD and RP algorithms and update matrices

--- src/algorithms/fardetectors/MatrixTransferStatic_OMD.cc
+++ src/algorithms/fardetectors/MatrixTransferStatic_OMD.cc
@@ -3,7 +3,7 @@
 //
 // This converted from: https://eicweb.phy.anl.gov/EIC/juggler/-/blob/master/JugReco/src/components/FarForwardParticles.cpp
 
-#include "MatrixTransferStatic.h"
+#include "MatrixTransferStatic_OMD.h"
 
 #include <DD4hep/Alignments.h>
 #include <DD4hep/DetElement.h>
@@ -19,15 +19,15 @@
 #include <gsl/pointers>
 #include <vector>
 
-#include "algorithms/fardetectors/MatrixTransferStaticConfig.h"
+#include "algorithms/fardetectors/MatrixTransferStaticConfig_OMD.h"
 
-void eicrecon::MatrixTransferStatic::init() {
+void eicrecon::MatrixTransferStatic_OMD::init() {
 
 }
 
-void eicrecon::MatrixTransferStatic::process(
-    const MatrixTransferStatic::Input& input,
-    const MatrixTransferStatic::Output& output) const {
+void eicrecon::MatrixTransferStatic_OMD::process(
+    const MatrixTransferStatic_OMD::Input& input,
+    const MatrixTransferStatic_OMD::Output& output) const {
 
   const auto [mcparts, rechits] = input;
   auto [outputParticles] = output;
@@ -63,7 +63,6 @@ void eicrecon::MatrixTransferStatic::process(
                 runningMomentum += p.getMomentum().z;
                 numBeamProtons++;
         }
-
   }
 
   if(numBeamProtons == 0) {error("No beam protons to choose matrix!! Skipping!!"); return;}
@@ -75,88 +74,34 @@ void eicrecon::MatrixTransferStatic::process(
   //This is a temporary solution to get the beam energy information
   //needed to select the correct matrix
 
-  if(std::abs(275.0 - nomMomentum)/275.0 < nomMomentumError){
+  if(std::abs(130.0 - nomMomentum)/130.0 < nomMomentumError){
 
-     aX[0][0] = 3.251116; //a
-     aX[0][1] = 30.285734; //b
-     aX[1][0] = 0.186036375; //c
-     aX[1][1] = 0.196439472; //d
+     aX[0][0] = 1.61591; //a
+     aX[0][1] = 12.6786; //b
+     aX[1][0] = 0.184206; //c
+     aX[1][1] = -2.907; //d
 
-     aY[0][0] = 0.4730500000; //a
-     aY[0][1] = 3.062999454; //b
-     aY[1][0] = 0.0204108951; //c
-     aY[1][1] = -0.139318692; //d
+     aY[0][0] = -0.789385; //a
+     aY[0][1] = -28.5578; //b
+     aY[1][0] = -0.0721796; //c
+     aY[1][1] = -2.8763; //d
 
-     local_x_offset       = -1209.29;//-0.339334; these are the local coordinate values
-     local_y_offset       = 0.00132511;//-0.000299454;
-     local_x_slope_offset = -45.4772;//-0.219603248;
-     local_y_slope_offset = 0.000745498;//-0.000176128;
-
-  }
-  else if(std::abs(130.0 - nomMomentum)/130.0 < nomMomentumError){ //NOT TUNED -- just for testing purposes
-
-     aX[0][0] = 3.16912; //a
-     aX[0][1] = 22.4693; //b
-     aX[1][0] = 0.182402; //c
-     aX[1][1] = -0.218209; //d
-
-     aY[0][0] = 0.520743; //a
-     aY[0][1] = 3.17339; //b
-     aY[1][0] = 0.0222482; //c
-     aY[1][1] = -0.0923779; //d
-
-     local_x_offset       = -1209.29;//-0.339334; these are the local coordinate values
-     local_y_offset       = 0.00132511;//-0.000299454;
-     local_x_slope_offset = -45.4772;//-0.219603248;
-     local_y_slope_offset = 0.000745498;//-0.000176128;
-
-  }
-  else if(std::abs(100.0 - nomMomentum)/100.0 < nomMomentumError){
-
-     aX[0][0] = 3.152158; //a
-     aX[0][1] = 20.852072; //b
-     aX[1][0] = 0.181649517; //c
-     aX[1][1] = -0.303998487; //d
-
-     aY[0][0] = 0.5306100000; //a
-     aY[0][1] = 3.19623343; //b
-     aY[1][0] = 0.0226283320; //c
-     aY[1][1] = -0.082666019; //d
-
-     local_x_offset       = -1209.27;//-0.329072;
-     local_y_offset       = 0.00355218;//-0.00028343;
-     local_x_slope_offset = -45.4737;//-0.218525084;
-     local_y_slope_offset = 0.00204394;//-0.00015321;
-
-  }
-  else if(std::abs(41.0 - nomMomentum)/41.0 < nomMomentumError){
-
-         aX[0][0] = 3.135997; //a
-         aX[0][1] = 18.482273; //b
-         aX[1][0] = 0.176479921; //c
-         aX[1][1] = -0.497839483; //d
-
-         aY[0][0] = 0.4914400000; //a
-         aY[0][1] = 4.53857451; //b
-         aY[1][0] = 0.0179664765; //c
-         aY[1][1] = 0.004160679; //d
-
-         local_x_offset       = -1209.22;//-0.283273;
-         local_y_offset       = 0.00868737;//-0.00552451;
-         local_x_slope_offset = -45.4641;//-0.21174031;
-         local_y_slope_offset = 0.00498786;//-0.003212011;
+     local_x_offset       = -881.631;
+     local_y_offset       = -0.00552173;
+     local_x_slope_offset = -59.7386;
+     local_y_slope_offset = -0.00360656;
 
   }
 
   else {
-    error("MatrixTransferStatic:: No valid matrix found to match beam momentum!! Skipping!!");
+    error("MatrixTransferStatic_OMD:: No valid matrix found to match beam momentum!! Skipping!!");
     return;
   }
 
   double determinant = aX[0][0] * aX[1][1] - aX[0][1] * aX[1][0];
 
   if (determinant == 0) {
-    error("Reco matrix determinant = 0! Matrix cannot be inverted! Double-check matrix!");
+    error("OMD Reco matrix determinant = 0! Matrix cannot be inverted! Double-check matrix!");
     return;
   }
 
@@ -169,7 +114,7 @@ void eicrecon::MatrixTransferStatic::process(
   determinant = aY[0][0] * aY[1][1] - aY[0][1] * aY[1][0];
 
   if (determinant == 0) {
-    error("Reco matrix determinant = 0! Matrix cannot be inverted! Double-check matrix!");
+    error("OMD Reco matrix determinant = 0! Matrix cannot be inverted! Double-check matrix!");
     return;
   }
 
@@ -192,7 +137,7 @@ void eicrecon::MatrixTransferStatic::process(
   int numGoodHits1 = 0;
   int numGoodHits2 = 0;
 
-  trace("size of RP hit array = {}", rechits->size());
+  trace("size of OMD hit array = {}", rechits->size());
 
   for (const auto &h: *rechits) {
 
@@ -209,11 +154,11 @@ void eicrecon::MatrixTransferStatic::process(
     gpos = gpos/dd4hep::mm;
     pos0 = pos0/dd4hep::mm;
 
-   trace("gpos.z() = {}, pos0.z() = {}, E_dep = {}", gpos.z(), pos0.z(), h.getEdep());
+   trace("OMD --> gpos.z() = {}, pos0.z() = {}, E_dep = {}", gpos.z(), pos0.z(), h.getEdep());
 
     if(gpos.z() > m_cfg.hit2minZ && gpos.z() < m_cfg.hit2maxZ){
 
-      trace("[gpos.x(), gpos.y(), gpos.z()] = {}, {}, {};  E_dep = {} MeV", gpos.x(), gpos.y(), gpos.z(), h.getEdep()*1000);
+      trace("OMD --> [gpos.x(), gpos.y(), gpos.z()] = {}, {}, {};  E_dep = {} MeV", gpos.x(), gpos.y(), gpos.z(), h.getEdep()*1000);
       numGoodHits2++;
       goodHit[1].x = gpos.x(); //pos0.x() - pos0 is local coordinates, gpos is global
       goodHit[1].y = gpos.y(); //pos0.y() - temporarily changing to global to solve the local coordinate issue
@@ -224,7 +169,7 @@ void eicrecon::MatrixTransferStatic::process(
     }
     if(gpos.z() > m_cfg.hit1minZ && gpos.z() < m_cfg.hit1maxZ){
 
-      trace("[gpos.x(), gpos.y(), gpos.z()] = {}, {}, {};  E_dep = {} MeV", gpos.x(), gpos.y(), gpos.z(), h.getEdep()*1000);
+      trace("--> [gpos.x(), gpos.y(), gpos.z()] = {}, {}, {};  E_dep = {} MeV", gpos.x(), gpos.y(), gpos.z(), h.getEdep()*1000);
       numGoodHits1++;
       goodHit[0].x = gpos.x(); //pos0.x()
       goodHit[0].y = gpos.y(); //pos0.y()
@@ -250,7 +195,7 @@ void eicrecon::MatrixTransferStatic::process(
     double base = goodHit[1].z - goodHit[0].z;
 
     if (base == 0) {
-      info("Detector separation = 0! Cannot calculate slope!");
+      info("OMD --> Detector separation = 0! Cannot calculate slope!");
     }
     else{
 
@@ -259,7 +204,7 @@ void eicrecon::MatrixTransferStatic::process(
       double Yip[2] = {0.0, 0.0};
       double Yrp[2] = {YL[1], ((YL[1] - YL[0]) / (base))/dd4hep::mrad - local_y_slope_offset};
 
-      // use the hit information and calculated slope at the RP + the transfer matrix inverse to calculate the
+      // use the hit information and calculated slope at the OMD + the transfer matrix inverse to calculate the
       // Polar Angle and deltaP at the IP
 
       for (unsigned i0 = 0; i0 < 2; i0++) {
@@ -283,7 +228,7 @@ void eicrecon::MatrixTransferStatic::process(
                                 static_cast<float>(p / norm)};
       auto refPoint = goodHit[0];
 
-      trace("RP Reco Momentum ---> px = {},  py = {}, pz = {}", prec.x, prec.y, prec.z);
+      trace("OMD Reco Momentum ---> px = {},  py = {}, pz = {}", prec.x, prec.y, prec.z);
 
       //----- end reconstruction code ------
 
