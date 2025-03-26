1124c7f2d8d759bf2f8431c343f2a5c97f15ccc0
Wouter Deconinck <wdconinc@gmail.com>
2025-03-26 11:41:40 -0500

    fix: add ActsTrajectoriesMerger

---
+++ src/global/tracking/ActsTrajectoriesMerger_factory.h
@@ -0,0 +1,39 @@
+// Created by Joe Osborn
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#pragma once
+
+#include <ActsExamples/EventData/Trajectories.hpp>
+#include <JANA/JEvent.h>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "extensions/jana/JOmniFactory.h"
+
+namespace eicrecon {
+
+class ActsTrajectoriesMerger_factory : public JOmniFactory<ActsTrajectoriesMerger_factory> {
+private:
+  Input<ActsExamples::Trajectories> m_acts_trajectories1_input{this};
+  Input<ActsExamples::Trajectories> m_acts_trajectories2_input{this};
+  Output<const ActsExamples::Trajectories> m_acts_trajectories_output{this};
+
+public:
+  void Configure() {}
+
+  void ChangeRun(int64_t run_number) {}
+
+  void Process(int64_t run_number, uint64_t event_number) {
+    m_acts_trajectories_output().insert(m_acts_trajectories_output().end(),
+                                        m_acts_trajectories1_input().begin(),
+                                        m_acts_trajectories1_input().end());
+    m_acts_trajectories_output().insert(m_acts_trajectories_output().end(),
+                                        m_acts_trajectories2_input().begin(),
+                                        m_acts_trajectories2_input().end());
+  }
+};
+
+} // namespace eicrecon
