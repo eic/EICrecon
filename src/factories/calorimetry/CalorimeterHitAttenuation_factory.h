94f6e0c6e51ac351beebc598f8fb0dc06154e657
minho.kim@anl.gov <minho.kim@anl.gov>
2025-04-05 21:00:10 -0500

    Updated for PR

--- src/factories/calorimetry/CalorimeterHitAttenuation_factory.h
+++ src/factories/calorimetry/CalorimeterHitAttenuation_factory.h
@@ -9,37 +9,37 @@
 
 namespace eicrecon {
 
-        class CalorimeterHitAttenuation_factory : public JOmniFactory<CalorimeterHitAttenuation_factory, CalorimeterHitAttenuationConfig> {
+	class CalorimeterHitAttenuation_factory : public JOmniFactory<CalorimeterHitAttenuation_factory, CalorimeterHitAttenuationConfig> {
 
-                public:
-                        using AlgoT = eicrecon::CalorimeterHitAttenuation;
-                private:
-                        std::unique_ptr<AlgoT> m_algo;
+		public:
+			using AlgoT = eicrecon::CalorimeterHitAttenuation;
+		private:
+			std::unique_ptr<AlgoT> m_algo;
 
-                        PodioInput<edm4hep::SimCalorimeterHit> m_hits_input {this};
-                        PodioOutput<edm4hep::SimCalorimeterHit> m_hits_output {this};
+			PodioInput<edm4hep::SimCalorimeterHit> m_hits_input {this};
+			PodioOutput<edm4hep::SimCalorimeterHit> m_hits_output {this};
 
-                        ParameterRef<std::vector<double>> m_attenuationParameters {this, "attenuationParameters", config().attPars};
-                        ParameterRef<std::vector<double>> m_layerParameters {this, "layerParameters", config().layPars};
-                        ParameterRef<std::vector<std::string>> m_fields {this, "fields", config().fields};
-                        ParameterRef<std::string> m_readout {this, "readout", config().readout};
+			ParameterRef<std::vector<double>> m_attenuationParameters {this, "attenuationParameters", config().attPars};
+			ParameterRef<std::string> m_lengthField {this, "lengthField", config().lengthField};
+			ParameterRef<std::string> m_zField {this, "zField", config().zField};
+			ParameterRef<std::string> m_readout {this, "readout", config().readout};
 
-                        Service<AlgorithmsInit_service> m_algorithmsInit {this};
+			Service<AlgorithmsInit_service> m_algorithmsInit {this};
 
-                public:
-                        void Configure() {
-                                m_algo = std::make_unique<AlgoT>(GetPrefix());
-                                m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
-                                m_algo->applyConfig(config());
-                                m_algo->init();
-                        }
+		public:
+			void Configure() {
+				m_algo = std::make_unique<AlgoT>(GetPrefix());
+				m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
+				m_algo->applyConfig(config());
+				m_algo->init();
+			}
 
-                        void ChangeRun(int64_t run_number) {
-                        }
+			void ChangeRun(int64_t run_number) {
+			}
 
-                        void Process(int64_t run_number, uint64_t event_number) {
-                                m_algo->process({m_hits_input()}, {m_hits_output().get()});
-                        }
-        };
+			void Process(int64_t run_number, uint64_t event_number) {
+				m_algo->process({m_hits_input()}, {m_hits_output().get()});
+			}
+	};
 
 } // eicrecon
