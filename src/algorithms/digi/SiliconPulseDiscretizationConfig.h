63e8f3777c1bab2814c898f9c724912cda9fd42f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-04-07 18:35:52 -0400

    SiliconPulseDiscretizationConfig: use edm4eic/unit_system.h

--- src/algorithms/digi/SiliconPulseDiscretizationConfig.h
+++ src/algorithms/digi/SiliconPulseDiscretizationConfig.h
@@ -3,11 +3,13 @@
 
 #pragma once
 
+#include <edm4eic/unit_system.h>
+
 namespace eicrecon {
 
 struct SiliconPulseDiscretizationConfig {
-    double EICROC_period = 25 * dd4hep::ns;
-    double local_period = 25 * dd4hep::ns / 1024; // 1024 TDC bin
+    double EICROC_period = 25 * edm4eic::unit::ns;
+    double local_period = 25 * edm4eic::unit::ns / 1024; // 1024 TDC bin
     double global_offset = 0; // off-set to convert pulse time to global time
 };
 
