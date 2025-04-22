b0d3645508bcbd69ba1b0d98ee28e33f394367e8
simonge <simon.gardner@glasgow.ac.uk>
2025-04-22 14:05:15 +0100

    Include AlgorithmsInit

--- src/factories/digi/SiliconChargeSharing_factory.h
+++ src/factories/digi/SiliconChargeSharing_factory.h
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2024 Chun Yuen Tsang
+// Copyright (C) 2024-2025 Chun Yuen Tsang, Simon Gardner
 
 #pragma once
 
-#include "extensions/jana/JOmniFactory.h"
-
 #include "algorithms/digi/SiliconChargeSharing.h"
-#include <iostream>
+#include "services/algorithms_init/AlgorithmsInit_service.h"
+#include "extensions/jana/JOmniFactory.h"
 
 namespace eicrecon {
 
@@ -19,19 +18,18 @@ private:
   std::unique_ptr<AlgoT> m_algo;
 
   PodioInput<edm4hep::SimTrackerHit> m_in_sim_track{this};
-
   PodioOutput<edm4hep::SimTrackerHit> m_out_reco_particles{this};
+
+  Service<AlgorithmsInit_service> m_algorithmsInit{this};
 
   ParameterRef<float> m_sigma_sharingx{this, "sigmaSharingX", config().sigma_sharingx};
   ParameterRef<float> m_sigma_sharingy{this, "sigmaSharingY", config().sigma_sharingy};
   ParameterRef<float> m_min_edep{this, "minEDep", config().min_edep};
   ParameterRef<std::string> m_readout{this, "readout", config().readout};
 
-  Service<AlgorithmsInit_service> m_algorithmsInit{this};
-
 public:
   void Configure() {
-    m_algo = std::make_unique<eicrecon::SiliconChargeSharing>(GetPrefix());
+    m_algo = std::make_unique<AlgoT>(GetPrefix());
     m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
     m_algo->applyConfig(config());
     m_algo->init();
