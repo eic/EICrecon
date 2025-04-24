780fc48c51258452fae10bf75f36ba0de9cd4d0a
Wouter Deconinck <wdconinc@gmail.com>
2025-04-23 23:46:47 -0500

    fix: a few loose ends to avoid compiler warnings

--- src/benchmarks/reconstruction/lfhcal_studies/lfhcal_studiesProcessor.cc
+++ src/benchmarks/reconstruction/lfhcal_studies/lfhcal_studiesProcessor.cc
@@ -400,7 +400,7 @@ void lfhcal_studiesProcessor::Process(const std::shared_ptr<const JEvent>& event
     //loop over input_tower_sim and find if there is already a tower with the same cellID
     bool found = false;
     for (auto& tower : input_tower_sim) {
-      if (tower.cellID == cellID) {
+      if (tower.cellID == static_cast<decltype(tower.cellID)>(cellID)) {
         tower.energy += energy;
         found = true;
         break;
@@ -468,7 +468,7 @@ void lfhcal_studiesProcessor::Process(const std::shared_ptr<const JEvent>& event
     //loop over input_tower_rec and find if there is already a tower with the same cellID
     bool found = false;
     for (auto& tower : input_tower_rec) {
-      if (tower.cellID == cellID) {
+      if (tower.cellID == static_cast<decltype(tower.cellID)>(cellID)) {
         tower.energy += energy;
         found = true;
         break;
