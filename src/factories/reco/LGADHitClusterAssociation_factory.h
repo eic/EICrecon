c8ea9a6eb4694c266fdae02f443a402f289dd2bd
ssedd1123 <tommytsang3108@hotmail.com.hk>
2025-04-24 11:59:01 -0400

    Add missing config parameters to cluster association factory.

--- src/factories/reco/LGADHitClusterAssociation_factory.h
+++ src/factories/reco/LGADHitClusterAssociation_factory.h
@@ -19,6 +19,8 @@ private:
   PodioOutput<edm4eic::TrackerHit> m_asso_hits_output{this};
 
   ParameterRef<std::string> m_readout{this, "readout", config().readout};
+  ParameterRef<double>      m_assoDeltaT{this, "assoDeltaT", config().assoDeltaT};
+
 
 public:
   void Configure() {
