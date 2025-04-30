7da9d0144b08da6c339f022ba8e27e3ed4bf816e
Wouter Deconinck <wdconinc@gmail.com>
2025-04-30 12:20:29 -0500

    fix: rename log<lvl> back to report_fmt<lvl>

--- src/algorithms/pid/MergeParticleID.h
+++ src/algorithms/pid/MergeParticleID.h
@@ -48,7 +48,7 @@ public:
 private:
   template <algorithms::LogLevel lvl, typename... T>
   constexpr void log(fmt::format_string<T...> fmt, T&&... args) const {
-    algorithms::LoggerMixin::log<lvl>(fmt, std::forward<decltype(args)>(args)...);
+    algorithms::LoggerMixin::report_fmt<lvl>(fmt, std::forward<decltype(args)>(args)...);
   }
 
   friend class MergeParticleIDConfig;
