0ab56e5a2cc6561aa54d820145d3a42930c538f9
ssedd1123 <tommytsang3108@hotmail.com.hk>
2025-05-06 09:49:48 -0400

    Fixed type mismatch error.

--- src/factories/reco/LGADHitClusterAssociation_factory.h
+++ src/factories/reco/LGADHitClusterAssociation_factory.h
@@ -29,9 +29,9 @@ public:
     m_algo->init();
   }
 
-  void ChangeRun(int64_t run_number) {}
+  void ChangeRun(int32_t /* run_number */) {}
 
-  void Process(int64_t run_number, uint64_t event_number) {
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
     m_algo->process({m_meas_hits_input(), m_raw_hits_input()}, {m_asso_hits_output().get()});
   }
 };
