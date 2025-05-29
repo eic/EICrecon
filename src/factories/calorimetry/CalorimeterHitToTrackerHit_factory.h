0bef660ee9a2b48a976bf86ff681c9391695abd3
Wouter Deconinck <wdconinc@gmail.com>
2025-05-29 13:54:42 -0500

    fix: use correct run and event number type

--- src/factories/calorimetry/CalorimeterHitToTrackerHit_factory.h
+++ src/factories/calorimetry/CalorimeterHitToTrackerHit_factory.h
@@ -30,9 +30,9 @@ public:
     m_algo->init();
   }
 
-  void ChangeRun(int64_t run_number) {}
+  void ChangeRun(int32_t /* run_number */) {}
 
-  void Process(int64_t run_number, uint64_t event_number) {
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
     m_algo->process({m_calorimeter_hits_input()}, {m_tracker_hits_output().get()});
   }
 };
