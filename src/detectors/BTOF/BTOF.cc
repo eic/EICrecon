fc9120b122b6cbab2a5ebfd604f515e067622c56
Simon Gardner <simon.gardner@glasgow.ac.uk>
2025-06-02 20:38:54 +0100

    Move PID look up tables into global pid_lut file (#1745)
    ### Briefly, what does this PR introduce?
    Moves all of the PID LUT factories out of their detector plugin into the
    global pid plugin. Also moves PID factory files into the factories
    directory.
    
    Maybe a matter of personal taste but this would make the data flow
    through the PID algorithms more transparent.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [x] Other: Refactoring
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No
    ### Does this PR change default behavior?
    No
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/detectors/BTOF/BTOF.cc
+++ src/detectors/BTOF/BTOF.cc
@@ -5,19 +5,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2024, Dmitry Kalinkin
 
-#include <DD4hep/Detector.h>
 #include <Evaluator/DD4hepUnits.h>
-#include <JANA/JApplication.h>
 #include <JANA/JApplicationFwd.h>
 #include <TMath.h>
 #include <edm4eic/unit_system.h>
-#include <algorithm>
-#include <gsl/pointers>
 #include <memory>
-#include <stdexcept>
 
 #include "algorithms/interfaces/WithPodConfig.h"
-#include "algorithms/pid_lut/PIDLookupConfig.h"
 #include "extensions/jana/JOmniFactoryGeneratorT.h"
 #include "factories/digi/EICROCDigitization_factory.h"
 #include "factories/digi/PulseCombiner_factory.h"
@@ -26,8 +20,6 @@
 #include "factories/digi/SiliconPulseGeneration_factory.h"
 #include "factories/digi/SiliconTrackerDigi_factory.h"
 #include "factories/tracking/TrackerHitReconstruction_factory.h"
-#include "factories/pid_lut/PIDLookup_factory.h"
-#include "services/geometry/dd4hep/DD4hep_service.h"
 
 extern "C" {
 void InitPlugin(JApplication* app) {
@@ -104,52 +96,5 @@ void InitPlugin(JApplication* app) {
 
   app->Add(new JOmniFactoryGeneratorT<EICROCDigitization_factory>(
       "EICROCDigitization", {"TOFBarrelPulses"}, {"TOFBarrelADCTDC"}, {}, app));
-
-  int BarrelTOF_ID = 0;
-  try {
-    auto detector = app->GetService<DD4hep_service>()->detector();
-    BarrelTOF_ID  = detector->constant<int>("BarrelTOF_ID");
-  } catch (const std::runtime_error&) {
-    // Nothing
-  }
-  PIDLookupConfig pid_cfg{
-      .filename       = "calibrations/tof.lut",
-      .system         = BarrelTOF_ID,
-      .pdg_values     = {11, 211, 321, 2212},
-      .charge_values  = {1},
-      .momentum_edges = {0.0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4, 2.7, 3.0,
-                         3.3, 3.6, 3.9, 4.2, 4.5, 4.8, 5.1, 5.4, 5.7, 6.0},
-      .polar_edges    = {2.50, 10.95, 19.40, 27.85, 36.30, 44.75, 53.20, 61.65, 70.10, 78.55, 87.00,
-                         95.45, 103.90, 112.35, 120.80, 129.25, 137.70, 146.15, 154.60},
-      .azimuthal_binning           = {0., 360., 360.}, // lower, upper, step
-      .momentum_bin_centers_in_lut = true,
-      .polar_bin_centers_in_lut    = true,
-  };
-
-  app->Add(new JOmniFactoryGeneratorT<PIDLookup_factory>(
-      "CombinedTOFTruthSeededLUTPID",
-      {
-          "ReconstructedTruthSeededChargedWithPFRICHPIDParticles",
-          "ReconstructedTruthSeededChargedWithPFRICHPIDParticleAssociations",
-      },
-      {
-          "ReconstructedTruthSeededChargedWithPFRICHTOFPIDParticles",
-          "ReconstructedTruthSeededChargedWithPFRICHTOFPIDParticleAssociations",
-          "CombinedTOFTruthSeededParticleIDs",
-      },
-      pid_cfg, app));
-
-  app->Add(new JOmniFactoryGeneratorT<PIDLookup_factory>(
-      "CombinedTOFLUTPID",
-      {
-          "ReconstructedChargedWithPFRICHPIDParticles",
-          "ReconstructedChargedWithPFRICHPIDParticleAssociations",
-      },
-      {
-          "ReconstructedChargedWithPFRICHTOFPIDParticles",
-          "ReconstructedChargedWithPFRICHTOFPIDParticleAssociations",
-          "CombinedTOFParticleIDs",
-      },
-      pid_cfg, app));
 }
 } // extern "C"
