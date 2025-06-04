c34cced02c82fcd5acbae0c5e6f468e5637a2495
Wouter Deconinck <wdconinc@gmail.com>
2025-06-04 13:18:12 -0500

    fix: reduce JApplication/JApplicationFwd duplication

--- src/detectors/DRICH/DRICH.cc
+++ src/detectors/DRICH/DRICH.cc
@@ -7,7 +7,6 @@
 #include <DD4hep/DetElement.h>
 #include <DD4hep/Detector.h>
 #include <Evaluator/DD4hepUnits.h>
-#include <JANA/JApplication.h>
 #include <JANA/JApplicationFwd.h>
 #include <functional>
 #include <map>
