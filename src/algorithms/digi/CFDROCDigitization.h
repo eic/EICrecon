f6fcfb176c42cebf50c538ef2d2da0ae7929c531
ssedd1123 <tommytsang3108@hotmail.com.hk>
2025-06-25 22:53:57 -0400

    Switched to using CFD for BTOF.

---
+++ src/algorithms/digi/CFDROCDigitization.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Chun Yuen Tsang, Prithwish Tribedy
+// Special Acknowledgement: Kolja Kauder
+//
+// Convert ADC pulses from CFDROCGeneration into ADC and TDC values
+
+#pragma once
+
+#include <algorithms/algorithm.h>
+#include <edm4eic/RawTrackerHitCollection.h>
+#include <edm4hep/RawTimeSeriesCollection.h>
+#include <string>
+#include <string_view>
+
+#include "algorithms/digi/CFDROCDigitizationConfig.h"
+#include "algorithms/interfaces/WithPodConfig.h"
+
+namespace eicrecon {
+
+using CFDROCDigitizationAlgorithm =
+    algorithms::Algorithm<algorithms::Input<edm4hep::RawTimeSeriesCollection>,
+                          algorithms::Output<edm4eic::RawTrackerHitCollection>>;
+
+class CFDROCDigitization : public CFDROCDigitizationAlgorithm,
+                           public WithPodConfig<CFDROCDigitizationConfig> {
+
+public:
+  CFDROCDigitization(std::string_view name)
+      : CFDROCDigitizationAlgorithm{name, {"CFDROC"}, {"ADCTDCOutput"}, {}} {}
+  void init(){};
+  void process(const Input&, const Output&) const final;
+};
+
+} // namespace eicrecon
