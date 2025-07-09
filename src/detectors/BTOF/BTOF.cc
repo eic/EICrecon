b754dcfd1893d4cea463397b0b51efb74c846836
Wouter Deconinck <wdconinc@gmail.com>
2025-07-08 21:54:41 -0500

    fix: iwyu

--- src/detectors/BTOF/BTOF.cc
+++ src/detectors/BTOF/BTOF.cc
@@ -7,6 +7,7 @@
 
 #include <Evaluator/DD4hepUnits.h>
 #include <JANA/Components/JOmniFactoryGeneratorT.h>
+#include <JANA/JApplication.h>
 #include <JANA/JApplicationFwd.h>
 #include <JANA/Utils/JTypeInfo.h>
 #include <TMath.h>
