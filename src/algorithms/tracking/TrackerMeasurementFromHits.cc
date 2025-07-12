222bfa0ef0c9214160f77fc6c9ba065e5088a208
Wouter Deconinck <wdconinc@gmail.com>
2025-07-11 22:01:45 -0500

    fix: license

--- src/algorithms/tracking/TrackerMeasurementFromHits.cc
+++ src/algorithms/tracking/TrackerMeasurementFromHits.cc
@@ -1,7 +1,5 @@
-// Original license from Gaudi algorithm:
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2023 Shujie Li
-//
+// Copyright (C) 2023 - 2025 Shujie Li, Wouter Deconinck
 
 #include "TrackerMeasurementFromHits.h"
 
