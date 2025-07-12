0212f3c3bea464927bfa6e781dcca3c65ee1f47f
Wouter Deconinck <wdconinc@gmail.com>
2025-07-11 22:04:07 -0500

    fix: license

--- src/algorithms/reco/ElectronReconstruction.cc
+++ src/algorithms/reco/ElectronReconstruction.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2023 Daniel Brandenburg
+// Copyright (C) 2023 - 2025 Daniel Brandenburg, Wouter Deconinck
 #include "ElectronReconstruction.h"
 
 #include <edm4eic/ClusterCollection.h>
