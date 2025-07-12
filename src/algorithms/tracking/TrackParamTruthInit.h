826044f6d1a6429a23da07d70aea44acf2ecb430
Wouter Deconinck <wdconinc@gmail.com>
2025-07-11 22:12:23 -0500

    fix: license

--- src/algorithms/tracking/TrackParamTruthInit.h
+++ src/algorithms/tracking/TrackParamTruthInit.h
@@ -1,6 +1,5 @@
-// Created by Dmitry Romanov
-// Subject to the terms in the LICENSE file found in the top-level directory.
-//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2022 - 2025 Whitney Armstrong, Wouter Deconinck, Sylvester Joosten, Dmitry Romanov
 
 #pragma once
 
