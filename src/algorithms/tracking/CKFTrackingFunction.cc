115e489de8de2918fdd021e5522a705802f87bde
Wouter Deconinck <wdconinc@gmail.com>
2025-07-13 13:21:29 -0500

    fix: version guard CKFTracking<ActsPodioEdm>::makeCKFTrackingFunction

--- src/algorithms/tracking/CKFTrackingFunction.cc
+++ src/algorithms/tracking/CKFTrackingFunction.cc
@@ -116,6 +116,7 @@ CKFTracking<ActsExamplesEdm>::makeCKFTrackingFunction(
   return std::make_shared<CKFTrackingFunctionImpl<ActsExamplesEdm>>(std::move(trackFinder));
 }
 
+#if Acts_VERSION_MAJOR >= 36
 template <>
 std::shared_ptr<typename CKFTracking<ActsPodioEdm>::CKFTrackingFunction>
 CKFTracking<ActsPodioEdm>::makeCKFTrackingFunction(
@@ -135,5 +136,6 @@ CKFTracking<ActsPodioEdm>::makeCKFTrackingFunction(
   // build the track finder functions. owns the track finder object.
   return std::make_shared<CKFTrackingFunctionImpl<ActsPodioEdm>>(std::move(trackFinder));
 }
+#endif
 
 } // namespace eicrecon
