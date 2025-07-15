c37dcbb90b73121078738f008bdd7e74dcd5afb6
Minjung Kim <minjung.kim@cern.ch>
2025-07-15 12:53:07 -0700

    restore SiliconTrackerDigi into main version

--- src/algorithms/digi/SiliconTrackerDigiConfig.h
+++ src/algorithms/digi/SiliconTrackerDigiConfig.h
@@ -4,26 +4,14 @@
 #pragma once
 
 #include <DD4hep/DD4hepUnits.h>
-#include <vector>
-#include <utility>
 
 namespace eicrecon {
-
-enum class SubdetectorRegion { barrel, backward, forward };
 
 struct SiliconTrackerDigiConfig {
   // sub-systems should overwrite their own
   // NB: be aware of thresholds in npsim! E.g. https://github.com/eic/npsim/pull/9/files
   double threshold      = 0 * dd4hep::keV;
   double timeResolution = 8; /// TODO 8 of what units??? Same TODO in juggler. Probably [ns]
-  bool addNoise         = true;
-  // A vector of pairs to specify target layers, where each pair consists of
-  // a SubdetectorRegion and a layer index (int).
-  std::vector<std::pair<SubdetectorRegion, int>> target_layers = {
-    {SubdetectorRegion::barrel, 4},
-    {SubdetectorRegion::forward, 3},
-    {SubdetectorRegion::backward, 1}
-};
 };
 
 } // namespace eicrecon
--- src/factories/digi/SiliconTrackerDigi_factory.h
+++ src/factories/digi/SiliconTrackerDigi_factory.h
@@ -4,7 +4,6 @@
 #pragma once
 
 #include "algorithms/digi/SiliconTrackerDigi.h"
-#include "services/geometry/dd4hep/DD4hep_service.h"
 #include "services/algorithms_init/AlgorithmsInit_service.h"
 #include "extensions/jana/JOmniFactory.h"
 
@@ -14,35 +13,32 @@ class SiliconTrackerDigi_factory
     : public JOmniFactory<SiliconTrackerDigi_factory, SiliconTrackerDigiConfig> {
 
 public:
-    using AlgoT = eicrecon::SiliconTrackerDigi;
-    
+  using AlgoT = eicrecon::SiliconTrackerDigi;
+
 private:
-    std::unique_ptr<AlgoT> m_algo;
+  std::unique_ptr<AlgoT> m_algo;
 
-    PodioInput<edm4hep::SimTrackerHit> m_sim_hits_input{this};
+  PodioInput<edm4hep::SimTrackerHit> m_sim_hits_input{this};
 
-    PodioOutput<edm4eic::RawTrackerHit> m_raw_hits_output{this};
-    PodioOutput<edm4eic::MCRecoTrackerHitAssociation> m_assoc_output{this};
+  PodioOutput<edm4eic::RawTrackerHit> m_raw_hits_output{this};
+  PodioOutput<edm4eic::MCRecoTrackerHitAssociation> m_assoc_output{this};
 
-    ParameterRef<double> m_threshold{this, "threshold", config().threshold};
-    ParameterRef<double> m_timeResolution{this, "timeResolution", config().timeResolution};
-    Service<DD4hep_service> m_geoSvc{this};
+  ParameterRef<double> m_threshold{this, "threshold", config().threshold};
+  ParameterRef<double> m_timeResolution{this, "timeResolution", config().timeResolution};
 
 public:
-    void Configure() {
-        m_algo = std::make_unique<AlgoT>(GetPrefix());
-        // Assuming your algorithm has a 'logger()' method or similar
-        // m_algo->level(static_cast<algorithms::LogLevel>(logger()->level())); 
-        m_algo->applyConfig(config());
-        // Correct way to call service members is with '->'
-        m_algo->init(m_geoSvc().detector(), m_geoSvc().converter());
-    }
+  void Configure() {
+    m_algo = std::make_unique<AlgoT>(GetPrefix());
+    m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
+    m_algo->applyConfig(config());
+    m_algo->init();
+  }
 
-    void ChangeRun(int32_t /* run_number */) {}
+  void ChangeRun(int32_t /* run_number */) {}
 
-    void Process(int32_t /* run_number */, uint64_t /* event_number */) {
-        m_algo->process({m_sim_hits_input()}, {m_raw_hits_output().get(), m_assoc_output().get()});
-    }
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
+    m_algo->process({m_sim_hits_input()}, {m_raw_hits_output().get(), m_assoc_output().get()});
+  }
 };
 
 } // namespace eicrecon
