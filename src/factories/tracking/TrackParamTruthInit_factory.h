ef7ff9ce01039b89b84ad5e3e8a320d112af0e4e
Wouter Deconinck <wdconinc@gmail.com>
2025-07-16 14:24:00 -0500

    fix: move empty ChangeRun up a level in inheritance

--- src/factories/tracking/TrackParamTruthInit_factory.h
+++ src/factories/tracking/TrackParamTruthInit_factory.h
@@ -58,8 +58,6 @@ public:
     m_algo->init();
   }
 
-  void ChangeRun(int32_t /* run_number */) {}
-
   void Process(int32_t /* run_number */, uint64_t /* event_number */) {
     m_algo->process({m_headers_input(), m_particles_input()}, {m_parameters_output().get()});
   }
