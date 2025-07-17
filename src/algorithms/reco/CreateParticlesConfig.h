568ca2e54a4ba98bf33a3e510e6c5372da23d35d
Tristan Protzman <tlprotzman@gmail.com>
2025-07-17 16:55:25 -0400

    start of reconstructed particle algorithms

---
+++ src/algorithms/reco/CreateParticlesConfig.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Tristan Protzman
+
+#pragma once
+
+#include <string>
+#include <DD4hep/DD4hepUnits.h>
+
+namespace eicrecon {
+struct CreateParticlesConfig {
+};
+} // namespace eicrecon
