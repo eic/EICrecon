b2d2240bb5139480fa8d00df1000be87a78cc810
ssedd1123 <tommytsang3108@hotmail.com.hk>
2025-07-23 10:02:02 -0400

    Switched from BFS to using cellID from segmentation to look for cellID from position for speed improvement.

--- src/algorithms/digi/SiliconChargeSharing.cc
+++ src/algorithms/digi/SiliconChargeSharing.cc
@@ -77,10 +77,10 @@ void SiliconChargeSharing::process(const SiliconChargeSharing::Input& input,
 
     // globalHitPos can sometimes lie outside of cell with cellID given by hit.getCellID()
     // therefore, we search neighbors in a sensor to find the cell ID that correspond to globalHitPos
-    auto hitCellID = this->findHitCellID(cellID, hitPos, segmentationIt->second);
-    // if findHitCellID fails, revert back to hit.getCellID()
-    if (hitCellID)
-      cellID = hitCellID;
+    // Precise reason unknown, but we suspect it's cause by steps in Geant4
+    // Perhaps position is the average of all steps in volume while cellID is just the first cell the track hits
+    // They disagree when there are multiple step and scattering inside the volume
+    cellID = segmentationIt->second->cellID(hitPos, hitPos, cellID);
 
     std::unordered_set<dd4hep::rec::CellID> tested_cells;
     std::unordered_map<dd4hep::rec::CellID, float> cell_charge;
@@ -92,52 +92,6 @@ void SiliconChargeSharing::process(const SiliconChargeSharing::Input& input,
 
   } // for simhits
 } // SiliconChargeSharing:process
-
-dd4hep::rec::CellID SiliconChargeSharing::findHitCellID(
-    const dd4hep::rec::CellID testCellID, const dd4hep::Position& hitPos,
-    const dd4hep::DDSegmentation::CartesianGridXY* segmentation) const {
-  // use breath first search (BFS) to look for cellID that corresponds to hit Pos
-  // if cellID for hitPos is close to testCellID, BFS should be more efficient than depth first search
-  auto xDimension = segmentation->gridSizeX();
-  auto yDimension = segmentation->gridSizeY();
-
-  // queue of cellID to search
-  std::queue<dd4hep::rec::CellID> q;
-  q.push(testCellID);
-
-  // mark visited cells
-  std::unordered_set<dd4hep::rec::CellID> visited;
-  visited.insert(testCellID);
-
-  // iterate all cell candidates
-  while (!q.empty()) {
-    auto currID = q.front();
-    q.pop();
-
-    auto localPos = cell2LocalPosition(currID);
-
-    if (((localPos.x() - 0.5 * xDimension) <= hitPos.x() &&
-         hitPos.x() <= (localPos.x() + 0.5 * xDimension)) &&
-        ((localPos.y() - 0.5 * yDimension) <= hitPos.y() &&
-         hitPos.y() <= (localPos.y() + 0.5 * yDimension))) {
-      // we've found the cell
-      return currID;
-    }
-
-    // ask neighbors if the hit belongs to them
-    std::set<dd4hep::rec::CellID> testCellNeighbours;
-    segmentation->neighbours(currID, testCellNeighbours);
-
-    for (const auto& neighbourCell : testCellNeighbours) {
-      if (visited.find(neighbourCell) == visited.end()) {
-        visited.insert(neighbourCell);
-        q.push(neighbourCell);
-      }
-    }
-  }
-
-  return 0; // find nothing
-}
 
 // Recursively find neighbors where a charge is deposited
 void SiliconChargeSharing::findAllNeighborsInSensor(
