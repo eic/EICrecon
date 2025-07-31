ce1fc972850adac1265b2b25a7eaaea8535e09fc
Tomas Sosa <tomassosa.23@gmail.com>
2025-07-31 18:55:31 -0500

    Add optional

--- src/factories/calorimetry/CalorimeterParticleIDPreML_factory.h
+++ src/factories/calorimetry/CalorimeterParticleIDPreML_factory.h
@@ -12,8 +12,6 @@ namespace eicrecon {
 class CalorimeterParticleIDPreML_factory
     : public JOmniFactory<CalorimeterParticleIDPreML_factory, NoConfig> {
 
-  using Base = JOmniFactory<CalorimeterParticleIDPreML_factory, NoConfig>;
-
 public:
   using AlgoT = eicrecon::CalorimeterParticleIDPreML;
 
@@ -22,7 +20,7 @@ private:
 
   PodioInput<edm4eic::Cluster> m_cluster_input{this};
   PodioInput<edm4eic::MCRecoClusterParticleAssociation> m_cluster_assoc_input{this};
-  PodioInput<edm4hep::ParticleID> m_pid_input{this};
+  PodioInput<edm4hep::ParticleID, /* optional = */ true> m_pid_input{this};
 
   PodioOutput<edm4eic::Tensor> m_feature_tensor_output{this};
   PodioOutput<edm4eic::Tensor> m_target_tensor_output{this};
@@ -40,16 +38,6 @@ public:
   void Process(int32_t /* run_number */, uint64_t /* event_number */) {
     m_algo->process({m_cluster_input(), m_cluster_assoc_input(), m_pid_input()},
                     {m_feature_tensor_output().get(), m_target_tensor_output().get()});
-  }
-
-  void PreInit(std::string plugin_name, std::vector<std::string> input_names,
-               std::vector<std::string> output_names) {
-    // pad with empty strings so JOmniFactory::PreInit
-    // never complains about “3 expected, 2 found”
-    while (input_names.size() < 3) {
-      input_names.emplace_back("");
-    }
-    Base::PreInit(std::move(plugin_name), std::move(input_names), std::move(output_names));
   }
 };
 
