9d45393b77d4db25fadef1b0bd28f5fb6a21bfbc
Tomas Sosa <tomassosa.23@gmail.com>
2025-07-31 19:24:11 -0500

    second step to solve bug

---
+++ src/factories/calorimetry/CalorimeterParticleIDPreML_factory.h
@@ -0,0 +1,46 @@
+CalorimeterParticleIDPreML_factory
+
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2024, Dmitry Kalinkin
+
+#pragma once
+
+#include "algorithms/onnx/CalorimeterParticleIDPreML.h"
+#include "services/algorithms_init/AlgorithmsInit_service.h"
+#include "extensions/jana/JOmniFactory.h"
+
+namespace eicrecon {
+
+class CalorimeterParticleIDPreML_factory
+    : public JOmniFactory<CalorimeterParticleIDPreML_factory, NoConfig> {
+
+public:
+  using AlgoT = eicrecon::CalorimeterParticleIDPreML;
+
+private:
+  std::unique_ptr<AlgoT> m_algo;
+
+  PodioInput<edm4eic::Cluster> m_cluster_input{this};
+  PodioInput<edm4eic::MCRecoClusterParticleAssociation> m_cluster_assoc_input{this};
+  PodioInput<edm4hep::ParticleID, /* optional = */ true> m_pid_input{this};
+
+  PodioOutput<edm4eic::Tensor> m_feature_tensor_output{this};
+  PodioOutput<edm4eic::Tensor> m_target_tensor_output{this};
+
+public:
+  void Configure() {
+    m_algo = std::make_unique<AlgoT>(GetPrefix());
+    m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
+    m_algo->applyConfig(config());
+    m_algo->init();
+  }
+
+  void ChangeRun(int32_t /* run_number */) {}
+
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
+    m_algo->process({m_cluster_input(), m_cluster_assoc_input(), m_pid_input()},
+                    {m_feature_tensor_output().get(), m_target_tensor_output().get()});
+  }
+};
+
+} // namespace eicrecon
