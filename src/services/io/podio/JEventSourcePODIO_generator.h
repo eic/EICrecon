2edd9cab2f482eedb05665891a8184e84b7f6b35
Nathan Brei <nbrei@jlab.org>
2025-08-06 20:26:06 -0400

    Turn off splitting plugin for JANA2 <= v2.4.2

--- src/services/io/podio/JEventSourcePODIO_generator.h
+++ src/services/io/podio/JEventSourcePODIO_generator.h
@@ -17,6 +17,9 @@ class JEventSourcePODIO_generator : public JEventSourceGenerator {
     // If so, we assume the file contains timeslices, otherwise it contains physics events.
     // Another approach might be to peek at the file's contents
     if (mApplication->RegisterParameter<bool>("split_timeframes", false, "Enable timeframe splitting")) {
+      if constexpr (10000 * JVersion::major + 100 * JVersion::minor + 1 * JVersion::patch < 20403) {
+        throw JException("Timeframe splitting requires JANA2 v2.4.3 or greater");
+      }
       source->SetLevel(JEventLevel::Timeslice);
     } else {
       source->SetLevel(JEventLevel::PhysicsEvent);
