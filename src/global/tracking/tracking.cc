de2c7584fbd346704a7e1fbf4a0bf59ab5e28c5f
Wouter Deconinck <wdconinc@gmail.com>
2025-08-07 21:34:21 -0500

    Update src/global/tracking/tracking.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/global/tracking/tracking.cc
+++ src/global/tracking/tracking.cc
@@ -234,9 +234,9 @@ void InitPlugin(JApplication* app) {
               "- 1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)",
               "1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)"},
           eicrecon::CylinderSurfaceConfig{
-              "HcalBarrel_ID", "HcalBarrel_rmin + 150*mm",
-              "- 1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)",
-              "1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)"},
+              .id="HcalBarrel_ID", .rmin="HcalBarrel_rmin + 150*mm",
+              .zmin="- 1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)",
+              .zmax="1.1*max(HcalBarrelBackward_zmax,HcalBarrelForward_zmax)"},
           eicrecon::DiscSurfaceConfig{
               .id = "LFHCAL_ID", .zmin = "LFHCAL_zmin", .rmin = 0., .rmax = "1.1*LFHCAL_rmax"},
           eicrecon::DiscSurfaceConfig{.id   = "LFHCAL_ID",
