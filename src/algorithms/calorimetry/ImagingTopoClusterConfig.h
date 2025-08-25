a7eed6fc79d64ea10ba5fa5d61e828677a3c08b4
akshaya <99753263+AkshayaVijay@users.noreply.github.com>
2025-08-25 10:05:08 -0500

    Update src/algorithms/calorimetry/ImagingTopoClusterConfig.h
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- src/algorithms/calorimetry/ImagingTopoClusterConfig.h
+++ src/algorithms/calorimetry/ImagingTopoClusterConfig.h
@@ -26,13 +26,8 @@ struct ImagingTopoClusterConfig {
   // maximum distance of global (x, y) to be considered as neighbors at different layers (if layerMode==xy)
   std::vector<std::variant<std::string, double>> diffLayerDistXY = {1.0 * dd4hep::mm,
                                                                     1.0 * dd4hep::mm};
-  // maximum distance of global (eta, phi) to be considered as neighbors at same layers (if layerMode==etaphi)
-  std::vector<double> sameLayerDistEtaPhi = {0.01, 0.01};
   // maximum distance of global (eta, phi) to be considered as neighbors at different layers (if layerMode==etaphi)
   std::vector<double> diffLayerDistEtaPhi = {0.01, 0.01};
-  // maximum distance of global (x, y) to be considered as neighbors at same layers (if layerMode==phiz)
-  std::vector<std::variant<std::string, double>> sameLayerDistPhiZ = {1.0 * dd4hep::mm,
-                                                                      1.0 * dd4hep::mm};
   // maximum distance of global (x, y) to be considered as neighbors at different layers (if layerMode==phiz)
   std::vector<std::variant<std::string, double>> diffLayerDistPhiZ = {1.0 * dd4hep::mm,
                                                                       1.0 * dd4hep::mm};
