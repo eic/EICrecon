0efe6ef979bb46c3e8d1f484755693203046d9c3
Honey Khindri <ph24r002@smail.iitm.ac.in>
2025-08-28 12:57:29 +0530

    Added branch for shared hits and ADCTDC for Forward Time of Flight detector
    Added "TOFEndcapSharedHits" and "TOFEndcapADCTDC"

--- src/services/io/podio/JEventProcessorPODIO.cc
+++ src/services/io/podio/JEventProcessorPODIO.cc
@@ -90,6 +90,9 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
       "TOFBarrelHits",
       "TOFBarrelADCTDC",
       "TOFEndcapHits",
+  
+      "TOFEndcapSharedHits",
+      "TOFEndcapADCTDC",
 
       "TOFBarrelRawHitAssociations",
       "TOFEndcapRawHitAssociations",
