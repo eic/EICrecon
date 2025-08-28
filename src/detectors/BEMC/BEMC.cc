6e993c2d5329dec4ae5aa8b393f88ca183e9ed9e
akshaya <99753263+AkshayaVijay@users.noreply.github.com>
2025-08-28 12:44:25 -0500

    Update BEMC.cc

--- src/detectors/BEMC/BEMC.cc
+++ src/detectors/BEMC/BEMC.cc
@@ -203,10 +203,9 @@ void InitPlugin(JApplication* app) {
       {"EcalBarrelImagingProtoClusters"},
       {
           .neighbourLayersRange = 2, //  # id diff for adjacent layer
-          .sameLayerDistPhiZ    = {2.0 * dd4hep::mm, 2 * dd4hep::mm},     //  # same layer
+          .sameLayerDistTZ    = {2.0 * dd4hep::mm, 2 * dd4hep::mm},     //  # same layer
           .diffLayerDistEtaPhi  = {10 * dd4hep::mrad, 10 * dd4hep::mrad}, //  # adjacent layer
-          .sameLayerMode        = eicrecon::ImagingTopoClusterConfig::ELayerMode::
-              phiz, // Layer mode 'phiz' uses the average phi of the hits to define a rotated direction. The coordinate is a distance, not an angle.
+          .sameLayerMode        = eicrecon::ImagingTopoClusterConfig::ELayerMode::tz, 
           .diffLayerMode        = eicrecon::ImagingTopoClusterConfig::ELayerMode::etaphi,
           .sectorDist           = 3.0 * dd4hep::cm,
           .minClusterHitEdep    = 0,
