b16138b5bd423e5f4b0770da8a94563a6b5e7d60
Wouter Deconinck <wdconinc@gmail.com>
2025-08-28 15:41:11 -0500

    Fix reference from x,y to t,z in comment

--- src/algorithms/calorimetry/ImagingTopoClusterConfig.h
+++ src/algorithms/calorimetry/ImagingTopoClusterConfig.h
@@ -20,7 +20,7 @@ struct ImagingTopoClusterConfig {
                                                                     1.0 * dd4hep::mm};
   // maximum distance of global (eta, phi) to be considered as neighbors at same layers (if layerMode==etaphi)
   std::vector<double> sameLayerDistEtaPhi = {0.01, 0.01};
-  // maximum distance of global (x, y) to be considered as neighbors at same layers (if layerMode==tz)
+  // maximum distance of global (t, z) to be considered as neighbors at same layers (if layerMode==tz)
   std::vector<std::variant<std::string, double>> sameLayerDistTZ = {1.0 * dd4hep::mm,
                                                                     1.0 * dd4hep::mm};
   // maximum distance of global (x, y) to be considered as neighbors at different layers (if layerMode==xy)
@@ -28,7 +28,7 @@ struct ImagingTopoClusterConfig {
                                                                     1.0 * dd4hep::mm};
   // maximum distance of global (eta, phi) to be considered as neighbors at different layers (if layerMode==etaphi)
   std::vector<double> diffLayerDistEtaPhi = {0.01, 0.01};
-  // maximum distance of global (x, y) to be considered as neighbors at different layers (if layerMode==tz)
+  // maximum distance of global (t, z) to be considered as neighbors at different layers (if layerMode==tz)
   std::vector<std::variant<std::string, double>> diffLayerDistTZ = {1.0 * dd4hep::mm,
                                                                     1.0 * dd4hep::mm};
   // Layermodes
