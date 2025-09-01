f361fa71cf043a1363875f6bd5e8568c2ff8889e
Wouter Deconinck <wdconinc@gmail.com>
2025-09-01 14:04:40 -0500

    fix: remove duplication of edm4eic_indexed_units in src/algorithms/tracking

--- src/algorithms/tracking/ActsToTracks.cc
+++ src/algorithms/tracking/ActsToTracks.cc
@@ -32,21 +32,9 @@
 
 #include "ActsToTracks.h"
 
-namespace eicrecon {
+#include "extensions/edm4eic/EDM4eicToActs.h"
 
-// This array relates the Acts and EDM4eic covariance matrices, including
-// the unit conversion to get from Acts units into EDM4eic units.
-//
-// Note: std::map is not constexpr, so we use a constexpr std::array
-// std::array initialization need double braces since arrays are aggregates
-// ref: https://en.cppreference.com/w/cpp/language/aggregate_initialization
-static constexpr std::array<std::pair<Acts::BoundIndices, double>, 6> edm4eic_indexed_units{
-    {{Acts::eBoundLoc0, Acts::UnitConstants::mm},
-     {Acts::eBoundLoc1, Acts::UnitConstants::mm},
-     {Acts::eBoundPhi, 1.},
-     {Acts::eBoundTheta, 1.},
-     {Acts::eBoundQOverP, 1. / Acts::UnitConstants::GeV},
-     {Acts::eBoundTime, Acts::UnitConstants::ns}}};
+namespace eicrecon {
 
 void ActsToTracks::init() {}
 
--- src/algorithms/tracking/CKFTracking.cc
+++ src/algorithms/tracking/CKFTracking.cc
@@ -93,26 +93,13 @@
 
 #include "ActsGeometryProvider.h"
 #include "DD4hepBField.h"
+#include "extensions/edm4eic/EDM4eicToActs.h"
 #include "extensions/spdlog/SpdlogFormatters.h" // IWYU pragma: keep
 #include "extensions/spdlog/SpdlogToActs.h"
 
 namespace eicrecon {
 
 using namespace Acts::UnitLiterals;
-
-// This array relates the Acts and EDM4eic covariance matrices, including
-// the unit conversion to get from Acts units into EDM4eic units.
-//
-// Note: std::map is not constexpr, so we use a constexpr std::array
-// std::array initialization need double braces since arrays are aggregates
-// ref: https://en.cppreference.com/w/cpp/language/aggregate_initialization
-static constexpr std::array<std::pair<Acts::BoundIndices, double>, 6> edm4eic_indexed_units{
-    {{Acts::eBoundLoc0, Acts::UnitConstants::mm},
-     {Acts::eBoundLoc1, Acts::UnitConstants::mm},
-     {Acts::eBoundPhi, 1.},
-     {Acts::eBoundTheta, 1.},
-     {Acts::eBoundQOverP, 1. / Acts::UnitConstants::GeV},
-     {Acts::eBoundTime, Acts::UnitConstants::ns}}};
 
 CKFTracking::CKFTracking() = default;
 
