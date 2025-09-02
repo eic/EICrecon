02471de5f2a85538e9b298c07d5a4c976b44fdf3
Wouter Deconinck <wdconinc@gmail.com>
2025-09-02 10:26:26 -0500

    fix: remove duplication of edm4eic_indexed_units in `src/algorithms/tracking` (#2062)
    ### Briefly, what does this PR introduce?
    This PR removes a duplicated static object in `src/algorithms/tracking`,
    which causes symbol clashes in unity builds, and otherwise just violates
    DRY.
    
    Note: There may be ways to provide this as part of EDM4eic as well, but
    in any case we would still need to keep this around for a while here. It
    would also add an Acts dependency to EDM4eic, which is hardly ideal.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/algorithms/tracking/CKFTracking.cc
+++ src/algorithms/tracking/CKFTracking.cc
@@ -67,7 +67,6 @@
 #include <ActsExamples/EventData/Measurement.hpp>
 #include <ActsExamples/EventData/MeasurementCalibration.hpp>
 #include <ActsExamples/EventData/Track.hpp>
-#include <boost/container/detail/std_fwd.hpp>
 #include <boost/container/vector.hpp>
 #include <edm4eic/Cov3f.h>
 #include <edm4eic/Cov6f.h>
@@ -93,26 +92,13 @@
 
 #include "ActsGeometryProvider.h"
 #include "DD4hepBField.h"
+#include "extensions/edm4eic/EDM4eicToActs.h"
 #include "extensions/spdlog/SpdlogFormatters.h" // IWYU pragma: keep
 #include "extensions/spdlog/SpdlogToActs.h"
 
 namespace eicrecon {
 
 using namespace Acts::UnitLiterals;
-
-// This array relates the Acts and EDM4eic covariance matrices, including
-// the unit conversion to get from Acts units into EDM4eic units.
-//
-// Note: std::map is not constexpr, so we use a constexpr std::array
-// std::array initialization need double braces since arrays are aggregates
-// ref: https://en.cppreference.com/w/cpp/language/aggregate_initialization
-static constexpr std::array<std::pair<Acts::BoundIndices, double>, 6> edm4eic_indexed_units{
-    {{Acts::eBoundLoc0, Acts::UnitConstants::mm},
-     {Acts::eBoundLoc1, Acts::UnitConstants::mm},
-     {Acts::eBoundPhi, 1.},
-     {Acts::eBoundTheta, 1.},
-     {Acts::eBoundQOverP, 1. / Acts::UnitConstants::GeV},
-     {Acts::eBoundTime, Acts::UnitConstants::ns}}};
 
 CKFTracking::CKFTracking() = default;
 
