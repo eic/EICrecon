80c376edb61e012a1956af71d55b18d654ed1b6c
Tommy Chun Yuen Tsang <tommytsang3108@hotmail.com.hk>
2025-09-23 10:02:44 -0400

    Update src/detectors/BTOF/BTOF.cc
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/detectors/BTOF/BTOF.cc
+++ src/detectors/BTOF/BTOF.cc
@@ -119,7 +119,7 @@ void InitPlugin(JApplication* app) {
 
   double risetime = 0.45 * edm4eic::unit::ns;
   app->Add(new JOmniFactoryGeneratorT<SiliconPulseDiscretization_factory>(
-      "TOFBarrelPulseDiscretization", {"TOFBarrelCombinedPulses"}, {"TOFBarrelPulses"},
+      "TOFBarrelPulses", {"TOFBarrelCombinedPulses"}, {"TOFBarrelPulses"},
       {
           .EICROC_period = 25 * edm4eic::unit::ns,
           .local_period  = 25 * edm4eic::unit::ns / 1024,
