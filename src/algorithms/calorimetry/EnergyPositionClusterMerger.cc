947373701a0be744e125e0bd9a633d3df53de928
Wouter Deconinck <wdconinc@gmail.com>
2025-10-13 12:22:35 -0500

    clang-tidy: readability-container-size-empty (#2116)
    ### Briefly, what does this PR introduce?
    This PR applies clang-tidy readability-container-size-empty check fixes
    from main.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: code base should satisfy PR clang-tidy
    requirements)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/algorithms/calorimetry/EnergyPositionClusterMerger.cc
+++ src/algorithms/calorimetry/EnergyPositionClusterMerger.cc
@@ -3,7 +3,6 @@
 
 #include "algorithms/calorimetry/EnergyPositionClusterMerger.h"
 
-#include <edm4eic/Cov3f.h>
 #include <edm4hep/MCParticle.h>
 #include <edm4hep/Vector3f.h>
 #include <edm4hep/utils/vector_utils.h>
@@ -26,7 +25,7 @@ void EnergyPositionClusterMerger::process(const Input& input, const Output& outp
 
   debug("Merging energy and position clusters for new event");
 
-  if (energy_clus->size() == 0 && pos_clus->size() == 0) {
+  if (energy_clus->empty() && pos_clus->empty()) {
     debug("Nothing to do for this event, returning...");
     return;
   }
