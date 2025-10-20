e3829a56e69e47e90722efda203fa6700ee68bc7
Alex Jentsch <alexmichaeljentsch@gmail.com>
2025-10-20 13:18:36 -0400

    Update src/algorithms/fardetectors/PolynomialMatrixReconstruction.cc
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- src/algorithms/fardetectors/PolynomialMatrixReconstruction.cc
+++ src/algorithms/fardetectors/PolynomialMatrixReconstruction.cc
@@ -71,7 +71,7 @@ void eicrecon::PolynomialMatrixReconstruction::process(
       numBeamProtons++;
     }
     if (p.getGeneratorStatus() == 1 && p.getPDG() == 2212) {
-      double xMom_noXAngle = sin(0.025) * p.getMomentum().z + cos(0.025) * p.getMomentum().x;
+      double xMom_noXAngle = sin(m_cfg.crossingAngle) * p.getMomentum().z + cos(m_cfg.crossingAngle) * p.getMomentum().x;
 
       trace("Final-state proton MCParticle momentum --> px = {}, py = {}, pz = {}, theta_x_IP = "
             "{}, theta_y_IP = {}",
