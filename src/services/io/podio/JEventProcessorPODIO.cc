4c8a98a0e9c41d648a42b97cd4c791ab4e975424
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-10-23 17:38:10 -0400

    Temporarily add charged candidates to default output

--- src/services/io/podio/JEventProcessorPODIO.cc
+++ src/services/io/podio/JEventProcessorPODIO.cc
@@ -408,6 +408,11 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
       "HcalEndcapPInsertRemnantClusters",
       "HcalEndcapPInsertExpectedClusters",
       "HcalEndcapPInsertTrackExpectedClusterMatches",
+
+      "EndcapNChargedCandidateParticlesAlpha",
+      "BarrelChargedCandidateParticlesAlpha",
+      "EndcapPChargedCandidateParticlesAlpha",
+      "EndcapPInsertChargedCandidateParticlesAlpha",
   };
   std::vector<std::string> output_exclude_collections; // need to get as vector, then convert to set
   std::string output_include_collections = "DEPRECATED";
