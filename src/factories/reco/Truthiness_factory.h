59961cd0e0a72f3e3ce2fb1b331e64cdddd41da7
Wouter Deconinck <wdconinc@gmail.com>
2025-10-28 20:45:04 -0500

    feat: add Truthiness algorithm for event quality assessment

---
+++ src/factories/reco/Truthiness_factory.h
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Wouter Deconinck
+
+#pragma once
+
+#include <JANA/JEvent.h>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "algorithms/reco/Truthiness.h"
+#include "extensions/jana/JOmniFactory.h"
+#include "services/algorithms_init/AlgorithmsInit_service.h"
+
+namespace eicrecon {
+
+class Truthiness_factory : public JOmniFactory<Truthiness_factory> {
+
+public:
+  using FactoryT = JOmniFactory<Truthiness_factory>;
+
+private:
+  std::unique_ptr<Truthiness> m_algo;
+
+  FactoryT::PodioInput<edm4hep::MCParticle> m_mc_particles_input{this};
+  FactoryT::PodioInput<edm4eic::ReconstructedParticle> m_rc_particles_input{this};
+  FactoryT::PodioInput<edm4eic::MCRecoParticleAssociation> m_associations_input{this};
+
+  FactoryT::Service<AlgorithmsInit_service> m_algorithmsInit{this};
+
+public:
+  void Configure() {
+    m_algo = std::make_unique<Truthiness>(this->GetPrefix());
+    m_algo->level(static_cast<algorithms::LogLevel>(this->logger()->level()));
+    m_algo->init();
+  }
+
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
+    m_algo->process({m_mc_particles_input(), m_rc_particles_input(), m_associations_input()}, {});
+  }
+};
+
+} // namespace eicrecon
