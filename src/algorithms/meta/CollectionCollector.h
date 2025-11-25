68261ebebbcd1151cca84a2045f03ae0e027b8a1
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-11-25 11:58:41 -0500

    two missing withpodconfig

--- src/algorithms/meta/CollectionCollector.h
+++ src/algorithms/meta/CollectionCollector.h
@@ -18,7 +18,8 @@ using CollectionCollectorAlgorithm =
     algorithms::Algorithm<typename algorithms::Input<std::vector<const T>>,
                           typename algorithms::Output<T>>;
 
-template <class T> class CollectionCollector : public CollectionCollectorAlgorithm<T> {
+template <class T> class CollectionCollector : public CollectionCollectorAlgorithm<T>,
+                                               public WithPodConfig<NoConfig> {
 
 public:
   CollectionCollector(std::string_view name)
