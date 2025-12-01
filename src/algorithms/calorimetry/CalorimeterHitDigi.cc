6c61f0d287f5d1fdf4e2947fe88b598bbc3fdd97
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-12-01 14:18:51 -0500

    update copyright

--- src/algorithms/calorimetry/CalorimeterHitDigi.cc
+++ src/algorithms/calorimetry/CalorimeterHitDigi.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022 Chao Peng, Wouter Deconinck, Sylvester Joosten, Barak Schmookler, David Lawrence
+// Copyright (C) 2022 - 2025 Chao Peng, Wouter Deconinck, Sylvester Joosten, Barak Schmookler, David Lawrence, Akio Ogawa
 
 // A general digitization for CalorimeterHit from simulation
 // 1. Smear energy deposit with a/sqrt(E/GeV) + b + c/E or a/sqrt(E/GeV) (relative value)
