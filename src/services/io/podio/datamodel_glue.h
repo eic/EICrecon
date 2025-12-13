2a5cbf73d5db97dd26f707623891589f3060a81f
Wouter Deconinck <wdconinc@gmail.com>
2025-12-13 15:06:15 -0600

    feat: implement podio>=1.3 C++-based datamodel glue code

---
+++ src/services/io/podio/datamodel_glue.h
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Wouter Deconinck
+//
+// Modern datamodel glue for podio >= 1.3
+// This file uses podio's built-in TypeList support instead of code generation
+
+#pragma once
+
+#include <stdexcept>
+#include <string_view>
+#include <map>
+#include <podio/CollectionBase.h>
+#include <podio/utilities/TypeHelpers.h>
+
+// Use umbrella headers if available (podio >= 1.3)
+#include "services/io/podio/datamodel_includes.h"
+
+// PodioTypeMap provides type traits for podio types
+template <typename T> struct PodioTypeMap {
+  using collection_t = typename T::collection_type;
+  using mutable_t = typename T::mutable_type;
+};
+
+// Helper to build a map of typename -> visitor function
+template <typename Visitor>
+class CollectionVisitorMap {
+public:
+  using FunctionType = void (*)(Visitor&, const podio::CollectionBase&);
+  
+private:
+  std::map<std::string_view, FunctionType> m_map;
+  
+  template <typename CollectionT>
+  static void visitCollection(Visitor& visitor, const podio::CollectionBase& collection) {
+    visitor(*static_cast<const CollectionT*>(&collection));
+  }
+  
+  template <typename CollectionT>
+  void addToMap() {
+    m_map[CollectionT::typeName] = &visitCollection<CollectionT>;
+  }
+  
+  template <typename DataT>
+  void addDataTypeToMap() {
+    addToMap<typename DataT::collection_type>();
+  }
+  
+  template <typename... DataTypes>
+  void addAllDataCollections(podio::utils::TypeList<DataTypes...>) {
+    (addDataTypeToMap<DataTypes>(), ...);
+  }
+  
+  template <typename... LinkTypes>
+  void addAllLinkCollections(podio::utils::TypeList<LinkTypes...>) {
+    (addDataTypeToMap<LinkTypes>(), ...);
+  }
+  
+public:
+  CollectionVisitorMap() {
+    // Add all EDM4hep and EDM4eic data collection types
+    addAllDataCollections(typename edm4hep::edm4hepDataTypes{});
+    addAllDataCollections(typename edm4eic::edm4eicDataTypes{});
+    
+    // Add all EDM4hep and EDM4eic link collections
+    addAllLinkCollections(typename edm4hep::edm4hepLinkTypes{});
+    addAllLinkCollections(typename edm4eic::edm4eicLinkTypes{});
+  }
+  
+  const auto& getMap() const { return m_map; }
+};
+
+// VisitPodioCollection: visitor pattern for type-safe collection processing
+template <typename Visitor> 
+struct VisitPodioCollection {
+  void operator()(Visitor& visitor, const podio::CollectionBase& collection) {
+    // Build the map once (static initialization)
+    static const CollectionVisitorMap<Visitor> visitorMap;
+    
+    auto typeName = collection.getTypeName();
+    auto it = visitorMap.getMap().find(typeName);
+    
+    if (it != visitorMap.getMap().end()) {
+      it->second(visitor, collection);
+    } else {
+      throw std::runtime_error(std::string("Unrecognized podio typename: ") + std::string(typeName));
+    }
+  }
+};
---
+++ src/services/io/podio/datamodel_includes_compat.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Wouter Deconinck
+//
+// Datamodel includes compatibility header
+// For podio >= 1.3, use umbrella headers. For older versions, this includes
+// the Python-generated file.
+
+#pragma once
+
+#include <podio/podioVersion.h>
+
+#if defined(podio_VERSION_MAJOR) && defined(podio_VERSION_MINOR)
+#  if (podio_VERSION_MAJOR > 1) || (podio_VERSION_MAJOR == 1 && podio_VERSION_MINOR >= 3)
+#    define USE_MODERN_PODIO_GLUE 1
+#  endif
+#endif
+
+#ifdef USE_MODERN_PODIO_GLUE
+// Use umbrella headers for modern podio
+#include "services/io/podio/datamodel_includes.h"
+#else
+// Legacy Python-generated includes
+#include "services/io/podio/datamodel_includes_legacy.h"
+#endif
