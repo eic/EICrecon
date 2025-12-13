8ea786dd46a0e7b3cd8c187fc19d29c6a9fb7cb2
Wouter Deconinck <wdconinc@gmail.com>
2025-12-12 21:57:30 -0600

    Add conditional alias for DD4hepFieldAdapter

--- src/algorithms/tracking/IterativeVertexFinder.h
+++ src/algorithms/tracking/IterativeVertexFinder.h
@@ -22,6 +22,13 @@
 #include "IterativeVertexFinderConfig.h"
 #include "algorithms/interfaces/WithPodConfig.h"
 
+// Alias DD4hepFieldAdapter for different Acts versions
+#if __has_include(<ActsPlugins/DD4hep/DD4hepFieldAdapter.hpp>)
+using DD4hepFieldAdapter = ActsPlugins::DD4hepFieldAdapter;
+#else
+using DD4hepFieldAdapter = Acts::DD4hepFieldAdapter;
+#endif
+
 namespace eicrecon {
 class IterativeVertexFinder
     : public eicrecon::WithPodConfig<eicrecon::IterativeVertexFinderConfig> {
