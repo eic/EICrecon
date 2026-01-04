038e47ccc654f791774ff8f43b748f5787726fa9
epic-capybara <139920704+epic-capybara@users.noreply.github.com>
2026-01-04 11:44:42 -0500

    fix: skip IRT1 hypotheses with nan weight (fix: iwyu) (#2291)
    This PR applies the include-what-you-use fixes as suggested by
    https://github.com/eic/EICrecon/actions/runs/20695723662.
    Please merge this PR into the branch `IRT1-skip-nan`
    to resolve failures in PR #2290.
    
    Auto-generated by [create-pull-request][1]
    
    [1]: https://github.com/peter-evans/create-pull-request
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/algorithms/pid/IrtCherenkovParticleID.cc
+++ src/algorithms/pid/IrtCherenkovParticleID.cc
@@ -18,8 +18,8 @@
 #include <edm4hep/Vector2f.h>
 #include <edm4hep/Vector3d.h>
 #include <edm4hep/Vector3f.h>
-#include <fmt/core.h>
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 #include <podio/ObjectID.h>
 #include <podio/RelationRange.h>
 #include <algorithm>
