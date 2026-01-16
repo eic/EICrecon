ca35d67c6922190338404d322db2384d8c6390a6
epic-capybara <139920704+epic-capybara@users.noreply.github.com>
2026-01-16 10:50:42 -0500

    Use `MCRecoCalorimeterHitAssociation`s in `CalorimeterTruthClustering` (fix: iwyu) (#2340)
    This PR applies the include-what-you-use fixes as suggested by
    https://github.com/eic/EICrecon/actions/runs/21071874468.
    Please merge this PR into the branch
    `issue-2335-use-associations-truth-clustering`
    to resolve failures in PR #2336.
    
    Auto-generated by [create-pull-request][1]
    
    [1]: https://github.com/peter-evans/create-pull-request
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/detectors/FEMC/FEMC.cc
+++ src/detectors/FEMC/FEMC.cc
@@ -6,7 +6,7 @@
 #include <JANA/JApplication.h>
 #include <JANA/JApplicationFwd.h>
 #include <JANA/Utils/JTypeInfo.h>
-#include <fmt/core.h>
+#include <fmt/format.h>
 #include <spdlog/logger.h>
 #include <cmath>
 #include <gsl/pointers>
