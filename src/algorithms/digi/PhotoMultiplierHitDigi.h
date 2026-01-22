cadd8331028535eb3c55c1047e22900ec638e6b8
Wouter Deconinck <wdconinc@gmail.com>
2026-01-22 17:13:11 -0600

    fix: accept map in InsertHit
    Changed the data structure for hit_groups from unordered_map to map.

--- src/algorithms/digi/PhotoMultiplierHitDigi.h
+++ src/algorithms/digi/PhotoMultiplierHitDigi.h
@@ -105,7 +105,7 @@ protected:
 
 private:
   // add a hit to local `hit_groups` data structure
-  void InsertHit(std::unordered_map<CellIDType, std::vector<HitData>>& hit_groups, CellIDType id,
+  void InsertHit(std::map<CellIDType, std::vector<HitData>>& hit_groups, CellIDType id,
                  double amp, TimeType time, std::size_t sim_hit_index,
                  std::default_random_engine& generator, std::normal_distribution<double>& gaussian,
                  bool is_noise_hit = false) const;
