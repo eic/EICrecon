d99b8d9a3b501f461c8ac0fe000f8ee1ba0eeb48
Wouter Deconinck <wdconinc@gmail.com>
2026-01-25 18:51:46 -0600

    fix: std::hash<podio::ObjectID>
    Added custom hash function for podio::ObjectID based on version checks.

--- src/algorithms/calorimetry/association_utils.h
+++ src/algorithms/calorimetry/association_utils.h
@@ -22,6 +22,24 @@
 #include <ranges>
 #include <type_traits>
 
+// Define necessary hash functions
+namespace std {
+
+#if defined(podio_VERSION_MAJOR) && defined(podio_VERSION_MINOR)
+#if podio_VERSION <= PODIO_VERSION(1, 2, 0)
+// Hash for podio::ObjectID
+template <> struct hash<podio::ObjectID> {
+  size_t operator()(const podio::ObjectID& id) const noexcept {
+    size_t h1 = std::hash<uint32_t>{}(id.collectionID);
+    size_t h2 = std::hash<int>{}(id.index);
+    return h1 ^ (h2 << 1);
+  }
+};
+#endif // podio version check
+#endif // defined(podio_VERSION_MAJOR) && defined(podio_VERSION_MINOR)
+
+} // namespace std
+
 namespace edm4eic {
 
 /**
