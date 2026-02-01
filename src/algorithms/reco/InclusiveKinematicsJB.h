6b7c774355f59efa79dc7c50cdd69766b36e6825
Wouter Deconinck <wdconinc@gmail.com>
2026-02-01 15:23:50 -0600

    feat: use only beam electron and proton as input to kinematics reco (#2385)
    ### Briefly, what does this PR introduce?
    We have a factory that splits `MCParticles` into `MCBeamElectrons` and
    `MCBeamProtons`, yet we still pass `MCParticles` to the kinematics
    algorithms to determine (again) what the beam electron and proton is.
    This PR modifies the inclusive algorithms to us the `MCBeamElectrons`
    and `MCBeamProtons` as input.
    
    Note: Not all inclusive and hadronic final state algorithms can get rid
    of `MCParticles` as input, since there is still some reliance on other
    truth information (e.g. to determine what the scattered electron is).
    See also #1653 for example.
    
    Keeping for a next PR: UndoAfterBurner, TransformBreitFrame.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: duplicate determinations)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.
    
    ---------
    
    Co-authored-by: epic-capybara <139920704+epic-capybara@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/algorithms/reco/InclusiveKinematicsJB.h
+++ src/algorithms/reco/InclusiveKinematicsJB.h
@@ -17,7 +17,8 @@
 namespace eicrecon {
 
 using InclusiveKinematicsJBAlgorithm = algorithms::Algorithm<
-    algorithms::Input<edm4hep::MCParticleCollection, edm4eic::ReconstructedParticleCollection,
+    algorithms::Input<edm4hep::MCParticleCollection, edm4hep::MCParticleCollection,
+                      edm4eic::ReconstructedParticleCollection,
                       edm4eic::HadronicFinalStateCollection>,
     algorithms::Output<edm4eic::InclusiveKinematicsCollection>>;
 
@@ -28,7 +29,7 @@ public:
   InclusiveKinematicsJB(std::string_view name)
       : InclusiveKinematicsJBAlgorithm{
             name,
-            {"MCParticles", "scatteredElectron", "hadronicFinalState"},
+            {"MCBeamElectrons", "MCBeamProtons", "scatteredElectron", "hadronicFinalState"},
             {"inclusiveKinematics"},
             "Determine inclusive kinematics using Jacquet-Blondel method."} {}
 
