fb25ba3ef9c0264eb992599fc932c456cb418295
mhkim <minho.kim@anl.gov>
2026-02-12 14:00:32 -0600

    Use only the RawnEntry struct & Fill edm4eic out via lambda

--- src/algorithms/digi/CALOROCDigitization.cc
+++ src/algorithms/digi/CALOROCDigitization.cc
@@ -20,25 +20,14 @@
 
 #include "CALOROCDigitization.h"
 
-class CALOROCRawSample {
-public:
-  struct RawEntry {
-    double adc{0};
-    double toa{0};
-    double tot{0};
-  };
+namespace {
 
-  CALOROCRawSample(std::size_t n_samps) { rawEntries.resize(n_samps); }
-
-  void setADC(std::size_t idx, double adc) { rawEntries[idx].adc = adc; }
-  void setTOA(std::size_t idx, double toa) { rawEntries[idx].toa = toa; }
-  void setTOT(std::size_t idx, double tot) { rawEntries[idx].tot = tot; }
-
-  const std::vector<RawEntry>& getEntries() const { return rawEntries; }
-
-private:
-  std::vector<RawEntry> rawEntries;
+struct RawEntry {
+  double adc{0};
+  double toa{0};
+  double tot{0};
 };
+} // namespace
 
 namespace eicrecon {
 
@@ -54,7 +43,7 @@ void CALOROCDigitization::process(const CALOROCDigitization::Input& input,
     double pulse_dt    = pulse.getInterval();
     std::size_t n_amps = pulse.getAmplitude().size();
 
-    CALOROCRawSample raw_sample(m_cfg.n_samples);
+    std::vector<RawEntry> raw_samples(m_cfg.n_samples);
 
     // For ADC, amplitudes are measured with a fixed phase.
     // This was reproduced by sample_tick and adc_counter as follows.
@@ -78,8 +67,8 @@ void CALOROCDigitization::process(const CALOROCDigitization::Input& input,
 
       // Measure amplitudes for ADC
       if (adc_counter == sample_tick) {
-        raw_sample.setADC(idx_sample, pulse.getAmplitude()[i]);
-        adc_counter = 0;
+        raw_samples[idx_sample].adc = pulse.getAmplitude()[i];
+        adc_counter                 = 0;
         idx_sample++;
         if (idx_sample == m_cfg.n_samples)
           break;
@@ -90,17 +79,17 @@ void CALOROCDigitization::process(const CALOROCDigitization::Input& input,
         idx_toa   = idx_sample;
         t_upcross = get_crossing_time(m_cfg.toa_thres, pulse_dt, t, pulse.getAmplitude()[i],
                                       pulse.getAmplitude()[i - 1]);
-        raw_sample.setTOA(idx_toa,
-                          m_cfg.adc_phase + (time_stamp + idx_toa) * m_cfg.time_window - t_upcross);
+        raw_samples[idx_toa].toa =
+            m_cfg.adc_phase + (time_stamp + idx_toa) * m_cfg.time_window - t_upcross;
         tot_progress = true;
       }
 
       // Measure down-crossing time for TOT
       if (tot_progress && pulse.getAmplitude()[i] < m_cfg.tot_thres) {
-        raw_sample.setTOT(idx_toa,
-                          get_crossing_time(m_cfg.tot_thres, pulse_dt, t, pulse.getAmplitude()[i],
-                                            pulse.getAmplitude()[i - 1]) -
-                              t_upcross);
+        raw_samples[idx_toa].tot =
+            get_crossing_time(m_cfg.tot_thres, pulse_dt, t, pulse.getAmplitude()[i],
+                              pulse.getAmplitude()[i - 1]) -
+            t_upcross;
         tot_progress = false;
       }
 
@@ -113,28 +102,34 @@ void CALOROCDigitization::process(const CALOROCDigitization::Input& input,
     out_digi_hit.setSamplePhase(std::llround(m_cfg.adc_phase / m_cfg.dyRangeTOA * m_cfg.capTOA));
     out_digi_hit.setTimeStamp(time_stamp);
 
-    const auto& entries = raw_sample.getEntries();
+    for (const auto& raw_sample : raw_samples) {
+      auto adc =
+          std::max(std::llround(raw_sample.adc / m_cfg.dyRangeSingleGainADC * m_cfg.capADC), 0LL);
+      auto toa = std::max(std::llround(raw_sample.toa / m_cfg.dyRangeTOA * m_cfg.capTOA), 0LL);
+      auto tot = std::max(std::llround(raw_sample.tot / m_cfg.dyRangeTOT * m_cfg.capTOT), 0LL);
 
-    for (const auto& entry : entries) {
-      edm4eic::CALOROC1ASample aSample;
-      auto adc = std::max(std::llround(entry.adc / m_cfg.dyRangeSingleGainADC * m_cfg.capADC), 0LL);
-      aSample.ADC = adc > m_cfg.capADC ? m_cfg.capADC : adc;
-      auto toa    = std::max(std::llround(entry.toa / m_cfg.dyRangeTOA * m_cfg.capTOA), 0LL);
-      aSample.timeOfArrival = toa > m_cfg.capTOA ? m_cfg.capTOA : toa;
-      auto tot = std::max(std::llround(entry.tot / m_cfg.dyRangeTOT * m_cfg.capTOT), 0LL);
-      aSample.timeOverThreshold = tot > m_cfg.capTOT ? m_cfg.capTOT : tot;
-      out_digi_hit.addToASamples(aSample);
+      out_digi_hit.addToASamples([&]() {
+        edm4eic::CALOROC1ASample aSample;
+        aSample.ADC               = adc > m_cfg.capADC ? m_cfg.capADC : adc;
+        aSample.timeOfArrival     = toa > m_cfg.capTOA ? m_cfg.capTOA : toa;
+        aSample.timeOverThreshold = tot > m_cfg.capTOT ? m_cfg.capTOT : tot;
+        return aSample;
+      }());
 
-      edm4eic::CALOROC1BSample bSample;
       auto high_adc =
-          std::max(std::llround(entry.adc / m_cfg.dyRangeHighGainADC * m_cfg.capADC), 0LL);
-      bool overflow       = high_adc > m_cfg.capADC;
-      bSample.highGainADC = overflow ? m_cfg.capADC : high_adc;
+          std::max(std::llround(raw_sample.adc / m_cfg.dyRangeHighGainADC * m_cfg.capADC), 0LL);
       auto low_adc =
-          std::max(std::llround(entry.adc / m_cfg.dyRangeLowGainADC * m_cfg.capADC), 0LL);
-      bSample.lowGainADC = overflow ? std::min(low_adc, static_cast<long long>(m_cfg.capADC)) : 0LL;
-      bSample.timeOfArrival = toa > m_cfg.capTOA ? m_cfg.capTOA : toa;
-      out_digi_hit.addToBSamples(bSample);
+          std::max(std::llround(raw_sample.adc / m_cfg.dyRangeLowGainADC * m_cfg.capADC), 0LL);
+
+      out_digi_hit.addToBSamples([&]() {
+        edm4eic::CALOROC1BSample bSample;
+        bool overflow       = high_adc > m_cfg.capADC;
+        bSample.highGainADC = overflow ? m_cfg.capADC : high_adc;
+        bSample.lowGainADC =
+            overflow ? std::min(low_adc, static_cast<long long>(m_cfg.capADC)) : 0LL;
+        bSample.timeOfArrival = toa > m_cfg.capTOA ? m_cfg.capTOA : toa;
+        return bSample;
+      }());
     }
   }
 
