59827990a4ba87e0e2993e561f57bc9aad4db6e4
emolinac <emolinac92@gmail.com>
2026-02-13 11:17:37 -0500

    chore(license): implemented standard license format

--- src/algorithms/particle/ParticleConverter.h
+++ src/algorithms/particle/ParticleConverter.h
@@ -1,5 +1,5 @@
-// author : Esteban Molina, Derek Anderson
-// date   : January 2026
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2026 Esteban Molina, Derek Anderson
 
 #pragma once
 
