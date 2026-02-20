53b1e64696ef2b704dd999c3b37eda77df391661
emolinac <emolinac92@gmail.com>
2026-02-20 14:35:35 -0500

    fix(ParticleConverter)!: somewhow a big chunk of code was outside the particle loop. Now is fixed!

--- src/algorithms/particle/ParticleConverter.cc
+++ src/algorithms/particle/ParticleConverter.cc
@@ -80,7 +80,8 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
         continue;
       }
 
-      calo_hit = cluster.getHits()[0];
+      const auto calo_hit = cluster.getHits()[0];
+
       /* check type */
       const auto cell_id = calo_hit.getCellID();
 
@@ -93,71 +94,70 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
         std::string det_element_type = det_element.type();
 
         // Note: will probably change
-        if (type.is(ddh4ep::DetType::CALORIMETER) && type.is(dd4hep::DetType::ELECTROMAGNETIC))
+        if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::ELECTROMAGNETIC))
           isECal = true;
 
-        if (type.is(ddh4ep::DetType::CALORIMETER) && type.is(dd4hep::DetType::HADRONIC))
+        if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::HADRONIC))
           isHCal = true;
       }
 
       if (isECal)
         ecal_energy += cluster.getEnergy();
-      
+
       if (isHCal)
         hcal_energy += cluster.getEnergy();
     }
+
+    if (isECal && !isHCal && !isTrack)
+      prelim_pid = 22; //photon
+    if (!isECal && isHCal && !isTrack)
+      prelim_pid = 2112; // neutron
+
+    // Step 2 : Calculate calo energy (PRELIMINARY IMPLEMENTATION)
+    if (ecal_energy > 0)
+      calo_energy += ecal_energy;
+    if (hcal_energy > 0)
+      calo_energy += m_cfg.calo_hadron_scale * hcal_energy;
+
+    calo_energy *= m_cfg.calo_energy_norm;
+
+    // Step 3 : Calculate avge energy (PRELIMINARY IMPLEMENTATION)
+    double weight_tracking_resolution = 1. / std::pow(m_cfg.tracking_resolution, 2);
+    double weight_calo_resolution =
+        1. / std::pow(m_cfg.ecal_resolution, 2); // USING ECAL RESOLUTION AS PLACEHOLDER!
+
+    double normalization = 0;
+
+    if (track_energy > 0 && calo_energy > 0)
+      normalization = weight_tracking_resolution + weight_calo_resolution;
+    else if (track_energy > 0 && calo_energy == 0)
+      normalization = weight_tracking_resolution;
+    else if (track_energy == 0 && calo_energy > 0)
+      normalization = weight_calo_resolution;
+
+    avge_energy = (weight_tracking_resolution * track_energy + weight_calo_resolution * calo_energy) /
+                  normalization;
+
+    // Step 4 : Store information on a mutable collection
+    double mass_calculated = std::sqrt(std::pow(avge_energy, 2) - std::pow(track_momentum_mag, 2));
+
+    edm4eic::MutableReconstructedParticle out_reco_particle = particle.clone();
+
+    out_reco_particle.setEnergy(avge_energy);
+    out_reco_particle.setMomentum(
+        edm4hep::Vector3f(track_momentum_vector.x, track_momentum_vector.y, track_momentum_vector.z));
+    out_reco_particle.setReferencePoint(particle.getReferencePoint());
+
+    out_reco_particle.setCharge(particle.getCharge());
+    out_reco_particle.setMass(mass_calculated);
+    out_reco_particle.setGoodnessOfPID(particle.getGoodnessOfPID());
+    out_reco_particle.setCovMatrix(particle.getCovMatrix());
+    out_reco_particle.setPDG(prelim_pid);
+
+    out_reco_particle.setStartVertex(particle.getStartVertex());
+    out_reco_particle.setParticleIDUsed(particle.getParticleIDUsed());
+
+    out_particles->push_back(out_reco_particle);
   }
-
-  if (isECal && !isHCal && !isTrack)
-    prelim_pid = 22; //photon
-  if (!isECal && isHCal && !isTrack)
-    prelim_pid = 2112; // neutron
-
-  // Step 2 : Calculate calo energy (PRELIMINARY IMPLEMENTATION)
-  if (ecal_energy > 0)
-    calo_energy += ecal_energy;
-  if (hcal_energy > 0)
-    calo_energy += m_cfg.calo_hadron_scale * hcal_energy;
-
-  calo_energy *= m_cfg.calo_energy_norm;
-
-  // Step 3 : Calculate avge energy (PRELIMINARY IMPLEMENTATION)
-  double weight_tracking_resolution = 1. / std::pow(m_cfg.tracking_resolution, 2);
-  double weight_calo_resolution =
-      1. / std::pow(m_cfg.ecal_resolution, 2); // USING ECAL RESOLUTION AS PLACEHOLDER!
-
-  double normalization = 0;
-
-  if (track_energy > 0 && calo_energy > 0)
-    normalization = weight_tracking_resolution + weight_calo_resolution;
-  else if (track_energy > 0 && calo_energy == 0)
-    normalization = weight_tracking_resolution;
-  else if (track_energy == 0 && calo_energy > 0)
-    normalization = weight_calo_resolution;
-
-  avge_energy = (weight_tracking_resolution * track_energy + weight_calo_resolution * calo_energy) /
-                normalization;
-
-  // Step 4 : Store information on a mutable collection
-  double mass_calculated = std::sqrt(std::pow(avge_energy, 2) - std::pow(track_momentum_mag, 2));
-
-  edm4eic::MutableReconstructedParticle out_reco_particle = particle.clone();
-
-  out_reco_particle.setEnergy(avge_energy);
-  out_reco_particle.setMomentum(
-      edm4hep::Vector3f(track_momentum_vector.x, track_momentum_vector.y, track_momentum_vector.z));
-  out_reco_particle.setReferencePoint(particle.getReferencePoint());
-
-  out_reco_particle.setCharge(particle.getCharge());
-  out_reco_particle.setMass(mass_calculated);
-  out_reco_particle.setGoodnessOfPID(particle.getGoodnessOfPID());
-  out_reco_particle.setCovMatrix(particle.getCovMatrix());
-  out_reco_particle.setPDG(prelim_pid);
-
-  out_reco_particle.setStartVertex(particle.getStartVertex());
-  out_reco_particle.setParticleIDUsed(particle.getParticleIDUsed());
-
-  out_particles->push_back(out_reco_particle);
-}
 };
 } // namespace eicrecon
