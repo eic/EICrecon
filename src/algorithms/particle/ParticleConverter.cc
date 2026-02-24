b55c25ff0ed8775be3eca51422336f7a7fadcb31
emolinac <emolinac92@gmail.com>
2026-02-24 14:16:14 -0500

    feat(ParticleConverter)!: Functioning neutral particle momentum, built with information from PV. refactor(ParticleConverter): using previous hcal/ecal discriminator to test results

--- src/algorithms/particle/ParticleConverter.cc
+++ src/algorithms/particle/ParticleConverter.cc
@@ -46,8 +46,6 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
     primary_vertex.x = (*in_vertices)[0].getPosition().x;
     primary_vertex.y = (*in_vertices)[0].getPosition().y;
     primary_vertex.z = (*in_vertices)[0].getPosition().z;
-
-    debug("PV = ({},{},{})", primary_vertex.x, primary_vertex.y, primary_vertex.z);
   } else {
     debug("No primary vertex found. Set PV to (0,0,0).");
   }
@@ -67,9 +65,14 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
     double ecal_energy = 0;
     double hcal_energy = 0;
 
-    double estimated_energy = 0;
+    double reconstructed_energy = 0;
 
-    edm4hep::Vector3f track_momentum_vector;
+    edm4hep::Vector3f ecal_cluster_position(0.0, 0.0, 0.0);
+    edm4hep::Vector3f hcal_cluster_position(0.0, 0.0, 0.0);
+
+    edm4hep::Vector3f track_momentum_vector(0.0, 0.0, 0.0);
+
+    edm4hep::Vector3f reconstructed_momentum_vector(0.0, 0.0, 0.0);
 
     // Step 1 : Assign preliminary PID
 
@@ -98,6 +101,10 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
                     std::pow(track_momentum_vector.z, 2));
 
       track_energy = std::sqrt(std::pow(track_momentum_mag, 2) + std::pow(track_mass, 2));
+
+      reconstructed_momentum_vector.x = track_momentum_vector.x;
+      reconstructed_momentum_vector.y = track_momentum_vector.y;
+      reconstructed_momentum_vector.z = track_momentum_vector.z;
     }
 
     // Looking for clusters
@@ -109,7 +116,6 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
 
       const auto calo_hit = cluster.getHits()[0];
 
-      /* check type */
       const auto cell_id = calo_hit.getCellID();
 
       const dd4hep::VolumeManagerContext* context = m_converter->findContext(cell_id);
@@ -120,19 +126,51 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
 
         std::string det_element_type = det_element.type();
 
-        // Note: will probably change
-        if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::ELECTROMAGNETIC))
+        // When all testing is done, return to this form of Calo discriminator
+        // if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::ELECTROMAGNETIC))
+        //   hasECal = true;
+
+        // if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::HADRONIC))
+        //   hasHCal = true;
+
+        std::string ecal_string = "Ecal";
+        std::string hcal_string = "Hcal";
+
+        if (det_element_type.find(ecal_string) != std::string::npos)
           hasECal = true;
 
-        if (type.is(dd4hep::DetType::CALORIMETER) && type.is(dd4hep::DetType::HADRONIC))
+        if (det_element_type.find(hcal_string) != std::string::npos)
           hasHCal = true;
       }
 
-      if (hasECal)
+      if (hasECal) {
         ecal_energy += cluster.getEnergy();
 
-      if (hasHCal)
+        ecal_cluster_position.x += cluster.getEnergy() * cluster.getPosition().x;
+        ecal_cluster_position.y += cluster.getEnergy() * cluster.getPosition().y;
+        ecal_cluster_position.z += cluster.getEnergy() * cluster.getPosition().z;
+      }
+
+      if (hasHCal) {
         hcal_energy += cluster.getEnergy();
+
+        hcal_cluster_position.x += cluster.getEnergy() * cluster.getPosition().x;
+        hcal_cluster_position.y += cluster.getEnergy() * cluster.getPosition().y;
+        hcal_cluster_position.z += cluster.getEnergy() * cluster.getPosition().z;
+      }
+    }
+
+    // Energy weighted cluster position to determine momentum direction of non-track
+    if (hasECal) {
+      ecal_cluster_position.x /= ecal_energy;
+      ecal_cluster_position.y /= ecal_energy;
+      ecal_cluster_position.z /= ecal_energy;
+    }
+
+    if (hasHCal) {
+      hcal_cluster_position.x /= hcal_energy;
+      hcal_cluster_position.y /= hcal_energy;
+      hcal_cluster_position.z /= hcal_energy;
     }
 
     if (hasECal && !hasHCal && !hasTrack)
@@ -168,27 +206,77 @@ void ParticleConverter::process(const Input& input, const Output& output) const 
       normalization = weight_caloResolution;
 
     if (!hasTrack) {
-      estimated_energy = calo_energy;
+      reconstructed_energy = calo_energy;
     } else if (hasECal || hasHCal) {
-      estimated_energy =
+      reconstructed_energy =
           (weight_trackingResolution * track_energy + weight_caloResolution * calo_energy) /
           normalization;
     } else {
-      estimated_energy = track_energy;
+      reconstructed_energy = track_energy;
     }
 
-    // Step 4 : Store information on a mutable collection
-    double mass_calculated =
-        std::sqrt(std::pow(estimated_energy, 2) - std::pow(track_momentum_mag, 2));
+    // Step 5 (for neutrals): estimate the momentum direction
+    edm4hep::Vector3f neutrals_direction(0.0, 0.0, 0.0);
+
+    if (hasECal) {
+      debug("ECAL!!!!!!!!!!");
+
+      neutrals_direction.x = ecal_cluster_position.x - primary_vertex.x;
+      neutrals_direction.y = ecal_cluster_position.y - primary_vertex.y;
+      neutrals_direction.z = ecal_cluster_position.z - primary_vertex.z;
+    }
+
+    if (hasHCal) {
+      neutrals_direction.x = hcal_cluster_position.x - primary_vertex.x;
+      neutrals_direction.y = hcal_cluster_position.y - primary_vertex.y;
+      neutrals_direction.z = hcal_cluster_position.z - primary_vertex.z;
+    }
+
+    if (neutrals_direction.x != 0 && neutrals_direction.y != 0 && neutrals_direction.z != 0) {
+      double mag_neutrals_direction = std::sqrt(std::pow(neutrals_direction.x,2) + 
+                                                std::pow(neutrals_direction.y,2) + 
+                                                std::pow(neutrals_direction.z,2));
+
+      neutrals_direction.x /= mag_neutrals_direction;
+      neutrals_direction.y /= mag_neutrals_direction;
+      neutrals_direction.z /= mag_neutrals_direction;
+    }
+
+    if ((hasECal || hasHCal) && !hasTrack) {
+      double neutrals_momentum_mag = std::sqrt(std::pow(reconstructed_energy,2) - std::pow(m_particleSvc.particle(prelim_pid).mass,2));
+
+      reconstructed_momentum_vector.x = neutrals_direction.x * neutrals_momentum_mag;
+      reconstructed_momentum_vector.y = neutrals_direction.y * neutrals_momentum_mag;
+      reconstructed_momentum_vector.z = neutrals_direction.z * neutrals_momentum_mag;
+    }
+
+    // Step 6: write on the out output collection
+    double reconstructed_momentum_mag = std::sqrt(std::pow(reconstructed_momentum_vector.x,2) + 
+                                              std::pow(reconstructed_momentum_vector.y,2) +
+                                              std::pow(reconstructed_momentum_vector.z,2));
+
+    double reconstructed_mass = std::sqrt(std::pow(reconstructed_energy, 2) - std::pow(reconstructed_momentum_mag, 2));
 
     edm4eic::MutableReconstructedParticle out_reco_particle = particle.clone();
 
-    // TODO: momentum has to be calculated for the calo case!
+    debug("P = ({}, {}, {})", reconstructed_momentum_vector.x, reconstructed_momentum_vector.y, reconstructed_momentum_vector.z);
+    debug("M = {}", reconstructed_mass);
+    debug("E = {}", reconstructed_energy);
+
+    if (std::isnan(reconstructed_mass)) {
+      debug("M2 = {}", std::pow(reconstructed_energy, 2) - std::pow(reconstructed_momentum_mag, 2));
+    }
+
+    if (hasTrack && track_momentum_vector.x == 0) {
+      debug("Preco = ({}, {}, {})", reconstructed_momentum_vector.x, reconstructed_momentum_vector.y, reconstructed_momentum_vector.z);
+      debug("Ptrck = ({}, {}, {})", track_momentum_vector.x, track_momentum_vector.y, track_momentum_vector.z);
+    }
+
     out_reco_particle.setMomentum(edm4hep::Vector3f(
-        track_momentum_vector.x, track_momentum_vector.y, track_momentum_vector.z));
-    out_reco_particle.setEnergy(estimated_energy);
+        reconstructed_momentum_vector.x, reconstructed_momentum_vector.y, reconstructed_momentum_vector.z));
+    out_reco_particle.setEnergy(reconstructed_energy);
     out_reco_particle.setCharge(m_particleSvc.particle(prelim_pid).charge);
-    out_reco_particle.setMass(mass_calculated);
+    out_reco_particle.setMass(reconstructed_mass);
     out_reco_particle.setPDG(prelim_pid);
 
     out_particles->push_back(out_reco_particle);
