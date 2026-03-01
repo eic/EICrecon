44417efa5d08ffae3dd564df3d544341c2b9489d
epic-capybara <139920704+epic-capybara@users.noreply.github.com>
2026-03-01 17:18:25 -0500

    feat: add minTrackHits option to IterativeVertexFinder (fix: iwyu) (#2528)
    This PR applies the include-what-you-use fixes as suggested by
    https://github.com/eic/EICrecon/actions/runs/22553753150.
    Please merge this PR into the branch `pr/ivf_hit_cut`
    to resolve failures in PR #2527.
    
    Auto-generated by [create-pull-request][1]
    
    [1]: https://github.com/peter-evans/create-pull-request
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/algorithms/tracking/IterativeVertexFinder.cc
+++ src/algorithms/tracking/IterativeVertexFinder.cc
@@ -6,7 +6,6 @@
 
 #include <Acts/Definitions/TrackParametrization.hpp>
 #include <Acts/Definitions/Units.hpp>
-#include <Acts/EventData/TrackContainer.hpp>
 #include <Acts/EventData/TrackParameters.hpp>
 #include <Acts/EventData/TrackProxy.hpp>
 #include <Acts/Propagator/EigenStepper.hpp>
@@ -36,10 +35,9 @@
 #include <edm4hep/Vector4f.h>
 #include <podio/RelationRange.h>
 #include <spdlog/common.h>
-#include <Eigen/Core>
 #include <cmath>
-#include <gsl/pointers>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
