b973d8d9c4ba38090cb736faad52222b087196c8
Tomas Sosa <tomassosa.23@gmail.com>
2026-03-01 20:04:50 -0500

    New code with the suggested changes

--- src/algorithms/calorimetry/CalorimeterEOverPCutConfig.h
+++ src/algorithms/calorimetry/CalorimeterEOverPCutConfig.h
@@ -2,12 +2,15 @@
 // Copyright (C) 2025 Tomas Sosa, Wouter Deconinck
 
 #pragma once
+#include <string>
 
 namespace eicrecon {
 
-struct CalorimeterEoverPCutConfig {
+struct CalorimeterEOverPCutConfig {
   double eOverPCut = 0.74;
   int maxLayer     = 12;
+  std::string readout = "";
+  std::string layerField = "layer";
 };
 
 } // namespace eicrecon
