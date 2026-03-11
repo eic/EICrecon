ad14bf3344655de3a3988a54b38146e282f07aaa
Sakib Rahman <rahmans@myumanitoba.ca>
2026-03-11 18:19:07 -0400

    Matching brace

--- src/services/io/podio/JEventSourcePODIO.cc
+++ src/services/io/podio/JEventSourcePODIO.cc
@@ -151,6 +151,7 @@ void JEventSourcePODIO::Open() {
     for (auto category : m_reader->getAvailableCategories()) {
       if (category == "events") {
         continue;
+      }
         std::string cat(category);
         std::size_t n = m_reader->getEntries(cat);
         for (std::size_t i = 0; i < n; ++i) {
