d6aa0ad94e717358fe3042d231005b98746d0dc1
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-03-18 11:11:48 -0400

    Implement ObjectID comparator

---
+++ src/algorithms/interfaces/CompareObjectID.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2026 Derek Anderson
+
+#pragma once
+
+#include <podio/ObjectID.h>
+
+namespace eicrecon {
+
+/*! Compareator struct for PODIO::ObjectID. Organizes PODIO objects by
+ *  their ObjectID's in decreasing collection ID first, and second by
+ *  decreasing index second.
+ */
+template <typename T> struct CompareObjectID {
+  bool operator()(const T& lhs, const T& rhs) const {
+    if (lhs.getObjectID().collectionID == rhs.getObjectID().collectionID) {
+      return (lhs.getObjectID().index < rhs.getObjectID().index);
+    } else {
+      return (lhs.getObjectID().collectionID < rhs.getObjectID().collectionID);
+    }
+  }
+};
+
+} // namespace eicrecon
