8a61375308a3c64787913c087dfd8c07924d4229
Tomas Sosa <tomassosa.23@gmail.com>
2026-04-17 13:59:13 -0500

    PreML files update

--- src/algorithms/onnx/CalorimeterParticleIDPreML.h
+++ src/algorithms/onnx/CalorimeterParticleIDPreML.h
@@ -7,6 +7,7 @@
 #include <edm4eic/ClusterCollection.h>
 #include <edm4eic/MCRecoClusterParticleAssociationCollection.h>
 #include <edm4eic/TensorCollection.h>
+#include <edm4hep/ParticleIDCollection.h>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -17,7 +18,8 @@ namespace eicrecon {
 
 using CalorimeterParticleIDPreMLAlgorithm = algorithms::Algorithm<
     algorithms::Input<edm4eic::ClusterCollection,
-                      std::optional<edm4eic::MCRecoClusterParticleAssociationCollection>>,
+                      std::optional<edm4eic::MCRecoClusterParticleAssociationCollection>,
+                      std::optional<edm4hep::ParticleIDCollection>>,
     algorithms::Output<edm4eic::TensorCollection, std::optional<edm4eic::TensorCollection>>>;
 
 class CalorimeterParticleIDPreML : public CalorimeterParticleIDPreMLAlgorithm,
@@ -26,7 +28,10 @@ class CalorimeterParticleIDPreML : public CalorimeterParticleIDPreMLAlgorithm,
 public:
   CalorimeterParticleIDPreML(std::string_view name)
       : CalorimeterParticleIDPreMLAlgorithm{
-            name, {"inputClusters"}, {"outputFeatureTensor", "outputTargetTensor"}, ""} {}
+            name,
+            {"inputClusters", "inputClusterParticleAssociations", "inputParticleIDs"},
+            {"outputFeatureTensor", "outputTargetTensor"},
+            ""} {}
 
   void init() final;
   void process(const Input&, const Output&) const final;
