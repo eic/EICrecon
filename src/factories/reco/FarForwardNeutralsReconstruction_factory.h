1f2598da1077611aab5443c60a0d0bc4c730714c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-04-22 09:18:26 -0400

    FarForwardNeutralsReconstruction_factory.h: fix copyright

--- src/factories/reco/FarForwardNeutralsReconstruction_factory.h
+++ src/factories/reco/FarForwardNeutralsReconstruction_factory.h
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2025 Sebouh Paul
-// Update/modification 2026 by Baptiste Fraisse
+// Copyright (C) 2025 - 2026 Sebouh Paul, Baptiste Fraisse
 
 #pragma once
 
