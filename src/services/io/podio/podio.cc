cb5766b6b8fdb143bc0e5f07261e02b5dc77050e
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-06-01 22:34:07 -0400

    Add JEventSourceManagedPODIO without JEventSourceGeneratorT

--- src/services/io/podio/podio.cc
+++ src/services/io/podio/podio.cc
@@ -17,23 +17,14 @@
 extern "C" {
 void InitPlugin(JApplication* app) {
   InitJANAPlugin(app);
-  app->Add(new JEventSourceGeneratorT<JEventSourcePODIO>());
-  app->Add(new JEventSourceGeneratorT<JEventSourceManagedPODIO>());
-
-  // Disable this behavior for now so one can run eicrecon with only the
-  // input file as an argument.
-  // Only add a EICRootWriter if the user has specified a configuration parameter relevant to writing
-  //    if( app->GetJParameterManager()->Exists("podio:output_file")
-  //        ||  app->GetJParameterManager()->Exists("podio:output_file_copy_dir")
-  //        ||  app->GetJParameterManager()->Exists("podio:output_include_collections")
-  //        ||  app->GetJParameterManager()->Exists("podio:output_exclude_collections")        ){
 
   // Check if managed mode is requested
   if (app->GetJParameterManager()->Exists("podio:managed_socket_path")) {
+    app->Add(new JEventSourceManagedPODIO());
     app->Add(new JEventProcessorManagedPODIO());
   } else {
+    app->Add(new JEventSourceGeneratorT<JEventSourcePODIO>());
     app->Add(new JEventProcessorPODIO());
   }
-  //    }
 }
 }
