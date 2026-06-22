262a917cdf2556b933d919501cf54c5e2530507e
Derek M Anderson <derek.murphy.anderson@protonmail.com>
2026-06-22 14:11:12 -0400

    Implement Track-Cluster Subtraction (PFA1a) (#1627)
    ### Briefly, what does this PR introduce?
    
    This PR implements the `TrackClusterSubtraction` algorithm, which
    ingests clusters from a specified calorimeter, any matched tracks, and
    subtracts off the expected energy deposited of the sum of tracks
    pointing to each cluster. This is the second stage of of the baseline
    particle flow routine, _PFAlpha_
    
    This algorithm outputs two collections of clusters:
    1. **Remnant clusters,** which consist of un-matched clusters and any
    cluster with nonzero energy left after the subtraction; and
    2. **Expected clusters,** which consist of matched clusters with any
    remnant energy subtracted from them.
    
    <img width="619" height="650" alt="PFA1_VisualExplainer"
    src="https://github.com/user-attachments/assets/9ed04da0-bc04-4f7a-83ea-7d8c5bdb7bbc"
    />
    
    In both cases, all properties of the cluster before subtraction are
    copied to the remnant and expected clusters with energies updated
    accordingly. The remnant clusters will be used to reconstruct neutral
    particles downstream, and the expected clusters will be used in the
    kinematic reconstruction of charged particles downstream.
    
    Users are provided two methods to check if the leftover energy is
    consistent with 0:
    1. A **basic method,** which checks if the leftover energy is less than
    0 or within floating point precision of 0; or a
    2. **Resolution method,** which checks if the leftover energy is less
    than 0 or within some number of standard deviations of 0 based on the
    resolution of the tracker and calorimeter.
    
    For the latter method, the number of sigma to use and the resolutions of
    the tracker/calorimeter are configurable parameters.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [x] New feature (issue #1341 #2127)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [x] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.
    
    ### To-do
    Will follow up with a benchmark for this stage of PFAlpha with a
    benchmark.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: epic-capybara <139920704+epic-capybara@users.noreply.github.com>
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

---
+++ src/algorithms/particle_flow/TrackClusterSubtractor.cc
@@ -0,0 +1,242 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2026 Derek Anderson
+
+#include <edm4eic/Track.h>
+#include <edm4eic/TrackPoint.h>
+#include <edm4hep/utils/vector_utils.h>
+#include <podio/ObjectID.h>
+#include <podio/RelationRange.h>
+#include <podio/detail/Link.h>
+#include <podio/detail/LinkCollectionImpl.h>
+#include <cmath>
+#include <cstdint>
+#include <limits>
+#include <map>
+#include <memory>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "TrackClusterSubtractor.h"
+#include "algorithms/interfaces/CompareObjectID.h"
+#include "algorithms/particle_flow/TrackClusterSubtractorConfig.h"
+
+namespace eicrecon {
+
+/*! Subtract energy of matched tracks via the following algorithm.
+ *
+ *    1. Build a map of each cluster onto a list of matched
+ *       track projections.
+ *    2. Flag any un-matched clusters as remnants.
+ *    3. For each cluster, subtract the sum of momenta of
+ *       all matched tracks scaled by the specified fraction
+ *       from the cluster's energy.
+ *    4. For each matched cluster:
+ *       a. If subtracted energy is greater than zero, create
+ *          a remnant cluster with the subtracted energy
+ *       b. And create an expected cluster with energy equal
+ *          to the difference between the total and the remnant.
+ */
+void TrackClusterSubtractor::process(const TrackClusterSubtractor::Input& input,
+                                     const TrackClusterSubtractor::Output& output) const {
+
+  // grab inputs/outputs
+  const auto [in_matches, in_clusters, in_projections] = input;
+#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
+  auto [out_remnants, out_expectants, out_links, out_matches] = output;
+#else
+  auto [out_remnants, out_expectants, out_matches] = output;
+#endif
+
+  // exit if no clusters in collection
+  if (in_clusters->size() == 0) {
+    debug("No clusters in collection");
+    return;
+  }
+
+  // emit debugging message if no matched tracks in collection
+  if (in_matches->size() == 0) {
+    debug("No matched tracks in collection.");
+  }
+
+  // --------------------------------------------------------------------------
+  // 1. Build map of clusters onto projections
+  // --------------------------------------------------------------------------
+  std::map<edm4eic::Cluster, segment_vector, CompareObjectID<edm4eic::Cluster>>
+      mapClusterToProjections;
+  for (const auto& match : *in_matches) {
+    for (const auto& project : *in_projections) {
+
+      // pick out corresponding projection from track
+      if (match.getTrack() != project.getTrack()) {
+        continue;
+      } else {
+        mapClusterToProjections[match.getCluster()].push_back(project);
+      }
+
+    } // end projection loop
+  } // end track-cluster match loop
+  debug("Built map of clusters-onto-tracks, size = {}", mapClusterToProjections.size());
+
+  // --------------------------------------------------------------------------
+  // 2. Any unmatched clusters are remnants by definition
+  // --------------------------------------------------------------------------
+  for (const auto& cluster : *in_clusters) {
+    if (mapClusterToProjections.count(cluster) == 0) {
+      auto remain_cluster = cluster.clone();
+      out_remnants->push_back(remain_cluster);
+    }
+  }
+  debug("Finished copying unmatched clusters to remnants, {} remnant clusters",
+        out_remnants->size());
+
+  // --------------------------------------------------------------------------
+  // 3. Subtract energy for tracks
+  // --------------------------------------------------------------------------
+  for (const auto& [cluster, projections] : mapClusterToProjections) {
+
+    // do subtraction
+    const double eTrackSum   = sum_track_energy(projections);
+    const double eToSubtract = m_cfg.energyFractionToSubtract * eTrackSum;
+    const double eSubtracted = cluster.getEnergy() - eToSubtract;
+    trace("Subtracted {} GeV from cluster with {} GeV", eToSubtract, cluster.getEnergy());
+
+    // if track sum is NOT greater than calorimeter energy within
+    // tolerances, set remainder to nonzero value
+    const bool isGreaterThan      = is_track_energy_greater_than_calo(eSubtracted);
+    const double eSubtractedToUse = isGreaterThan ? 0. : eSubtracted;
+
+    // ------------------------------------------------------------------------
+    // 4(a). If track energy not greater than calo, create output remnant
+    // ------------------------------------------------------------------------
+    if (!isGreaterThan) {
+      auto remain_cluster = cluster.clone();
+      remain_cluster.setEnergy(eSubtractedToUse);
+      out_remnants->push_back(remain_cluster);
+      trace("Created remnant cluster with {} GeV", remain_cluster.getEnergy());
+    }
+
+    // ------------------------------------------------------------------------
+    // 4(b). Create cluster with energy equal to eTotal - eRemnant and match
+    // ------------------------------------------------------------------------
+    auto expect_cluster = cluster.clone();
+    expect_cluster.setEnergy(cluster.getEnergy() - eSubtractedToUse);
+    out_expectants->push_back(expect_cluster);
+    trace("Created expected cluster with {} GeV (originally {} GeV)", expect_cluster.getEnergy(),
+          cluster.getEnergy());
+
+    // create a track-cluster match for expected clusters
+    for (const auto& project : projections) {
+#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
+      edm4eic::MutableTrackClusterLink link = out_links->create();
+      link.setFrom(expect_cluster);
+      link.setTo(project.getTrack());
+      link.setWeight(1.0); // FIXME placeholder
+#endif
+
+      edm4eic::MutableTrackClusterMatch match = out_matches->create();
+      match.setCluster(expect_cluster);
+      match.setTrack(project.getTrack());
+      match.setWeight(1.0); // FIXME placeholder
+      trace("Matched expected cluster {} to track {}", expect_cluster.getObjectID().index,
+            project.getTrack().getObjectID().index);
+    }
+  } // end cluster-to-projections loop
+  debug("Finished subtraction, {} remnant clusters and {} expected clusters", out_remnants->size(),
+        out_expectants->size());
+
+} // end 'process(Input&, Output&)'
+
+/*! Sums energy of tracks projected to the surface in the calorimeter
+ *  specified by `surfaceToUse`. Uses PDG of track to select mass
+ *  for energy; if not available, uses mass set by `defaultPDG`.
+ *
+ *  @param[in] projections vector of projections matched to
+ *                         a cluster
+ *
+ *  @return Sum of energy
+ */
+double TrackClusterSubtractor::sum_track_energy(const segment_vector& projections) const {
+
+  double energySum = 0.0;
+  for (const auto& project : projections) {
+
+    // measure momentum at specified surface
+    double momentum     = 0.0;
+    bool momentum_valid = false;
+    for (const auto& point : project.getPoints()) {
+      if (point.surface != m_cfg.surfaceToUse) {
+        continue;
+      } else {
+        momentum       = edm4hep::utils::magnitude(point.momentum);
+        momentum_valid = true;
+        break;
+      }
+    }
+    if (!momentum_valid) {
+      continue;
+    }
+
+    // get mass based on track pdg
+    int pdgToUse = project.getTrack().getPdg();
+    if (pdgToUse == 0) {
+      pdgToUse = m_cfg.defaultPDG;
+    }
+    const double mass = m_parSvc.particle(pdgToUse).mass;
+
+    // increment sum
+    energySum += std::sqrt((momentum * momentum) + (mass * mass));
+  }
+
+  trace("Sum of track energy = {} GeV", energySum);
+  return energySum;
+
+} // end 'sum_track_energy(segment_vector&)'
+
+/*! Checks if the sum of tracks' energy is greater than a calorimeter cluster
+ *  energy by checking if their difference is either:
+ *
+ *    1. smaller than than an epsilon (if doNSigmaCut is false), or
+ *    2. within nSigmaMax of zero (if doNSigmaCut is true) based on
+ *       the computed sum of tracks' variance and the calorimeter
+ *       resolution.
+ *
+ *  \param[in] difference energy difference between the cluster and
+ *                        sum of tracks
+ *  \param[in] variance   the variance on the sume of track energy
+ */
+bool TrackClusterSubtractor::is_track_energy_greater_than_calo(const double difference) const {
+
+  // if < 0, automatically return true
+  if (difference < 0) {
+    return true;
+  }
+
+  // do appropriate comparison
+  bool isGreaterThan = false;
+  if (m_cfg.doNSigmaCut) {
+
+    // calculate n sigma squared
+    const double totalVariance = (m_cfg.trackResolution * m_cfg.trackResolution) +
+                                 (m_cfg.calorimeterResolution * m_cfg.calorimeterResolution);
+    const uint32_t nSigma2 =
+        static_cast<uint32_t>(std::floor((difference * difference) / totalVariance));
+    const uint32_t nSigmaMax2 = m_cfg.nSigmaMax * m_cfg.nSigmaMax;
+
+    isGreaterThan = (nSigma2 < nSigmaMax2);
+    if (isGreaterThan) {
+      trace(
+          "Within {} NSigma^2, track energy sum greater than calorimeter cluster: difference = {} "
+          "GeV, nSigma^2 = {}",
+          nSigmaMax2, difference, nSigma2);
+    }
+  } else {
+    isGreaterThan = difference < std::numeric_limits<double>::epsilon();
+    if (isGreaterThan) {
+      trace("Track energy sum greater than calorimeter cluster: difference = {} GeV", difference);
+    }
+  }
+  return isGreaterThan;
+
+} // end 'is_track_energy_greater_than_calo(double)'
+} // namespace eicrecon
