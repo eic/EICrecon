6469be6706a511422107642df468f241233d1515
Tomas Sosa <tomassosa.23@gmail.com>
2026-06-22 10:12:37 -0500

    New PreML and PostML files

---
+++ src/factories/calorimetry/CalorimeterParticleIDBICPostML_factory.h
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2025 Tomas Sosa, Wouter Deconinck
+
+#pragma once
+
+#include "algorithms/onnx/CalorimeterParticleIDBICPostML.h"
+#include "extensions/jana/JOmniFactory.h"
+#include "services/algorithms_init/AlgorithmsInit_service.h"
+
+namespace eicrecon {
+
+class CalorimeterParticleIDBICPostML_factory
+    : public JOmniFactory<CalorimeterParticleIDBICPostML_factory, NoConfig> {
+
+public:
+  using AlgoT = eicrecon::CalorimeterParticleIDBICPostML;
+
+private:
+  std::unique_ptr<AlgoT> m_algo;
+
+  PodioInput<edm4eic::Cluster> m_cluster_input{this};
+  PodioInput<edm4eic::TrackClusterMatch> m_match_input{this};
+  PodioInput<edm4hep::ParticleID, true> m_pid_input{this};
+  PodioInput<edm4eic::Tensor> m_prediction_tensor_input{this};
+
+  PodioOutput<edm4eic::Cluster> m_cluster_output{this};
+  PodioOutput<edm4eic::TrackClusterMatch> m_match_output{this};
+  PodioOutput<edm4hep::ParticleID> m_particle_id_output{this};
+
+public:
+  void Configure() {
+    m_algo = std::make_unique<AlgoT>(GetPrefix());
+    m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
+    m_algo->applyConfig(config());
+    m_algo->init();
+  }
+
+  void Process(int32_t /* run_number */, uint64_t /* event_number */) {
+    m_algo->process(
+        {m_cluster_input(), m_match_input(), m_pid_input(), m_prediction_tensor_input()},
+        {m_cluster_output().get(), m_match_output().get(), m_particle_id_output().get()});
+  }
+};
+
+} // namespace eicrecon
