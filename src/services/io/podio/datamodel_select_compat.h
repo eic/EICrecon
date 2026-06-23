f2fca962dc92990012cd014ed0ad0f0d2a78abe3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-06-23 15:53:28 -0400

    initial jana2 minimal version bump (remove checks)

--- src/services/io/podio/datamodel_select_compat.h
+++ src/services/io/podio/datamodel_select_compat.h
@@ -3,12 +3,5 @@
 
 #pragma once
 
-// Note that the JANA version components are defined by the build system,
-// since they are not available for use in preprocessor directives when included
-// from JANA/JVersion.h.
-
-// Use modern implementation for JANA2 >= 2.4.3
-#if (JANA_VERSION_MAJOR > 2) || (JANA_VERSION_MAJOR == 2 && JANA_VERSION_MINOR > 4) ||             \
-    (JANA_VERSION_MAJOR == 2 && JANA_VERSION_MINOR == 4 && JANA_VERSION_PATCH >= 3)
+// JANA2 >= 2.4.3 is required; always use modern podio glue implementation
 #define USE_MODERN_PODIO_GLUE 1
-#endif
