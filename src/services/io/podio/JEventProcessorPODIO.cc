eic/EICrecon

commit 1f87dad8ea715bfc8f92abe72e562236ba899aa0

Use LGAD clustering sans digitization (#2616)
### Briefly, what does this PR introduce? Please link to any relevant
presentations or discussions.

Half a year ago, pixel dimensions of ETOF and BTOF sensors are reduced
to unrealistic sizes to compensate for the reduction in position
resolution due to lack of charge sharing algorithm:
https://github.com/eic/epic/pull/883

Now, TOF group believes that charge sharing algorithm is mature to an
acceptable state. We have submitted a pull request in epic, and this
request will pipe output of cluster algorithm to tracking algorithm.

Discussion with tracking group is on-going. Preliminary analysis shows
comparable efficiency and resolution with what we current have in the
main branch.

This is what this pull request entails:

1. Implements truth-association function for charge sharing class.
2. The digitization routine is bypassed, so it's just charge sharing ->
clustering algorithm directly to avoid hit lost due to digitization
effects such as inaccurate dynamic ranges, digitization error, threshold
effects, etc. The ultimate goal is still to implement digitization, but
currently there is no hardware to compare our results to so we are not
sure of its accuracy. It is bypassed until it's digitization effect is
understood.
3. LGADHitCluster class now output both Measurement2D and TrackerHit as
outputs. The hit information are identical, and I know this group
prefers that TOF return Measurement2D directly, but having TrackerHit as
output makes resolution analysis heaps easier. There is no global
position information in Measurement2D and having to perform coordinate
transform in a stand-alone TTreeReader analysis script isn't easy.
Directly accessing position information from TrackerHit is much more
straight-forward.
4. Output from the cluster class is now used for tracking instead of
"TOFBarrelRecHits" and "TOFEndcapRecHits".

Will update when I receive comments from the tracking group. 

### What is the urgency of this PR?

src/services/io/podio/JEventProcessorPODIO.cc
@@ -101,14 +101,9 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
       "SiEndcapTrackerRawHitAssociations",
 
       // TOF
-      "TOFBarrelRecHits",
-      "TOFEndcapRecHits",
-
-      "TOFBarrelRawHits",
-      "TOFEndcapRawHits",
-
       "TOFBarrelHits",
       "TOFBarrelClusterHits",
+      "TOFEndcapClusterHits",
       "TOFBarrelADCTDC",
       "TOFEndcapHits",
 

src/algorithms/digi/SiliconChargeSharing.cc
@@ -5,7 +5,6 @@
 
 #include <DD4hep/Alignments.h>
 #include <DD4hep/DetElement.h>
-#include <DD4hep/Handle.h>
 #include <DD4hep/Objects.h>
 #include <DD4hep/Readout.h>
 #include <DD4hep/Segmentations.h>
@@ -22,12 +21,12 @@
 #include <TGeoMatrix.h>
 #include <algorithms/geo.h>
 #include <edm4hep/Vector3d.h>
-#include <fmt/core.h>
 #include <cmath>
 #include <gsl/pointers>
 #include <numbers>
 #include <set>
 #include <stdexcept>
+#include <tuple>
 #include <typeinfo>
 #include <utility>
 

src/algorithms/tracking/LGADHitClustering.cc
@@ -7,7 +7,6 @@
 #include <Acts/Definitions/Units.hpp>
 #include <Acts/Geometry/GeometryIdentifier.hpp>
 #include <Acts/Surfaces/Surface.hpp>
-#include <DD4hep/Handle.h>
 #include <DD4hep/Readout.h>
 #include <DD4hep/VolumeManager.h>
 #include <DD4hep/detail/SegmentationsInterna.h>
@@ -21,13 +20,13 @@
 #include <edm4eic/Cov3f.h>
 #include <edm4eic/CovDiag3f.h>
 #include <edm4hep/Vector2f.h>
-#include <cstddef>
-#include <Eigen/Core>
 #include <cmath>
+#include <cstddef>
 #include <gsl/pointers>
 #include <limits>
 #include <set>
 #include <stdexcept>
+#include <tuple>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -133,7 +132,6 @@ void LGADHitClustering::_calcCluster(const Output& output,
     cluster.addToWeights(w);
   }
 
-  edm4eic::Cov3f covariance;
   edm4hep::Vector2f locPos{static_cast<float>(ave_x / mm), static_cast<float>(ave_y / mm)};
 
   const auto* context    = m_converter->findContext(cellID);

src/algorithms/tracking/LGADHitClustering.h
@@ -33,7 +33,7 @@ class LGADHitClustering : public LGADHitClusteringAlgorithm,
 
 public:
   LGADHitClustering(std::string_view name)
-      : LGADHitClusteringAlgorithm{name, {"TOFBarrelCalHit"}, {"TOFBarrelRecHit"}, ""} {};
+      : LGADHitClusteringAlgorithm{name, {"TOFBarrelCalHit"}, {"TOFBarrelRecMeas2D"}, ""} {};
 
   void init() final;
   void process(const Input&, const Output&) const final;

src/benchmarks/reconstruction/tracking_occupancy/HitReconstructionAnalysis.h
@@ -30,9 +30,6 @@ private:
       "OuterMPGDBarrelRecHits",
       "ForwardMPGDEndcapRecHits",
       "BackwardMPGDEndcapRecHits",
-      // TOF
-      "TOFEndcapRecHits",
-      "TOFBarrelRecHit",
   };
 
   /// Hits count histogram for each hits readout name

src/detectors/BTOF/BTOF.cc
@@ -37,27 +37,6 @@ void InitPlugin(JApplication* app) {
 
   using namespace eicrecon;
 
-  // Digitization
-  app->Add(new JOmniFactoryGeneratorT<SiliconTrackerDigi_factory>(
-      "TOFBarrelRawHits", {"EventHeader", "TOFBarrelHits"},
-      {"TOFBarrelRawHits",
-#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
-       "TOFBarrelRawHitLinks",
-#endif
-       "TOFBarrelRawHitAssociations"},
-      {
-          .threshold      = 6.0 * dd4hep::keV,
-          .timeResolution = 0.025, // [ns]
-      },
-      app));
-
-  // Convert raw digitized hits into hits with geometry info (ready for tracking)
-  app->Add(new JOmniFactoryGeneratorT<TrackerHitReconstruction_factory>(
-      "TOFBarrelRecHits", {"TOFBarrelRawHits"}, // Input data collection tags
-      {"TOFBarrelRecHits"},                     // Output data tag
-      {},
-      app)); // Hit reco default config for factories
-
   // Convert raw digitized hits into calibrated hits
   // time walk correction is still TBD
   app->Add(new JOmniFactoryGeneratorT<LGADHitCalibration_factory>(
@@ -70,20 +49,46 @@ void InitPlugin(JApplication* app) {
   // Currently it's just a simple weighted average
   // More sophisticated algorithm TBD
   app->Add(new JOmniFactoryGeneratorT<LGADHitClustering_factory>(
-      "TOFBarrelClusterHits", {"TOFBarrelCalibratedHits"}, // Input data collection tags
-      {"TOFBarrelClusterHits"},                            // Output data tag
-      {}, app));
+      "TOFBarrelClusterHits", {"TOFBarrelSharedRecHits"}, // Input data collection tags
+      {"TOFBarrelClusterHits"},                           // Output data tag
+      {
+          .readout = "TOFBarrelHits",
+          .useAve  = true,
+      },
+      app));
 
   app->Add(new JOmniFactoryGeneratorT<SiliconChargeSharing_factory>(
       "TOFBarrelSharedHits", {"TOFBarrelHits"}, {"TOFBarrelSharedHits"},
       {
           .sigma_mode     = SiliconChargeSharingConfig::ESigmaMode::rel,
-          .sigma_sharingx = 1,
+          .sigma_sharingx = 0.5,
           .sigma_sharingy = 0.5,
-          .min_edep       = 0.0 * edm4eic::unit::GeV,
+          .min_edep       = 6.0 * edm4eic::unit::keV,
           .readout        = "TOFBarrelHits",
       },
       app));
+
+  // temporary steps to bypass pulse digitization and jump right from ChargeSharing to clusters
+  // Avoid efficiency loss until we can simulate hardware accurately
+  app->Add(new JOmniFactoryGeneratorT<SiliconTrackerDigi_factory>(
+      "TOFBarrelSharedRawHits", {"EventHeader", "TOFBarrelSharedHits"},
+      {"TOFBarrelSharedRawHits",
+#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
+       "TOFBarrelSharedRawHitLinks",
+#endif
+       "TOFBarrelSharedRawHitAssociations"},
+      {
+          .threshold      = 0.0,
+          .timeResolution = 0.025, // [ns]
+      },
+      app));
+
+  // Convert raw digitized hits into hits with geometry info (ready for tracking)
+  app->Add(new JOmniFactoryGeneratorT<TrackerHitReconstruction_factory>(
+      "TOFBarrelSharedRecHits", {"TOFBarrelSharedRawHits"}, // Input data collection tags
+      {"TOFBarrelSharedRecHits"},                           // Output data tag
+      {},
+      app)); // Hit reco default config for factories
 
   // calculation of the extreme values for Landau distribution can be found on lin 514-520 of
   // https://root.cern.ch/root/html524/src/TMath.cxx.html#fsokrB Landau reaches minimum for mpv =

src/detectors/ECTOF/ECTOF.cc
@@ -20,6 +20,7 @@
 #include "factories/digi/EICROCDigitization_factory.h"
 #include "factories/digi/PulseCombiner_factory.h"
 #include "factories/digi/PulseGeneration_factory.h"
+#include "factories/tracking/LGADHitClustering_factory.h"
 #include "factories/digi/SiliconChargeSharing_factory.h"
 #include "factories/digi/SiliconPulseDiscretization_factory.h"
 #include "factories/digi/SiliconTrackerDigi_factory.h"
@@ -31,26 +32,15 @@ void InitPlugin(JApplication* app) {
 
   using namespace eicrecon;
 
-  // Digitization
-  app->Add(new JOmniFactoryGeneratorT<SiliconTrackerDigi_factory>(
-      "TOFEndcapRawHits", {"EventHeader", "TOFEndcapHits"},
-      {"TOFEndcapRawHits",
-#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
-       "TOFEndcapRawHitLinks",
-#endif
-       "TOFEndcapRawHitAssociations"},
+  // cluster all hits in a sensor into one hit location
+  // Currently it's just a simple weighted average
+  // More sophisticated algorithm TBD
+  app->Add(new JOmniFactoryGeneratorT<LGADHitClustering_factory>(
+      "TOFEndcapClusterHits", {"TOFEndcapSharedRecHits"}, // Input data collection tags
+      {"TOFEndcapClusterHits"},                           // Output data tag
       {
-          .threshold      = 6.0 * dd4hep::keV,
-          .timeResolution = 0.025,
-      },
-      app));
-
-  // Convert raw digitized hits into hits with geometry info (ready for tracking)
-  app->Add(new JOmniFactoryGeneratorT<TrackerHitReconstruction_factory>(
-      "TOFEndcapRecHits", {"TOFEndcapRawHits"}, // Input data collection tags
-      {"TOFEndcapRecHits"},                     // Output data tag
-      {
-          .timeResolution = 0.025,
+          .readout = "TOFEndcapHits",
+          .useAve  = true,
       },
       app));
 
@@ -59,12 +49,34 @@ void InitPlugin(JApplication* app) {
       {
 
           .sigma_mode     = SiliconChargeSharingConfig::ESigmaMode::rel,
-          .sigma_sharingx = 1,
-          .sigma_sharingy = 1,
-          .min_edep       = 0.001 * dd4hep::keV,
+          .sigma_sharingx = 0.5,
+          .sigma_sharingy = 0.5,
+          .min_edep       = 6 * dd4hep::keV,
           .readout        = "TOFEndcapHits",
       },
       app));
+
+  // temporary steps to bypass pulse digitization and jump right from ChargeSharing to clusters
+  // Avoid efficiency loss until we can simulate hardware accurately
+  app->Add(new JOmniFactoryGeneratorT<SiliconTrackerDigi_factory>(
+      "TOFEndcapSharedRawHits", {"EventHeader", "TOFEndcapSharedHits"},
+      {"TOFEndcapSharedRawHits",
+#if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
+       "TOFEndcapSharedRawHitLinks",
+#endif
+       "TOFEndcapSharedRawHitAssociations"},
+      {
+          .threshold      = 0.0,
+          .timeResolution = 0.025, // [ns]
+      },
+      app));
+
+  // Convert raw digitized hits into hits with geometry info (ready for tracking)
+  app->Add(new JOmniFactoryGeneratorT<TrackerHitReconstruction_factory>(
+      "TOFEndcapSharedRecHits", {"TOFEndcapSharedRawHits"}, // Input data collection tags
+      {"TOFEndcapSharedRecHits"},                           // Output data tag
+      {},
+      app)); // Hit reco default config for factories
 
   const double x_when_landau_min = -0.22278;
   const double landau_min        = TMath::Landau(x_when_landau_min, 0, 1, true);

src/global/tracking/tracking.cc
@@ -9,6 +9,8 @@
 #include <edm4eic/EDM4eicVersion.h>
 #include <edm4eic/MCRecoTrackParticleAssociationCollection.h>
 #include <edm4eic/MCRecoTrackerHitAssociationCollection.h>
+#include <edm4eic/Measurement2D.h>
+#include <cmath>
 #if EDM4EIC_BUILD_VERSION >= EDM4EIC_VERSION(8, 7, 0)
 #include <edm4eic/MCRecoTrackerHitLinkCollection.h>
 #endif
@@ -72,8 +74,8 @@ void InitPlugin(JApplication* app) {
   app->Add(new JOmniFactoryGeneratorT<CollectionCollector_factory<edm4eic::TrackerHit, true>>(
       "CentralTrackingRecHits",
       {"SiBarrelTrackerRecHits", "SiBarrelVertexRecHits", "SiEndcapTrackerRecHits",
-       "TOFBarrelRecHits", "TOFEndcapRecHits", "MPGDBarrelRecHits", "OuterMPGDBarrelRecHits",
-       "BackwardMPGDEndcapRecHits", "ForwardMPGDEndcapRecHits"},
+       "MPGDBarrelRecHits", "OuterMPGDBarrelRecHits", "BackwardMPGDEndcapRecHits",
+       "ForwardMPGDEndcapRecHits"},
       {"CentralTrackingRecHits"}, // Output collection name
       app));
 
@@ -82,8 +84,7 @@ void InitPlugin(JApplication* app) {
            CollectionCollector_factory<edm4eic::MCRecoTrackerHitAssociation, true>>(
       "CentralTrackingRawHitAssociations",
       {"SiBarrelRawHitAssociations", "SiBarrelVertexRawHitAssociations",
-       "SiEndcapTrackerRawHitAssociations", "TOFBarrelRawHitAssociations",
-       "TOFEndcapRawHitAssociations", "MPGDBarrelRawHitAssociations",
+       "SiEndcapTrackerRawHitAssociations", "MPGDBarrelRawHitAssociations",
        "OuterMPGDBarrelRawHitAssociations", "BackwardMPGDEndcapRawHitAssociations",
        "ForwardMPGDEndcapRawHitAssociations"},
       {"CentralTrackingRawHitAssociations"}, // Output collection name
@@ -95,7 +96,7 @@ void InitPlugin(JApplication* app) {
       new JOmniFactoryGeneratorT<CollectionCollector_factory<edm4eic::MCRecoTrackerHitLink, true>>(
           "CentralTrackingRawHitLinks",
           {"SiBarrelRawHitLinks", "SiBarrelVertexRawHitLinks", "SiEndcapTrackerRawHitLinks",
-           "TOFBarrelRawHitLinks", "TOFEndcapRawHitLinks", "MPGDBarrelRawHitLinks",
+           "TOFBarrelSharedRawHitLinks", "TOFEndcapSharedRawHitLinks", "MPGDBarrelRawHitLinks",
            "OuterMPGDBarrelRawHitLinks", "BackwardMPGDEndcapRawHitLinks",
            "ForwardMPGDEndcapRawHitLinks"},
           {"CentralTrackingRawHitLinks"}, // Output collection name
@@ -103,7 +104,14 @@ void InitPlugin(JApplication* app) {
 #endif
 
   app->Add(new JOmniFactoryGeneratorT<TrackerMeasurementFromHits_factory>(
-      "CentralTrackerMeasurements", {"CentralTrackingRecHits"}, {"CentralTrackerMeasurements"},
+      "CentralWithoutTOFTrackerMeasurements", {"CentralTrackingRecHits"},
+      {"CentralWithoutTOFTrackerMeasurements"}, app));
+
+  // add trackers that generate Measurement2D directly
+  app->Add(new JOmniFactoryGeneratorT<CollectionCollector_factory<edm4eic::Measurement2D, true>>(
+      "CentralTrackerMeasurements",
+      {"CentralWithoutTOFTrackerMeasurements", "TOFBarrelClusterHits", "TOFEndcapClusterHits"},
+      {"CentralTrackerMeasurements"}, // Output collection name
       app));
 
   app->Add(new JOmniFactoryGeneratorT<CKFTracking_factory>(

commit 6eeec0b4b97362b642ba0cf5334e21db0ae9c3d8

BUGFIX: JEventProcessorPODIO: write CentralWithoutTOFTrackerMeasurements (#2745)
### Briefly, what does this PR introduce? Please link to any relevant
presentations or discussions.

Issue discovered by @jaenam1991 

Fixes a regression from 1f87dad8ea715bfc8f92abe72e562236ba899aa0
The existing `CentralTrackerMeasurements` became a subset collection, so
we are not anymore storing the output of the
`TrackerMeasurementFromHits` algorithm because we don't write
`CentralWithoutTOFTrackerMeasurements`

Closes: #2744

### What is the urgency of this PR?
- [x] High (please describe reason below)
  may impede tracking studies
- [ ] Medium
- [ ] Low

### What kind of change does this PR introduce?
- [x] Bug fix (issue #__)
- [ ] New feature (issue #__)
- [ ] Optimization (issue #__)
- [ ] Updated parameters, constants (issue #__)
- [ ] Updated documentation
- [ ] other: __

### Please check if any of the following apply
- [ ] This PR requires changes to geometry (epic PR: __)
- [ ] This PR requires changes to EDM4eic (EDM PR: __)
- [ ] This PR introduces breaking changes. Please describe changes users
need to make below.
- [ ] This PR changes default behavior. Please describe changes below.
- [ ] AI was used in preparing this PR. Please describe usage below.

src/services/io/podio/JEventProcessorPODIO.cc
@@ -73,6 +73,7 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
       "CentralTrackSeeds",
       "CentralTrackSeedParameters",
       "CentralTrackerMeasurements",
+      "CentralWithoutTOFTrackerMeasurements",
 
       // Si tracker hits
       "SiBarrelTrackerRecHits",