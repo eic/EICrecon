569a1236e5dd279bfc7b3a7d074b63a6d2e1384a
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-07-21 13:19:15 -0400

    IrtConfig: remove explicit constructor

--- src/algorithms/pid/IrtInterfaceConfig.h
+++ src/algorithms/pid/IrtInterfaceConfig.h
@@ -13,14 +13,12 @@ namespace eicrecon {
 // bunch whatever is needed to pass in a single structure; do not want to repeat parsing
 // of either the optics file or a JSON configuration file twice;
 struct IrtConfig {
-  IrtConfig() : m_eta_min(0.0), m_eta_max(0.0) {};
-
   std::string m_detector_name;
   std::string m_json_config_file_name;
 
   // FIXME: perhaps do it better later; but in general see no reason in parsing
   // the same fields in a JSON file twice;
-  double m_eta_min, m_eta_max;
+  double m_eta_min{0}, m_eta_max{0};
 };
 
 } // namespace eicrecon
