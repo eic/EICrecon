d112c15fd06e8b2ab2258fed13419012c1b7a5f0
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-07-22 16:06:07 -0400

    mult factor

--- src/algorithms/pid/IrtInterface.cc
+++ src/algorithms/pid/IrtInterface.cc
@@ -215,8 +215,7 @@ void IrtInterface::process(const IrtInterface::Input& input,
 
     // Information inherited from photon MCParticle;
     photon->SetVertexPosition(Tools::PodioVector3_to_TVector3(mcparticle.getVertex()));
-    photon->SetVertexMomentum(Tools::PodioVector3_to_TVector3(mcparticle.getMomentum()) /
-                              edm4eic::unit::eV);
+    photon->SetVertexMomentum((1 / edm4eic::unit::eV) * Tools::PodioVector3_to_TVector3(mcparticle.getMomentum()));
     photon->SetVertexTime(mcparticle.getTime());
 
     auto parents = mcparticle.getParents();
