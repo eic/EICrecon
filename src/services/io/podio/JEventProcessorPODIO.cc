64dfe259cfbc3847a1d2d7ed71fe7aa0be6e0016
Wouter Deconinck <wdconinc@gmail.com>
2026-08-03 19:27:12 -0500

    fix: re-add ReconstructedChargedParticleAssociations

--- src/services/io/podio/JEventProcessorPODIO.cc
+++ src/services/io/podio/JEventProcessorPODIO.cc
@@ -218,6 +218,7 @@ JEventProcessorPODIO::JEventProcessorPODIO() {
       "ReconstructedChargedRealPIDParticleIDs",
       "ReconstructedChargedParticles",
       "ReconstructedChargedParticleLinks",
+      "ReconstructedChargedParticleAssociations", // Used by associations below
       "MCScatteredElectronLinks",           // Remove if/when used internally
       "MCScatteredElectronAssociations",    // Remove if/when used internally
       "MCNonScatteredElectronLinks",        // Remove if/when used internally
