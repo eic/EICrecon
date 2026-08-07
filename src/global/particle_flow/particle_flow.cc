0a56c2f1fcaa0ed924a5c53922cb43d8b0d4bcc6
Subhadip Pal <subhadippal7929@gmail.com>
2026-08-07 14:36:03 +0200

    Changing inputs from "Clusters" to "RemnantClusters".  And introducing "CombinedHcalEndcapPRemnantClusters"

--- src/global/particle_flow/particle_flow.cc
+++ src/global/particle_flow/particle_flow.cc
@@ -195,23 +195,23 @@ void InitPlugin(JApplication* app) {
   // backward -----------------------------------------------------------
 
   app->Add(new JOmniFactoryGeneratorT<CaloRemnantCombiner_factory>(
-      "EndcapNNeutralCandidateParticlesAlpha", {"EcalEndcapNClusters", "HcalEndcapNClusters"},
+      "EndcapNNeutralCandidateParticlesAlpha", {"EcalEndcapNRemnantClusters", "HcalEndcapNRemnantClusters"},
       {"EndcapNNeutralCandidateParticlesAlpha"}, {.ecalDeltaR = 0.03, .hcalDeltaR = 0.15}, app));
 
   // central ------------------------------------------------------------
 
   app->Add(new JOmniFactoryGeneratorT<CaloRemnantCombiner_factory>(
-      "BarrelNeutralCandidateParticlesAlpha", {"EcalBarrelClusters", "HcalBarrelClusters"},
+      "BarrelNeutralCandidateParticlesAlpha", {"EcalBarrelRemnantClusters", "HcalBarrelRemnantClusters"},
       {"BarrelNeutralCandidateParticlesAlpha"}, {.ecalDeltaR = 0.03, .hcalDeltaR = 0.15}, app));
 
   // forward ------------------------------------------------------------
 
   app->Add(new JOmniFactoryGeneratorT<CollectionCollector_factory<edm4eic::Cluster, false>>(
-      "HcalEndcapPClusters", {"LFHCALClusters", "HcalEndcapPInsertClusters"},
-      {"HcalEndcapPClusters"}, app));
+      "CombinedHcalEndcapPRemnantClusters", {"LFHCALRemnantClusters", "HcalEndcapPInsertRemnantClusters"},
+      {"CombinedHcalEndcapPRemnantClusters"}, app));
 
   app->Add(new JOmniFactoryGeneratorT<CaloRemnantCombiner_factory>(
-      "EndcapPNeutralCandidateParticlesAlpha", {"EcalEndcapPClusters", "HcalEndcapPClusters"},
+      "EndcapPNeutralCandidateParticlesAlpha", {"EcalEndcapPRemnantClusters", "CombinedHcalEndcapPRemnantClusters"},
       {"EndcapPNeutralCandidateParticlesAlpha"}, {.ecalDeltaR = 0.03, .hcalDeltaR = 0.15}, app));
 
   // --------------------------------------------------------------------
