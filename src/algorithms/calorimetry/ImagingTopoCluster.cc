81afacd5836bc7916de091075666f56e0f3b9205
akshaya <99753263+AkshayaVijay@users.noreply.github.com>
2026-08-10 12:27:32 -0500

    Update ImagingTopoCluster.cc

--- src/algorithms/calorimetry/ImagingTopoCluster.cc
+++ src/algorithms/calorimetry/ImagingTopoCluster.cc
@@ -90,14 +90,14 @@ void ImagingTopoCluster::init() {
   Img_diffLayerDistXY[1] = std::visit(_toDouble, m_cfg.Img_diffLayerDistXY[1]) / dd4hep::mm;
 
   if (m_cfg.sameLayerDistXYZ.size() >= 3) {
-    sameLayerDistXYZ[0] = std::visit(_toDouble, m_cfg.sameLayerDistXYZ[0]) / dd4hep::mm;
-    sameLayerDistXYZ[1] = std::visit(_toDouble, m_cfg.sameLayerDistXYZ[1]) / dd4hep::mm;
-    sameLayerDistXYZ[2] = std::visit(_toDouble, m_cfg.sameLayerDistXYZ[2]) / dd4hep::mm;
+  sameLayerDistXYZ[0] = m_cfg.sameLayerDistXYZ[0] / dd4hep::mm;
+  sameLayerDistXYZ[1] = m_cfg.sameLayerDistXYZ[1] / dd4hep::mm;
+  sameLayerDistXYZ[2] = m_cfg.sameLayerDistXYZ[2] / dd4hep::mm;
   }
   if (m_cfg.diffLayerDistXYZ.size() >= 3) {
-    diffLayerDistXYZ[0] = std::visit(_toDouble, m_cfg.diffLayerDistXYZ[0]) / dd4hep::mm;
-    diffLayerDistXYZ[1] = std::visit(_toDouble, m_cfg.diffLayerDistXYZ[1]) / dd4hep::mm;
-    diffLayerDistXYZ[2] = std::visit(_toDouble, m_cfg.diffLayerDistXYZ[2]) / dd4hep::mm;
+    diffLayerDistXYZ[0] = m_cfg.diffLayerDistXYZ[0] / dd4hep::mm;
+    diffLayerDistXYZ[1] = m_cfg.diffLayerDistXYZ[1] / dd4hep::mm;
+    diffLayerDistXYZ[2] = m_cfg.diffLayerDistXYZ[2] / dd4hep::mm;
   }
 
   ScFi_sameLayerDistXYZ[0] = m_cfg.ScFi_sameLayerDistXYZ[0] / dd4hep::mm;
