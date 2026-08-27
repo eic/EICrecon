2c464f3a5044942eddac34fa3a014b33f07bddce
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-08-27 11:23:30 -0400

    typo

--- src/detectors/FHCAL/FHCAL.cc
+++ src/detectors/FHCAL/FHCAL.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2023 - 2026 Friederike Bock, Wouter Deconinck, Aider Wu
+// Copyright (C) 2023 - 2026 Friederike Bock, Wouter Deconinck, Aiden Wu
 
 #include <DD4hep/Detector.h>
 #include <DD4hep/Readout.h>
