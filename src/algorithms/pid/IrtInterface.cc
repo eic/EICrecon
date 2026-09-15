77f8c2e63bb38e4e87cad9c80f695c46b7e35a16
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-09-15 16:14:21 -0400

    implement segfault guard

--- src/algorithms/pid/IrtInterface.cc
+++ src/algorithms/pid/IrtInterface.cc
@@ -285,10 +285,15 @@ void IrtInterface::process(const IrtInterface::Input& input,
       // a blanked out 'sector' field for dRICH) in ePIC;
       photon->SetVolumeCopy(sensorID);
 
+      if (pd->GetQE() == nullptr) {
+        static std::once_flag qe_warning_flag;
+        std::call_once(qe_warning_flag, [this]() {
+          error("QE is not set - skipping photon selection. Likely, IRT2 was built without JSON_IMPORT_EXPORT.");
+        });
+      } else {
       // The logic behind this multiplication and division by the same number is
       // to select calibration photons, which originate from the same QE(lambda)
       // parent distribution, but do not pass the overall efficiency test;
-      {
         double e  = photon->GetVertexMomentum().Mag();
         double qe = pd->GetQE()->WithinRange(e)
                         ? pd->GetQE()->GetInterpolatedValue(e, DataInterpolation::FirstOrder)
