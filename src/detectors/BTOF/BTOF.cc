3e437b55737dd37332369c8570386c063efdd0a3
ShujieL <ShujieL@users.noreply.github.com>
2026-09-19 09:35:51 -0700

    Enable noise addition for TOFBarrelSharedNoiseRawHits

--- src/detectors/BTOF/BTOF.cc
+++ src/detectors/BTOF/BTOF.cc
@@ -84,7 +84,7 @@ void InitPlugin(JApplication* app) {
   app->Add(new JOmniFactoryGeneratorT<RandomNoisePixel_factory>(
       "TOFBarrelSharedNoiseRawHits", {"EventHeader"}, {"TOFBarrelSharedNoiseRawHits"},
       {
-          .addNoise = false,
+          .addNoise = true,
           .noise_rate_per_pixel_per_event =
               30 * 2e-6, // noise is 30 Hz, and we expect data to come in every 2us
           .readout_name = "TOFBarrelHits",
